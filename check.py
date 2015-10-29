f0058e1fb8e1273a714f7a2526fe19addb2c0f4f
Alon Zakai <alonzakai@gmail.com>
2015-10-29 13:10:58 -0700

    check.py improvements

--- check.py
+++ check.py
@@ -7,9 +7,11 @@ print 'checking testcases...\n'
 for asm in os.listdir('test'):
   if asm.endswith('.asm.js'):
     print '  ', asm, ' ',
-    wasm = asm.replace('.asm.js', '.wasm')
+    wasm = asm.replace('.asm.js', '.wast')
     actual, err = subprocess.Popen([os.path.join('bin', 'asm2wasm'), os.path.join('test', asm)], stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
     assert err == '', 'bad err:' + err
+    if not os.path.exists(os.path.join('test', wasm)):
+      raise Exception('output .wast file does not exist')
     expected = open(os.path.join('test', wasm)).read()
     if actual != expected:
       raise Exception("Expected to have '%s' == '%s', diff:\n\n%s" % (
