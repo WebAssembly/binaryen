88b6549b74dd767733cd823de410e00067a79756
Alon Zakai <alonzakai@gmail.com>
2015-10-29 19:58:09 -0700

    add test auto updater

---
+++ auto_update_tests.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import os, sys, subprocess, difflib
+
+print '[ processing and updating testcases... ]\n'
+
+for asm in sorted(os.listdir('test')):
+  if asm.endswith('.asm.js'):
+    print '..', asm
+    wasm = asm.replace('.asm.js', '.wast')
+    actual, err = subprocess.Popen([os.path.join('bin', 'asm2wasm'), os.path.join('test', asm)], stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
+    assert err == '', 'bad err:' + err
+
+    # verify output
+    if not os.path.exists(os.path.join('test', wasm)):
+      print actual
+      raise Exception('output .wast file does not exist')
+    open(os.path.join('test', wasm), 'w').write(actual)
+
+print '\n[ success! ]'
+
