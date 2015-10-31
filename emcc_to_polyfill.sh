a8963f481e5db2c212a7e7fa83d3599c65d7ef5f
Alon Zakai <alonzakai@gmail.com>
2015-10-31 15:54:11 -0700

    make emcc_to_polyfill emit a.wasm.js

--- emcc_to_polyfill.sh
+++ emcc_to_polyfill.sh
@@ -1,8 +1,18 @@
 #!/bin/sh
+
+echo "calling emcc"
 emcc $1 -o a.html --separate-asm
-# we now have a.asm.js and a.js, combine them
-# a.normal.js: just normal combination of the files. no wasm yet
+
+# we now have a.asm.js and a.js
+
+echo 'constructing a.normal.js' # just normal combination of the files. no wasm yet
 cat src/templates/normal.js > a.normal.js
 cat a.asm.js >> a.normal.js
 cat a.js >> a.normal.js
+
+echo 'constructing a.wasm.js' # use wasm polyfill in place of asm.js THIS IS NOT A DRILL
+cp a.asm.js a.asm.code.js
+cat src/templates/wasm.js > a.wasm.js
+cat bin/wasm.js >> a.wasm.js
+cat a.js >> a.wasm.js
 
