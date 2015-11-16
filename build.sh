6457e86fe40f8b0ee5cfddd2685d537462ea24f9
Alon Zakai <alonzakai@gmail.com>
2015-11-16 15:36:45 -0800

    use memory growth in wasm.js, as some codebases are very large

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@ g++ -O2 -std=c++11 src/binaryen-shell.cpp src/pass.cpp src/passes/*.cpp -o bin/b
 echo "building asm2wasm"
 g++ -O2 -std=c++11 src/asm2wasm-main.cpp src/emscripten-optimizer/parser.cpp src/emscripten-optimizer/simple_ast.cpp src/emscripten-optimizer/optimizer-shared.cpp -o bin/asm2wasm
 echo "building interpreter/js"
-em++ -std=c++11 src/wasm-js.cpp src/emscripten-optimizer/parser.cpp src/emscripten-optimizer/simple_ast.cpp src/emscripten-optimizer/optimizer-shared.cpp -o bin/wasm.js -s MODULARIZE=1 -s 'EXPORT_NAME="WasmJS"' --memory-init-file 0 -O3 -s TOTAL_MEMORY=67108864 #-DWASM_JS_DEBUG #-DWASM_INTERPRETER_DEBUG
+em++ -std=c++11 src/wasm-js.cpp src/emscripten-optimizer/parser.cpp src/emscripten-optimizer/simple_ast.cpp src/emscripten-optimizer/optimizer-shared.cpp -o bin/wasm.js -s MODULARIZE=1 -s 'EXPORT_NAME="WasmJS"' --memory-init-file 0 -O3 -s ALLOW_MEMORY_GROWTH=1 #-DWASM_JS_DEBUG #-DWASM_INTERPRETER_DEBUG
 cat src/js/post.js >> bin/wasm.js
 
