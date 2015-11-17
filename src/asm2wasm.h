a2c0146a8540d3c8485a43767172ce56dd8f8621
Alon Zakai <alonzakai@gmail.com>
2015-11-16 21:02:05 -0800

    ASM_FLOAT

--- src/asm2wasm.h
+++ src/asm2wasm.h
@@ -206,6 +206,7 @@ private:
     switch (asmType) {
       case ASM_INT: return WasmType::i32;
       case ASM_DOUBLE: return WasmType::f64;
+      case ASM_FLOAT: return WasmType::f32;
       case ASM_NONE: return WasmType::none;
       default: {}
     }
@@ -215,6 +216,7 @@ private:
   AsmType wasmToAsmType(WasmType type) {
     switch (type) {
       case WasmType::i32: return ASM_INT;
+      case WasmType::f32: return ASM_FLOAT;
       case WasmType::f64: return ASM_DOUBLE;
       case WasmType::none: return ASM_NONE;
       default: {}
