0d2c26a5578447097d061a68ebaa6cd07b32d688
Alon Zakai <alonzakai@gmail.com>
2015-12-03 17:59:38 -0800

    start on wasm2asm tool

---
+++ src/wasm2asm-main.cpp
@@ -0,0 +1,53 @@
+//
+// wasm2asm console tool
+//
+
+#include "wasm2asm.h"
+
+using namespace cashew;
+using namespace wasm;
+
+namespace wasm {
+int debug = 0;
+}
+
+int main(int argc, char **argv) {
+  debug = getenv("WASM2ASM_DEBUG") ? getenv("WASM2ASM_DEBUG")[0] - '0' : 0;
+
+  char *infile = argv[1];
+
+  if (debug) std::cerr << "loading '" << infile << "'...\n";
+  FILE *f = fopen(argv[1], "r");
+  assert(f);
+  fseek(f, 0, SEEK_END);
+  int size = ftell(f);
+  char *input = new char[size+1];
+  rewind(f);
+  int num = fread(input, 1, size, f);
+  // On Windows, ftell() gives the byte position (\r\n counts as two bytes), but when
+  // reading, fread() returns the number of characters read (\r\n is read as one char \n, and counted as one),
+  // so return value of fread can be less than size reported by ftell, and that is normal.
+  assert((num > 0 || size == 0) && num <= size);
+  fclose(f);
+  input[num] = 0;
+
+  SExpressionParser parser(input);
+  Element& root = *parser.root;
+  AllocatingModule wasm;
+  SExpressionWasmBuilder builder(wasm, *root[0], [&]() { abort(); });
+
+  if (debug) std::cerr << "parsing...\n";
+  cashew::Parser<Ref, DotZeroValueBuilder> builder;
+  Ref asmjs = builder.parseToplevel(input);
+
+  if (debug) std::cerr << "asming...\n";
+  Wasm2AsmBuilder wasm2asm;
+  Ref asmjs = wasm2asm.processWasm(wasm);
+
+  if (debug) std::cerr << "printing...\n";
+  asmjs->stringify(std::cout);
+  std::cout << '\n';
+
+  if (debug) std::cerr << "done.\n";
+}
+
