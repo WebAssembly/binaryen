73345105034640d09d9557c55ce5e9077c2cb8f8
JF Bastien <jfb@chromium.org>
2015-12-21 12:56:01 -0800

    Fix the license headers
    This applies Apache 2.0 properly (as far as our lawyers have told me). We can do this early since all of the code was written by Alon Zakai.

--- src/asm2wasm.h
+++ src/asm2wasm.h
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2015 WebAssembly Community Group participants
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 //
 // asm.js-to-WebAssembly translator. Uses the Emscripten optimizer
@@ -1515,4 +1530,3 @@ void Asm2WasmBuilder::optimize() {
 }
 
 } // namespace wasm
-
--- src/js/wasm.js-post.js
+++ src/js/wasm.js-post.js
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2015 WebAssembly Community Group participants
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 function integrateWasmJS(Module) {
   // wasm.js has several methods for creating the compiled code module here:
@@ -179,4 +194,3 @@ function integrateWasmJS(Module) {
     return wasmJS['asmExports'];
   };
 }
-
