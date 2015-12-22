681b4d1c5b5e28ea96b8f54b48e6de1e823afd61
Alon Zakai <alonzakai@gmail.com>
2015-12-21 16:37:14 -0800

    use concrete list of passes in build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ git submodule sync --quiet
 git submodule init --quiet
 git submodule update --quiet
 echo "building binaryen shell"
-g++ -O2 -std=c++11 src/binaryen-shell.cpp src/pass.cpp src/passes/*.cpp -o bin/binaryen-shell -Isrc/ -msse2 -mfpmath=sse # use sse for math, avoid x87, necessarily for proper float rounding on 32-bit
+g++ -O2 -std=c++11 src/binaryen-shell.cpp src/pass.cpp src/passes/LowerIfElse.cpp src/passes/NameManager.cpp src/passes/RemoveImports.cpp -o bin/binaryen-shell -Isrc/ -msse2 -mfpmath=sse # use sse for math, avoid x87, necessarily for proper float rounding on 32-bit
 echo "building asm2wasm"
 g++ -O2 -std=c++11 src/asm2wasm-main.cpp src/emscripten-optimizer/parser.cpp src/emscripten-optimizer/simple_ast.cpp src/emscripten-optimizer/optimizer-shared.cpp -o bin/asm2wasm
 echo "building wasm2asm"
