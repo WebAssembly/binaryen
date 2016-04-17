d0c419f70938023bfc1c72365f9901a718a9eec6
Alon Zakai <alonzakai@gmail.com>
2016-04-17 10:33:59 -0700

    remove old comments in SimplifyLocals

--- src/passes/SimplifyLocals.cpp
+++ src/passes/SimplifyLocals.cpp
@@ -62,7 +62,7 @@ struct SimplifyLocals : public WalkerPass<LinearExecutionWalker<SimplifyLocals, 
   void visitGetLocal(GetLocal *curr) {
     auto found = sinkables.find(curr->index);
     if (found != sinkables.end()) {
-      // sink it, and nop the origin TODO: clean up nops
+      // sink it, and nop the origin
       replaceCurrent(*found->second.item);
       // reuse the getlocal that is dying
       *found->second.item = curr;
@@ -77,7 +77,6 @@ struct SimplifyLocals : public WalkerPass<LinearExecutionWalker<SimplifyLocals, 
   void visitSetLocal(SetLocal *curr) {
     // if we are a potentially-sinkable thing, forget it - this
     // write overrides the last TODO: optimizable
-    // TODO: if no get_locals left, can remove the set as well (== expressionizer in emscripten optimizer)
     auto found = sinkables.find(curr->index);
     if (found != sinkables.end()) {
       sinkables.erase(found);
