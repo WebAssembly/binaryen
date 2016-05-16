bc65c06e0ac44c2b9132d792f8b35a41f07dbe1f
Alon Zakai <alonzakai@gmail.com>
2016-05-16 10:06:42 -0700

    don't emit an extra endline in s2wasm (#506)

--- src/tools/s2wasm.cpp
+++ src/tools/s2wasm.cpp
@@ -134,7 +134,7 @@ int main(int argc, const char *argv[]) {
   if (options.debug) std::cerr << "Printing..." << std::endl;
   Output output(options.extra["output"], Flags::Text, options.debug ? Flags::Debug : Flags::Release);
   WasmPrinter::printModule(&linker.getOutput().wasm, output.getStream());
-  output << meta.str() << std::endl;
+  output << meta.str();
 
   if (options.debug) std::cerr << "Done." << std::endl;
 }
