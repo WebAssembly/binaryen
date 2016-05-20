dbd717d6400f1de960267d454f05d64e183a914c
BSalita <BSalita@yahoo.com>
2016-05-20 12:05:36 +0200

    Update wasm.h

--- src/wasm.h
+++ src/wasm.h
@@ -1237,7 +1237,7 @@ public:
     return localIndices[name];
   }
   Index getVarIndexBase() {
-    return (Index)params.size();
+    return toIndex(params.size());
   }
   WasmType getLocalType(Index index) {
     if (isParam(index)) {
