8615b1c4dfabbcaf8de13c3d136c814be9533390
BSalita <BSalita@yahoo.com>
2016-05-20 12:09:01 +0200

    Update wasm-builder.h

--- src/wasm-builder.h
+++ src/wasm-builder.h
@@ -52,15 +52,15 @@ public:
 
     for (auto& param : params) {
       func->params.push_back(param.type);
-      size_t i = func->localNames.size();
-      func->localNames.push_back(param.name);
-      func->localIndices[param.name] = toIndex(i, func->localNames.size());
+	  size_t i = func->localNames.size();
+	  func->localNames.push_back(param.name);
+	  func->localIndices[param.name] = toIndex(i);
     }
     for (auto& var : vars) {
       func->vars.push_back(var.type);
-      size_t i = func->localNames.size();
-      func->localNames.push_back(var.name);
-      func->localIndices[var.name] = toIndex(i, func->localNames.size());
+	  size_t i = func->localNames.size();
+	  func->localNames.push_back(var.name);
+	  func->localIndices[var.name] = toIndex(i);
     }
 
     return func;
@@ -205,9 +205,9 @@ public:
     // only ok to add a param if no vars, otherwise indices are invalidated
     assert(func->localIndices.size() == func->params.size());
     func->params.push_back(type);
-	  size_t i = func->localNames.size();
-	  func->localNames.push_back(name);
-	  Index index = toIndex(i, func->localNames.size());
+	size_t i = func->localNames.size();
+	func->localNames.push_back(name);
+	Index index = toIndex(i);
     func->localIndices[name] = index;
     return index;
   }
@@ -216,12 +216,10 @@ public:
     // always ok to add a var, it does not affect other indices
     assert(func->localIndices.size() == func->params.size() + func->vars.size());
     func->vars.emplace_back(type);
-	  size_t i = func->localNames.size();
-	  func->localNames.push_back(name);
-	  Index index = toIndex(i, func->localNames.size());
+	size_t i = func->localNames.size();
+	func->localNames.push_back(name);
+	Index index = toIndex(i);
     func->localIndices[name] = index;
-    return index;
-
     return index;
   }
 
