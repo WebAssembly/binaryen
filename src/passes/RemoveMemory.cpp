764b73578062a681b32644f147c7876ac74a1957
Alon Zakai <alonzakai@gmail.com>
2016-06-08 18:28:04 -0700

    add a pass to remove memory segments

---
+++ src/passes/RemoveMemory.cpp
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2016 WebAssembly Community Group participants
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+//
+// Removeds memory segments, leaving only code in the module.
+//
+
+#include <wasm.h>
+#include <pass.h>
+
+namespace wasm {
+
+struct RemoveMemory : public Pass {
+  void run(PassRunner* runner, Module* module) override {
+    module->memory.segments.clear();
+  }
+};
+
+static RegisterPass<RemoveMemory> registerPass("remove-memory", "removes memory segments");
+
+} // namespace wasm
---
+++ test/passes/remove-memory.wast
@@ -0,0 +1,6 @@
+(module
+  (memory 1024 1024
+    (segment 10 "123")
+    (segment 20 "149")
+  )
+)
