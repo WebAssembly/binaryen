804ed8ca6ff7616a4ed862e2cb9d95e3e15c9ac1
Jacob Gravelle <jgravelle@google.com>
2016-09-07 16:17:58 -0700

    Add copyright headers and sort #includes

--- src/wasm-emscripten.cpp
+++ src/wasm-emscripten.cpp
@@ -1,11 +1,27 @@
+/*
+ * Copyright 2016 WebAssembly Community Group participants
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #include "wasm-emscripten.h"
 
-#include "shared-constants.h"
-#include "wasm.h"
-#include "wasm-builder.h"
 #include "asm_v_wasm.h"
-#include "wasm-linker.h"
 #include "asmjs/shared-constants.h"
+#include "shared-constants.h"
+#include "wasm-builder.h"
+#include "wasm-linker.h"
+#include "wasm.h"
 
 namespace wasm {
 
