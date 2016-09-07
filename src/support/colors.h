fbe77b167002e8a49225b607ca8c37dc7e4b41fe
Loo Rong Jie <loorongjie@gmail.com>
2016-09-08 00:49:50 +0800

    Color support for Windows (#693)

--- src/support/colors.h
+++ src/support/colors.h
@@ -21,7 +21,9 @@
 
 namespace Colors {
 void disable();
-void outputColorCode(std::ostream&stream, const char *colorCode);
+
+#if defined(__linux__) || defined(__APPLE__)
+void outputColorCode(std::ostream& stream, const char *colorCode);
 inline void normal(std::ostream& stream) { outputColorCode(stream,"\033[0m"); }
 inline void red(std::ostream& stream) { outputColorCode(stream,"\033[31m"); }
 inline void magenta(std::ostream& stream) { outputColorCode(stream,"\033[35m"); }
@@ -30,6 +32,17 @@ inline void grey(std::ostream& stream) { outputColorCode(stream,"\033[37m"); }
 inline void green(std::ostream& stream) { outputColorCode(stream,"\033[32m"); }
 inline void blue(std::ostream& stream) { outputColorCode(stream,"\033[34m"); }
 inline void bold(std::ostream& stream) { outputColorCode(stream,"\033[1m"); }
+#elif defined(_WIN32)
+void outputColorCode(std::ostream& stream, const unsigned short &colorCode);
+inline void normal(std::ostream& stream) { outputColorCode(stream, 0x07); }
+inline void red(std::ostream& stream) { outputColorCode(stream, 0x0c); }
+inline void magenta(std::ostream& stream) { outputColorCode(stream, 0x05); }
+inline void orange(std::ostream& stream) { outputColorCode(stream, 0x06); }
+inline void grey(std::ostream& stream) { outputColorCode(stream, 0x08); }
+inline void green(std::ostream& stream) { outputColorCode(stream, 0x02); }
+inline void blue(std::ostream& stream) { outputColorCode(stream, 0x09); }
+inline void bold(std::ostream& stream) { /* Do nothing */ }
+#endif
 };
 
 #endif // wasm_support_color_h
