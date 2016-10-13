60792bdf9da06c007d72c4cd78b3a0eb249a239f
Jukka Jylänki <jujjyl@gmail.com>
2016-10-13 16:18:00 +0300

    Fix crash on loading archives, firstRegularData member field was not initialized to null which caused dereferencing a garbage pointer.

--- src/support/archive.cpp
+++ src/support/archive.cpp
@@ -60,7 +60,7 @@ uint32_t ArchiveMemberHeader::getSize() const {
   return static_cast<uint32_t>(sizeInt);
 }
 
-Archive::Archive(Buffer& b, bool& error) : data(b), symbolTable({nullptr, 0}), stringTable({nullptr, 0}) {
+Archive::Archive(Buffer& b, bool& error) : data(b), symbolTable({nullptr, 0}), stringTable({nullptr, 0}), firstRegularData(nullptr) {
   error = false;
   if (data.size() < strlen(magic) ||
       memcmp(data.data(), magic, strlen(magic))) {
