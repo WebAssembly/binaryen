17865c806a2be68a05061f448610dddedac3624e
Alon Zakai <alonzakai@gmail.com>
2016-10-19 15:28:19 -0700

    script fix

--- scripts/strip_local_names.py
+++ scripts/strip_local_names.py
@@ -4,12 +4,10 @@ Removes local names. When you don't care about local names but do want
 to diff for structural changes, this can help.
 '''
 
-import os
 import sys
 
 for line in open(sys.argv[1]).readlines():
-  if '(tee_local ' in line or '(set_local ' in line or '(get_local '  in line:
+  if '(tee_local ' in line or '(set_local ' in line or '(get_local ' in line:
     print line[:line.find('$')]
   else:
     print line,
-
