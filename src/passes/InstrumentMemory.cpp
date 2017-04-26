ce54bc768648e25bd3c10af9c1150edfd4efc804
Michael Bebenita <mbebenita@gmail.com>
2017-04-25 19:15:05 -0700

    Simplify instrumentation.

--- src/passes/InstrumentMemory.cpp
+++ src/passes/InstrumentMemory.cpp
@@ -32,147 +32,58 @@
 
 namespace wasm {
 
-Name empty("empty");
-
-Name i32_load8_s("i32_load8_s");
-Name i32_load8_u("i32_load8_u");
-Name i32_load16_s("i32_load16_s");
-Name i32_load16_u("i32_load16_u");
-Name i32_load("i32_load");
-Name i64_load8_s("i64_load8_s");
-Name i64_load8_u("i64_load8_u");
-Name i64_load16_s("i64_load16_s");
-Name i64_load16_u("i64_load16_u");
-Name i64_load32_s("i64_load32_s");
-Name i64_load32_u("i64_load32_u");
-Name i64_load("i64_load");
-Name f32_load("f32_load");
-Name f64_load("f64_load");
-
-Name loads [4][4][2] = {
-  {
-    { i32_load8_u,  i32_load8_s   },
-    { i32_load16_u, i32_load16_s  },
-    { i32_load,     i32_load      },
-  }, {
-    { i64_load8_u,  i64_load8_s   },
-    { i64_load16_u, i64_load16_s  },
-    { i64_load32_u, i64_load32_s  },
-    { i64_load,     i64_load      }
-  }, {
-    { empty                       },
-    { empty                       },
-    { f32_load,     f32_load      }
-  }, {
-    { empty                       },
-    { empty                       },
-    { empty                       },
-    { f64_load,     f64_load      }
-  }
-};
-
-Name i32_store8("i32_store8");
-Name i32_store16("i32_store16");
-Name i32_store("i32_store");
-Name i64_store8("i64_store8");
-Name i64_store16("i64_store16");
-Name i64_store32("i64_store32");
-Name i64_store("i64_store");
-Name f32_store("f32_store");
-Name f64_store("f64_store");
-
-Name stores [4][4] = {
-  { i32_store8,   i32_store16, i32_store              },
-  { i64_store8,   i64_store16, i64_store32, i64_store },
-  { empty,        empty,       f32_store              },
-  { empty,        empty,       empty,       f64_store }
-};
-
-static int32_t bytesToShift(unsigned bytes) {
-  switch (bytes) {
-    case 1: return 0;
-    case 2: return 1;
-    case 4: return 2;
-    case 8: return 3;
-    default: {}
-  }
-  abort();
-  return -1; // avoid warning
-}
+Name load("load");
+Name store("store");
 
 struct InstrumentMemory : public WalkerPass<PostWalker<InstrumentMemory>> {
   void visitLoad(Load* curr) {
-    replaceCurrent(makeLoadCall(curr));
+    makeLoadCall(curr);
   }
   void visitStore(Store* curr) {
-    replaceCurrent(makeStoreCall(curr));
+    makeStoreCall(curr);
   }
   void addImport(Module *curr, Name name, std::string sig) {
     auto import = new Import;
     import->name = name;
-    import->module = ENV;
+    import->module = INS;
     import->base = name;
     import->functionType = ensureFunctionType(sig, curr)->name;
     import->kind = ExternalKind::Function;
     curr->addImport(import);
   }
-  void visitModule(Module *curr) {
-    // Loads
-    addImport(curr, i32_load8_s,  "iiiii");
-    addImport(curr, i32_load8_u,  "iiiii");
-    addImport(curr, i32_load16_s, "iiiii");
-    addImport(curr, i32_load16_u, "iiiii");
-    addImport(curr, i32_load,     "iiiii");
-    addImport(curr, i64_load8_s,  "jiiii");
-    addImport(curr, i64_load8_u,  "jiiii");
-    addImport(curr, i64_load16_s, "jiiii");
-    addImport(curr, i64_load16_u, "jiiii");
-    addImport(curr, i64_load32_s, "jiiii");
-    addImport(curr, i64_load32_u, "jiiii");
-    addImport(curr, i64_load,     "jiiii");
-    addImport(curr, f32_load,     "fiiii");
-    addImport(curr, f64_load,     "diiii");
 
-    // Stores
-    addImport(curr, i32_store8,  "viiiii");
-    addImport(curr, i32_store16, "viiiii");
-    addImport(curr, i32_store,   "viiiii");
-    addImport(curr, i64_store8,  "viiiij");
-    addImport(curr, i64_store16, "viiiij");
-    addImport(curr, i64_store32, "viiiij");
-    addImport(curr, i64_store,   "viiiij");
-    addImport(curr, f32_store,   "viiiif");
-    addImport(curr, f64_store,   "viiiid");
+  void visitModule(Module *curr) {
+    // load(id, bytes, offset, address) => address
+    addImport(curr, load,  "iiiii");
+    // store(id, bytes, offset, address) => address
+    addImport(curr, store,  "iiiii");
   }
 
 private:
+  std::atomic<Index> id;
   Expression* makeLoadCall(Load* curr) {
-    static Index loadId = 0;
     Builder builder(*getModule());
-    return builder.makeCallImport(
-      loads[curr->type - 1][bytesToShift(curr->bytes)][curr->signed_],
-      { builder.makeConst(Literal(int32_t(loadId++))),
+    curr->ptr = builder.makeCallImport(load,
+      { builder.makeConst(Literal(int32_t(id.fetch_add(1)))),
+        builder.makeConst(Literal(int32_t(curr->bytes))),
         builder.makeConst(Literal(int32_t(curr->offset.addr))),
-        builder.makeConst(Literal(int32_t(curr->align.addr))),
-        curr->ptr },
-      curr->type
+        curr->ptr},
+      i32
     );
+    return curr;
   }
 
   Expression* makeStoreCall(Store* curr) {
-    static Index storeId = 0;
     Builder builder(*getModule());
-    return builder.makeCallImport(
-      stores[curr->valueType - 1][bytesToShift(curr->bytes)],
-      { builder.makeConst(Literal(int32_t(storeId++))),
+    curr->ptr = builder.makeCallImport(store,
+      { builder.makeConst(Literal(int32_t(id.fetch_add(1)))),
+        builder.makeConst(Literal(int32_t(curr->bytes))),
         builder.makeConst(Literal(int32_t(curr->offset.addr))),
-        builder.makeConst(Literal(int32_t(curr->align.addr))),
-        curr->ptr,
-        curr->value },
-      none
+        curr->ptr },
+      i32
     );
+    return curr;
   }
-
 };
 
 Pass *createInstrumentMemoryPass() {
