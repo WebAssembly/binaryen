9563cd2787006f25fcf24e2b18e2671dfbbf3d0f
Alon Zakai <alonzakai@gmail.com>
2017-05-23 14:09:27 -0700

    wip

--- src/passes/SSAify.cpp
+++ src/passes/SSAify.cpp
@@ -20,49 +20,45 @@
 // so the result is multiple assignments but with the guarantee that
 // they all travel directly to the same basic block, i.e., they are
 // a way to represent a phi in our AST.
+// TODO: consider adding a "proper" phi node to the AST, that passes
+//       can utilize
 //
 
 #include "wasm.h"
 #include "pass.h"
+#include "wasm-builder.h"
 #include "support/permutations.h"
+#include "ast/manipulation.h"
 
 namespace wasm {
 
 // Tracks assignments to locals, assuming single-assignment form, i.e.,
 // each assignment creates a new variable.
 
-template<typename SubType, typename VisitorType>
-struct SetTrackingWalker : public PostWalker<SubType, VisitorType> {
-  typedef std::vector<Index> NameMapping; // old index (in original code) => new index (in SSA form, new variables)
-
-  struct BreakInfo {
-    NameMapping mapping;
-    Expression** origin; // the origin of a node where a phi would go. note that *origin can be nullptr, in which case we can just fill it
-    enum PhiType {
-      Before = 0,  // a phi should go right before the origin (e.g., origin is a loop and this is the entrance)
-      After = 1,   // a phi should go right after the origin (e.g. this is an if body)
-      Internal = 2 // origin is the breaking instruction itself, we must add the phi internally (depending on if the break is condition or has a value, etc.,
-                   //                                                                            or for a block as the last instruction)
-    } type;
-    // TODO: move semantics?
-    BreakInfo(NameMapping mapping, Expression** origin, PhiType type) : mapping(mapping), origin(origin), type(type) {}
-  };
+struct SSAify : public PostWalker<SSAify> {
+  // we map (old local index) => the set_local for that index. The new
+  // index for the local can be seen in that set local.
+  // this can be nullptr if there is no set_local, and instead the value
+  // is a parameter, or the zero init.
+  typedef std::vector<SetLocal*> Mapping;
 
   Index numLocals;
-  NameMapping currMapping;
+  Mapping currMapping;
   Index nextIndex;
-  std::vector<NameMapping> mappingStack; // used in ifs, loops
-  std::map<Name, std::vector<BreakInfo>> breakInfos; // break target => infos that reach it
+  std::vector<Mapping> mappingStack; // used in ifs, loops
+  std::map<Name, std::vector<Mapping>> breakInfos; // break target => infos that reach it
+  std::vector<std::vector<GetLocal*>> loopGetStack; //  stack of loops, all the gets in each, so we can update them for phis
+  std::map<GetLocal*, Index> originalGetIndexes;
 
-  SetTrackingWalker(Function* func) {
+  void doWalkFunction(Function* func) {
     numLocals = func->getNumLocals();
     if (numLocals == 0) return; // nothing to do
     // We begin with each param being assigned from the incoming value, and the zero-init for the locals,
     // so the initial state is the identity permutation
     currMapping.resize(numLocals);
-    setIdentity(currMapping);
+    for (auto& set : currMapping) set = nullptr;
     nextIndex = numLocals;
-    PostWalker<SubType, VisitorType>::walk(func->body);
+    PostWalker<SSAify>::walk(func->body);
   }
 
   // control flow
@@ -75,45 +71,55 @@ struct SetTrackingWalker : public PostWalker<SubType, VisitorType> {
       self->currMapping = std::move(self->merge(infos), curr->name);
     }
   }
-  static void doIfCondition(SubType* self, Expression** currp) {
-    auto* curr = (*currp)->cast<If>();
-    if (!curr->ifFalse) {
-      self->mappingStack.push_back(self->currMapping);
-    }
+
+  void finishIf() {
+    // that's it for this if, merge
+    std::vector<BreakInfo> breaks;
+    breaks.emplace_back(std::move(currMapping));
+    breaks.emplace_back(std::move(mappingStack.back()));
+    mappingStack.pop_back();
+    currMapping = std::move(merge(breaks));
   }
-  static void doIfTrue(SubType* self, Expression** currp) {
+
+  static void afterIfCondition(SubType* self, Expression** currp) {
+    auto* curr = (*currp)->cast<If>();
+    self->mappingStack.push_back(self->currMapping);
+  }
+  static void afterIfTrue(SubType* self, Expression** currp) {
     auto* curr = (*currp)->cast<If>();
     if (curr->ifFalse) {
-      self->mappingStack.push_back(self->currMapping);
+      auto afterCondition = std::move(self.mappingStack.back());
+      self->mappingStack.back() = std::move(self->currMapping);
+      self->currMapping = std::move(afterCondition);
     } else {
-      // that's it for this if, merge
-      std::vector<BreakInfo> breaks;
-      breaks.emplace_back(std::move(self->currMapping), &curr->ifFalse, BreakInfo::After);
-      breaks.emplace_back(self->mappingStack.back(), &curr->condition, BreakInfo::After);
-      self->mappingStack.pop_back();
-      self->currMapping = std::move(merge(breaks));
+      self->finishIf();
     }
   }
-  static void doIfFalse(SubType* self, Expression** currp) {
-    auto* curr = (*currp)->cast<If>();
-    std::vector<BreakInfo> breaks;
-    breaks.emplace_back(std::move(self->currMapping), &curr->ifFalse, BreakInfo::After);
-    breaks.emplace_back(self->mappingStack.back(), &curr->ifTrue, BreakInfo::After);
-    self->mappingStack.pop_back();
-    self->currMapping = std::move(merge(breaks));
+  static void afterIfFalse(SubType* self, Expression** currp) {
+    self->finishIf();
   }
-  static void doPreLoop(SubType* self, Expression** currp) {
+  static void beforeLoop(SubType* self, Expression** currp) {
     // save the state before entering the loop, for calculation later of the merge at the loop top
     self->mappingStack.push_back(self->currMapping);
+    self->loopGetStack.push_back({});
   }
   static void doVisitLoop(SubType* self, Expression** currp) {
     auto* curr = (*currp)->cast<Loop>();
     if (curr->name.is() && self->breakInfos.find(curr->name) != self->breakInfos.end()) {
       auto& infos = self->breakInfos[curr->name];
-      infos.emplace_back(self->mappingStack.back(), currp, BreakInfo::Before);
-      self->merge(infos, curr->name); // output is not assigned anywhere, this is an interesting code path
+      infos.emplace_back(std::move(self->mappingStack.back()));
+      auto before = infos.back();
+      auto& merged = self->merge(infos);
+      // every local we created a phi for requires us to update get_local operations in
+      // the loop
+      auto& gets = self->loopGetStack.back();
+      for (auto* get : gets) {
+        auto original = self->originalGetIndexes[get];
+        get->index = merged[original];
+      }
     }
     self->mappingStack.pop_back();
+    self->loopGetStack.pop_back();
   }
   static void visitBreak(SubType* self, Expression** currp) {
     auto* curr = (*currp)->cast<Break>();
@@ -141,6 +147,18 @@ struct SetTrackingWalker : public PostWalker<SubType, VisitorType> {
     setUnreachable(currMapping);
   }
 
+  void visitGetLocal(GetLocal* curr) {
+    for (auto& loopGets : loopGetStack) {
+      loopGets.push_back(curr);
+    }
+    originalGetIndexes[curr] = curr->index;
+    curr->index = currMapping[curr->index];
+  }
+  void visitSetLocal(SetLocal* curr) {
+    currMapping[curr->index] = curr;
+    curr->index = nextIndex++;
+  }
+
   // assignments
 
   void visitSetLocal(SetLocal *curr) {
@@ -153,12 +171,12 @@ struct SetTrackingWalker : public PostWalker<SubType, VisitorType> {
     if (auto* iff = (*currp)->dynCast<If>()) {
       // if needs special handling
       if (iff->ifFalse) {
-        self->pushTask(SubType::doIfFalse, currp);
+        self->pushTask(SubType::afterIfFalse, currp);
         self->pushTask(SubType::scan, iff->ifFalse);
       }
-      self->pushTask(SubType::doIfTrue, currp);
+      self->pushTask(SubType::afterIfTrue, currp);
       self->pushTask(SubType::scan, iff->ifTrue);
-      self->pushTask(SubType::doIfCondition, currp);
+      self->pushTask(SubType::afterIfCondition, currp);
       self->pushTask(SubType::scan, iff->condition);
     } else {
       PostWalker<SubType, VisitorType>::scan(self, currp);
@@ -166,72 +184,74 @@ struct SetTrackingWalker : public PostWalker<SubType, VisitorType> {
 
     // loops need pre-order visiting too
     if ((*currp)->is<Loop>()) {
-      self->pushTask(SubType::doPreLoop, currp);
+      self->pushTask(SubType::beforeLoop, currp);
     }
   }
 
   // helpers
 
-  void setUnreachable(NameMapping& mapping) {
+  void setUnreachable(Mapping& mapping) {
     mapping.resize(numLocals); // may have been emptied by a move
     mapping[0] = Index(-1);
   }
 
-  bool isUnreachable(NameMapping& mapping) {
+  bool isUnreachable(Mapping& mapping) {
     return mapping[0] == Index(-1);
   }
 
-  // merges a bunch of infos into one. where necessary calls a phi hook.
-  NameMapping& merge(std::vector<BreakInfo>& infos, Name name = Name()) {
+  // merges a bunch of infos into one
+  Mapping& merge(std::vector<Info>& infos) {
+    assert(infos.size() > 0);
     auto& out = infos[0];
+    if (infos.size() == 1) {
+      return infos[0];
+    }
     for (Index i = 0; i < numLocals; i++) {
-      Index seen = -1;
+      SetLocal* merged;
+      bool seen = false;
       for (auto& info : infos) {
-        if (isUnreachable(info.mapping)) continue;
-        if (seen == -1) {
-          seen = info.mapping[i];
+        if (isUnreachable(info)) continue;
+        if (!seen) {
+          merged = info[i];
+          seen = true;
         } else {
-          if (info.mapping[i] != seen) {
-            // we need a phi here
-            seen = nextIndex++;
-            createPhi(infos, i, seen, name);
+          if (info[i] != merged) {
+            // we need a phi for this local
+            merged = nextIndex++;
+            createPhi(infos, i, merged);
             break;
           }
         }
       }
-      out.mapping[i] = seen;
-    }
-    return out.mapping;
-  }
-
-  void createPhi(std::vector<BreakInfo>& infos, Index old, Index new_, Name name) {
-    abort(); // override this in child classes
-  }
-};
-
-struct LoopPhiFinder : public SetTrackingWalker<LoopPhiFinder, Visitor<LoopPhiFinder>> {
-  std::map<Name, std::vector<Index>> loopPhis; // loop name => list of source indexes that need a phi
-
-  LoopPhiFinder(Function* func) : SetTrackingWalker(func) {}
-
-  void createPhi(std::vector<BreakInfo>& infos, Index old, Index new_, Name name) {
-    for (auto& info : infos) {
-      if (info.type == BreakInfo::Before) {
-        auto* loop = (*info.origin)->cast<Loop>();
-        loopPhis[loop->name].push_back(old);
+      if (seen) {
+        out[i] = merged;
       }
     }
+    return out;
   }
-};
 
-struct SSAify : public Pass {
-  bool isFunctionParallel() override { return true; }
-
-  Pass* create() override { return new SSAify; }
-
-  void runFunction(PassRunner* runner, Module* module, Function* function) override {
-    // count how many set_locals each local has. if it already has just 1, we can ignore it.
-    LoopPhiFinder finder(&numSetLocals, func);
+  void createPhi(std::vector<BreakInfo>& infos, Index old, Index new_) {
+    // assign the set value to the phi value as well
+    Builder builder(*getModule());
+    for (auto& info : infos) {
+      if (!info[index]) {
+        // this is a param or the zero init value. add a set first thing in
+        // the function
+        auto* block = builder.blockify(getFunction()->body);
+        getFunction()->body = block;
+        auto* set = builder.makeSetLocal(
+          old_,
+          builder.makeGetLocal(new_, getFunction()->getLocalType(old))
+        );
+        info[old] = set;
+        ExpressionManipulator::spliceIntoBlock(block, 0, set);
+      }
+      // now a set exists, just add a tee of its value, so we also set the phi merge var
+      info[old]->value = builder.makeTeeLocal(
+        new_,
+        info[old]->value
+      );
+    }
   }
 };
 
