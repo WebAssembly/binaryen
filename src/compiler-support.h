ec76c8c1d7bf28e025770eb3a84d5ee6c7040d82
Sam Clegg <sbc@chromium.org>
2017-06-12 14:43:54 -0700

    Log callImport fatal error to cerr so it is not buffered. (#1036)
    Use Fatal() rather than stdout or report callImport error
    
    Without this the write to stdout can be lost (Since the following line
    aborts)

--- src/compiler-support.h
+++ src/compiler-support.h
@@ -36,6 +36,14 @@
 # define WASM_UNREACHABLE() abort()
 #endif
 
+#ifdef __GNUC__
+#define WASM_NORETURN __attribute__((noreturn))
+#elif defined(_MSC_VER)
+#define WASM_NORETURN __declspec(noreturn)
+#else
+#define WASM_NORETURN
+#endif
+
 // The code might contain TODOs or stubs that read some values but do nothing
 // with them. The compiler might fail with [-Werror,-Wunused-variable].
 // The WASM_UNUSED(varible) is a wrapper that helps to suppress the error.
