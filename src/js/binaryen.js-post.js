fe4deb8d8f1e9825424fd9f221e45b5317e3423f
dcodeIO <dcode@dcode.io>
2017-07-03 21:41:51 +0200

    Added comments explaining the individual steps performed for UMD support

--- src/js/binaryen.js-post.js
+++ src/js/binaryen.js-post.js
@@ -911,11 +911,19 @@
     return Module['_BinaryenSetAPITracing'](on);
   };
 
-  if (typeof module !== "undefined" && module && module.exports)
-    module.exports = Module;
-  else if (typeof define === "function" && define["amd"])
+  // Support AMD-compatible loaders by defining a factory function that returns 'Module'
+  if (typeof define === "function" && define["amd"])
     define(function() { return Module; });
+
+  // Support CommonJS-compatible loaders by checking for 'require' and 'module.exports'
+  else if (typeof require === "function" && typeof module !== "undefined" && module && module.exports)
+    module.exports = Module;
+
+  // Otherwise expose as 'Binaryen' globally checking for common names of the global object
+  // first (namely 'global' and 'window') and fall back to 'this' (i.e. within web workers).
   else
-    (typeof global !== "undefined" ? global : typeof window !== "undefined" ? window : this)["Binaryen"] = Module;
+    (typeof global !== "undefined" && global ||
+     typeof window !== "undefined" && window ||
+     this)["Binaryen"] = Module;
 
 })();
