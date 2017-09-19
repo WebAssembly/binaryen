bde81471361ce4dd2d4c0272cf0495676c07aeab
Jeremy Day <jadaytime@gmail.com>
2017-09-19 16:00:54 -0400

    remove implicit fallthroughs (#1194)

--- src/tools/translate-to-fuzz.h
+++ src/tools/translate-to-fuzz.h
@@ -639,8 +639,8 @@ private:
           }
         }
         switch (conditions) {
-          case 0: if (!oneIn(4)) continue;
-          case 1: if (!oneIn(2)) continue;
+          case 0: if (!oneIn(4)) continue; break;
+          case 1: if (!oneIn(2)) continue; break;
           default: if (oneIn(conditions + 1)) continue;
         }
         return builder.makeBreak(name);
