90364e3b402fe2ed19f0378a92731e89d8ae869e
Jeremy Day <jadaytime@gmail.com>
2017-09-19 18:30:14 -0400

    fix case statements not adhering to convention

--- src/tools/translate-to-fuzz.h
+++ src/tools/translate-to-fuzz.h
@@ -639,14 +639,17 @@ private:
           }
         }
         switch (conditions) {
-          case 0:
+          case 0: {
             if (!oneIn(4)) continue;
             break;
-          case 1:
+          }
+          case 1: {
             if (!oneIn(2)) continue;
             break;
-          default:
+          }
+          default: {
             if (oneIn(conditions + 1)) continue;
+          }
         }
         return builder.makeBreak(name);
       }
