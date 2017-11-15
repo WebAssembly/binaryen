6736094ed25473afbaab79baaa43d3ac514e684c
Jacob Gravelle <jgravelle@google.com>
2017-11-15 11:16:04 -0800

    ast->ir, remove commented out code, include a debug module print because linking

--- src/tools/o2wasm.cpp
+++ src/tools/o2wasm.cpp
@@ -20,7 +20,7 @@
 
 #include <exception>
 
-#include "ast/trapping.h"
+#include "ir/trapping.h"
 #include "support/colors.h"
 #include "support/command-line.h"
 #include "support/file.h"
@@ -154,6 +154,10 @@ int main(int argc, const char *argv[]) {
 
   std::cerr << "Oh hey we made it, nice\n";
 
+  if (options.debug) {
+    WasmPrinter::printModule(&wasm, std::cerr);
+  }
+
   std::cerr << "User sections:\n";
   for (auto &section : wasm.userSections) {
     std::cerr << "  " << section.name << "\n";
@@ -172,84 +176,5 @@ int main(int argc, const char *argv[]) {
   ModuleWriter writer;
   writer.writeBinary(wasm, outfile);
 
-  // if (allowMemoryGrowth && !generateEmscriptenGlue) {
-  //   Fatal() << "Error: adding memory growth code without Emscripten glue. "
-  //     "This doesn't do anything.\n";
-  // }
-
-  // auto debugFlag = options.debug ? Flags::Debug : Flags::Release;
-  // auto input(read_file<std::string>(options.extra["infile"], Flags::Text, debugFlag));
-
-  // if (options.debug) std::cerr << "Parsing and wasming..." << std::endl;
-  // uint64_t globalBase = options.extra.find("global-base") != options.extra.end()
-  //                         ? std::stoull(options.extra["global-base"])
-  //                         : 0;
-  // uint64_t stackAllocation =
-  //     options.extra.find("stack-allocation") != options.extra.end()
-  //         ? std::stoull(options.extra["stack-allocation"])
-  //         : 0;
-  // uint64_t initialMem =
-  //     options.extra.find("initial-memory") != options.extra.end()
-  //         ? std::stoull(options.extra["initial-memory"])
-  //         : 0;
-  // uint64_t maxMem =
-  //     options.extra.find("max-memory") != options.extra.end()
-  //         ? std::stoull(options.extra["max-memory"])
-  //         : 0;
-  // if (options.debug) std::cerr << "Global base " << globalBase << '\n';
-
-  // Linker linker(globalBase, stackAllocation, initialMem, maxMem,
-  //               importMemory || generateEmscriptenGlue, ignoreUnknownSymbols, startFunction,
-  //               options.debug);
-
-  // S2WasmBuilder mainbuilder(input.c_str(), options.debug);
-  // linker.linkObject(mainbuilder);
-
-  // if (trapMode != TrapMode::Allow) {
-  //   Module* wasm = &(linker.getOutput().wasm);
-  //   PassRunner runner(wasm);
-  //   addTrapModePass(runner, trapMode);
-  //   runner.run();
-  // }
-
-  // for (const auto& m : archiveLibraries) {
-  //   auto archiveFile(read_file<std::vector<char>>(m, Flags::Binary, debugFlag));
-  //   bool error;
-  //   Archive lib(archiveFile, error);
-  //   if (error) Fatal() << "Error opening archive " << m << "\n";
-  //   linker.linkArchive(lib);
-  // }
-
-  // if (generateEmscriptenGlue) {
-  //   emscripten::generateRuntimeFunctions(linker.getOutput());
-  // }
-
-  // linker.layout();
-
-  // std::stringstream meta;
-  // if (generateEmscriptenGlue) {
-  //   if (options.debug) std::cerr << "Emscripten gluing..." << std::endl;
-  //   if (allowMemoryGrowth) {
-  //     emscripten::generateMemoryGrowthFunction(linker.getOutput().wasm);
-  //   }
-
-  //   // dyncall thunks
-  //   linker.emscriptenGlue(meta);
-  // }
-
-  // if (options.extra["validate"] != "none") {
-  //   if (options.debug) std::cerr << "Validating..." << std::endl;
-  //   if (!wasm::WasmValidator().validate(linker.getOutput().wasm,
-  //        WasmValidator::Globally | (options.extra["validate"] == "web" ? WasmValidator::Web : 0))) {
-  //     Fatal() << "Error: linked module is not valid.\n";
-  //   }
-  // }
-
-  // if (options.debug) std::cerr << "Printing..." << std::endl;
-  // Output output(options.extra["output"], Flags::Text, options.debug ? Flags::Debug : Flags::Release);
-  // WasmPrinter::printModule(&linker.getOutput().wasm, output.getStream());
-  // output << meta.str();
-
-  // if (options.debug) std::cerr << "Done." << std::endl;
   return 0;
 }
