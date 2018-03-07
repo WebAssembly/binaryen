4a1409959b682c5e4c5b853b80a725e6b47fb98b
Alon Zakai (kripken) <alonzakai@gmail.com>
2018-03-07 14:10:15 -0800

    also lock, because those warnings can come from multiple threads

--- src/asm2wasm.h
+++ src/asm2wasm.h
@@ -1231,13 +1231,18 @@ void Asm2WasmBuilder::processAsm(Ref ast) {
 
     void notifyAboutWrongOperands(std::string why, Function* calledFunc) {
       // in e.g. Python there can be thousands of such warnings; limit the output.
-      static const int MAX_SHOWN = 10;
+      static const int MAX_SHOWN = 20;
       static int numShown = 0;
       if (numShown >= MAX_SHOWN) return;
-      std::cerr << why << " in call from " << getFunction()->name << " to " << calledFunc->name << " (this is likely due to undefined behavior in C, like defining a function one way and calling it in another, which is important to fix)\n";
-      numShown++;
-      if (numShown >= MAX_SHOWN) {
-        std::cerr << "(" << numShown << " such warnings shown; not showing any more)\n";
+      // use a mutex as this may be shown from multiple threads
+      {
+        static std::mutex mutex;
+        std::unique_lock<std::mutex> lock(mutex);
+        std::cerr << why << " in call from " << getFunction()->name << " to " << calledFunc->name << " (this is likely due to undefined behavior in C, like defining a function one way and calling it in another, which is important to fix)\n";
+        numShown++;
+        if (numShown >= MAX_SHOWN) {
+          std::cerr << "(" << numShown << " such warnings shown; not showing any more)\n";
+        }
       }
     }
 
