b51f19b0b2bdc5f21f8bee32efe4d04506373ff5
Michael Ferris <micfer@microsoft.com>
2018-03-25 16:13:54 -0700

    Support wasm-reduce for Windows

--- src/support/path.h
+++ src/support/path.h
@@ -44,11 +44,13 @@ inline std::string getBinaryenRoot() {
   return ".";
 }
 
+inline std::string getBinaryenBinariesRoot() {
+  return getBinaryenRoot() + getPathSeparator() + "bin" + getPathSeparator();
+}
+
 // Gets the path to a binaryen binary tool, like wasm-opt
 inline std::string getBinaryenBinaryTool(std::string name) {
-  return getBinaryenRoot() + getPathSeparator() +
-         "bin" + getPathSeparator() +
-         name;
+  return getBinaryenBinariesRoot() + name;
 }
 
 } // namespace Path
