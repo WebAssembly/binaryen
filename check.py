7ecaa8223aa93e5097ee8063f2cd181194bb077c
Alon Zakai (kripken) <alonzakai@gmail.com>
2018-05-05 10:38:34 -0700

    testing

--- check.py
+++ check.py
@@ -250,6 +250,7 @@ def run_wasm_metadce_tests():
 def run_wasm_reduce_tests():
   print '\n[ checking wasm-reduce ]\n'
 
+  # fixed testcases
   test_dir = os.path.join(options.binaryen_test, 'reduce')
   for t in os.listdir(test_dir):
     if t.endswith('.wast'):
@@ -257,11 +258,18 @@ def run_wasm_reduce_tests():
       t = os.path.join(test_dir, t)
       # convert to wasm
       run_command(WASM_AS + [t, '-o', 'a.wasm'])
-      print run_command(WASM_REDUCE + ['a.wasm', '--command=%s b.wasm --fuzz-exec' % WASM_OPT[0], '-t', 'b.wasm', '-w', 'c.wasm'])
+      run_command(WASM_REDUCE + ['a.wasm', '--command=%s b.wasm --fuzz-exec' % WASM_OPT[0], '-t', 'b.wasm', '-w', 'c.wasm'])
       expected = t + '.txt'
       run_command(WASM_DIS + ['c.wasm', '-o', 'a.wast'])
       with open('a.wast') as seen:
         fail_if_not_identical_to_file(seen.read(), expected)
+
+  # run on a nontrivial fuzz testcase, for general coverage
+  run_command(WASM_OPT + [os.path.join(options.binaryen_test, 'unit.asm.js'), '-ttf', '-o', 'a.wasm'])
+  before = os.stat('a.wasm').st_size
+  run_command(WASM_REDUCE + ['a.wasm', '--command=%s b.wasm --fuzz-exec' % WASM_OPT[0], '-t', 'b.wasm', '-w', 'c.wasm'])
+  after = os.stat('c.wasm').st_size
+  assert after < 0.5 * before, [before, after]
 
 def run_spec_tests():
   print '\n[ checking wasm-shell spec testcases... ]\n'
