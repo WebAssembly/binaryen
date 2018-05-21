d4a3e08aa6ca7e7e085eb95399b31360b8eb1f29
Alex Crichton <alex@alexcrichton.com>
2018-05-20 20:29:31 -0700

    Match up the header type and the defined type

--- src/passes/embedwast.cpp
+++ src/passes/embedwast.cpp
@@ -10,14 +10,15 @@ int main(int argc, char **argv) {
   std::ofstream out(argv[1]);
   auto len = contents.size() + 1;
   out << "namespace wasm {\n";
-  out << "extern const char IntrinsicsModule[" << len << "];\n";
-  out << "const char IntrinsicsModule[" << len << "] = {\n";
+  out << "extern const char* IntrinsicsModule;\n";
+  out << "const char theModule[" << len << "] = {\n";
   for (char c : contents) {
     out << (int) c;
     out << ", ";
   }
   out << 0;
   out << "\n};\n";
+  out << "const char* IntrinsicsModule = theModule;\n";
   out << "}\n";
   out.close();
 }
