5e69ca9ff4c5117f48583e616aa61d6d18894dd4
Alex Crichton <alex@alexcrichton.com>
2018-05-20 20:16:30 -0700

    Move injected intrinsics to a separate wast file
    Then use CMake to inject a build step which will run a program at build time to
    generate a C++ source file to embed the file as part of the main build.

---
+++ src/passes/embedwast.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <fstream>
+#include <streambuf>
+
+int main(int argc, char **argv) {
+  std::ifstream t("wasm-intrinsics.wast");
+  std::string contents((std::istreambuf_iterator<char>(t)),
+                       std::istreambuf_iterator<char>());
+
+  std::ofstream out(argv[1]);
+  auto len = contents.size() + 1;
+  out << "namespace wasm {\n";
+  out << "extern const char IntrinsicsModule[" << len << "];\n";
+  out << "const char IntrinsicsModule[" << len << "] = {\n";
+  for (char c : contents) {
+    out << (int) c;
+    out << ", ";
+  }
+  out << 0;
+  out << "\n};\n";
+  out << "}\n";
+  out.close();
+}
