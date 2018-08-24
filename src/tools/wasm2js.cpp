c4b0d4f049339e095fc03fbc0e0a1cd1743aaf48
Alex Crichton <alex@alexcrichton.com>
2018-08-24 15:26:49 -0700

    wasm2js: Support `*.wasm` input files
    Previously `wasm2js` only supported `*.wast` files but to make it a bit easier
    to use in tooling pipelines this commit adds support for reading in a `*.wasm`
    file directly. Determining which parser to use depends on the input filename,
    where the binary parser is used with `*.wasm` files and the wast parser is used
    for all other files.

--- src/tools/wasm2js.cpp
+++ src/tools/wasm2js.cpp
@@ -29,7 +29,7 @@ using namespace wasm;
 
 int main(int argc, const char *argv[]) {
   Wasm2JsBuilder::Flags builderFlags;
-  Options options("wasm2js", "Transform .wast files to asm.js");
+  Options options("wasm2js", "Transform .wasm/.wast files to asm.js");
   options
       .add("--output", "-o", "Output file (stdout if not specified)",
            Options::Arguments::One,
@@ -55,28 +55,43 @@ int main(int argc, const char *argv[]) {
   options.parse(argc, argv);
   if (options.debug) builderFlags.debug = true;
 
-  auto input(
-      read_file<std::vector<char>>(options.extra["infile"], Flags::Text, options.debug ? Flags::Debug : Flags::Release));
-
   Element* root;
   Module wasm;
   Ref asmjs;
 
   try {
-    if (options.debug) std::cerr << "s-parsing..." << std::endl;
-    SExpressionParser parser(input.data());
-    root = parser.root;
+    // If the input filename ends in `.wasm`, then parse it in binary form
+    auto &input = options.extra["infile"];
+    std::string suffix(".wasm");
+    if (input.size() >= suffix.size() &&
+        input.compare(input.size() - suffix.size(), suffix.size(), suffix) == 0) {
+      ModuleReader reader;
+      reader.setDebug(options.debug);
+      reader.read(input, wasm, "");
 
-    if (options.debug) std::cerr << "w-parsing..." << std::endl;
-    SExpressionWasmBuilder builder(wasm, *(*root)[0]);
+      if (options.debug) std::cerr << "asming..." << std::endl;
+      Wasm2JsBuilder wasm2js(builderFlags);
+      asmjs = wasm2js.processWasm(&wasm);
 
-    if (options.debug) std::cerr << "asming..." << std::endl;
-    Wasm2JsBuilder wasm2js(builderFlags);
-    asmjs = wasm2js.processWasm(&wasm);
+    // Otherwise assume it's a `*.wast` file and go from there
+    } else {
+      auto input(
+          read_file<std::vector<char>>(options.extra["infile"], Flags::Text, options.debug ? Flags::Debug : Flags::Release));
+      if (options.debug) std::cerr << "s-parsing..." << std::endl;
+      SExpressionParser parser(input.data());
+      root = parser.root;
 
-    if (options.extra["asserts"] == "1") {
-      if (options.debug) std::cerr << "asserting..." << std::endl;
-      flattenAppend(asmjs, wasm2js.processAsserts(&wasm, *root, builder));
+      if (options.debug) std::cerr << "w-parsing..." << std::endl;
+      SExpressionWasmBuilder builder(wasm, *(*root)[0]);
+
+      if (options.debug) std::cerr << "asming..." << std::endl;
+      Wasm2JsBuilder wasm2js(builderFlags);
+      asmjs = wasm2js.processWasm(&wasm);
+
+      if (options.extra["asserts"] == "1") {
+        if (options.debug) std::cerr << "asserting..." << std::endl;
+        flattenAppend(asmjs, wasm2js.processAsserts(&wasm, *root, builder));
+      }
     }
   } catch (ParseException& p) {
     p.dump(std::cerr);
