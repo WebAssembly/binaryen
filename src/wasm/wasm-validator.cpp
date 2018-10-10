WebAssembly/binaryen

commit e7756828726ec87afeb923a94d785f594de96aa7

No atomic float operations (#1693)
SafeHeap was emitting them, but it looks like they are invalid according to the wasm-threads spec.

Fixes kripken/emscripten#7208

src/wasm/wasm-validator.cpp
@@ -502,7 +502,10 @@ void FunctionValidator::visitLoad(Load* curr) {
   validateMemBytes(curr->bytes, curr->type, curr);
   validateAlignment(curr->align, curr->type, curr->bytes, curr->isAtomic, curr);
   shouldBeEqualOrFirstIsUnreachable(curr->ptr->type, i32, curr, "load pointer type must be i32");
-  if (curr->isAtomic) shouldBeFalse(curr->signed_, curr, "atomic loads must be unsigned");
+  if (curr->isAtomic) {
+    shouldBeFalse(curr->signed_, curr, "atomic loads must be unsigned");
+    shouldBeTrue(isIntegerType(curr->type), curr, "atomic loads must be of integers");
+  }
 }
 
 void FunctionValidator::visitStore(Store* curr) {
@@ -513,6 +516,9 @@ void FunctionValidator::visitStore(Store* curr) {
   shouldBeEqualOrFirstIsUnreachable(curr->ptr->type, i32, curr, "store pointer type must be i32");
   shouldBeUnequal(curr->value->type, none, curr, "store value type must not be none");
   shouldBeEqualOrFirstIsUnreachable(curr->value->type, curr->valueType, curr, "store value type must match");
+  if (curr->isAtomic) {
+    shouldBeTrue(isIntegerType(curr->valueType), curr, "atomic stores must be of integers");
+  }
 }
 
 void FunctionValidator::visitAtomicRMW(AtomicRMW* curr) {

src/passes/SafeHeap.cpp
@@ -152,6 +152,10 @@ struct SafeHeap : public Pass {
     }
   }
 
+  bool isPossibleAtomicOperation(Index align, Index bytes, bool shared, Type type) {
+    return align == bytes && shared && isIntegerType(type);
+  }
+
   void addGlobals(Module* module) {
     // load funcs
     Load load;
@@ -168,8 +172,10 @@ struct SafeHeap : public Pass {
             if (align > bytes) continue;
             for (auto isAtomic : { true, false }) {
               load.isAtomic = isAtomic;
-              if (isAtomic && align != bytes) continue;
-              if (isAtomic && !module->memory.shared) continue;
+              if (isAtomic &&
+                  !isPossibleAtomicOperation(align, bytes, module->memory.shared, type)) {
+                continue;
+              }
               addLoadFunc(load, module);
             }
           }
@@ -189,8 +195,10 @@ struct SafeHeap : public Pass {
           if (align > bytes) continue;
           for (auto isAtomic : { true, false }) {
             store.isAtomic = isAtomic;
-            if (isAtomic && align != bytes) continue;
-            if (isAtomic && !module->memory.shared) continue;
+            if (isAtomic &&
+                !isPossibleAtomicOperation(align, bytes, module->memory.shared, valueType)) {
+              continue;
+            }
             addStoreFunc(store, module);
           }
         }

commit 08ce8d238cf32e16bb444f408d1c3f099e5c6e62

properly handle unreachable atomic operations, fixes a regression from #1693

src/wasm/wasm-validator.cpp
@@ -504,7 +504,7 @@ void FunctionValidator::visitLoad(Load* curr) {
   shouldBeEqualOrFirstIsUnreachable(curr->ptr->type, i32, curr, "load pointer type must be i32");
   if (curr->isAtomic) {
     shouldBeFalse(curr->signed_, curr, "atomic loads must be unsigned");
-    shouldBeTrue(isIntegerType(curr->type), curr, "atomic loads must be of integers");
+    shouldBeIntOrUnreachable(curr->type, curr, "atomic loads must be of integers");
   }
 }
 
@@ -517,7 +517,7 @@ void FunctionValidator::visitStore(Store* curr) {
   shouldBeUnequal(curr->value->type, none, curr, "store value type must not be none");
   shouldBeEqualOrFirstIsUnreachable(curr->value->type, curr->valueType, curr, "store value type must match");
   if (curr->isAtomic) {
-    shouldBeTrue(isIntegerType(curr->valueType), curr, "atomic stores must be of integers");
+    shouldBeIntOrUnreachable(curr->valueType, curr, "atomic stores must be of integers");
   }
 }
 