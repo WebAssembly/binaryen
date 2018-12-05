67d837f1adaf897d1ca21faacfa5aada24dbc49c
Sam Clegg <sbc@chromium.org>
2018-12-05 11:20:34 -0800

    Format metadata json using mutliple lines for readability

--- src/wasm/wasm-emscripten.cpp
+++ src/wasm/wasm-emscripten.cpp
@@ -758,38 +758,40 @@ std::string EmscriptenGlueGenerator::generateEmscriptenMetadata(
   auto maybeComma = [&commaFirst]() {
     if (commaFirst) {
       commaFirst = false;
-      return "";
+      return "\n  ";
     } else {
-      return ",";
+      return ",\n  ";
     }
   };
 
   std::stringstream meta;
-  meta << "{ ";
+  meta << "{\n";
 
   AsmConstWalker emAsmWalker = fixEmAsmConstsAndReturnWalker(wasm);
 
   // print
   commaFirst = true;
-  meta << "\"asmConsts\": {";
-  for (auto& pair : emAsmWalker.sigsForCode) {
-    auto& code = pair.first;
-    auto& sigs = pair.second;
-    meta << maybeComma();
-    meta << '"' << emAsmWalker.ids[code] << "\": [\"" << code << "\", ";
-    printSet(meta, sigs);
-    meta << ", ";
+  if (!emAsmWalker.sigsForCode.empty()) {
+    meta << "\"asmConsts\": {";
+    for (auto& pair : emAsmWalker.sigsForCode) {
+      auto& code = pair.first;
+      auto& sigs = pair.second;
+      meta << maybeComma();
+      meta << '"' << emAsmWalker.ids[code] << "\": [\"" << code << "\", ";
+      printSet(meta, sigs);
+      meta << ", ";
 
-    // TODO: proxying to main thread. Currently this is unsupported, so proxy
-    // mode is "none", represented by an empty string.
-    meta << "[\"\"]";
+      // TODO: proxying to main thread. Currently this is unsupported, so proxy
+      // mode is "none", represented by an empty string.
+      meta << "[\"\"]";
 
-    meta << "]";
+      meta << "]";
+    }
+    meta << "\n},\n";
   }
-  meta << "},";
 
   EmJsWalker emJsWalker = fixEmJsFuncsAndReturnWalker(wasm);
-  if (emJsWalker.codeByName.size() > 0) {
+  if (!emJsWalker.codeByName.empty()) {
     meta << "\"emJsFuncs\": {";
     commaFirst = true;
     for (auto& pair : emJsWalker.codeByName) {
@@ -798,30 +800,31 @@ std::string EmscriptenGlueGenerator::generateEmscriptenMetadata(
       meta << maybeComma();
       meta << '"' << name << "\": \"" << code << '"';
     }
-    meta << "},";
+    meta << "\n},\n";
   }
 
-  meta << "\"staticBump\": " << staticBump << ", ";
+  meta << "\"staticBump\": " << staticBump << ",\n";
 
-  meta << "\"initializers\": [";
-  commaFirst = true;
-  for (const auto& func : initializerFunctions) {
-    meta << maybeComma();
-    meta << "\"" << func.c_str() << "\"";
+  if (!initializerFunctions.empty()) {
+    meta << "\"initializers\": [";
+    commaFirst = true;
+    for (const auto& func : initializerFunctions) {
+      meta << maybeComma();
+      meta << "\"" << func.c_str() << "\"";
+    }
+    meta << "\n],\n";
   }
-  meta << "]";
 
   if (numReservedFunctionPointers) {
     JSCallWalker jsCallWalker = getJSCallWalker(wasm);
-    meta << ", ";
-    meta << "\"jsCallStartIndex\": " << jsCallWalker.jsCallStartIndex << ", ";
+    meta << "\"jsCallStartIndex\": " << jsCallWalker.jsCallStartIndex << ",\n";
     meta << "\"jsCallFuncType\": [";
     commaFirst = true;
     for (std::string sig : jsCallWalker.indirectlyCallableSigs) {
       meta << maybeComma();
       meta << "\"" << sig << "\"";
     }
-    meta << "]";
+    meta << "\n],\n";
   }
 
   // Avoid adding duplicate imports to `declares' or `invokeFuncs`.  Even
@@ -833,7 +836,7 @@ std::string EmscriptenGlueGenerator::generateEmscriptenMetadata(
   // We use the `base` rather than the `name` of the imports here and below
   // becasue this is the externally visible name that the embedder (JS) will
   // see.
-  meta << ", \"declares\": [";
+  meta << "\"declares\": [";
   commaFirst = true;
   ModuleUtils::iterImportedFunctions(wasm, [&](Function* import) {
     if (emJsWalker.codeByName.count(import->base.str) == 0 &&
@@ -845,32 +848,34 @@ std::string EmscriptenGlueGenerator::generateEmscriptenMetadata(
       }
     }
   });
-  meta << "]";
+  meta << "\n],\n";
 
-  meta << ", \"externs\": [";
+  meta << "\"externs\": [";
   commaFirst = true;
   ModuleUtils::iterImportedGlobals(wasm, [&](Global* import) {
     meta << maybeComma() << "\"_" << import->base.str << '"';
   });
-  meta << "]";
+  meta << "\n],\n";
 
-  meta << ", \"implementedFunctions\": [";
-  commaFirst = true;
-  for (const auto& ex : wasm.exports) {
-    if (ex->kind == ExternalKind::Function) {
-      meta << maybeComma() << "\"_" << ex->name.str << '"';
+  if (!wasm.exports.empty()) {
+    meta << "\"implementedFunctions\": [";
+    commaFirst = true;
+    for (const auto& ex : wasm.exports) {
+      if (ex->kind == ExternalKind::Function) {
+        meta << maybeComma() << "\"_" << ex->name.str << '"';
+      }
     }
-  }
-  meta << "]";
+    meta << "\n],\n";
 
-  meta << ", \"exports\": [";
-  commaFirst = true;
-  for (const auto& ex : wasm.exports) {
-    meta << maybeComma() << '"' << ex->name.str << '"';
+    meta << "\"exports\": [";
+    commaFirst = true;
+    for (const auto& ex : wasm.exports) {
+      meta << maybeComma() << '"' << ex->name.str << '"';
+    }
+    meta << "\n],\n";
   }
-  meta << "]";
 
-  meta << ", \"invokeFuncs\": [";
+  meta << "\"invokeFuncs\": [";
   commaFirst = true;
   ModuleUtils::iterImportedFunctions(wasm, [&](Function* import) {
     if (import->base.startsWith("invoke_")) {
@@ -879,9 +884,8 @@ std::string EmscriptenGlueGenerator::generateEmscriptenMetadata(
       }
     }
   });
-  meta << "]";
-
-  meta << " }\n";
+  meta << "\n]\n";
+  meta << "}\n";
 
   return meta.str();
 }
