c5ad2e5ecc517030aa242c18fd738b9d79f11429
Thomas Lively <7121787+tlively@users.noreply.github.com>
2019-04-08 15:49:26 -0700

    Move segment merging to fit web limits into its own pass (#1980)
    It was previously part of writing a binary, but changing the number of
    segments at such a late stage would not work in the presence of bulk
    memory's datacount section. Also updates the memory packing pass
    to respect the web's limits on the number of data segments.

--- check.py
+++ check.py
@@ -240,7 +240,8 @@ def run_ctor_eval_tests():
       print '..', t
       t = os.path.join(test_dir, t)
       ctors = open(t + '.ctors').read().strip()
-      cmd = WASM_CTOR_EVAL + [t, '-o', 'a.wast', '-S', '--ctors', ctors]
+      # TODO: remove --disable-bulk-memory once default feature set is MVP
+      cmd = WASM_CTOR_EVAL + [t, '--disable-bulk-memory', '-o', 'a.wast', '-S', '--ctors', ctors]
       run_command(cmd)
       actual = open('a.wast').read()
       out = t + '.out'
