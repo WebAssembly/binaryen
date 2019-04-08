d48b0d01cea8d3faa9f9430ca7532cdc917134e3
Alon Zakai <azakai@google.com>
2019-04-08 13:42:54 -0700

    avoid UB in binary writing code when taking the address of an empty vector

--- src/wasm/wasm-binary.cpp
+++ src/wasm/wasm-binary.cpp
@@ -333,7 +333,7 @@ void WasmBinaryWriter::writeDataSegments() {
       writeExpression(segment.offset);
       o << int8_t(BinaryConsts::End);
     }
-    writeInlineBuffer(&segment.data[0], segment.data.size());
+    writeInlineBuffer(segment.data.data(), segment.data.size());
     emitted++;
   };
 
