b86fc66bba3c0b36cd91752f0f21e170fcc688b7
Alon Zakai (kripken) <alonzakai@gmail.com>
2019-04-17 19:09:58 -0700

    update tests

--- test/wasm2js/empty_imported_table.2asm.js
+++ test/wasm2js/empty_imported_table.2asm.js
@@ -1,5 +1,5 @@
 function asmFunc(global, env, buffer) {
- "use asm";
+ "almost asm";
  var HEAP8 = new global.Int8Array(buffer);
  var HEAP16 = new global.Int16Array(buffer);
  var HEAP32 = new global.Int32Array(buffer);
