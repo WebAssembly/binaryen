07c9048c69da1f20ae2d11873008846e0cd7e6ea
Heejin Ahn <aheejin@gmail.com>
2019-04-25 17:25:07 -0700

    JS file exclusion test + diff printing

--- clang-format-diff.sh
+++ clang-format-diff.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-FORMAT_MSG=$(git clang-format master HEAD -q --diff)
+FORMAT_MSG=$(git clang-format master HEAD -q --diff --extensions c,cpp,h)
 if [ -n "$FORMAT_MSG" -a "$FORMAT_MSG" != "no modified files to format" ]
 then
-  echo "Run git clang-format before committing"
+  echo "Run git clang-format before committing!"
+  echo
+  echo $FORMAT_MSG
   exit 1
 fi
