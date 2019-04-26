95f129bfede64072cdff992a9f9f720410983613
Heejin Ahn <aheejin@gmail.com>
2019-04-25 17:54:33 -0700

    Disable JavaScript formatting in .clang-format

--- clang-format-diff.sh
+++ clang-format-diff.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-FORMAT_MSG=$(git clang-format master HEAD -q --diff --extensions c,cpp,h)
+FORMAT_MSG=$(git clang-format master HEAD -q --diff)
 if [ -n "$FORMAT_MSG" -a "$FORMAT_MSG" != "no modified files to format" ]
 then
   echo "Run git clang-format before committing!"
   # Run git clang-format once again to show the error
-  git clang-format master HEAD -q --diff --extensions c,cpp,h
+  git clang-format master HEAD -q --diff
   exit 1
 fi
