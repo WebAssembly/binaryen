eb3fe7533562b2d1850a772359cfe0320d22eba6
Heejin Ahn <aheejin@gmail.com>
2019-04-25 18:03:52 -0700

    Print a newline before showing diff

--- clang-format-diff.sh
+++ clang-format-diff.sh
@@ -3,6 +3,7 @@ FORMAT_MSG=$(git clang-format master HEAD -q --diff)
 if [ -n "$FORMAT_MSG" -a "$FORMAT_MSG" != "no modified files to format" ]
 then
   echo "Run git clang-format before committing!"
+  echo
   # Run git clang-format once again to show the error
   git clang-format master HEAD -q --diff
   exit 1
