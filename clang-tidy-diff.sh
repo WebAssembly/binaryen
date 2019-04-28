d886a3d46c3dc5bf71528d6d03235800b366379f
Heejin Ahn <aheejin@gmail.com>
2019-04-27 18:39:32 -0700

    Remove special case handling

--- clang-tidy-diff.sh
+++ clang-tidy-diff.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
-
-# In some Travis CI settings in which the build directory is a subdirectory of
-# binaryen/, we don't have the compilation database file in binaryen/. We skip
-# the test in this case.
-if [ ! -f compile_commands.json ]
-then
-  echo "Compilation database file does not exist. Skipping clang-tidy test."
-  exit 0
-fi
-
 CLANG_DIR=$(dirname $(dirname $(which clang-tidy)))
 CLANG_TIDY_DIFF=$CLANG_DIR/share/clang/clang-tidy-diff.py
 MERGE_BASE=$(git merge-base master HEAD)
