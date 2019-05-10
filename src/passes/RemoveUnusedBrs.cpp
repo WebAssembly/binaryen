WebAssembly/binaryen

commit f5b8221e9759c37ef44158c2d2858dcee51b6c1f

Improve selectification in remove-unused-brs
We turned an if into a select when optimizing for size (and if
side effects etc. allow so). This patch improves that, doing it
not just when optimizing for size, but also when it looks
beneficial given the amount of work on both sides of the if. As
a result we can create selects in -O3 etc.

src/passes/RemoveUnusedBrs.cpp
@@ -21,9 +21,10 @@
 #include <wasm.h>
 #include <pass.h>
 #include <parsing.h>
-#include <ir/utils.h>
 #include <ir/branch-utils.h>
+#include <ir/cost.h>
 #include <ir/effects.h>
+#include <ir/utils.h>
 #include <wasm-builder.h>
 
 namespace wasm {
@@ -780,27 +781,44 @@ struct RemoveUnusedBrs : public WalkerPass<PostWalker<RemoveUnusedBrs>> {
 
       void visitIf(If* curr) {
         // we may have simplified ifs enough to turn them into selects
-        // this is helpful for code size, but can be a tradeoff with performance as we run both code paths
-        if (!shrink) return;
-        if (curr->ifFalse && isConcreteType(curr->ifTrue->type) && isConcreteType(curr->ifFalse->type)) {
-          // if with else, consider turning it into a select if there is no control flow
-          // TODO: estimate cost
-          EffectAnalyzer condition(passOptions, curr->condition);
-          if (!condition.hasSideEffects()) {
-            EffectAnalyzer ifTrue(passOptions, curr->ifTrue);
-            if (!ifTrue.hasSideEffects()) {
-              EffectAnalyzer ifFalse(passOptions, curr->ifFalse);
-              if (!ifFalse.hasSideEffects()) {
-                auto* select = getModule()->allocator.alloc<Select>();
-                select->condition = curr->condition;
-                select->ifTrue = curr->ifTrue;
-                select->ifFalse = curr->ifFalse;
-                select->finalize();
-                replaceCurrent(select);
-              }
+        if (auto* select = selectify(curr)) {
+          replaceCurrent(select);
+        }
+      }
+
+      // Convert an if into a select, if possible and beneficial to do so.
+      Select* selectify(If* iff) {
+        if (!iff->ifFalse ||
+            !isConcreteType(iff->ifTrue->type) ||
+            !isConcreteType(iff->ifFalse->type)) {
+          return nullptr;
+        }
+        // This is always helpful for code size, but can be a tradeoff with performance
+        // as we run both code paths. So when shrinking we always try to do this, but
+        // otherwise must consider more carefully.
+        if (!passOptions.shrinkLevel) {
+          // Consider the cost of executing all the code unconditionally
+          const auto MAX_COST = 7;
+          auto total = CostAnalyzer(iff->ifTrue).cost +
+                       CostAnalyzer(iff->ifFalse).cost;
+          if (total >= MAX_COST) return nullptr;
+        }
+        // Check if side effects allow this.
+        EffectAnalyzer condition(passOptions, iff->condition);
+        if (!condition.hasSideEffects()) {
+          EffectAnalyzer ifTrue(passOptions, iff->ifTrue);
+          if (!ifTrue.hasSideEffects()) {
+            EffectAnalyzer ifFalse(passOptions, iff->ifFalse);
+            if (!ifFalse.hasSideEffects()) {
+              return Builder(*getModule()).makeSelect(
+                iff->condition,
+                iff->ifTrue,
+                iff->ifFalse
+              );
             }
           }
         }
+        return nullptr;
       }
 
       void visitSetLocal(SetLocal* curr) {

test/passes/O.wast
@@ -24,5 +24,26 @@
     )
    )
   )
+  (func $many-selects (export "many-selects") (param $0 i32) (result i32)
+   (if
+    (i32.lt_s
+     (get_local $0)
+     (i32.const -1073741824)
+    )
+    (set_local $0
+     (i32.const -1073741824)
+    )
+    (if
+     (i32.gt_s
+      (get_local $0)
+      (i32.const 1073741823)
+     )
+     (set_local $0
+      (i32.const 1073741823)
+     )
+    )
+   )
+   (get_local $0)
+  )
 )
 

commit 6c3fefb10a7886ffbd213fa9b9f6c56203a5e318

Fixed a bug on iOS in Safari that was introduced in https://github.com/WebAssembly/binaryen/commit/f5b8221e9759c37ef44158c2d2858dcee51b6c1f
Added back if(!shrink) return in RemoveUnusedBrs.cpp

src/passes/RemoveUnusedBrs.cpp
@@ -856,6 +856,7 @@ struct RemoveUnusedBrs : public WalkerPass<PostWalker<RemoveUnusedBrs>> {
       }
 
       void visitIf(If* curr) {
+      	if(!shrink) return;
         // we may have simplified ifs enough to turn them into selects
         if (auto* select = selectify(curr)) {
           replaceCurrent(select);