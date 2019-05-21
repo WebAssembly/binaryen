9b5e470bcef1c6934ae74457d8094143bad8d74a
Derek Schuff <dschuff@chromium.org>
2019-05-21 16:08:30 -0700

    Don't use colons in filenames (#2134)
    Windows filenames can't contain colons. Use @ instead for passing arguments to passes.

--- src/passes/ExtractFunction.cpp
+++ src/passes/ExtractFunction.cpp
@@ -27,7 +27,7 @@ struct ExtractFunction : public Pass {
   void run(PassRunner* runner, Module* module) override {
     Name name = runner->options.getArgument(
       "extract",
-      "ExtractFunction usage:  wasm-opt --pass-arg=extract:FUNCTION_NAME");
+      "ExtractFunction usage:  wasm-opt --pass-arg=extract@FUNCTION_NAME");
     std::cerr << "extracting " << name << "\n";
     bool found = false;
     for (auto& func : module->functions) {
