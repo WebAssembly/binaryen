098a455a03c79c36ef45318667549c2827b2aaaa
Sam Clegg <sbc@chromium.org>
2019-05-31 14:02:40 -0700

    Fixes for clang-diff-tidy
    Run clang-diff-tidy early in travis.yml before we change directory
    to the build directory.
    
    Use realpath on clang-tidy to the python script on ubuntu where
    `/usr/bin/clang-tidy` might be symlink to e.g.
    `/usr/lib/llvm-4.0/share/clang/clang-tidy-diff.py`
    
    Use "@{upstream}" rather than master so it will work against
    any upstream.  This could have been fixed by switching to
    `origin/master` but I think this is more flexible.

--- clang-tidy-diff.sh
+++ clang-tidy-diff.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
-CLANG_DIR=$(dirname $(dirname $(which clang-tidy)))
+set -e
+CLANG_TIDY=$(realpath $(which clang-tidy))
+CLANG_DIR=$(dirname $(dirname $CLANG_TIDY))
 CLANG_TIDY_DIFF=$CLANG_DIR/share/clang/clang-tidy-diff.py
-MERGE_BASE=$(git merge-base master HEAD)
-TIDY_MSG=$(git diff -U0 $MERGE_BASE | $CLANG_TIDY_DIFF -quiet -p1 2> /dev/null)
+if [ ! -f $CLANG_TIDY_DIFF ]; then
+  echo "not found: $CLANG_TIDY_DIFF"
+  exit 1
+fi
+DIFF=$(git diff -U0 "@{upstream}")
+echo "Running $CLANG_TIDY_DIFF"
+TIDY_MSG=$(echo $DIFF | $CLANG_TIDY_DIFF -quiet -p1 2> /dev/null)
 if [ -n "$TIDY_MSG" -a "$TIDY_MSG" != "No relevant changes found." ]
 then
   echo "Fix clang-tidy errors before committing!"
   echo
   # Run clang-tidy once again to show the error
-  git diff -U0 $MERGE_BASE | $CLANG_TIDY_DIFF -quiet -p1 2> /dev/null
+  git diff -U0 "@{upstream}" | $CLANG_TIDY_DIFF -quiet -p1 2> /dev/null
   exit 1
 fi
