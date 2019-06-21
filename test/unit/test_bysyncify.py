30e31f76bb78a959a9aa7dfe17a0a9048e161ae7
Alon Zakai <azakai@google.com>
2019-06-21 11:15:31 -0700

    test

--- test/unit/test_bysyncify.py
+++ test/unit/test_bysyncify.py
@@ -1,11 +1,11 @@
 import os
 
-from scripts.test.shared import WASM_OPT, NODEJS, run_process
+from scripts.test.shared import WASM_OPT, WASM_DIS, WASM_SHELL, NODEJS, run_process
 from utils import BinaryenTestCase
 
 
 class BysyncifyTest(BinaryenTestCase):
-  def test_bysyncify(self):
+  def test_bysyncify_js(self):
     def test(args):
       print(args)
       run_process(WASM_OPT + args + [self.input_path('bysyncify-sleep.wast'), '--bysyncify', '-o', 'a.wasm'])
@@ -19,3 +19,10 @@ class BysyncifyTest(BinaryenTestCase):
     test(['--optimize-level=1'])
     test(['-O3'])
     test(['-Os', '-g'])
+
+  def test_bysyncify_pure_wasm(self):
+    run_process(WASM_OPT + [self.input_path('bysyncify-pure.wast'), '--bysyncify', '-o', 'a.wasm'])
+    run_process(WASM_DIS + ['a.wasm', '-o', 'a.wast'])
+    output = run_process(WASM_SHELL + ['a.wast'], capture_output=True).stdout
+    with open(self.input_path('bysyncify-pure.txt')) as f:
+      self.assertEqual(f.read(), output)
