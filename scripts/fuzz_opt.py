e170b4d35e702c377267fe9ef996cb5639f00c1c
Alon Zakai <azakai@google.com>
2019-06-24 16:51:51 -0700

    fix

--- scripts/fuzz_opt.py
+++ scripts/fuzz_opt.py
@@ -131,7 +131,7 @@ def run_vm(cmd):
   ]
   try:
     return run(cmd)
-  except Exception as e:
+  except Exception:
     output = run_unchecked(cmd)
     for issue in known_issues:
       if issue in output:
