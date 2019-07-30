f9d2b175680110093dbff76ba5f8ecbf30895f94
Alon Zakai <azakai@google.com>
2019-07-30 16:13:53 -0700

    fix

--- travis-emcc-tests.sh
+++ travis-emcc-tests.sh
@@ -1,8 +1,8 @@
-# FIXME set -e
+set -e
 echo "install"
 apt-get install python3
 echo "travis-test build"
 ./build-js.sh -g
 echo "travis-test test"
-./scripts/test/binaryenjs.py
+python3 ./scripts/test/binaryenjs.py
 echo "travis-test yay!"
