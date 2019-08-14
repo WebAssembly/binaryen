29946ab99e5193084cb46a6eabf4ec7782d4795c
Sam Clegg <sbc@chromium.org>
2019-08-14 15:12:06 -0700

    fix test

--- test/unit/test_parsing_error.py
+++ test/unit/test_parsing_error.py
@@ -6,13 +6,13 @@ import os
 class ParsingErrorTest(BinaryenTestCase):
     def test_parsing_error_msg(self):
         module = '''
-        (module
-         (func $foo
-            (abc)
-         )
-        )
-        '''
+(module
+  (func $foo
+    (abc)
+  )
+)
+'''
         p = run_process(WASM_OPT + ['--print', '-o', os.devnull], input=module,
                         check=False, capture_output=True)
         self.assertNotEqual(p.returncode, 0)
-        self.assertIn("parse exception: abc (at 4:6)", p.stderr)
+        self.assertIn("parse exception: abc (at 4:4)", p.stderr)
