6bbd41b13c6253883e0918fe985c58cc1723876d
Bradley Morris <Bradley.Morris@sas.com>
2019-10-17 15:45:58 -0400

    fix incorrect assert in BinaryenHostGetOperand

--- test/binaryen.js/host-expression.js
+++ test/binaryen.js/host-expression.js
@@ -1,4 +1,14 @@
 
+function cleanInfo(info) {
+  var ret = {};
+  for (var x in info) {
+    if (x !== 'body' && x !== 'operands') {
+      ret[x] = info[x];
+    }
+  }
+  return ret;
+}
+
 var module = Binaryen.parseText(
   `(module
     (type $0 (func (result i32)))
@@ -12,6 +22,6 @@ var module = Binaryen.parseText(
 );
 
 var funcInfo = Binaryen.getFunctionInfo(module.getFunction('0'));
-console.log("getFunctionInfo=" + JSON.stringify(funcInfo));
+console.log("getFunctionInfo=" + JSON.stringify(cleanInfo(funcInfo)));
 var expInfo = Binaryen.getExpressionInfo(funcInfo.body);
-console.log("getExpressionInfo(body)=" + JSON.stringify(expInfo));
+console.log("getExpressionInfo(body)=" + JSON.stringify(cleanInfo(expInfo)));
