0b1c4075d43ddff8a6a655126fe08eecd6a3ac74
Alon Zakai <azakai@google.com>
2019-10-18 12:55:25 -0700

    fix

--- scripts/test/shared.py
+++ scripts/test/shared.py
@@ -60,7 +60,7 @@ def parse_args(args):
     parser.add_argument(
         '--out-dir', dest='out_dir', default='',
         help=('Specifies a path to the output directory for temp files, which '
-              'is also where the test runner changes directory into.',
+              'is also where the test runner changes directory into.'
               ' Default:. out/test under the binaryen root.'))
     parser.add_argument(
         '--valgrind', dest='valgrind', default='',
