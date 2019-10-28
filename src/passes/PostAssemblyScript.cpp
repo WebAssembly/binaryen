ba238c5ac12baab6f53fabd05550257e4398a472
dcode <dcode@dcode.io>
2019-10-28 12:08:38 +0100

    cleanup

--- src/passes/PostAssemblyScript.cpp
+++ src/passes/PostAssemblyScript.cpp
@@ -41,10 +41,10 @@ static Name RELEASE = Name("~lib/rt/pure/__release");
 static Name ALLOC = Name("~lib/rt/tlsf/__alloc");
 static Name ALLOCARRAY = Name("~lib/rt/__allocArray");
 
-// A version of LocalGraph that considers only assignments
+// A variant of LocalGraph that considers only assignments
 // when computing influences.
-struct AssignmentGraph : wasm::LocalGraph {
-  AssignmentGraph(Function* func) : wasm::LocalGraph(func) {}
+struct AssignmentGraph : LocalGraph {
+  AssignmentGraph(Function* func) : LocalGraph(func) {}
   void computeInfluences() {
     for (auto& pair : locations) {
       auto* curr = pair.first;
@@ -63,11 +63,13 @@ struct AssignmentGraph : wasm::LocalGraph {
 };
 
 // A pass that eliminates redundant retain and release calls.
-// Does a cheap traversal first, remembering preconditions, and
-// goes all-in only if it finds ARC code.
+// Does a cheap traversal first, remembering ARC-style patterns,
+// and goes all-in only if it finds any.
 struct OptimizeARC : public WalkerPass<PostWalker<OptimizeARC>> {
 
+#ifndef POST_ASSEMBLYSCRIPT_DEBUG
   bool isFunctionParallel() override { return true; }
+#endif
 
   Pass* create() override { return new OptimizeARC; }
 
@@ -81,6 +83,7 @@ struct OptimizeARC : public WalkerPass<PostWalker<OptimizeARC>> {
   std::unordered_set<LocalGet*> returns;
 
   void visitLocalSet(LocalSet* curr) {
+    // local.set(X, __retain(...)) ?
     if (auto* call = curr->value->dynCast<Call>()) {
       if (call->target == RETAIN) {
         retains[curr] = getCurrentPointer();
@@ -89,23 +92,17 @@ struct OptimizeARC : public WalkerPass<PostWalker<OptimizeARC>> {
   }
 
   void visitCall(Call* curr) {
+    // __release(local.get(X, ...)) ?
     if (curr->target == RELEASE && curr->operands.size() == 1) {
       auto* operand = curr->operands[0];
       if (auto* localGet = operand->dynCast<LocalGet>()) {
-        // __release(local.get(...))
         releases[localGet] = getCurrentPointer();
-      } else if (auto* call = operand->dynCast<Call>()) {
-        if (call->target == RETAIN && call->operands.size() == 1) {
-          // trivial __release(__retain(...))
-          // shouldn't actually be emitted, but to be sure
-          Builder builder(*getModule());
-          replaceCurrent(builder.makeDrop(call->operands[0]));
-        }
       }
     }
   }
 
   void visitReturn(Return* curr) {
+    // return(local.get(X, ...)) ?
     auto* value = curr->value;
     if (value) {
       if (auto* localGet = value->dynCast<LocalGet>()) {
@@ -168,6 +165,14 @@ struct OptimizeARC : public WalkerPass<PostWalker<OptimizeARC>> {
     }
   }
 
+  // Given a retain, gets the retained expression
+  static Expression* getRetainedExpression(LocalSet* retain) {
+    // assumes local.set(X, __retain(...))
+    auto* retainCall = retain->value->cast<Call>();
+    assert(retainCall->operands.size() == 1);
+    return retainCall->operands[0];
+  }
+
   // Tests if a retained value originates at an allocation
   bool testRetainsAllocation(Expression* retained, AssignmentGraph& graph) {
     if (auto* call = retained->dynCast<Call>()) {
@@ -187,10 +192,17 @@ struct OptimizeARC : public WalkerPass<PostWalker<OptimizeARC>> {
     return false;
   }
 
-  // Tests if a released value has balanced retains
-  bool testBalancedRetains(LocalGet* released, AssignmentGraph& graph) {
-    bool atLeastOne = false;
-    for (auto* localSet : graph.getSetses[released]) {
+  // Given a release location, gets the release
+  static LocalGet* getReleaseByLocation(Expression** releaseLocation) {
+    // assumes __release(local.get(X, ...))
+    auto* releaseCall = (*releaseLocation)->cast<Call>();
+    assert(releaseCall->operands.size() == 1);
+    return releaseCall->operands[0]->cast<LocalGet>();
+  }
+
+  // Tests if a release has balanced retains
+  bool testBalancedRetains(LocalGet* release, AssignmentGraph& graph) {
+    for (auto* localSet : graph.getSetses[release]) {
       if (localSet == nullptr) {
         return false;
       }
@@ -203,22 +215,18 @@ struct OptimizeARC : public WalkerPass<PostWalker<OptimizeARC>> {
           return false;
         }
       }
-      atLeastOne = true;
     }
-    assert(atLeastOne);
     return true;
   }
 
   void doWalkFunction(Function* func) {
-    if (func->name == RETAIN || func->name == RELEASE) {
-      return;
-    }
     retains.clear();
     releases.clear();
     returns.clear();
     Flat::verifyFlatness(func);
 #ifdef POST_ASSEMBLYSCRIPT_DEBUG
-    std::cerr << "walking " << func->name << "\n";
+    std::cerr << "[PostAssemblyScript::OptimizeARC] walking " << func->name
+              << "\n";
 #endif
     super::doWalkFunction(func);
     if (retains.empty()) {
@@ -240,20 +248,18 @@ struct OptimizeARC : public WalkerPass<PostWalker<OptimizeARC>> {
     // * reaches at least one release
     // * reaches only releases with balanced retains
     //
-    for (auto pair : retains) {
-      auto* set = pair.first;
-      auto** setLocation = pair.second;
-      if (!testReachesReturn(set, graph)) {
-        if (!testRetainsAllocation(set->value->cast<Call>()->operands[0],
-                                   graph)) {
+    for (auto& pair : retains) {
+      auto* retain = pair.first;
+      auto** retainLocation = pair.second;
+      if (!testReachesReturn(retain, graph)) {
+        if (!testRetainsAllocation(getRetainedExpression(retain), graph)) {
           std::unordered_set<Expression**> found;
-          collectReleases(set, graph, found);
+          collectReleases(retain, graph, found);
           if (!found.empty()) {
             bool allBalanced = true;
             for (auto** getLocation : found) {
-              if (!testBalancedRetains(
-                    (*getLocation)->cast<Call>()->operands[0]->cast<LocalGet>(),
-                    graph)) {
+              if (!testBalancedRetains(getReleaseByLocation(getLocation),
+                                       graph)) {
                 allBalanced = false;
                 break;
               }
@@ -261,10 +267,10 @@ struct OptimizeARC : public WalkerPass<PostWalker<OptimizeARC>> {
             if (allBalanced) {
 #ifdef POST_ASSEMBLYSCRIPT_DEBUG
               std::cerr << "  eliminating ";
-              WasmPrinter::printExpression(set, std::cerr, true);
+              WasmPrinter::printExpression(retain, std::cerr, true);
               std::cerr << " reaching\n";
 #endif
-              redundantRetains.insert(setLocation);
+              redundantRetains.insert(retainLocation);
               for (auto** getLocation : found) {
 #ifdef POST_ASSEMBLYSCRIPT_DEBUG
                 std::cerr << "    ";
@@ -276,28 +282,28 @@ struct OptimizeARC : public WalkerPass<PostWalker<OptimizeARC>> {
 #ifdef POST_ASSEMBLYSCRIPT_DEBUG
             } else {
               std::cerr << "  cannot eliminate ";
-              WasmPrinter::printExpression(set, std::cerr, true);
+              WasmPrinter::printExpression(retain, std::cerr, true);
               std::cerr << " - unbalanced\n";
 #endif
             }
 #ifdef POST_ASSEMBLYSCRIPT_DEBUG
           } else {
             std::cerr << "  cannot eliminate ";
-            WasmPrinter::printExpression(set, std::cerr, true);
+            WasmPrinter::printExpression(retain, std::cerr, true);
             std::cerr << " - zero releases\n";
 #endif
           }
 #ifdef POST_ASSEMBLYSCRIPT_DEBUG
         } else {
           std::cerr << "  cannot eliminate ";
-          WasmPrinter::printExpression(set, std::cerr, true);
+          WasmPrinter::printExpression(retain, std::cerr, true);
           std::cerr << " - retains allocation\n";
 #endif
         }
 #ifdef POST_ASSEMBLYSCRIPT_DEBUG
       } else {
         std::cerr << "  cannot eliminate ";
-        WasmPrinter::printExpression(set, std::cerr, true);
+        WasmPrinter::printExpression(retain, std::cerr, true);
         std::cerr << " - reaches return\n";
 #endif
       }
@@ -321,9 +327,15 @@ struct OptimizeARC : public WalkerPass<PostWalker<OptimizeARC>> {
 // and this pass finalizes such output.
 struct FinalizeARC : public WalkerPass<PostWalker<FinalizeARC>> {
 
+#ifndef POST_ASSEMBLYSCRIPT_DEBUG
   bool isFunctionParallel() override { return true; }
+#endif
 
   Pass* create() override { return new FinalizeARC; }
+
+  uint32_t eliminatedAllocations = 0;
+  uint32_t eliminatedRetains = 0;
+  uint32_t eliminatedReleases = 0;
 
   void visitCall(Call* curr) {
     if (curr->target == RELEASE && curr->operands.size() == 1) {
@@ -334,26 +346,74 @@ struct FinalizeARC : public WalkerPass<PostWalker<FinalizeARC>> {
             if (retainedCall->target == ALLOC ||
                 retainedCall->target == ALLOCARRAY) {
               // __release(__retain(__alloc(...))) - unnecessary allocation
+#ifdef POST_ASSEMBLYSCRIPT_DEBUG
+              std::cerr << "  finalizing ";
+              WasmPrinter::printExpression(curr, std::cerr, true);
+              std::cerr << " - unnecessary allocation\n";
+#endif
               Builder builder(*getModule());
               replaceCurrent(builder.makeNop());
+              ++eliminatedAllocations;
+              ++eliminatedRetains;
+              ++eliminatedReleases;
               return;
             }
           }
           // __release(__retain(...)) - unnecessary pair
+#ifdef POST_ASSEMBLYSCRIPT_DEBUG
+          std::cerr << "  finalizing ";
+          WasmPrinter::printExpression(curr, std::cerr, true);
+          std::cerr << " - unnecessary pair\n";
+#endif
           Builder builder(*getModule());
           replaceCurrent(builder.makeDrop(releasedCall->operands[0]));
+          ++eliminatedRetains;
+          ++eliminatedReleases;
         }
       } else if (curr->operands[0]->is<Const>()) {
-        // __release(42) - unnecessary static RC
+        // __release(42) - unnecessary static release
+#ifdef POST_ASSEMBLYSCRIPT_DEBUG
+        std::cerr << "  finalizing ";
+        WasmPrinter::printExpression(curr, std::cerr, true);
+        std::cerr << " - static release\n";
+#endif
         Builder builder(*getModule());
         replaceCurrent(builder.makeNop());
+        ++eliminatedReleases;
       }
     } else if (curr->target == RETAIN && curr->operands.size() == 1) {
       if (auto* retainedConst = curr->operands[0]->dynCast<Const>()) {
-        // __retain(42) - unnecessary static RC
+        // __retain(42) - unnecessary static retain
+#ifdef POST_ASSEMBLYSCRIPT_DEBUG
+        std::cerr << "  finalizing ";
+        WasmPrinter::printExpression(curr, std::cerr, true);
+        std::cerr << " - static retain\n";
+#endif
         replaceCurrent(retainedConst);
+        ++eliminatedRetains;
       }
     }
+  }
+
+  void doWalkFunction(Function* func) {
+    eliminatedAllocations = 0;
+    eliminatedRetains = 0;
+    eliminatedReleases = 0;
+#ifdef POST_ASSEMBLYSCRIPT_DEBUG
+    std::cerr << "[PostAssemblyScript::FinalizeARC] walking " << func->name
+              << "\n";
+#endif
+    super::doWalkFunction(func);
+#ifdef POST_ASSEMBLYSCRIPT_DEBUG
+    if (eliminatedAllocations > 0 || eliminatedRetains > 0 ||
+        eliminatedReleases > 0) {
+      std::cerr << "  finalized " << eliminatedAllocations << " allocations, "
+                << eliminatedRetains << " retains and" << eliminatedReleases
+                << " releases\n";
+    } else {
+      std::cerr << "  nothing to do\n";
+    }
+#endif
   }
 };
 
