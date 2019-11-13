284e3946029d347b8bf1dc189988081a5636f89f
COFFEETALES <46571757+COFFEETALES@users.noreply.github.com>
2019-11-13 04:18:28 +0100

    uint32_t instead of int64_t as return type for GetMemorySegmentByteOffset (#2432)
    `uint32_t` instead of `int64_t` as return type for `GetMemorySegmentByteOffset` and minor fixes on tests.

--- src/binaryen-c.h
+++ src/binaryen-c.h
@@ -1190,7 +1190,7 @@ BINARYEN_API void BinaryenSetMemory(BinaryenModuleRef module,
 // Memory segments. Query utilities.
 
 BINARYEN_API uint32_t BinaryenGetNumMemorySegments(BinaryenModuleRef module);
-BINARYEN_API int64_t
+BINARYEN_API uint32_t
 BinaryenGetMemorySegmentByteOffset(BinaryenModuleRef module, BinaryenIndex id);
 BINARYEN_API size_t BinaryenGetMemorySegmentByteLength(BinaryenModuleRef module,
                                                        BinaryenIndex id);
