1c5bd799511debc1c35f47809bd30120e2e2a15b
Alon Zakai <azakai@google.com>
2019-11-14 16:39:32 -0800

    4 spaces

--- test/unit/test_warnings.py
+++ test/unit/test_warnings.py
@@ -8,13 +8,13 @@ from .utils import BinaryenTestCase
 
 class WarningsText(BinaryenTestCase):
     def test_warn_on_no_passes(self):
-      err = run_process(WASM_OPT + [self.input_path('asyncify-pure.wast'), '-o', 'a.wasm'], stderr=subprocess.PIPE).stderr
-      self.assertIn('warning: no passes specified, not doing any work', err)
+        err = run_process(WASM_OPT + [self.input_path('asyncify-pure.wast'), '-o', 'a.wasm'], stderr=subprocess.PIPE).stderr
+        self.assertIn('warning: no passes specified, not doing any work', err)
 
     def test_warn_on_no_output(self):
-      err = run_process(WASM_OPT + [self.input_path('asyncify-pure.wast'), '-O1'], stderr=subprocess.PIPE).stderr
-      self.assertIn('warning: no output file specified, not emitting output', err)
+        err = run_process(WASM_OPT + [self.input_path('asyncify-pure.wast'), '-O1'], stderr=subprocess.PIPE).stderr
+        self.assertIn('warning: no output file specified, not emitting output', err)
 
     def test_quiet_suppresses_warnings(self):
-      err = run_process(WASM_OPT + [self.input_path('asyncify-pure.wast')], stderr=subprocess.PIPE).stderr
-      self.assertNotIn('warning', err)
+        err = run_process(WASM_OPT + [self.input_path('asyncify-pure.wast')], stderr=subprocess.PIPE).stderr
+        self.assertNotIn('warning', err)
