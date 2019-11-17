da35cb12c3005a0e09f321c2d4a36d87aad6b708
Alon Zakai (kripken) <alonzakai@gmail.com>
2019-11-17 11:03:31 -0800

    fixes

--- travis-emcc-tests.sh
+++ travis-emcc-tests.sh
@@ -3,6 +3,7 @@ echo "travis-test build"
 cd js
 emmake make -j4 binaryen.debug.js
 cd ..
+cp out/binaryen.debug.js out/binaryen.js
 echo "travis-test test"
 python -m scripts.test.binaryenjs
 echo "travis-test yay!"
