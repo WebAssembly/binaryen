d4811cf392404e8be7bd0fa253ad2ef789c418a3
Heejin Ahn <aheejin@gmail.com>
2019-11-20 04:35:34 -0800

    Remove an unused import

--- scripts/test/wasm2js.py
+++ scripts/test/wasm2js.py
@@ -14,7 +14,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import glob
 import os
 
 from .support import run_command, split_wast, write_wast
