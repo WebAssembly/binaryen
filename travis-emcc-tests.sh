cbcb662c11a7742981bebce4efef8cac3dde80be
Alon Zakai <azakai@google.com>
2019-11-21 17:27:05 -0800

    fixes

--- travis-emcc-tests.sh
+++ travis-emcc-tests.sh
@@ -2,6 +2,7 @@ set -e
 echo "travis-test build"
 emconfigure cmake -DCMAKE_BUILD_TYPE=Release
 emmake make -j4 binaryen_js
+mkdir out
 cp bin/binaryen_js.js out/binaryen.js
 echo "travis-test test"
 python -m scripts.test.binaryenjs
