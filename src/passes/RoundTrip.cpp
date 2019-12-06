dfa540501ba123454f08d57a64008139f4c6ddfe
Alon Zakai <azakai@google.com>
2019-12-06 15:02:47 -0800

    fix

--- src/passes/RoundTrip.cpp
+++ src/passes/RoundTrip.cpp
@@ -37,6 +37,7 @@ struct RoundTrip : public Pass {
   void run(PassRunner* runner, Module* module) override {
     std::string templateName = "byn_round_trip_XXXXXX";
     std::vector<char> buffer(templateName.begin(), templateName.end());
+    buffer.push_back(0);
     (void)mkstemp(buffer.data());
     std::string tempName(buffer.begin(), buffer.end());
     // Write
