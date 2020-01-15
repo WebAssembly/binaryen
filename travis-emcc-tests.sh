5ca79a71b2a2379083093d4d9136b2ae4095dfe8
Daniel Wirtz <dcode@dcode.io>
2020-01-15 01:00:08 +0100

    Align binaryen.js with the npm package (#2551)
    Binaryen.js now uses binaryen (was Binaryen) as its global
    name to align with the npm package. Also fixes issues with
    emitting and testing both the JS and Wasm builds.

--- travis-emcc-tests.sh
+++ travis-emcc-tests.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 
 set -e
+
 echo "travis-test build:wasm"
 emconfigure cmake -DCMAKE_BUILD_TYPE=Release
 emmake make -j4 binaryen_wasm
 echo "travis-test test:wasm"
-python3 -m scripts.test.binaryenjs
+python3 -m scripts.test.binaryenjs wasm
 echo "travis-test done:wasm"
 
 echo "travis-test build:js"
 emmake make -j4 binaryen_js
 echo "travis-test test:js"
-python3 -m scripts.test.binaryenjs
+python3 -m scripts.test.binaryenjs js
 echo "travis-test done:js"
