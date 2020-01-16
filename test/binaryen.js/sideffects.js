ac9938f464a6dc67f7bc3460257c2e52642fdbf5
dcode <dcode@dcode.io>
2020-01-16 17:46:05 +0100

    update test

--- test/binaryen.js/sideffects.js
+++ test/binaryen.js/sideffects.js
@@ -1,103 +1,94 @@
-function assert(x) {
-  if (!x) throw 'error!';
-}
+console.log("SideEffects.None=" + binaryen.SideEffects.None);
+console.log("SideEffects.Branches=" + binaryen.SideEffects.Branches);
+console.log("SideEffects.Calls=" + binaryen.SideEffects.Calls);
+console.log("SideEffects.ReadsLocal=" + binaryen.SideEffects.ReadsLocal);
+console.log("SideEffects.WritesLocal=" + binaryen.SideEffects.WritesLocal);
+console.log("SideEffects.ReadsGlobal=" + binaryen.SideEffects.ReadsGlobal);
+console.log("SideEffects.WritesGlobal=" + binaryen.SideEffects.WritesGlobal);
+console.log("SideEffects.ReadsMemory=" + binaryen.SideEffects.ReadsMemory);
+console.log("SideEffects.WritesMemory=" + binaryen.SideEffects.WritesMemory);
+console.log("SideEffects.ImplicitTrap=" + binaryen.SideEffects.ImplicitTrap);
+console.log("SideEffects.IsAtomic=" + binaryen.SideEffects.IsAtomic);
+console.log("SideEffects.Any=" + binaryen.SideEffects.Any);
 
-function test() {
-
-  console.log("SideEffects.None=" + Binaryen.SideEffects.None);
-  console.log("SideEffects.Branches=" + Binaryen.SideEffects.Branches);
-  console.log("SideEffects.Calls=" + Binaryen.SideEffects.Calls);
-  console.log("SideEffects.ReadsLocal=" + Binaryen.SideEffects.ReadsLocal);
-  console.log("SideEffects.WritesLocal=" + Binaryen.SideEffects.WritesLocal);
-  console.log("SideEffects.ReadsGlobal=" + Binaryen.SideEffects.ReadsGlobal);
-  console.log("SideEffects.WritesGlobal=" + Binaryen.SideEffects.WritesGlobal);
-  console.log("SideEffects.ReadsMemory=" + Binaryen.SideEffects.ReadsMemory);
-  console.log("SideEffects.WritesMemory=" + Binaryen.SideEffects.WritesMemory);
-  console.log("SideEffects.ImplicitTrap=" + Binaryen.SideEffects.ImplicitTrap);
-  console.log("SideEffects.IsAtomic=" + Binaryen.SideEffects.IsAtomic);
-  console.log("SideEffects.Any=" + Binaryen.SideEffects.Any);
-
-  var module = new Binaryen.Module();
-  assert(
-    Binaryen.getSideEffects(
+var module = new binaryen.Module();
+assert(
+  binaryen.getSideEffects(
+    module.i32.const(1)
+  )
+  ==
+  binaryen.SideEffects.None
+);
+assert(
+  binaryen.getSideEffects(
+    module.br("test")
+  )
+  ==
+  binaryen.SideEffects.Branches
+);
+assert(
+  binaryen.getSideEffects(
+    module.call("test", [], binaryen.i32)
+  )
+  ==
+  binaryen.SideEffects.Calls
+);
+assert(
+  binaryen.getSideEffects(
+    module.local.get("test", binaryen.i32)
+  )
+  ==
+  binaryen.SideEffects.ReadsLocal
+);
+assert(
+  binaryen.getSideEffects(
+    module.local.set("test",
       module.i32.const(1)
     )
-    ==
-    Binaryen.SideEffects.None
-  );
-  assert(
-    Binaryen.getSideEffects(
-      module.br("test")
+  )
+  ==
+  binaryen.SideEffects.WritesLocal
+);
+assert(
+  binaryen.getSideEffects(
+    module.global.get("test", binaryen.i32)
+  )
+  ==
+  binaryen.SideEffects.ReadsGlobal
+);
+assert(
+  binaryen.getSideEffects(
+    module.global.set("test", module.i32.const(1))
+  )
+  ==
+  binaryen.SideEffects.WritesGlobal
+);
+assert(
+  binaryen.getSideEffects(
+    module.i32.load(0, 0,
+      module.i32.const(0)
     )
-    ==
-    Binaryen.SideEffects.Branches
-  );
-  assert(
-    Binaryen.getSideEffects(
-      module.call("test", [], Binaryen.i32)
+  )
+  ==
+  binaryen.SideEffects.ReadsMemory | binaryen.SideEffects.ImplicitTrap
+);
+assert(
+  binaryen.getSideEffects(
+    module.i32.store(0, 0,
+      module.i32.const(0),
+      module.i32.const(1)
     )
-    ==
-    Binaryen.SideEffects.Calls
-  );
-  assert(
-    Binaryen.getSideEffects(
-      module.local.get("test", Binaryen.i32)
+  )
+  ==
+  binaryen.SideEffects.WritesMemory | binaryen.SideEffects.ImplicitTrap
+);
+assert(
+  binaryen.getSideEffects(
+    module.i32.div_s(
+      module.i32.const(1),
+      module.i32.const(0)
     )
-    ==
-    Binaryen.SideEffects.ReadsLocal
-  );
-  assert(
-    Binaryen.getSideEffects(
-      module.local.set("test",
-        module.i32.const(1)
-      )
-    )
-    ==
-    Binaryen.SideEffects.WritesLocal
-  );
-  assert(
-    Binaryen.getSideEffects(
-      module.global.get("test", Binaryen.i32)
-    )
-    ==
-    Binaryen.SideEffects.ReadsGlobal
-  );
-  assert(
-    Binaryen.getSideEffects(
-      module.global.set("test", module.i32.const(1))
-    )
-    ==
-    Binaryen.SideEffects.WritesGlobal
-  );
-  assert(
-    Binaryen.getSideEffects(
-      module.i32.load(0, 0,
-        module.i32.const(0)
-      )
-    )
-    ==
-    Binaryen.SideEffects.ReadsMemory | Binaryen.SideEffects.ImplicitTrap
-  );
-  assert(
-    Binaryen.getSideEffects(
-      module.i32.store(0, 0,
-        module.i32.const(0),
-        module.i32.const(1)
-      )
-    )
-    ==
-    Binaryen.SideEffects.WritesMemory | Binaryen.SideEffects.ImplicitTrap
-  );
-  assert(
-    Binaryen.getSideEffects(
-      module.i32.div_s(
-        module.i32.const(1),
-        module.i32.const(0)
-      )
-    )
-    ==
-    Binaryen.SideEffects.ImplicitTrap
-  );
-}
-
-Binaryen.ready.then(test);
+  )
+  ==
+  binaryen.SideEffects.ImplicitTrap
+);
