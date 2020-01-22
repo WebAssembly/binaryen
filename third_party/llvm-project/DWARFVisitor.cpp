7c33ae700811130f5734eeb5873f26bb438203f6
Alon Zakai <azakai@google.com>
2020-01-21 16:16:29 -0800

    style

--- third_party/llvm-project/DWARFVisitor.cpp
+++ third_party/llvm-project/DWARFVisitor.cpp
@@ -57,7 +57,9 @@ template <typename T> void DWARFYAML::VisitorImpl<T>::traverseDebugInfo() {
         continue;
       // XXX BINARYEN
       if (Entry.AbbrCode - FirstAbbrevCode >= DebugInfo.AbbrevDecls.size()) {
-        errs() << "warning: invalid abbreviation code " << Entry.AbbrCode << " (range: " << FirstAbbrevCode << "-" << (DebugInfo.AbbrevDecls.size() - FirstAbbrevCode) << ")\n";
+        errs() << "warning: invalid abbreviation code " << Entry.AbbrCode <<
+               " (range: " << FirstAbbrevCode << "-" <<
+               (DebugInfo.AbbrevDecls.size() - FirstAbbrevCode) << ")\n";
         continue;
       }
       auto &Abbrev = DebugInfo.AbbrevDecls[Entry.AbbrCode - FirstAbbrevCode];
