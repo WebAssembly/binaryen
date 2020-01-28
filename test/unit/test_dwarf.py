a9772b45a865c2aff7b0e2ee81a75f4695e5aba4
Alon Zakai <azakai@google.com>
2020-01-28 09:40:50 -0800

    style

--- test/unit/test_dwarf.py
+++ test/unit/test_dwarf.py
@@ -11,7 +11,6 @@ class DWARFTest(utils.BinaryenTestCase):
         # just check that no assertion are hit.
         path = self.input_path('dwarf')
         for name in os.listdir(path):
-          args = [os.path.join(path, name)] + \
-                 ['-g', '--dwarfdump', '--roundtrip', '--dwarfdump']
-          shared.run_process(shared.WASM_OPT + args, capture_output=True)
-
+            args = [os.path.join(path, name)] + \
+                   ['-g', '--dwarfdump', '--roundtrip', '--dwarfdump']
+            shared.run_process(shared.WASM_OPT + args, capture_output=True)
