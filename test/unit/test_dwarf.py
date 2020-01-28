c3d23020965273e619ad65cd4d8ccfb3248ea118
Alon Zakai <azakai@google.com>
2020-01-27 17:01:28 -0800

    big tests

---
+++ test/unit/test_dwarf.py
@@ -0,0 +1,17 @@
+import os
+
+from scripts.test import shared
+from . import utils
+
+
+class DWARFTest(utils.BinaryenTestCase):
+    def test_no_crash(self):
+        # run dwarf processing on some interesting large files, too big to be
+        # worth putting in passes where the text output would be massive. We
+        # just check that no assertion are hit.
+        path = self.input_path('dwarf')
+        for name in os.listdir(path):
+          args = [os.path.join(path, name)] + \
+                 ['-g', '--dwarfdump', '--roundtrip', '--dwarfdump']
+          shared.run_process(shared.WASM_OPT + args, capture_output=True)
+
