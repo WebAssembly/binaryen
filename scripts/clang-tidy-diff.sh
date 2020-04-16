0f128e489529f92b770f56ff33083a27e02fa3db
Sam Clegg <sbc@chromium.org>
2020-04-16 15:09:42 -0700

    Report errors in scripts/clang-tidy-diff.sh if clang-tidy is not found

--- scripts/clang-tidy-diff.sh
+++ scripts/clang-tidy-diff.sh
@@ -15,11 +15,20 @@ else
   BRANCH=origin/master
 fi
 
-CLANG_DIR=$(dirname $(dirname $(which clang-tidy)))
+CLANG_TIDY=$(which clang-tidy)
+if [ ! -e "$CLANG_TIDY" ]; then
+  echo "Failed to find clang-tidy-diff.py ($CLANG_TIDY)"
+  exit 1
+fi
+
+CLANG_DIR=$(dirname $(dirname $CLANG_TIDY))
 CLANG_TIDY_DIFF=$CLANG_DIR/share/clang/clang-tidy-diff.py
+if [ ! -e "$CLANG_TIDY_DIFF" ]; then
+  echo "Failed to find clang-tidy-diff.py ($CLANG_TIDY_DIFF)"
+  exit 1
+fi
 TIDY_MSG=$(git diff -U0 $BRANCH... | $CLANG_TIDY_DIFF -quiet -p1 2> /dev/null)
-if [ -n "$TIDY_MSG" -a "$TIDY_MSG" != "No relevant changes found." ]
-then
+if [ -n "$TIDY_MSG" -a "$TIDY_MSG" != "No relevant changes found." ]; then
   echo "Please fix clang-tidy errors before committing"
   echo
   # Run clang-tidy once again to show the error
