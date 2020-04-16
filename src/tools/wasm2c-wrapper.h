34b82e0d75009f1866f916d659967ee2df8428f1
Alon Zakai <azakai@google.com>
2020-04-16 13:16:55 -0700

    what year is this [ci skip]

--- src/tools/wasm2c-wrapper.h
+++ src/tools/wasm2c-wrapper.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 WebAssembly Community Group participants
+ * Copyright 2020 WebAssembly Community Group participants
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
