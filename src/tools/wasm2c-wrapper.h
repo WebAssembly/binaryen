32ede3ab6a91d268655aef11964e1d0f7fde09e5
Alon Zakai <azakai@google.com>
2020-04-22 10:49:03 -0700

    comments

--- src/tools/wasm2c-wrapper.h
+++ src/tools/wasm2c-wrapper.h
@@ -26,15 +26,14 @@
 namespace wasm {
 
 static std::string generateWasm2CWrapper(Module& wasm) {
-  std::string ret;
-  ret += R"(
+  // First, emit implementations of the wasm's imports so that the wasm2c code
+  // can call them. The names use wasm2c's name mangling.
+  std::string ret = R"(
 #include <stdint.h>
 #include <stdio.h>
 
 #include "wasm-rt-impl.h"
 #include "wasm.h"
-
-// Logging imports
 
 void _Z_fuzzingZ2DsupportZ_logZ2Di32Z_vi(u32 x) {
   printf("[LoggingExternalInterface logging %d]\n", x);
@@ -82,19 +81,25 @@ int main(int argc, char** argv) {
 
 )";
 
+  // For each export in the wasm, emit code to call it and log its result,
+  // similar to the other wrappers.
   for (auto& exp : wasm.exports) {
     if (exp->kind != ExternalKind::Function) {
       continue;
     }
 
-    auto* func = wasm.getFunction(exp->value);
-
+    // Always call the hang limit initializer before each export.
     ret += "  (*Z_hangLimitInitializerZ_vv)();\n";
 
+    auto* func = wasm.getFunction(exp->value);
+
+    // Emit a setjmp so that we can handle traps from the compiled wasm.
     ret +=
       std::string("  puts(\"[fuzz-exec] calling ") + exp->name.str + "\");\n";
     ret += "  if (setjmp(g_jmp_buf) == 0) {\n";
     auto result = func->sig.results;
+
+    // Emit the call itself.
     ret += "    ";
     if (result != Type::none) {
       ret += std::string("printf(\"[fuzz-exec] note result: ") + exp->name.str +
@@ -116,8 +121,9 @@ int main(int argc, char** argv) {
           Fatal() << "unhandled wasm2c wrapper result type: " << result;
       }
     }
+
+    // Emit the callee's name with wasm2c name mangling.
     ret += std::string("(*Z_") + exp->name.str + "Z_";
-    // wasm2c emits names with a special signature
     auto params = func->sig.params.expand();
 
     auto wasm2cSignature = [](Type type) {
@@ -146,6 +152,8 @@ int main(int argc, char** argv) {
       }
     }
     ret += ")(";
+
+    // Emit the parameters (all 0s, like the other wrappers).
     bool first = true;
     for (auto param : params) {
       WASM_UNUSED(param);
@@ -159,6 +167,8 @@ int main(int argc, char** argv) {
       ret += ")";
     }
     ret += ");\n";
+
+    // Handle a longjmp which indicates a trap happened.
     ret += "  } else {\n";
     ret += "    puts(\"exception!\");\n";
     ret += "  }\n";
