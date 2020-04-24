3ccad72b5668d68a69a9390e2dcd03aefdacf07d
Sam Clegg <sbc@chromium.org>
2020-04-24 15:05:36 -0700

    Fix binaryenjs testing
    These tests are now optional. However, if you run them and the
    build is not found they will now error out, in order to avoid silently
    failing.

--- scripts/emcc-tests.sh
+++ scripts/emcc-tests.sh
@@ -6,11 +6,11 @@ echo "emcc-tests: build:wasm"
 emcmake cmake -DCMAKE_BUILD_TYPE=Release
 emmake make -j4 binaryen_wasm
 echo "emcc-tests: test:wasm"
-python3 -m scripts.test.binaryenjs wasm
+./check binaryenjs
 echo "emcc-tests: done:wasm"
 
 echo "emcc-tests: build:js"
 emmake make -j4 binaryen_js
 echo "emcc-tests: test:js"
-python3 -m scripts.test.binaryenjs js
+./check binaryenjs_wasm
 echo "emcc-tests: done:js"
