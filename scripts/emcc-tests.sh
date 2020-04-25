39ca5841c5cb2ef39b7e6ed2da3ececf538d5280
Sam Clegg <sbc@chromium.org>
2020-04-24 17:21:32 -0700

    oops

--- scripts/emcc-tests.sh
+++ scripts/emcc-tests.sh
@@ -6,11 +6,11 @@ echo "emcc-tests: build:wasm"
 emcmake cmake -DCMAKE_BUILD_TYPE=Release
 emmake make -j4 binaryen_wasm
 echo "emcc-tests: test:wasm"
-./check.py binaryenjs
+./check.py binaryenjs_wasm
 echo "emcc-tests: done:wasm"
 
 echo "emcc-tests: build:js"
 emmake make -j4 binaryen_js
 echo "emcc-tests: test:js"
-./check.py binaryenjs_wasm
+./check.py binaryenjs
 echo "emcc-tests: done:js"
