b1b7a745051f7397fdff847a154481edda94d5fa
Heejin Ahn <aheejin@gmail.com>
2020-05-10 01:06:53 -0700

    final version

--- scripts/fuzz_opt.py
+++ scripts/fuzz_opt.py
@@ -884,13 +884,16 @@ After reduction, the reduced file will be in %(working_wasm)s
                    'reduce_sh': os.path.abspath('reduce.sh')})
             break
         if given_seed is not None:
-            if given_seed_passed:
-                print('(finished running seed %d without error)' % given_seed)
-                sys.exit(0)
-            else:
-                print('(finished running seed %d, see error above)' % given_seed)
-                sys.exit(1)
+          break
 
         print('\nInvocations so far:')
         for testcase_handler in testcase_handlers:
             print('  ', testcase_handler.__class__.__name__ + ':', testcase_handler.count_runs())
+
+    if given_seed is not None:
+        if given_seed_passed:
+            print('(finished running seed %d without error)' % given_seed)
+            sys.exit(0)
+        else:
+            print('(finished running seed %d, see error above)' % given_seed)
+            sys.exit(1)
