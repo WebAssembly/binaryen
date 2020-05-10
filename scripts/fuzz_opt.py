4c5639b896b4242626b8dda48b72d3d890202b52
Heejin Ahn <aheejin@gmail.com>
2020-05-10 00:44:44 -0700

    Address comments in another way

--- scripts/fuzz_opt.py
+++ scripts/fuzz_opt.py
@@ -882,16 +882,15 @@ After reduction, the reduced file will be in %(working_wasm)s
                    'working_wasm': os.path.abspath('w.wasm'),
                    'wasm_reduce': in_bin('wasm-reduce'),
                    'reduce_sh': os.path.abspath('reduce.sh')})
-        if given_seed is not None:
             break
+        if given_seed is not None:
+            if given_seed_passed:
+                print('(finished running seed %d without error)' % given_seed)
+                sys.exit(0)
+            else:
+                print('(finished running seed %d, see error above)' % given_seed)
+                sys.exit(1)
 
         print('\nInvocations so far:')
         for testcase_handler in testcase_handlers:
             print('  ', testcase_handler.__class__.__name__ + ':', testcase_handler.count_runs())
-
-    if given_seed is not None:
-        if given_seed_passed:
-            print('(finished running seed %d without error)' % given_seed)
-        else:
-            print('(finished running seed %d, see error above)' % given_seed)
-            sys.exit(1)
