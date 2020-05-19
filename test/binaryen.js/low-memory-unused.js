3e419171f17f5f50c8e1a38568482f2261c0e3d7
Thomas Lively <tlively@google.com>
2020-05-18 17:20:08 -0700

    Fix low-memory-unused.js

--- test/binaryen.js/low-memory-unused.js
+++ test/binaryen.js/low-memory-unused.js
@@ -26,6 +26,7 @@ module.optimize();
 assert(module.validate());
 console.log(module.emitText());
 
+module = binaryen.parseText(wast);
 binaryen.setLowMemoryUnused(true);
 assert(binaryen.getLowMemoryUnused());
 console.log();
