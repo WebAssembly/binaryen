a712e00f0da07084e4027db07dacf28f9bd1d743
Alon Zakai <azakai@google.com>
2020-07-22 11:26:27 -0700

    flake

--- test/unit/test_wasm2c.py
+++ test/unit/test_wasm2c.py
@@ -1,5 +1,3 @@
-import os
-
 from scripts.test import shared
 from . import utils
 
@@ -12,12 +10,12 @@ class Wasm2CTest(utils.BinaryenTestCase):
         args = [empty_wasm, '--emit-wasm2c-wrapper=output.c']
         shared.run_process(shared.WASM_OPT + args)
         with open('output.c') as f:
-          normal_output = f.read()
+            normal_output = f.read()
         # running with ttf generates a new wasm for fuzzing, which always
         # includes the hang limit initializer function
         shared.run_process(shared.WASM_OPT + args + ['-ttf'])
         with open('output.c') as f:
-          ttf_output = f.read()
+            ttf_output = f.read()
         hang_limit_name = 'hangLimitInitializer'
         self.assertIn(hang_limit_name, ttf_output)
         self.assertNotIn(hang_limit_name, normal_output)
