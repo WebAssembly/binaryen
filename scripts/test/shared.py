e71a48d2672a399ca4a04432b571de89a1297d37
dcode <dcode@dcode.io>
2020-09-08 18:57:59 +0200

    Stay on C++14 for now

--- scripts/test/shared.py
+++ scripts/test/shared.py
@@ -25,7 +25,7 @@ import sys
 
 # The C++ standard whose features are required to build Binaryen.
 # Keep in sync with CMakeLists.txt CXX_STANDARD
-cxx_standard = 17
+cxx_standard = 14
 
 
 def parse_args(args):
