5b05457930fabad147311fec8ae504f77ae7136c
MaxGraey <maxgraey@gmail.com>
2020-09-11 15:35:33 +0300

    refactor

--- src/passes/OptimizeInstructions.cpp
+++ src/passes/OptimizeInstructions.cpp
@@ -683,15 +683,12 @@ struct OptimizeInstructions
       optimizeMemoryAccess(load->ptr, load->offset);
     } else if (auto* store = curr->dynCast<Store>()) {
       optimizeMemoryAccess(store->ptr, store->offset);
-      if (store->valueType.isInteger()) {
+      if (store->bytes != 8 && store->valueType.isInteger()) {
         // truncates constant values during stores
         // (i32|i64).store(8|16|32)(p, C)   ==>
         //    (i32|i64).store(8|16|32)(p, C & mask)
-        if (store->bytes != 8) {
-          if (auto* c = store->value->dynCast<Const>()) {
-            c->value =
-              c->value.and_(Literal(Bits::lowBitMask(store->bytes * 8)));
-          }
+        if (auto* c = store->value->dynCast<Const>()) {
+          c->value = c->value.and_(Literal(Bits::lowBitMask(store->bytes * 8)));
         }
       }
       // stores of fewer bits truncates anyhow
