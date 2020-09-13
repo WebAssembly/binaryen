652ec52fe24f794e1f457c2369d25620504d329f
MaxGraey <maxgraey@gmail.com>
2020-09-14 01:41:41 +0300

    typo in comments

--- src/ir/properties.h
+++ src/ir/properties.h
@@ -63,7 +63,7 @@ inline bool isSymmetric(Binary* binary) {
     case AddFloat64:
     case MulFloat64: {
       // We should be more careful with different forrm of NaNs so
-      // check if lhs is non-NaN constant. We don't interesting rhs
+      // check if LHS is non-NaN constant. We are not interested in RHS
       // in term of canonization which prefer constant on right side.
       if (auto* c = binary->left->dynCast<Const>()) {
         return !c->value.isNaN();
