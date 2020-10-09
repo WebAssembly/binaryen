8e8dc0e28f57b8469f616259c952dd57f8b9316c
Alon Zakai <azakai@google.com>
2020-10-09 16:02:22 -0700

    wasm-reduce: don't try to replace a non-number (like a reference) with a Const

--- src/tools/wasm-reduce.cpp
+++ src/tools/wasm-reduce.cpp
@@ -1024,6 +1024,9 @@ struct Reducer
     if (curr->is<Const>()) {
       return false;
     }
+    if (!curr->type.isNumber()) {
+      return false;
+    }
     // try to replace with a trivial value
     if (curr->type.isNullable()) {
       RefNull* n = builder->makeRefNull(curr->type);
