20850bf8c3c79840fe9c37e254a01eb104063ebb
MaxGraey <maxgraey@gmail.com>
2020-10-28 18:02:29 +0200

    fix pow2

--- src/passes/OptimizeInstructions.cpp
+++ src/passes/OptimizeInstructions.cpp
@@ -1108,7 +1108,7 @@ private:
   Expression* optimizeAddedConstants(Binary* binary) {
     assert(binary->type.isInteger());
 
-    uint64_t constant = 0;
+    int64_t constant = 0;
     std::vector<Const*> constants;
 
     struct SeekState {
@@ -1241,7 +1241,7 @@ private:
     ZeroRemover remover(getPassOptions());
     remover.setModule(getModule());
     remover.walk(walked);
-    if (constant == 0ULL) {
+    if (constant == 0LL) {
       return walked; // nothing more to do
     }
     if (auto* c = walked->dynCast<Const>()) {
