c961ccbcbfca05f743df82e4d522447dc1682edf
Alon Zakai <azakai@google.com>
2020-11-16 08:19:56 -0800

    format

--- src/wasm/wasm-s-parser.cpp
+++ src/wasm/wasm-s-parser.cpp
@@ -2032,8 +2032,7 @@ Expression* SExpressionWasmBuilder::makeCallRef(Element& s, bool isReturn) {
   ret->target = parseExpression(s[s.size() - 1]);
   ret->isReturn = isReturn;
   if (!ret->target->isRef()) {
-    throw ParseException(
-      "Non-reference type for a ref_call", s.line, s.col);
+    throw ParseException("Non-reference type for a ref_call", s.line, s.col);
   }
   auto heapType = ret->target->type.getHeapType();
   if (!heapType.isSignature()) {
