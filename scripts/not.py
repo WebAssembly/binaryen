1415f22ce9d5c5e01965196d87f3d46a43667039
Thomas Lively <tlively@google.com>
2020-11-16 19:05:43 -0800

    Fix flake8 errors

--- scripts/not.py
+++ scripts/not.py
@@ -17,12 +17,14 @@
 import sys
 import subprocess
 
+
 # Emulate the `not` tool from LLVM's test infrastructure for use with lit and
 # FileCheck. It succeeds if the given subcommand fails and vice versa.
 def main():
     cmd = sys.argv[1:]
     result = subprocess.run(cmd)
     sys.exit(0 if result.returncode != 0 else 1)
+
 
 if __name__ == '__main__':
     main()
