fbe4a5c27bacc2c5161efa1e5bbdeba7e8fd0adb
Thomas Lively <tlively@google.com>
2020-11-16 21:30:56 -0800

    Debug Windows failure

--- test/lit/lit.cfg.py
+++ test/lit/lit.cfg.py
@@ -11,6 +11,7 @@ config.test_exec_root = os.path.join(config.binaryen_root, 'test')
 # Replace all Binaryen tools with their absolute paths
 bin_dir = os.path.join(config.binaryen_root, 'bin')
 for tool in os.listdir(bin_dir):
+    print("substituting " + tool " for " + config.binaryen_root + '/bin/' + tool)
     config.substitutions.append((tool, config.binaryen_root + '/bin/' + tool))
 
 # Also make the `not` command available
