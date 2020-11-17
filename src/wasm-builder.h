8c252e8c10dc802f0a90787883583201fb2eba37
Alon Zakai <azakai@google.com>
2020-11-17 15:52:58 -0800

    fix

--- src/wasm-builder.h
+++ src/wasm-builder.h
@@ -942,7 +942,12 @@ public:
       return makeConstantExpression(Literal::makeZeros(curr->type));
     }
     if (curr->type.isFunction()) {
-      return ExpressionManipulator::refNull(curr, curr->type);
+      if (curr->type.isNullable()) {
+        return ExpressionManipulator::refNull(curr, curr->type);
+      } else {
+        // We can't do any better, keep the original.
+        return curr;
+      }
     }
     Literal value;
     // TODO: reuse node conditionally when possible for literals
