08671eb7ef07356ef8a558642dab1a49f5e2009f
Alon Zakai <azakai@google.com>
2020-11-25 13:13:53 -0800

    format

--- src/wasm/wasm-validator.cpp
+++ src/wasm/wasm-validator.cpp
@@ -1963,9 +1963,8 @@ void FunctionValidator::visitRefNull(RefNull* curr) {
   shouldBeTrue(getModule()->features.hasReferenceTypes(),
                curr,
                "ref.null requires reference-types to be enabled");
-  shouldBeTrue(curr->type.isNullable(),
-               curr,
-               "ref.null types must be nullable");
+  shouldBeTrue(
+    curr->type.isNullable(), curr, "ref.null types must be nullable");
 }
 
 void FunctionValidator::visitRefIsNull(RefIsNull* curr) {
