4491a87bc85bffec355b64679ed6e9587ef6d97a
Alon Zakai <azakai@google.com>
2020-12-11 15:47:26 -0800

    format

--- src/wasm-interpreter.h
+++ src/wasm-interpreter.h
@@ -1424,9 +1424,7 @@ public:
     NOTE_ENTER("BrOnCast");
     WASM_UNREACHABLE("TODO (gc): br_on_cast");
   }
-  Flow visitRttCanon(RttCanon* curr) {
-    return Literal(curr->type);
-  }
+  Flow visitRttCanon(RttCanon* curr) { return Literal(curr->type); }
   Flow visitRttSub(RttSub* curr) {
     Flow parent = this->visit(curr->parent);
     if (parent.breaking()) {
