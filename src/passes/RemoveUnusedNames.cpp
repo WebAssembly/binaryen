ba726db216f114527366efa07771dd987c1fe6bb
Alon Zakai <azakai@google.com>
2020-12-13 16:08:50 -0800

    better

--- src/passes/RemoveUnusedNames.cpp
+++ src/passes/RemoveUnusedNames.cpp
@@ -45,32 +45,9 @@ struct RemoveUnusedNames
       visitLoop(loop);
       return;
     }
-
-    // For all break instructions, note their information.
-
-#define DELEGATE_ID curr->_id
-
-#define DELEGATE_START(id)                                                     \
-  auto* cast = curr->cast<id>();                                               \
-  WASM_UNUSED(cast);
-
-#define DELEGATE_GET_FIELD(id, name) cast->name
-
-#define DELEGATE_FIELD_SCOPE_NAME_USE(id, name)                                \
-  branchesSeen[cast->name].insert(curr);
-
-#define DELEGATE_FIELD_CHILD(id, name)
-#define DELEGATE_FIELD_INT(id, name)
-#define DELEGATE_FIELD_LITERAL(id, name)
-#define DELEGATE_FIELD_NAME(id, name)
-#define DELEGATE_FIELD_SCOPE_NAME_DEF(id, name)
-#define DELEGATE_FIELD_SIGNATURE(id, name)
-#define DELEGATE_FIELD_TYPE(id, name)
-#define DELEGATE_FIELD_ADDRESS(id, name)
-#define DELEGATE_FIELD_CHILD_VECTOR(id, name)
-#define DELEGATE_FIELD_INT_ARRAY(id, name)
-
-#include "wasm-delegations-fields.h"
+    BranchUtils::operateOnScopeNameUses(curr, [&](Name& name) {
+      branchesSeen[name].insert(curr);
+    });
   }
 
   void handleBreakTarget(Name& name) {
