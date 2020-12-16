6d47af466eeeea88fbd5d3b8a481933390e316bd
Alon Zakai <azakai@google.com>
2020-12-16 14:40:25 -0800

    feedback2

--- src/wasm/wasm.cpp
+++ src/wasm/wasm.cpp
@@ -305,31 +305,13 @@ void Block::finalize() {
     type = Type::none;
     return;
   }
+  // The default type is what is at the end. Next we need to see if breaks and/
+  // or unreachabitily change that.
+  type = list.back()->type;
   if (!name.is()) {
-    // nothing branches here, so this is easy
-    // normally the type is the type of the final child
-    type = list.back()->type;
-    // and even if we have an unreachable child somewhere,
-    // we still mark ourselves as having that type,
-    // (block (result i32)
-    //  (return)
-    //  (i32.const 10)
-    // )
-    if (type.isConcrete()) {
-      return;
-    }
-    // if we are unreachable, we are done
-    if (type == Type::unreachable) {
-      return;
-    }
-    // we may still be unreachable if we have an unreachable
-    // child
-    for (auto* child : list) {
-      if (child->type == Type::unreachable) {
-        type = Type::unreachable;
-        return;
-      }
-    }
+    // Nothing branches here, so this is easy.
+    handleUnreachable(
+      this, /* breakabilityKnown = */ true, /* hasBreak = */ false);
     return;
   }
 
