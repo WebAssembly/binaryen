278eabd7e8116bbd30d5a2d43f798324d38cbb18
Thomas Lively <tlively@google.com>
2020-12-17 12:35:28 -0800

    [module-splitting] Fix a crash when a function is exported twice
    `ModuleSplitter::thunkExportedSecondaryFunctions` creates a thunk for each
    secondary function that needs to be exported from the main module. Previously,
    if a secondary function was exported twice, this code would try to create two
    thunks for it rather than just making one thunk and exporting it twice. This
    caused a fatal error because the second thunk had the same name as the first
    thunk and therefore could not be added to the module. This PR fixes the issue by
    creating no more than one thunk per function.

--- test/example/module-splitting.cpp
+++ test/example/module-splitting.cpp
@@ -387,4 +387,12 @@ int main() {
       (call $foo (i32.const 1))
      )
     ))");
+
+  // Multiple exports of a secondary function
+  do_test({}, R"(
+    (module
+     (export "foo1" (func $foo))
+     (export "foo2" (func $foo))
+     (func $foo)
+    ))");
 }
