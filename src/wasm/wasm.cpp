3c6f8dd84e10d6fb288727336f76a08a22df259d
Alon Zakai <azakai@google.com>
2020-12-16 18:55:01 -0800

    Restore dump()

--- src/wasm/wasm.cpp
+++ src/wasm/wasm.cpp
@@ -98,6 +98,13 @@ Name ATTR("attr");
 
 // Expressions
 
+void Expression::dump() {
+  WasmPrinter::printExpression(this,
+                               std::cerr,
+                               /*minify=*/false,
+                               /*full=*/true);
+}
+
 const char* getExpressionName(Expression* curr) {
   switch (curr->_id) {
     case Expression::Id::InvalidId:
