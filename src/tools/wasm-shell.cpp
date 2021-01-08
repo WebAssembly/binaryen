6c719258feef955e1440fc93e96bb9fbe0b70d32
Alon Zakai <azakai@google.com>
2021-01-08 09:54:02 -0800

    work

--- src/tools/wasm-shell.cpp
+++ src/tools/wasm-shell.cpp
@@ -329,14 +329,14 @@ int main(int argc, const char* argv[]) {
     exit(1);
   }
 
-{
-Literal x(Type(HeapType::i31, Nullable));
-std::cout << x << '\n';
-}
-{
-Literal x(Type(HeapType::i31, NonNullable));
-std::cout << x << '\n';
-}
+  {
+    Literal x(Type(HeapType::i31, Nullable));
+    std::cout << x << '\n';
+  }
+  {
+    Literal x(Type(HeapType::i31, NonNullable));
+    std::cout << x << '\n';
+  }
 
   if (checked) {
     Colors::green(std::cerr);
