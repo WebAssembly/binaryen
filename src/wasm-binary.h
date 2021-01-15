883e500dfe70cc85e577cfd4567211351836391f
Alon Zakai <azakai@google.com>
2021-01-15 15:30:39 -0800

    format

--- src/wasm-binary.h
+++ src/wasm-binary.h
@@ -357,7 +357,7 @@ enum EncodedType {
   // run-time type info type, with depth index n
   rtt_n = -0x17, // 0x69
   // run-time type info type, without depth index n
-  rtt = -0x18, // 0x68
+  rtt = -0x18,     // 0x68
   dataref = -0x19, // 0x67
   // exception reference type TODO remove; the code for now is incorrect
   exnref = -0x1A, // 0x66
