ca8b5e02ce86a9af10dce5ac4bd05e127f268bac
Alon Zakai <azakai@google.com>
2021-01-21 16:15:07 -0800

    format

--- src/wasm/wasm-debug.cpp
+++ src/wasm/wasm-debug.cpp
@@ -1096,9 +1096,7 @@ void writeDWARFSections(Module& wasm, const BinaryLocations& newLocations) {
   std::cerr << "warning: no DWARF updating support present\n";
 }
 
-bool shouldPreserveDWARF(PassOptions& options, Module& wasm) {
-  return false;
-}
+bool shouldPreserveDWARF(PassOptions& options, Module& wasm) { return false; }
 
 #endif // BUILD_LLVM_DWARF
 
