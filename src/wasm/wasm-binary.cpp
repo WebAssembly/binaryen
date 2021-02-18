629012c3f9a58ff2ecbac066788af57c16471969
Heejin Ahn <aheejin@gmail.com>
2021-02-19 05:26:04 +0900

    [EH] Change catch_all's opcode (#3574)
    We decided to change `catch_all`'s opcode from 0x05, which is the same
    as `else`, to 0x19, to avoid some complicated handling in the tools.
    
    See: https://github.com/WebAssembly/exception-handling/issues/147
    
    lso this contains the original cpp file used to generate
    dwarf_with_exceptions.wasm; instructions to generate the wasm from that
    cpp file are in the comments.

--- src/wasm/wasm-binary.cpp
+++ src/wasm/wasm-binary.cpp
@@ -2967,16 +2967,13 @@ BinaryConsts::ASTNodes WasmBinaryBuilder::readExpression(Expression*& curr) {
       }
       break;
     case BinaryConsts::Else:
-    case BinaryConsts::Catch: {
+    case BinaryConsts::Catch:
+    case BinaryConsts::CatchAll: {
       curr = nullptr;
       if (DWARF && currFunction) {
         assert(!controlFlowStack.empty());
         auto currControlFlow = controlFlowStack.back();
         BinaryLocation delimiterId;
-        // Else and CatchAll have the same binary ID, so differentiate them
-        // using the control flow stack.
-        static_assert(BinaryConsts::CatchAll == BinaryConsts::Else,
-                      "Else and CatchAll should have identical codes");
         if (currControlFlow->is<If>()) {
           delimiterId = BinaryLocations::Else;
         } else {
