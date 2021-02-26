b3d91b53e00bdbb63d2d042801ba36e6c7d06f01
Thomas Lively <tlively@google.com>
2021-02-25 19:34:11 -0800

    Feedback

--- scripts/clang-tidy-diff.sh
+++ scripts/clang-tidy-diff.sh
@@ -2,13 +2,6 @@
 
 set -o errexit
 
-# When we are running on travis and *not* part of a pull request we don't
-# have any upstream branch to compare against.
-if [ -z "$GITHUB_BASE_REF" ]; then
-  echo "Skipping since not running on GitHub Actions"
-  exit 0
-fi
-
 CLANG_TIDY=$(which clang-tidy)
 if [ ! -e "$CLANG_TIDY" ]; then
   echo "Failed to find clang-tidy ($CLANG_TIDY)"
@@ -21,11 +14,11 @@ if [ ! -e "$CLANG_TIDY_DIFF" ]; then
   echo "Failed to find clang-tidy-diff.py ($CLANG_TIDY_DIFF)"
   exit 1
 fi
-TIDY_MSG=$(git diff -U0 $GITHUB_BASE_REF $GITHUB_HEAD_REF | $CLANG_TIDY_DIFF -quiet -p1 2> /dev/null)
+TIDY_MSG=$(git diff -U0 @{upstream}...HEAD | $CLANG_TIDY_DIFF -quiet -p1 2> /dev/null)
 if [ -n "$TIDY_MSG" -a "$TIDY_MSG" != "No relevant changes found." ]; then
   echo "Please fix clang-tidy errors before committing"
   echo
   # Run clang-tidy once again to show the error
-  git diff -U0 $GITHUB_BASE_REF $GITHUB_HEAD_REF | $CLANG_TIDY_DIFF -quiet -p1 2> /dev/null
+  git diff -U0 @{upstream}...HEAD | $CLANG_TIDY_DIFF -quiet -p1 2> /dev/null
   exit 1
 fi
