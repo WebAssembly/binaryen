d54b00f6dc624ac535d9c06b3c6515ef61a67afc
Alon Zakai <azakai@google.com>
2021-03-01 16:41:35 -0800

    fix

--- src/binaryen-c.autogen.cpp
+++ src/binaryen-c.autogen.cpp
@@ -2,8 +2,12 @@
 
 BINARYEN_API BinaryenExpressionRef
 BinaryenStructNew(BinaryenModuleRef module, BinaryenExpressionRef rtt, BinaryenExpressionRef* operands, BinaryenIndex num_operands) {
+  std::vector<Expression*> operands_list;
+  for (BinaryenIndex i = 0; i < num_operands; i++) {
+    operands_list.push_back((Expression*)operands[i]);
+  }
   return static_cast<Expression*>(
-    Builder(*(Module*)module).makeStructNew(rtt, operands));
+    Builder(*(Module*)module).makeStructNew(rtt, operands_list));
 }
 
 BINARYEN_API BinaryenExpressionRef
--- src/tools/wasm-autogen.cpp
+++ src/tools/wasm-autogen.cpp
@@ -114,13 +114,13 @@ template<typename T> void autogenOneCAPIImpl() {
   MixedArena allocator;
   T curr(allocator);
 
+  std::string className;
   std::vector<std::string> params;
 
 #define DELEGATE_ID curr._id
 
 #define DELEGATE_START(id)                                                     \
-  std::cout << "  return static_cast<Expression*>(\n";                         \
-  std::cout << "    Builder(*(Module*)module).make" << #id << '(';
+  className = #id;
 
 #define DELEGATE_FIELD_CHILD(id, name) params.push_back(#name);
 
@@ -146,7 +146,12 @@ template<typename T> void autogenOneCAPIImpl() {
 
 #define DELEGATE_FIELD_ADDRESS(id, name) params.push_back(#name);
 
-#define DELEGATE_FIELD_CHILD_VECTOR(id, name) params.push_back(#name);
+#define DELEGATE_FIELD_CHILD_VECTOR(id, name) \
+  std::cout << "  std::vector<Expression*> " #name "_list;\n"; \
+  std::cout << "  for (BinaryenIndex i = 0; i < num_" #name "; i++) {\n"; \
+  std::cout << "    " #name "_list.push_back((Expression*)" #name "[i]);\n"; \
+  std::cout << "  }\n"; \
+  params.push_back(#name "_list");
 
 #include "wasm-delegations-fields.h"
 
@@ -156,6 +161,9 @@ template<typename T> void autogenOneCAPIImpl() {
   if (std::is_same<T, StructGet>()) {
     params.push_back("Type(type)");
   }
+
+  std::cout << "  return static_cast<Expression*>(\n";
+  std::cout << "    Builder(*(Module*)module).make" << className << '(';
 
   std::string sep = "";
   for (auto& param : params) {
