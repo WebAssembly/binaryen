14bea59a5fc2afa14cc06c40c64a80be4b6165cc
Alon Zakai <azakai@google.com>
2021-03-08 14:44:29 -0800

    fix

--- scripts/gen-c-api.sh
+++ scripts/gen-c-api.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-bin/wasm-autogen c-api-decl > src/binaryen-c.autogen.h
-bin/wasm-autogen c-api-impl > src/binaryen-c.autogen.cpp
+bin/wasm-api-gen c-api-decl > src/binaryen-c.autogen.h
+bin/wasm-api-gen c-api-impl > src/binaryen-c.autogen.cpp
