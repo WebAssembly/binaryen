WebAssembly/binaryen

commit f2abcbc8d1659d3e6506b1d4128646b892ebc218

[Wasm GC] Add support for non-nullable types, all except for locals (#3710)
After this PR we still do not support non-nullable locals. But we no longer
turn all types into nullable upon load. In particular, we support non-nullable
types on function parameters and struct fields, etc. This should be enough to
experiment with optimizations in both binaryen and in VMs regarding non-
nullability (since we expect that optimizing VMs can do well inside functions
anyhow; it's non-nullability across calls and from data that the VM can't be
expected to think about).

Let is handled as before, by lowering it into gets and sets. In addition, we
turn non-nullable locals into nullable ones, and add a ref.as_non_null on
all their gets (to keep the type identical there). This is used not just for
loading code with a let but also is needed after inlining.

Most of the code changes here are removing FIXMEs for allowing
non-nullable types. But there is also code to handle the issues mentioned
above.

Most of the test updates are removing extra nulls that we added before
when we turned all types nullable. A few tests had actual issues, though,
and also some new tests are added to cover the code changes here.

src/wasm/wasm-binary.cpp
@@ -19,6 +19,7 @@
 
 #include "ir/module-utils.h"
 #include "ir/table-utils.h"
+#include "ir/type-updating.h"
 #include "support/bits.h"
 #include "support/debug.h"
 #include "wasm-binary.h"
@@ -1595,12 +1596,10 @@ bool WasmBinaryBuilder::getBasicType(int32_t code, Type& out) {
       out = Type::eqref;
       return true;
     case BinaryConsts::EncodedType::i31ref:
-      // FIXME: for now, force all inputs to be nullable
-      out = Type(HeapType::i31, Nullable);
+      out = Type(HeapType::i31, NonNullable);
       return true;
     case BinaryConsts::EncodedType::dataref:
-      // FIXME: for now, force all inputs to be nullable
-      out = Type(HeapType::data, Nullable);
+      out = Type(HeapType::data, NonNullable);
       return true;
     default:
       return false;
@@ -1647,9 +1646,9 @@ Type WasmBinaryBuilder::getType(int initial) {
     case BinaryConsts::EncodedType::Empty:
       return Type::none;
     case BinaryConsts::EncodedType::nullable:
-    case BinaryConsts::EncodedType::nonnullable:
-      // FIXME: for now, force all inputs to be nullable
       return Type(getHeapType(), Nullable);
+    case BinaryConsts::EncodedType::nonnullable:
+      return Type(getHeapType(), NonNullable);
     case BinaryConsts::EncodedType::rtt_n: {
       auto depth = getU32LEB();
       auto heapType = getIndexedHeapType();
@@ -1790,16 +1789,18 @@ void WasmBinaryBuilder::readTypes() {
     switch (typeCode) {
       case BinaryConsts::EncodedType::nullable:
       case BinaryConsts::EncodedType::nonnullable: {
-        // FIXME: for now, force all inputs to be nullable
+        auto nullability = typeCode == BinaryConsts::EncodedType::nullable
+                             ? Nullable
+                             : NonNullable;
         int64_t htCode = getS64LEB(); // TODO: Actually s33
         HeapType ht;
         if (getBasicHeapType(htCode, ht)) {
-          return Type(ht, Nullable);
+          return Type(ht, nullability);
         }
         if (size_t(htCode) >= numTypes) {
           throwError("invalid type index: " + std::to_string(htCode));
         }
-        return builder.getTempRefType(size_t(htCode), Nullable);
+        return builder.getTempRefType(size_t(htCode), nullability);
       }
       case BinaryConsts::EncodedType::rtt_n:
       case BinaryConsts::EncodedType::rtt: {
@@ -2172,6 +2173,9 @@ void WasmBinaryBuilder::readFunctions() {
         throwError("binary offset at function exit not at expected location");
       }
     }
+
+    TypeUpdating::handleNonNullableLocals(func, wasm);
+
     std::swap(func->epilogLocation, debugLocation);
     currFunction = nullptr;
     debugLocation.clear();
@@ -6072,8 +6076,8 @@ void WasmBinaryBuilder::visitRefFunc(RefFunc* curr) {
   functionRefs[index].push_back(curr); // we don't know function names yet
   // To support typed function refs, we give the reference not just a general
   // funcref, but a specific subtype with the actual signature.
-  // FIXME: for now, emit a nullable type here
-  curr->finalize(Type(HeapType(getSignatureByFunctionIndex(index)), Nullable));
+  curr->finalize(
+    Type(HeapType(getSignatureByFunctionIndex(index)), NonNullable));
 }
 
 void WasmBinaryBuilder::visitRefEq(RefEq* curr) {

src/ir/CMakeLists.txt
@@ -6,6 +6,7 @@ set(ir_SOURCES
   ReFinalize.cpp
   stack-utils.cpp
   table-utils.cpp
+  type-updating.cpp
   module-splitting.cpp
   ${ir_HEADERS}
 )

src/ir/literal-utils.h
@@ -31,6 +31,12 @@ inline Expression* makeFromInt32(int32_t x, Type type, Module& wasm) {
   return ret;
 }
 
+inline bool canMakeZero(Type type) {
+  // We can make a "zero" - a 0, or a null, or a trivial rtt, etc. - for pretty
+  // much anything except a non-nullable reference type.
+  return !type.isRef() || type.isNullable();
+}
+
 inline Expression* makeZero(Type type, Module& wasm) {
   // TODO: Remove this function once V8 supports v128.const
   // (https://bugs.chromium.org/p/v8/issues/detail?id=8460)

src/ir/type-updating.cpp
@@ -1,0 +1,60 @@
+/*
+ * Copyright 2021 WebAssembly Community Group participants
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "type-updating.h"
+#include "find_all.h"
+
+namespace wasm {
+
+namespace TypeUpdating {
+
+void handleNonNullableLocals(Function* func, Module& wasm) {
+  // Check if this is an issue.
+  bool hasNonNullable = false;
+  for (auto type : func->vars) {
+    if (type.isRef() && !type.isNullable()) {
+      hasNonNullable = true;
+      break;
+    }
+  }
+  if (!hasNonNullable) {
+    return;
+  }
+  // Rewrite the local.gets.
+  Builder builder(wasm);
+  for (auto** getp : FindAllPointers<LocalGet>(func->body).list) {
+    auto* get = (*getp)->cast<LocalGet>();
+    auto type = func->getLocalType(get->index);
+    if (type.isRef() && !type.isNullable()) {
+      // The get should now return a nullable value, and a ref.as_non_null
+      // fixes that up.
+      get->type = Type(type.getHeapType(), Nullable);
+      *getp = builder.makeRefAs(RefAsNonNull, get);
+    }
+  }
+
+  // Rewrite the types of the function's vars (which we can do now, after we
+  // are done using them to know which local.gets to fix).
+  for (auto& type : func->vars) {
+    if (type.isRef() && !type.isNullable()) {
+      type = Type(type.getHeapType(), Nullable);
+    }
+  }
+}
+
+} // namespace TypeUpdating
+
+} // namespace wasm

src/ir/type-updating.h
@@ -305,6 +305,15 @@ struct TypeUpdater
   }
 };
 
+namespace TypeUpdating {
+
+// Finds non-nullable locals, which are currently not supported, and handles
+// them. Atm this turns them into nullable ones, and adds ref.as_non_null on
+// their uses (which keeps the type of the users identical).
+void handleNonNullableLocals(Function* func, Module& wasm);
+
+} // namespace TypeUpdating
+
 } // namespace wasm
 
 #endif // wasm_ir_type_updating_h

src/passes/Inlining.cpp
@@ -33,6 +33,7 @@
 #include "ir/debug.h"
 #include "ir/literal-utils.h"
 #include "ir/module-utils.h"
+#include "ir/type-updating.h"
 #include "ir/utils.h"
 #include "parsing.h"
 #include "pass.h"
@@ -296,6 +297,7 @@ doInlining(Module* module, Function* into, const InliningAction& action) {
     // Make the block reachable by adding a break to it
     block->list.push_back(builder.makeBreak(block->name));
   }
+  TypeUpdating::handleNonNullableLocals(into, *module);
   return block;
 }
 

src/passes/Vacuum.cpp
@@ -145,13 +145,19 @@ struct Vacuum : public WalkerPass<ExpressionStackWalker<Vacuum>> {
         ExpressionAnalyzer::isResultUsed(expressionStack, getFunction());
       auto* optimized = optimize(child, used, true);
       if (!optimized) {
-        if (child->type.isConcrete()) {
-          // We can't just skip a final concrete element, even if it isn't used.
-          // Instead, replace it with something that's easy to optimize out (for
-          // example, code-folding can merge out identical zeros at the end of
-          // if arms).
-          optimized = LiteralUtils::makeZero(child->type, *getModule());
-        } else if (child->type == Type::unreachable) {
+        auto childType = child->type;
+        if (childType.isConcrete()) {
+          if (LiteralUtils::canMakeZero(childType)) {
+            // We can't just skip a final concrete element, even if it isn't
+            // used. Instead, replace it with something that's easy to optimize
+            // out (for example, code-folding can merge out identical zeros at
+            // the end of if arms).
+            optimized = LiteralUtils::makeZero(childType, *getModule());
+          } else {
+            // Don't optimize it out.
+            optimized = child;
+          }
+        } else if (childType == Type::unreachable) {
           // Don't try to optimize out an unreachable child (dce can do that
           // properly).
           optimized = child;

src/tools/fuzzing.h
@@ -1490,7 +1490,7 @@ private:
     for (const auto& type : target->sig.params) {
       args.push_back(make(type));
     }
-    auto targetType = Type(HeapType(target->sig), Nullable);
+    auto targetType = Type(HeapType(target->sig), NonNullable);
     // TODO: half the time make a completely random item with that type.
     return builder.makeCallRef(
       builder.makeRefFunc(target->name, targetType), args, type, isReturn);
@@ -2096,9 +2096,7 @@ private:
       }
       // TODO: randomize the order
       for (auto& func : wasm.functions) {
-        // FIXME: RefFunc type should be non-nullable, but we emit nullable
-        //        types for now.
-        if (type == Type(HeapType(func->sig), Nullable)) {
+        if (type == Type(HeapType(func->sig), NonNullable)) {
           return builder.makeRefFunc(func->name, type);
         }
       }

commit a7b7cab1cbbdbf5ba93c6808e3e6c16e296a3535

Fix binary reading of tuples containing non-nullable types (#3734)
We must write them to a tuple with nullable types, then fix that up when
reading. This is similar to what we do in handleNonNullableLocals, except
that it operates on the entire tuple type, so it can't share that code.

This fixes a regression from #3710 that was harder to notice by the fuzzer
until now.

src/wasm/wasm-binary.cpp
@@ -2516,14 +2516,30 @@ void WasmBinaryBuilder::skipUnreachableCode() {
 }
 
 void WasmBinaryBuilder::pushExpression(Expression* curr) {
-  if (curr->type.isTuple()) {
+  auto type = curr->type;
+  if (type.isTuple()) {
     // Store tuple to local and push individual extracted values
     Builder builder(wasm);
-    Index tuple = builder.addVar(currFunction, curr->type);
+    // Non-nullable types require special handling as they cannot be stored to
+    // a local.
+    std::vector<Type> nullableTypes;
+    for (auto t : type) {
+      if (t.isRef() && !t.isNullable()) {
+        t = Type(t.getHeapType(), Nullable);
+      }
+      nullableTypes.push_back(t);
+    }
+    auto nullableType = Type(Tuple(nullableTypes));
+    Index tuple = builder.addVar(currFunction, nullableType);
     expressionStack.push_back(builder.makeLocalSet(tuple, curr));
-    for (Index i = 0; i < curr->type.size(); ++i) {
-      expressionStack.push_back(
-        builder.makeTupleExtract(builder.makeLocalGet(tuple, curr->type), i));
+    for (Index i = 0; i < nullableType.size(); ++i) {
+      Expression* value =
+        builder.makeTupleExtract(builder.makeLocalGet(tuple, nullableType), i);
+      if (nullableType[i] != type[i]) {
+        // We modified this to be nullable; undo that.
+        value = builder.makeRefAs(RefAsNonNull, value);
+      }
+      expressionStack.push_back(value);
     }
   } else {
     expressionStack.push_back(curr);

test/lit/passes/roundtrip.wast
@@ -1,0 +1,46 @@
+;; NOTE: Assertions have been generated by update_lit_checks.py and should not be edited.
+;; RUN: wasm-opt %s -all --roundtrip -S -o - | filecheck %s
+
+(module
+ (type $none (func))
+ ;; CHECK:      (func $foo
+ ;; CHECK-NEXT:  (local $0 (funcref (ref null $none)))
+ ;; CHECK-NEXT:  (local $1 funcref)
+ ;; CHECK-NEXT:  (local.set $0
+ ;; CHECK-NEXT:   (block $label$1 (result funcref (ref $none))
+ ;; CHECK-NEXT:    (tuple.make
+ ;; CHECK-NEXT:     (ref.null func)
+ ;; CHECK-NEXT:     (ref.func $foo)
+ ;; CHECK-NEXT:    )
+ ;; CHECK-NEXT:   )
+ ;; CHECK-NEXT:  )
+ ;; CHECK-NEXT:  (drop
+ ;; CHECK-NEXT:   (block (result funcref)
+ ;; CHECK-NEXT:    (local.set $1
+ ;; CHECK-NEXT:     (tuple.extract 0
+ ;; CHECK-NEXT:      (local.get $0)
+ ;; CHECK-NEXT:     )
+ ;; CHECK-NEXT:    )
+ ;; CHECK-NEXT:    (drop
+ ;; CHECK-NEXT:     (ref.as_non_null
+ ;; CHECK-NEXT:      (tuple.extract 1
+ ;; CHECK-NEXT:       (local.get $0)
+ ;; CHECK-NEXT:      )
+ ;; CHECK-NEXT:     )
+ ;; CHECK-NEXT:    )
+ ;; CHECK-NEXT:    (local.get $1)
+ ;; CHECK-NEXT:   )
+ ;; CHECK-NEXT:  )
+ ;; CHECK-NEXT: )
+ (func $foo
+  (drop
+   ;; a tuple type with a non-nullable element, that must be carefully handled
+   (block (result funcref (ref $none))
+    (tuple.make
+     (ref.null func)
+     (ref.func $foo)
+    )
+   )
+  )
+ )
+)