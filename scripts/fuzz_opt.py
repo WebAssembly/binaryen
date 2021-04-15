d945bf451cacd2d6c164c771fc04bc418bb5290e
Alon Zakai <azakai@google.com>
2021-04-14 19:56:50 -0700

    Update scripts/fuzz_opt.py
    Co-authored-by: Heejin Ahn <aheejin@gmail.com>

--- scripts/fuzz_opt.py
+++ scripts/fuzz_opt.py
@@ -214,7 +214,7 @@ def pick_initial_contents():
         '--disable-memory64',
         # avoid multivalue for now due to bad interactions with gc rtts in
         # stacky code. for example, this fails to roundtrip as the tuple code
-        # ends up creating stack binary code that needs to spill rtts to locals,
+        # ends up creating stacky binary code that needs to spill rtts to locals,
         # which is not allowed:
         #
         # (module
