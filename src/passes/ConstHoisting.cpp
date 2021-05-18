0101a129f8e059452d479f7b0606b2be8f54de38
Thomas Lively <tlively@google.com>
2021-05-18 11:17:43 -0700

    Determinism in const hoisting

--- src/passes/ConstHoisting.cpp
+++ src/passes/ConstHoisting.cpp
@@ -31,6 +31,7 @@
 //
 
 #include "pass.h"
+#include "support/insert_ordered.h"
 #include "wasm-binary.h"
 #include "wasm-builder.h"
 #include "wasm.h"
@@ -45,7 +46,7 @@ struct ConstHoisting : public WalkerPass<PostWalker<ConstHoisting>> {
 
   Pass* create() override { return new ConstHoisting; }
 
-  std::unordered_map<Literal, std::vector<Expression**>> uses;
+  InsertOrderedMap<Literal, std::vector<Expression**>> uses;
 
   void visitConst(Const* curr) {
     uses[curr->value].push_back(getCurrentPointer());
