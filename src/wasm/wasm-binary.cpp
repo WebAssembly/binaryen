e1b6ad24efdb50367a2f0a08fe9757b70d679c96
Thomas Lively <tlively@google.com>
2021-06-09 19:38:03 -0700

    Update comment

--- src/wasm/wasm-binary.cpp
+++ src/wasm/wasm-binary.cpp
@@ -6053,7 +6053,11 @@ void WasmBinaryBuilder::visitRefIs(RefIs* curr, uint8_t code) {
 void WasmBinaryBuilder::visitRefFunc(RefFunc* curr) {
   BYN_TRACE("zz node: RefFunc\n");
   Index index = getU32LEB();
-  // We don't know function names yet.
+  // We don't know function names yet, so record this use to be updated later.
+  // Note that we do not need to check that 'index' is in bounds, as that will
+  // be verified in the next line. (Also, note that functionRefs[index] may
+  // write to an odd place in the functionRefs map if index is invalid, but that
+  // is harmless.)
   functionRefs[index].push_back(curr);
   // To support typed function refs, we give the reference not just a general
   // funcref, but a specific subtype with the actual signature.
