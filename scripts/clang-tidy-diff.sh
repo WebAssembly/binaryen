cc58e2480a63214805fb48e4680c77335d2f7509
MaxGraey <maxgraey@gmail.com>
2021-07-17 08:38:20 +0300

    fix

--- scripts/clang-tidy-diff.sh
+++ scripts/clang-tidy-diff.sh
@@ -17,7 +17,11 @@ if [ ! -e "$CLANG_TIDY" ]; then
   exit 1
 fi
 
-CLANG_DIR=$(dirname $(dirname $(readlink -f $CLANG_TIDY)))
+function realpath() {
+  python -c "import os,sys; print(os.path.realpath(sys.argv[1]))" $CLANG_TIDY;
+}
+
+CLANG_DIR=$(dirname $(dirname $(realpath $CLANG_TIDY)))
 CLANG_TIDY_DIFF=$CLANG_DIR/share/clang/clang-tidy-diff.py
 if [ ! -e "$CLANG_TIDY_DIFF" ]; then
   echo "Failed to find clang-tidy-diff.py ($CLANG_TIDY_DIFF)"
