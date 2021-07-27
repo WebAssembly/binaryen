14091397a8034235852cdfe5a0a6158797ff759d
Thomas Lively <tlively@google.com>
2021-07-27 11:23:34 -0700

    Parse and print pops of compound types
    This is necessary when using GC and EH together, for instance.

--- src/wasm/wasm-s-parser.cpp
+++ src/wasm/wasm-s-parser.cpp
@@ -2159,7 +2159,11 @@ Expression* SExpressionWasmBuilder::makePop(Element& s) {
   auto ret = allocator.alloc<Pop>();
   std::vector<Type> types;
   for (size_t i = 1; i < s.size(); ++i) {
-    types.push_back(stringToType(s[i]->str()));
+    if (s[i]->isStr()) {
+      types.push_back(stringToType(s[i]->str()));
+    } else {
+      types.push_back(elementToType(*s[i]));
+    }
   }
   ret->type = Type(types);
   ret->finalize();
