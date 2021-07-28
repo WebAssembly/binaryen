14c868db82216bd5aad90048d9292cd3e51d2a1d
Max Graey <maxgraey@gmail.com>
2021-07-28 08:40:46 +0300

    Update src/wasm/literal.cpp
    Co-authored-by: Thomas Lively <7121787+tlively@users.noreply.github.com>

--- src/wasm/literal.cpp
+++ src/wasm/literal.cpp
@@ -2393,7 +2393,7 @@ template<LaneOrder Side> Literal extendF32(const Literal& vec) {
   LaneArray<2> result;
   for (size_t i = 0; i < 2; ++i) {
     size_t idx = (Side == LaneOrder::Low) ? i : i + 2;
-    result[i] = Literal((double)(float)lanes[idx].getf32());
+    result[i] = Literal((double)lanes[idx].getf32());
   }
   return Literal(result);
 }
