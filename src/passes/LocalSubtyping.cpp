7705f4110e5b0bfa60c3eaa7bd115095e4712c6f
Alon Zakai <azakai@google.com>
2021-08-03 08:25:40 -0700

    [Wasm GC] Fix LocalSubtyping on unreachable sets with incompatible values

--- src/passes/LocalSubtyping.cpp
+++ src/passes/LocalSubtyping.cpp
@@ -28,6 +28,7 @@
 #include <ir/utils.h>
 #include <pass.h>
 #include <wasm.h>
+#include <wasm-builder.h>
 
 namespace wasm {
 
@@ -178,9 +179,37 @@ struct LocalSubtyping : public WalkerPass<PostWalker<LocalSubtyping>> {
         get->type = func->getLocalType(get->index);
       }
       for (auto* set : FindAll<LocalSet>(func->body).list) {
+        auto newType = func->getLocalType(set->index);
         if (set->isTee()) {
-          set->type = func->getLocalType(set->index);
+          set->type = newType;
           set->finalize();
+        }
+
+        // If this set was not processed earlier - that is, if it is in
+        // unreachable code - then it may have an incompatible type. That is,
+        // If we saw a reachable tee that writes type A, and this tee writes
+        // type B, we may have specialized the local type to A, but the value
+        // of type B in this unreachable tee is no longer valid to write to
+        // that local. In such a case we must do additional work.
+        if (!Type::isSubType(set->value->type, newType)) {
+          // The type is incompatible. To fix this, replace
+          //
+          //  (tee (bad-value))
+          //
+          // with
+          //
+          //  (tee (block
+          //   (drop (bad-value))
+          //   (get)
+          //  ))
+          //
+          // A get of the same local will definitely have the proper type for
+          // IR to validate.
+          Builder builder(*getModule());
+          set->value = builder.makeSequence(
+            builder.makeDrop(set->value),
+            builder.makeLocalGet(set->index, newType)
+          );
         }
       }
 
