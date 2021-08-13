d38c949bb3b9c4b6e0d1e2a19cc22ec933b4746e
Max Graey <maxgraey@gmail.com>
2021-08-14 00:56:18 +0300

    [JS/C API] Expose zeroFilledMemory option for JS and C API (#4071)

--- src/binaryen-c.h
+++ src/binaryen-c.h
@@ -2284,6 +2284,13 @@ BINARYEN_API bool BinaryenGetLowMemoryUnused(void);
 // when optimizing. Applies to all modules, globally.
 BINARYEN_API void BinaryenSetLowMemoryUnused(bool on);
 
+// Gets whether to assume that an imported memory is zero-initialized.
+BINARYEN_API bool BinaryenGetZeroFilledMemory(void);
+
+// Enables or disables whether to assume that an imported memory is
+// zero-initialized.
+BINARYEN_API void BinaryenSetZeroFilledMemory(bool on);
+
 // Gets whether fast math optimizations are enabled, ignoring for example
 // corner cases of floating-point math like NaN changes.
 // Applies to all modules, globally.
