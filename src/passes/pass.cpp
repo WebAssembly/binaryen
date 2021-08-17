6b1878934a737ccaf5dafa612709d20409951933
Alon Zakai <azakai@google.com>
2021-08-17 16:22:07 -0700

    cleanup

--- src/passes/pass.cpp
+++ src/passes/pass.cpp
@@ -477,7 +477,7 @@ void PassRunner::addDefaultFunctionOptimizationPasses() {
   }
   addIfNoDWARFIssues("coalesce-locals");
   if (options.optimizeLevel >= 3 || options.shrinkLevel >= 1) {
-    addIfNoDWARFIssues("local-cse"); // 1% code size on emscripten benchmark suite, or less. But huge on j2cl.
+    addIfNoDWARFIssues("local-cse");
   }
   addIfNoDWARFIssues("simplify-locals");
   addIfNoDWARFIssues("vacuum");
