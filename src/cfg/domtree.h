4bdfa2696209d7f227d0370ef9dcd8b039d8626c
Alon Zakai <azakai@google.com>
2021-08-27 16:32:58 -0700

    format

--- src/cfg/domtree.h
+++ src/cfg/domtree.h
@@ -49,9 +49,7 @@ template<typename BasicBlock> struct DomTree {
 
   // Use a nonsense value to indicate what has yet to be initialized or what is
   // irrelevant.
-  enum {
-    nonsense = Index(-1)
-  };
+  enum { nonsense = Index(-1) };
 
   DomTree(std::vector<std::unique_ptr<BasicBlock>>& blocks);
 };
