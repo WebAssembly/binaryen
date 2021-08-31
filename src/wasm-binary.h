41fff9e2284e9be6e8a99da05beda54f398b0305
Sam Clegg <sbc@chromium.org>
2021-08-31 10:28:18 -0700

    Handle extra info in dylink section (#4112)
    If extra data is found in this section simply propagate it.
    
    Also, remove some dead code from wasm-binary.cpp.

--- src/wasm-binary.h
+++ src/wasm-binary.h
@@ -1242,6 +1242,7 @@ public:
   void writeInlineString(const char* name);
   void writeEscapedName(const char* name);
   void writeInlineBuffer(const char* data, size_t size);
+  void writeData(const char* data, size_t size);
 
   struct Buffer {
     const char* data;
@@ -1250,12 +1251,6 @@ public:
     Buffer(const char* data, size_t size, size_t pointerLocation)
       : data(data), size(size), pointerLocation(pointerLocation) {}
   };
-
-  std::vector<Buffer> buffersToWrite;
-
-  void emitBuffer(const char* data, size_t size);
-  void emitString(const char* str);
-  void finishUp();
 
   Module* getModule() { return wasm; }
 
--- test/lit/lit.cfg.py
+++ test/lit/lit.cfg.py
@@ -10,6 +10,8 @@ config.test_exec_root = os.path.join(config.binaryen_build_root, 'test')
 
 # Replace all Binaryen tools with their absolute paths
 bin_dir = os.path.join(config.binaryen_build_root, 'bin')
+assert(os.path.isdir(bin_dir))
+
 for tool_file in os.listdir(bin_dir):
     tool_path = config.binaryen_build_root + '/bin/' + tool_file
     tool = tool_file[:-4] if tool_file.endswith('.exe') else tool_file
