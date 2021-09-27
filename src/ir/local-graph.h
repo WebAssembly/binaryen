3cc28f45a8c85670321fe66fd86c591744b851bb
Alon Zakai <azakai@google.com>
2021-09-27 13:16:04 -0700

    Update src/ir/local-graph.h
    Co-authored-by: Heejin Ahn <aheejin@gmail.com>

--- src/ir/local-graph.h
+++ src/ir/local-graph.h
@@ -39,7 +39,7 @@ struct LocalGraph {
   // The local.sets relevant for an index or a get. The most common case is to
   // have a single set; after that, to be a phi of 2 items, so we use a small
   // set of size 2 to avoid allocations there.
-  typedef SmallSet<LocalSet*, 2> Sets;
+  using Sets = SmallSet<LocalSet*, 2>;
 
   typedef std::unordered_map<LocalGet*, Sets> GetSetses;
 
