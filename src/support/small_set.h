ddde7c64207ab44c4c3bbaeff02db654b0566487
Alon Zakai <azakai@google.com>
2021-09-27 13:15:07 -0700

    Update src/support/small_set.h
    Co-authored-by: Heejin Ahn <aheejin@gmail.com>

--- src/support/small_set.h
+++ src/support/small_set.h
@@ -163,9 +163,9 @@ public:
 
   template<typename Parent, typename Iterator, typename FlexibleIterator>
   struct IteratorBase : public std::iterator<std::forward_iterator_tag, T> {
-    typedef T value_type;
-    typedef long difference_type;
-    typedef T& reference;
+    using value_type = T;
+    using difference_type = long;
+    using reference = T&
 
     Parent* parent;
 
