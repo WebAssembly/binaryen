e6dcc64f68ea382c4b455a022ef99326a3d81751
MaxGraey <maxgraey@gmail.com>
2021-10-06 23:31:35 +0300

    fix

--- src/wasm/wasm-binary.cpp
+++ src/wasm/wasm-binary.cpp
@@ -6228,8 +6228,8 @@ void WasmBinaryBuilder::visitTableSet(TableSet* curr) {
   if (tableIdx >= tables.size()) {
     throwError("bad table index");
   }
-  curr->index = popNonVoidExpression();
   curr->value = popNonVoidExpression();
+  curr->index = popNonVoidExpression();
   curr->finalize();
   // Defer setting the table name for later, when we know it.
   tableRefs[tableIdx].push_back(curr);
