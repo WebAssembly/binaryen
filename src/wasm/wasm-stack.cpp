9228837745e7ff72f1f3e690689b700391a0edf1
Alon Zakai <azakai@google.com>
2021-10-08 11:21:28 -0700

    missing param

--- src/wasm/wasm-stack.cpp
+++ src/wasm/wasm-stack.cpp
@@ -83,7 +83,7 @@ void BinaryInstWriter::visitCallIndirect(CallIndirect* curr) {
   Index tableIdx = parent.getTableIndex(curr->table);
   int8_t op =
     curr->isReturn ? BinaryConsts::RetCallIndirect : BinaryConsts::CallIndirect;
-  o << op << U32LEB(parent.getTypeIndex(curr->getHeapType()))
+  o << op << U32LEB(parent.getTypeIndex(curr->getHeapType(parent.getModule())))
     << U32LEB(tableIdx);
 }
 
