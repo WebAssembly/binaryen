dae4ea8e00485ac67234315066baf206809597d0
Alon Zakai <azakai@google.com>
2021-10-09 11:24:48 -0700

    fix

--- src/passes/Directize.cpp
+++ src/passes/Directize.cpp
@@ -226,12 +226,10 @@ struct Directize : public Pass {
       }
     }
 
-    // Without typed function references, all we can do is optimize table
-    // accesses, so if we can't do that, stop.
-    if (validTables.empty() && !module->features.hasTypedFunctionReferences()) {
+    if (validTables.empty()) {
       return;
     }
-    // The table exists and is constant, so this is possible.
+
     FunctionDirectizer(validTables).run(runner, module);
   }
 };
