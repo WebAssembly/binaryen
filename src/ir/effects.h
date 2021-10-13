e1b423a11f200fdf882028232e028b3515c113b6
MaxGraey <maxgraey@gmail.com>
2021-10-14 01:19:01 +0300

    more

--- src/ir/effects.h
+++ src/ir/effects.h
@@ -617,7 +617,7 @@ private:
       parent.calls = true;
       // table.grow technically does a read-modify-write operation on the
       // table size in the successful case, modifying the set of valid
-      // addresses, and just a read operation in the failure case
+      // indices, and just a read operation in the failure case
       parent.readsTable = true;
       parent.writesTable = true;
     }
