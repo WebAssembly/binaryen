321ea2b375cbe8febf5f94d15ec1e8ebcd643c81
dcode <dcode@dcode.io>
2021-10-27 03:07:17 +0200

    fix

--- src/js/binaryen.js-extern-pre.js
+++ src/js/binaryen.js-extern-pre.js
@@ -1,4 +1,4 @@
-// FIXME: he Emscripten shell requires this variable to be present, even though
+// FIXME: The Emscripten shell requires this variable to be present, even though
 // we are building to ES6 (where it doesn't exist) and the .wasm blob is inlined
 // see: https://github.com/emscripten-core/emscripten/issues/11792
 var __dirname = "";
