b604ad935d1adc8498872e8afdb18df2f9b3b572
Alon Zakai <azakai@google.com>
2021-11-05 16:05:35 -0700

    [Wasm GC] Enable GlobalTypeOptimization (#4305)
    Now that all known issues with that pass are fixed, enable it by
    default. This adds it in a place that seems to make sense on j2wasm,
    but in general multiple cycles of optimization will be needed.
    
    This adds a test showing that we run this pass and that it helps
    ConstantFieldPropagation by running before it.

--- src/passes/pass.cpp
+++ src/passes/pass.cpp
@@ -524,7 +524,12 @@ void PassRunner::addDefaultGlobalOptimizationPrePasses() {
   }
   if (wasm->features.hasGC() && getTypeSystem() == TypeSystem::Nominal &&
       options.optimizeLevel >= 2) {
-    // TODO: investigate enabling --gto and --remove-module-elements before cfp
+    // Global type optimization can remove fields that are not needed, which can
+    // remove ref.funcs that were once assigned to vtables but are no longer
+    // needed, which can allow more code to be removed globally. After those,
+    // constant field propagation can be more effective.
+    addIfNoDWARFIssues("gto");
+    addIfNoDWARFIssues("remove-unused-module-elements");
     addIfNoDWARFIssues("cfp");
   }
 }
