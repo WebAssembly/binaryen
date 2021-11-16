52696625d003f80a8ae77150b6f1e626e9d98e57
Alon Zakai <azakai@google.com>
2021-11-15 16:17:15 -0800

    [NFC] Rename GlobalSubtyping => HeapRefining (#4331)

--- src/passes/pass.cpp
+++ src/passes/pass.cpp
@@ -156,9 +156,9 @@ void PassRegistry::registerPasses() {
     "generate-stack-ir", "generate Stack IR", createGenerateStackIRPass);
   registerPass(
     "gto", "globally optimize GC types", createGlobalTypeOptimizationPass);
-  registerPass("global-subtyping",
+  registerPass("heap-refining",
                "apply more specific subtypes to type fields where possible",
-               createGlobalSubtypingPass);
+               createHeapRefiningPass);
   registerPass(
     "heap2local", "replace GC allocations with locals", createHeap2LocalPass);
   registerPass(
@@ -527,7 +527,7 @@ void PassRunner::addDefaultGlobalOptimizationPrePasses() {
   }
   if (wasm->features.hasGC() && getTypeSystem() == TypeSystem::Nominal &&
       options.optimizeLevel >= 2) {
-    addIfNoDWARFIssues("global-subtyping");
+    addIfNoDWARFIssues("heap-refining");
     // Global type optimization can remove fields that are not needed, which can
     // remove ref.funcs that were once assigned to vtables but are no longer
     // needed, which can allow more code to be removed globally. After those,
