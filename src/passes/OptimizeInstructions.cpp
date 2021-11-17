6958192e4db73337c9fccddb1358b00187edafc7
MaxGraey <maxgraey@gmail.com>
2021-11-17 02:27:22 +0200

    init

--- src/passes/OptimizeInstructions.cpp
+++ src/passes/OptimizeInstructions.cpp
@@ -2509,6 +2509,21 @@ private:
         return inner;
       }
     }
+    {
+      // (i32(x) < 0) & (i32(y) < 0)   ==>   i32(x & y) < 0
+      // (i64(x) < 0) & (i64(y) < 0)   ==>   i64(x & y) < 0
+      Expression *x, *y;
+      if (matches(curr,
+                  binary(AndInt32,
+                         binary(LtS, any(&x), ival(0)),
+                         binary(LtS, any(&y), ival(0)))) &&
+          x->type == y->type) {
+        auto* inner = curr->left->cast<Binary>();
+        inner->left = Builder(*getModule())
+                        .makeBinary(Abstract::getBinary(x->type, And), x, y);
+        return inner;
+      }
+    }
     return nullptr;
   }
 
