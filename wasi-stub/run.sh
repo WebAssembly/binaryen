fecc68b749b988119ce62586060903e5a0921d00
Bo Yao <bo@near.org>
2021-12-22 12:43:59 +0800

    fix for call run.sh from another script

--- wasi-stub/run.sh
+++ wasi-stub/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-LD_LIBRARY_PATH=${SCRIPT_DIR}/../lib
+export LD_LIBRARY_PATH=${SCRIPT_DIR}/../lib
 ${SCRIPT_DIR}/wasi-stub $@
