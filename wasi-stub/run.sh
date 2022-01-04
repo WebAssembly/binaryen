8a63a050c23f36fc3711ea5a0450bdde413715bb
Bo Yao <bo@near.org>
2022-01-04 10:30:33 +0800

    make run.sh also works for mac

--- wasi-stub/run.sh
+++ wasi-stub/run.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-export LD_LIBRARY_PATH=${SCRIPT_DIR}/../lib
+SYSTEM=$(uname)
+if [ "${SYSTEM}" = "Linux" ]; then
+    export LD_LIBRARY_PATH=${SCRIPT_DIR}/../lib
+elif [ "${SYSTEM}" = "Darwin" ]; then
+    export DYLD_LIBRARY_PATH=${SCRIPT_DIR}/../lib
+else
+    echo "Unsupported system ${SYSTEM}"
+    exit 1
+fi
 ${SCRIPT_DIR}/wasi-stub $@
