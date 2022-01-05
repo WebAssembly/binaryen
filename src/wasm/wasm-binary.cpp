5c0e664839cb10b5b4ab9a8b62aa57734dc7fad7
Alon Zakai <azakai@google.com>
2022-01-04 16:39:57 -0800

    Turn an assertion on not colliding with an internal name into an error. See #4410

--- src/wasm/wasm-binary.cpp
+++ src/wasm/wasm-binary.cpp
@@ -6497,7 +6497,10 @@ void WasmBinaryBuilder::visitRethrow(Rethrow* curr) {
   BYN_TRACE("zz node: Rethrow\n");
   curr->target = getExceptionTargetName(getU32LEB());
   // This special target is valid only for delegates
-  assert(curr->target != DELEGATE_CALLER_TARGET);
+  if (curr->target == DELEGATE_CALLER_TARGET) {
+    throwError(std::string("rethrow target cannot use internal name ") +
+               DELEGATE_CALLER_TARGET.str);
+  }
   curr->finalize();
 }
 
