24111706ac0be85db84615b2aabbc19c7b2f30d8
Jukka Jylänki <jujjyl@gmail.com>
2022-04-01 18:54:18 +0300

    Add needed canRun function

--- src/cfg/liveness-traversal.h
+++ src/cfg/liveness-traversal.h
@@ -171,6 +171,10 @@ struct LivenessWalker : public CFGWalker<SubType, VisitorType, Liveness> {
     return nullptr;
   }
 
+  bool canRun(Function*) {
+    return true;
+  }
+
   // main entry point
 
   void doWalkFunction(Function* func) {
