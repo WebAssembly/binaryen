eecd36ec4267c1fc550da43038ea103a0e3bf6ea
Jukka Jylänki <jujjyl@gmail.com>
2022-04-07 14:55:08 +0300

    Lint

--- test/example/sparse_square_matrix.cpp
+++ test/example/sparse_square_matrix.cpp
@@ -16,19 +16,19 @@ int main() {
   assert(m.usingDenseStorage());
 
   // Setting and getting element values in dense storage should work.
-  for(int y = 0; y < 100; ++y)
-    for(int x = 0; x < 100; ++x)
-      m.set(y, x, y*100+x);
-  for(int y = 0; y < 100; ++y)
-    for(int x = 0; x < 100; ++x)
-      assert(m.get(y, x) == y*100+x);
+  for (int y = 0; y < 100; ++y)
+    for (int x = 0; x < 100;. ++x)
+      m.set(y, x, y * 100 + x);
+  for (int y = 0; y < 100; ++y)
+    for (int x = 0; x < 100; ++x)
+      assert(m.get(y, x) == y * 100 + x);
 
   // Recreating should clear the matrix elements to zero,
   // even if recreating to same size as before.
   assert(m.width() == 100);
   m.recreate(100);
-  for(int y = 0; y < 100; ++y)
-    for(int x = 0; x < 100; ++x)
+  for (int y = 0; y < 100; ++y)
+    for (int x = 0; x < 100; ++x)
       assert(m.get(y, x) == 0);
 
   // Large matrices should use sparse storage.
@@ -36,18 +36,18 @@ int main() {
   assert(!m.usingDenseStorage());
 
   // Setting and getting element values in sparse storage should work.
-  for(int y = 0; y < m.DenseLimit; ++y)
-    for(int x = 0; x < m.DenseLimit; ++x)
+  for (int y = 0; y < m.DenseLimit; ++y)
+    for (int x = 0; x < m.DenseLimit; ++x)
       m.set(y, x, y*m.DenseLimit+x);
-  for(int y = 0; y < m.DenseLimit; ++y)
-    for(int x = 0; x < m.DenseLimit; ++x)
-      assert(m.get(y, x) == y*m.DenseLimit+x);
+  for (int y = 0; y < m.DenseLimit; ++y)
+    for (int x = 0; x < m.DenseLimit; ++x)
+      assert(m.get(y, x) == y * m.DenseLimit + x);
 
   // Recreating matrix in sparse mode should reset values in sparse
   // storage to zero.
-  m.recreate(m.DenseLimit+1);
-  for(int y = 0; y < m.width(); ++y)
-    for(int x = 0; x < m.width(); ++x)
+  m.recreate(m.DenseLimit + 1);
+  for (int y = 0; y < m.width(); ++y)
+    for (int x = 0; x < m.width(); ++x)
       assert(m.get(y, x) == 0);
 
   std::cout << "ok.\n";
