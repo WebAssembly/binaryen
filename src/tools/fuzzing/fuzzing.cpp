65f09f262e00348462e3371fbc89c6c1d398fba1
Alon Zakai <azakai@google.com>
2022-04-21 07:33:04 -0700

    [NominalFuzzing] Fix TranslateToFuzzReader::getSubType(Rtt) (#4604)
    Randomly selecting a depth is ok for structural typing, but in nominal it
    must match the actual hierarchy of types.

--- src/tools/fuzzing/fuzzing.cpp
+++ src/tools/fuzzing/fuzzing.cpp
@@ -3041,6 +3041,12 @@ HeapType TranslateToFuzzReader::getSubType(HeapType type) {
 }
 
 Rtt TranslateToFuzzReader::getSubType(Rtt rtt) {
+  if (getTypeSystem() == TypeSystem::Nominal ||
+      getTypeSystem() == TypeSystem::Isorecursive) {
+    // With nominal or isorecursive typing the depth in rtts must match the
+    // nominal hierarchy, so we cannot create a random depth like we do below.
+    return rtt;
+  }
   uint32_t depth = rtt.depth != Rtt::NoDepth
                      ? rtt.depth
                      : oneIn(2) ? Rtt::NoDepth : upTo(MAX_RTT_DEPTH + 1);
