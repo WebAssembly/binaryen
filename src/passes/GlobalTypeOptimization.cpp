6000629ae7cc2962483cd0d7ae4a770c8f2a34a0
Alon Zakai <azakai@google.com>
2022-04-21 17:04:01 -0700

    [NominalFuzzing] GTO: trap on null ref in removed struct.set (#4607)
    When a field has no reads, we remove all its writes, but we did this:
    
    (struct.set $foo A B)
    =>
    (drop A) (drop B)
    
    We also need to trap if A, the reference, is null, which this PR
    fixes,
    
    (struct.set $foo A B)
    =>
    (drop (ref.as_non_null A)) (drop B)

--- src/passes/GlobalTypeOptimization.cpp
+++ src/passes/GlobalTypeOptimization.cpp
@@ -391,10 +391,12 @@ struct GlobalTypeOptimization : public Pass {
           // Map to the new index.
           curr->index = newIndex;
         } else {
-          // This field was removed, so just emit drops of our children.
+          // This field was removed, so just emit drops of our children (plus a
+          // trap if the input is null).
           Builder builder(*getModule());
-          replaceCurrent(builder.makeSequence(builder.makeDrop(curr->ref),
-                                              builder.makeDrop(curr->value)));
+          replaceCurrent(builder.makeSequence(
+            builder.makeDrop(builder.makeRefAs(RefAsNonNull, curr->ref)),
+            builder.makeDrop(curr->value)));
         }
       }
 
