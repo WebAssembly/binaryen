2a53ba62240a0d9a46c3c9906002cba352afe693
Alon Zakai <azakai@google.com>
2022-05-06 09:22:07 -0700

    fix

--- scripts/fuzz_opt.py
+++ scripts/fuzz_opt.py
@@ -188,7 +188,7 @@ def init_important_initial_contents():
         os.path.join('lit', 'passes', 'inlining_splitting.wast'),
         os.path.join('heap-types.wast'),
     ]
-    RECENT_DAYS = 1
+    RECENT_DAYS = 10
 
     # Returns the list of test wast/wat files added or modified within the
     # RECENT_DAYS number of days counting from the commit time of HEAD
