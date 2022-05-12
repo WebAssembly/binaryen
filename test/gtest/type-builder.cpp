b1a37951800f7ff394b33e38fa49a5f42560625a
Alon Zakai <azakai@google.com>
2022-05-12 13:49:31 -0700

    fix

--- test/gtest/type-builder.cpp
+++ test/gtest/type-builder.cpp
@@ -503,11 +503,11 @@ TEST_F(IsorecursiveTest, CanonicalizeBasicTypes) {
 // Test SubTypes utility code.
 TEST_F(NominalTest, testSubTypes) {
   // Build type types, the second of which is a subtype.
-  TypeBuilder typeBuilder(2);
-  typeBuilder[0] = Struct({Field(Type::anyref, Immutable)});
-  typeBuilder[1] = Struct({Field(Type::funcref, Immutable)});
-  typeBuilder[1].subTypeOf(typeBuilder[0]);
-  auto built = *typeBuilder.build();
+  TypeBuilder builder(2);
+  builder[0] = Struct({Field(Type::anyref, Immutable)});
+  builder[1] = Struct({Field(Type::funcref, Immutable)});
+  builder[1].subTypeOf(builder[0]);
+  auto built = *builder.build();
 
   // Build a tiny wasm module that uses the types, so that we can test the
   // SubTypes utility code.
@@ -520,10 +520,10 @@ TEST_F(NominalTest, testSubTypes) {
     wasmBuilder.makeNop()));
   SubTypes subTypes(wasm);
   auto subTypes0 = subTypes.getStrictSubTypes(built[0]);
-  assert(subTypes0.size() == 1 && subTypes0[0] == built[1]);
+  EXPECT_TRUE(subTypes0.size() == 1 && subTypes0[0] == built[1]);
   auto subTypes0Inclusive = subTypes.getAllSubTypes(built[0]);
-  assert(subTypes0Inclusive.size() == 2 && subTypes0Inclusive[0] == built[1] &&
+  EXPECT_TRUE(subTypes0Inclusive.size() == 2 && subTypes0Inclusive[0] == built[1] &&
          subTypes0Inclusive[1] == built[0]);
   auto subTypes1 = subTypes.getStrictSubTypes(built[1]);
-  assert(subTypes1.size() == 0);
+  EXPECT_EQ(subTypes1.size(), 0);
 }
