ca5cc94ba9c4d5980806d2f07165b52b5bc2d3b7
MaxGraey <maxgraey@gmail.com>
2022-06-13 13:52:23 +0300

    disable some tests

--- scripts/fuzz_opt.py
+++ scripts/fuzz_opt.py
@@ -767,8 +767,9 @@ class CompareVMs(TestCaseHandler):
                     D8(),
                     D8Liftoff(),
                     D8TurboFan(),
-                    Wasm2C(),
-                    Wasm2C2Wasm()]
+                    # Wasm2C(),
+                    # Wasm2C2Wasm()
+                    ]
 
     def handle_pair(self, input, before_wasm, after_wasm, opts):
         before = self.run_vms(before_wasm)
@@ -1021,7 +1022,7 @@ class RoundtripText(TestCaseHandler):
 testcase_handlers = [
     FuzzExec(),
     CompareVMs(),
-    CheckDeterminism(),
+    # CheckDeterminism(),
     Wasm2JS(),
     Asyncify(),
     # FIXME: Re-enable after https://github.com/WebAssembly/binaryen/issues/3989
