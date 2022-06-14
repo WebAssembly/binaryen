ffca020980d14d68e97830ccdc8dcf6d88679a7c
MaxGraey <maxgraey@gmail.com>
2022-06-14 09:03:21 +0300

    lint

--- scripts/fuzz_opt.py
+++ scripts/fuzz_opt.py
@@ -767,9 +767,9 @@ class CompareVMs(TestCaseHandler):
                     D8(),
                     D8Liftoff(),
                     D8TurboFan(),
-                    #TODO: Enable Wasm2C & Wasm2C2Wasm after #4723 issue resolved
-                    #Wasm2C(),
-                    #Wasm2C2Wasm()
+                    # TODO: Enable Wasm2C & Wasm2C2Wasm after #4723 issue resolved
+                    # Wasm2C(),
+                    # Wasm2C2Wasm()
                     ]
 
     def handle_pair(self, input, before_wasm, after_wasm, opts):
