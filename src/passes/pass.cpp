ce2fc89d66d652c67768be3ddec597f499093f02
Alon Zakai <azakai@google.com>
2022-06-16 15:00:19 -0700

    Enable GlobalStructInference by default (#4734)
    This pass helps on at least one Java microbenchmark in a clear way. Real-world data
    is mixed, with no obvious benefit. But it does optimize 819 callsites on the real-world
    14 MB J2Wasm binary, so we may find it helps if/when we run into those code paths.
    
    On that binary (the biggest we have for GC) this pass runs in 0.12 seconds, so there
    is very little downside to enabling it. It is a fast linear-time operation.

--- src/passes/pass.cpp
+++ src/passes/pass.cpp
@@ -567,6 +567,7 @@ void PassRunner::addDefaultGlobalOptimizationPrePasses() {
     addIfNoDWARFIssues("gto");
     addIfNoDWARFIssues("remove-unused-module-elements");
     addIfNoDWARFIssues("cfp");
+    addIfNoDWARFIssues("gsi");
   }
 }
 
