49e5428c5bff65562aa36f5b91e7f456f51bbe2e
MaxGraey <maxgraey@gmail.com>
2022-08-02 12:19:03 +0300

    fix?

--- src/js/binaryen.jsoo-extern-pre.js
+++ src/js/binaryen.jsoo-extern-pre.js
@@ -1,6 +1,11 @@
 // js_of_ocaml needs a special syntax for exposing variables to the compiler
 // and it is not built for nodejs so we don't need the __dirname hack
 
+/**
+ * @fileoverview This is an externs binaryen.js file.
+ * @externs
+ */
+
 //Provides: Binaryen
 var Binaryen = {};
 
