WebAssembly/binaryen

commit 9534e6927c41f4a6a5d06d58d00c271c9f066e9a

Cleanup em_asm/em_js strings as part of PostEmscripten (#4871)
Rather than doing it as a side effect of dumping the metadata in
wasm-emscripten-finalize.

src/wasm/wasm-emscripten.cpp
@@ -188,41 +188,6 @@ struct AsmConst {
   std::string code;
 };
 
-struct SegmentRemover : WalkerPass<PostWalker<SegmentRemover>> {
-  SegmentRemover(Index segment) : segment(segment) {}
-
-  bool isFunctionParallel() override { return true; }
-
-  Pass* create() override { return new SegmentRemover(segment); }
-
-  void visitMemoryInit(MemoryInit* curr) {
-    if (segment == curr->segment) {
-      Builder builder(*getModule());
-      replaceCurrent(builder.blockify(builder.makeDrop(curr->dest),
-                                      builder.makeDrop(curr->offset),
-                                      builder.makeDrop(curr->size)));
-    }
-  }
-
-  void visitDataDrop(DataDrop* curr) {
-    if (segment == curr->segment) {
-      Builder builder(*getModule());
-      replaceCurrent(builder.makeNop());
-    }
-  }
-
-  Index segment;
-};
-
-static void removeSegment(Module& wasm, Index segment) {
-  PassRunner runner(&wasm);
-  SegmentRemover(segment).run(&runner, &wasm);
-  // Resize the segment to zero.  In theory we should completely remove it
-  // but that would mean re-numbering the segments that follow which is
-  // non-trivial.
-  wasm.dataSegments[segment]->data.resize(0);
-}
-
 static Address getExportedAddress(Module& wasm, Export* export_) {
   Global* g = wasm.getGlobal(export_->value);
   auto* addrConst = g->init->dynCast<Const>();
@@ -246,7 +211,6 @@ static std::vector<AsmConst> findEmAsmConsts(Module& wasm,
     Fatal() << "Found only one of __start_em_asm and __stop_em_asm";
   }
 
-  std::vector<AsmConst> asmConsts;
   StringConstantTracker stringTracker(wasm);
   Address startAddress = getExportedAddress(wasm, start);
   Address endAddress = getExportedAddress(wasm, end);
@@ -255,40 +219,26 @@ static std::vector<AsmConst> findEmAsmConsts(Module& wasm,
     size_t segmentSize = wasm.dataSegments[i]->data.size();
     if (segmentStart <= startAddress &&
         segmentStart + segmentSize >= endAddress) {
+      std::vector<AsmConst> asmConsts;
       Address address = startAddress;
       while (address < endAddress) {
         auto code = stringTracker.stringAtAddr(address);
         asmConsts.push_back({address, code});
         address.addr += strlen(code) + 1;
       }
-
-      if (segmentStart == startAddress &&
-          segmentStart + segmentSize == endAddress) {
-        removeSegment(wasm, i);
-      } else {
-        // If we can't remove the whole segment then just set the string
-        // data to zero.
-        size_t segmentOffset = startAddress - segmentStart;
-        char* startElem = &wasm.dataSegments[i]->data[segmentOffset];
-        memset(startElem, 0, endAddress - startAddress);
-      }
-      break;
+      assert(asmConsts.size());
+      return asmConsts;
     }
   }
-
-  assert(asmConsts.size());
-  wasm.removeExport("__start_em_asm");
-  wasm.removeExport("__stop_em_asm");
-  return asmConsts;
+  Fatal() << "Segment data not found between symbols __start_em_asm and "
+             "__stop_em_asm";
 }
 
 struct EmJsWalker : public PostWalker<EmJsWalker> {
   Module& wasm;
   StringConstantTracker stringTracker;
-  std::vector<Export> toRemove;
 
   std::map<std::string, std::string> codeByName;
-  std::map<Address, size_t> codeAddresses; // map from address to string len
 
   EmJsWalker(Module& _wasm) : wasm(_wasm), stringTracker(_wasm) {}
 
@@ -318,50 +268,15 @@ struct EmJsWalker : public PostWalker<EmJsWalker> {
       return;
     }
 
-    toRemove.push_back(*curr);
     auto code = stringTracker.stringAtAddr(address);
     auto funcName = std::string(curr->name.stripPrefix(EM_JS_PREFIX.str));
     codeByName[funcName] = code;
-    codeAddresses[address] = strlen(code) + 1;
   }
 };
 
 EmJsWalker findEmJsFuncsAndReturnWalker(Module& wasm) {
   EmJsWalker walker(wasm);
   walker.walkModule(&wasm);
-
-  for (const Export& exp : walker.toRemove) {
-    if (exp.kind == ExternalKind::Function) {
-      wasm.removeFunction(exp.value);
-    } else {
-      wasm.removeGlobal(exp.value);
-    }
-    wasm.removeExport(exp.name);
-  }
-
-  wasm.removeExport("__start_em_js");
-  wasm.removeExport("__stop_em_js");
-
-  // With newer versions of emscripten/llvm we pack all EM_JS strings into
-  // single segment.
-  // We can detect this by checking for segments that contain only JS strings.
-  // When we find such segements we remove them from the final binary.
-  for (Index i = 0; i < wasm.dataSegments.size(); i++) {
-    Address start = walker.stringTracker.segmentOffsets[i];
-    Address cur = start;
-
-    while (cur < start + wasm.dataSegments[i]->data.size()) {
-      if (walker.codeAddresses.count(cur) == 0) {
-        break;
-      }
-      cur.addr += walker.codeAddresses[cur];
-    }
-
-    if (cur == start + wasm.dataSegments[i]->data.size()) {
-      // Entire segment is contains JS strings.  Remove it.
-      removeSegment(wasm, i);
-    }
-  }
   return walker;
 }
 

test/lld/em_asm.wat.mem.out
@@ -12,6 +12,8 @@
  (export "memory" (memory $0))
  (export "__wasm_call_ctors" (func $__wasm_call_ctors))
  (export "main" (func $main))
+ (export "__start_em_asm" (global $global$1))
+ (export "__stop_em_asm" (global $global$2))
  (func $__wasm_call_ctors
   (nop)
  )
@@ -84,6 +86,8 @@
     "main"
   ],
   "namedGlobals": {
+    "__start_em_asm" : "574",
+    "__stop_em_asm" : "665"
   },
   "invokeFuncs": [
   ],

src/passes/PostEmscripten.cpp
@@ -41,12 +41,192 @@ static bool isInvoke(Function* F) {
   return F->imported() && F->module == ENV && F->base.startsWith("invoke_");
 }
 
+struct SegmentRemover : WalkerPass<PostWalker<SegmentRemover>> {
+  SegmentRemover(Index segment) : segment(segment) {}
+
+  bool isFunctionParallel() override { return true; }
+
+  Pass* create() override { return new SegmentRemover(segment); }
+
+  void visitMemoryInit(MemoryInit* curr) {
+    if (segment == curr->segment) {
+      Builder builder(*getModule());
+      replaceCurrent(builder.blockify(builder.makeDrop(curr->dest),
+                                      builder.makeDrop(curr->offset),
+                                      builder.makeDrop(curr->size)));
+    }
+  }
+
+  void visitDataDrop(DataDrop* curr) {
+    if (segment == curr->segment) {
+      Builder builder(*getModule());
+      replaceCurrent(builder.makeNop());
+    }
+  }
+
+  Index segment;
+};
+
+static void calcSegmentOffsets(Module& wasm,
+                               std::vector<Address>& segmentOffsets) {
+  const Address UNKNOWN_OFFSET(uint32_t(-1));
+
+  std::unordered_map<Index, Address> passiveOffsets;
+  if (wasm.features.hasBulkMemory()) {
+    // Fetch passive segment offsets out of memory.init instructions
+    struct OffsetSearcher : PostWalker<OffsetSearcher> {
+      std::unordered_map<Index, Address>& offsets;
+      OffsetSearcher(std::unordered_map<unsigned, Address>& offsets)
+        : offsets(offsets) {}
+      void visitMemoryInit(MemoryInit* curr) {
+        // The desitination of the memory.init is either a constant
+        // or the result of an addition with __memory_base in the
+        // case of PIC code.
+        auto* dest = curr->dest->dynCast<Const>();
+        if (!dest) {
+          auto* add = curr->dest->dynCast<Binary>();
+          if (!add) {
+            return;
+          }
+          dest = add->left->dynCast<Const>();
+          if (!dest) {
+            return;
+          }
+        }
+        auto it = offsets.find(curr->segment);
+        if (it != offsets.end()) {
+          Fatal() << "Cannot get offset of passive segment initialized "
+                     "multiple times";
+        }
+        offsets[curr->segment] = dest->value.getInteger();
+      }
+    } searcher(passiveOffsets);
+    searcher.walkModule(&wasm);
+  }
+  for (unsigned i = 0; i < wasm.dataSegments.size(); ++i) {
+    auto& segment = wasm.dataSegments[i];
+    if (segment->isPassive) {
+      auto it = passiveOffsets.find(i);
+      if (it != passiveOffsets.end()) {
+        segmentOffsets.push_back(it->second);
+      } else {
+        // This was a non-constant offset (perhaps TLS)
+        segmentOffsets.push_back(UNKNOWN_OFFSET);
+      }
+    } else if (auto* addrConst = segment->offset->dynCast<Const>()) {
+      auto address = addrConst->value.getUnsigned();
+      segmentOffsets.push_back(address);
+    } else {
+      // TODO(sbc): Wasm shared libraries have data segments with non-const
+      // offset.
+      segmentOffsets.push_back(0);
+    }
+  }
+}
+
+static void removeSegment(Module& wasm, Index segment) {
+  PassRunner runner(&wasm);
+  SegmentRemover(segment).run(&runner, &wasm);
+  // Resize the segment to zero.  In theory we should completely remove it
+  // but that would mean re-numbering the segments that follow which is
+  // non-trivial.
+  wasm.dataSegments[segment]->data.resize(0);
+}
+
+static Address getExportedAddress(Module& wasm, Export* export_) {
+  Global* g = wasm.getGlobal(export_->value);
+  auto* addrConst = g->init->dynCast<Const>();
+  return addrConst->value.getUnsigned();
+}
+
+static void removeData(Module& wasm,
+                       const std::vector<Address>& segmentOffsets,
+                       Name start_sym,
+                       Name end_sym) {
+  Export* start = wasm.getExportOrNull(start_sym);
+  Export* end = wasm.getExportOrNull(end_sym);
+  if (!start && !end) {
+    BYN_TRACE("removeData: start/stop symbols not found (" << start_sym << ", "
+                                                           << end_sym << ")\n");
+    return;
+  }
+
+  if (!start || !end) {
+    Fatal() << "Found only one of " << start_sym << " and " << end_sym;
+  }
+
+  Address startAddress = getExportedAddress(wasm, start);
+  Address endAddress = getExportedAddress(wasm, end);
+  for (Index i = 0; i < wasm.dataSegments.size(); i++) {
+    Address segmentStart = segmentOffsets[i];
+    size_t segmentSize = wasm.dataSegments[i]->data.size();
+    if (segmentStart <= startAddress &&
+        segmentStart + segmentSize >= endAddress) {
+
+      if (segmentStart == startAddress &&
+          segmentStart + segmentSize == endAddress) {
+        BYN_TRACE("removeData: removing whole segment\n");
+        removeSegment(wasm, i);
+      } else {
+        // If we can't remove the whole segment then just set the string
+        // data to zero.
+        BYN_TRACE("removeData: removing part of segment\n");
+        size_t segmentOffset = startAddress - segmentStart;
+        char* startElem = &wasm.dataSegments[i]->data[segmentOffset];
+        memset(startElem, 0, endAddress - startAddress);
+      }
+      return;
+    }
+  }
+  Fatal() << "Segment data not found between symbols " << start_sym << " ("
+          << startAddress << ") and " << end_sym << " (" << endAddress << ")";
+}
+
+cashew::IString EM_JS_PREFIX("__em_js__");
+
+struct EmJsWalker : public PostWalker<EmJsWalker> {
+  std::vector<Export> toRemove;
+
+  void visitExport(Export* curr) {
+    if (curr->name.startsWith(EM_JS_PREFIX.str)) {
+      toRemove.push_back(*curr);
+    }
+  }
+};
+
 } // namespace
 
 struct PostEmscripten : public Pass {
   void run(PassRunner* runner, Module* module) override {
+    removeExports(runner, *module);
+    removeEmJsExports(runner, *module);
     // Optimize exceptions
     optimizeExceptions(runner, module);
+  }
+
+  void removeExports(PassRunner* runner, Module& module) {
+    std::vector<Address> segmentOffsets; // segment index => address offset
+    calcSegmentOffsets(module, segmentOffsets);
+
+    removeData(module, segmentOffsets, "__start_em_asm", "__stop_em_asm");
+    removeData(module, segmentOffsets, "__start_em_js", "__stop_em_js");
+    module.removeExport("__start_em_asm");
+    module.removeExport("__stop_em_asm");
+    module.removeExport("__start_em_js");
+    module.removeExport("__stop_em_js");
+  }
+
+  void removeEmJsExports(PassRunner* runner, Module& module) {
+    EmJsWalker walker;
+    walker.walkModule(&module);
+    for (const Export& exp : walker.toRemove) {
+      if (exp.kind == ExternalKind::Function) {
+        module.removeFunction(exp.value);
+      } else {
+        module.removeGlobal(exp.value);
+      }
+      module.removeExport(exp.name);
+    }
   }
 
   // Optimize exceptions (and setjmp) by removing unnecessary invoke* calls.

test/lit/passes/post-emscripten.wast
@@ -1,0 +1,28 @@
+;; NOTE: Assertions have been generated by update_lit_checks.py --all-items and should not be edited.
+;; RUN: wasm-opt %s --post-emscripten -S -o - | filecheck %s
+
+;; Checks that the start/stop exports are removed and that the data they
+;; refer to is either zero'd out, or the segment emptied.
+
+(module
+ ;; CHECK:      (global $g1 i32 (i32.const 1000))
+ (global $g1 i32 (i32.const 1000))
+ ;; CHECK:      (global $g2 i32 (i32.const 1011))
+ (global $g2 i32 (i32.const 1011))
+ ;; CHECK:      (global $g3 i32 (i32.const 2006))
+ (global $g3 i32 (i32.const 2006))
+ ;; CHECK:      (global $g4 i32 (i32.const 2015))
+ (global $g4 i32 (i32.const 2015))
+ (memory 10 10)
+ ;; CHECK:      (memory $0 10 10)
+
+ ;; CHECK:      (data $data1 (i32.const 1000) "")
+ (data $data1 (i32.const 1000) "hello world")
+ ;; CHECK:      (data $data2 (i32.const 2000) "hello \00\00\00\00\00\00\00\00\00 world")
+ (data $data2 (i32.const 2000) "hello DELETE ME world")
+ (export "__start_em_asm" (global $g1))
+ (export "__stop_em_asm" (global $g2))
+ (export "__start_em_js" (global $g3))
+ (export "__stop_em_js" (global $g4))
+)
+

test/lit/wasm-emscripten-finalize/em_asm.wat
@@ -6,17 +6,10 @@
 ;; Check that the data segment that contains only EM_ASM strings resized to
 ;; zero, and that the string are extracted into the metadata.
 
-;; CHECK:      (data (i32.const 100) "normal data")
-;; CHECK-NEXT: (data (i32.const 512) "")
-;; CHECK-NEXT: (data (i32.const 1024) "more data")
-
 ;; CHECK:       "asmConsts": {
 ;; CHECK-NEXT:     "512": "{ console.log('JS hello'); }",
 ;; CHECK-NEXT:     "541": "{ console.log('hello again'); }"
 ;; CHECK-NEXT:   },
-
-;; Check that the exports are removed
-;; CHECK-NOT: export
 
 (module
  (memory 1 1)

test/lit/wasm-emscripten-finalize/em_asm_partial.wat
@@ -1,24 +1,0 @@
-;; Test that em_asm string are extraced correctly when the __start_em_asm
-;; and __stop_em_asm globals are exported.
-
-;; RUN: wasm-emscripten-finalize %s -S | filecheck %s
-
-;; Check for the case when __start_em_asm and __stop_em_asm don't define an
-;; entire segment. In this case we preserve the segment but zero the data.
-
-;; CHECK: (data (i32.const 512) "xx\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00\00yy")
-
-;; CHECK:       "asmConsts": {
-;; CHECK-NEXT:     "514": "{ console.log('JS hello'); }",
-;; CHECK-NEXT:     "543": "{ console.log('hello again'); }"
-;; CHECK-NEXT:   },
-
-;; Check that the exports are removed
-;; CHECK-NOT: export
-
-(module
- (memory 1 1)
- (global (export "__start_em_asm") i32 (i32.const 514))
- (global (export "__stop_em_asm") i32 (i32.const 575))
- (data (i32.const 512) "xx{ console.log('JS hello'); }\00{ console.log('hello again'); }\00yy")
-)

test/lit/wasm-emscripten-finalize/em_js.wat
@@ -3,16 +3,6 @@
 
 ;; RUN: wasm-emscripten-finalize %s -S | filecheck %s
 
-;; All functions should be stripped from the binary, regardless
-;; of internal name
-;; CHECK-NOT: (global
-
-;; The data section that contains only em_js strings should
-;; be stripped (shrunk to zero size):
-;; CHECK: (data (i32.const 1024) "some JS string data\00xxx")
-;; CHECK: (data (i32.const 512) "")
-;; CHECK: (data (i32.const 2048) "more JS string data\00yyy")
-
 ;;      CHECK:  "emJsFuncs": {
 ;; CHECK-NEXT:    "bar": "more JS string data",
 ;; CHECK-NEXT:    "baz": "Only em_js strings here",

commit dbc121fd8b4647c0fb70177622c725e0b7fa99ec

wasm-emscripten-finalize: Remove em_js/em_asm start/stop symbols when stripping data segments.
This avoid a fatal crash in `--post-emscripten` where it tries to remove
data that is no longer part of the file.

This fixes bug introduced by #4871 that causes emscripten tests to fail.

src/wasm/wasm-emscripten.cpp
@@ -445,6 +445,15 @@ void EmscriptenGlueGenerator::separateDataSegments(Output* outfile,
     lastEnd = offset + seg->data.size();
   }
   wasm.dataSegments.clear();
+  // Remove the start/stop symbols that the PostEmscripten uses to remove
+  // em_asm/em_js data.  Since we just removed all the data segments from the
+  // file there is nothing more for that pass to do.
+  // TODO(sbc): Fix the ordering so that the removal the EM_ASM/EM_JS data comes
+  // before this pass.
+  wasm.removeExport("__start_em_asm");
+  wasm.removeExport("__stop_em_asm");
+  wasm.removeExport("__start_em_js");
+  wasm.removeExport("__stop_em_js");
 }
 
 } // namespace wasm

test/lld/em_asm.wat.mem.out
@@ -12,8 +12,6 @@
  (export "memory" (memory $0))
  (export "__wasm_call_ctors" (func $__wasm_call_ctors))
  (export "main" (func $main))
- (export "__start_em_asm" (global $global$1))
- (export "__stop_em_asm" (global $global$2))
  (func $__wasm_call_ctors
   (nop)
  )