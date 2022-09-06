54b2e99ee140f2fbfa3f6b6b3d2d119b6400735b
Alon Zakai <azakai@google.com>
2022-09-06 09:24:00 -0700

    fix

--- src/wasm/wasm.cpp
+++ src/wasm/wasm.cpp
@@ -182,7 +182,7 @@ void Block::finalize() {
     return;
   }
   // The default type is what is at the end. Next we need to see if breaks and/
-  // or unreachabitily change that.
+  // or unreachability change that.
   type = list.back()->type;
   if (!name.is()) {
     // Nothing branches here, so this is easy.
