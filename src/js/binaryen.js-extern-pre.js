88cbdff5eca55de632ae6a854fb2efa9ec0348ff
dcode <dcode@dcode.io>
2022-09-22 19:33:40 +0200

    temporary `require` fix?

--- src/js/binaryen.js-extern-pre.js
+++ src/js/binaryen.js-extern-pre.js
@@ -2,3 +2,10 @@
 // we are building to ES6 (where it doesn't exist) and the .wasm blob is inlined
 // see: https://github.com/emscripten-core/emscripten/issues/11792
 var __dirname = "";
+
+// FIXME: The Emscripten shell requires this function to be present, even though
+// we are building to ESM (where it doesn't exist) and the result is not used
+// see: https://github.com/emscripten-core/emscripten/pull/17851
+function require() {
+  return {};
+}
