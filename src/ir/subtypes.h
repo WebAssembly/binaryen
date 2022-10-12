520bb41ed3624f4332625bf2309fef7a96f5eba3
Alon Zakai <azakai@google.com>
2022-10-12 10:57:22 -0700

    work

--- src/ir/subtypes.h
+++ src/ir/subtypes.h
@@ -62,6 +62,45 @@ struct SubTypes {
     return ret;
   }
 
+  // Efficiently traverse subtypes of a type, up to a particular depth (depth =
+  // 0 means not to traverse subtypes, etc.). The callback function receives
+  // (type, depth).
+  template<typename F>
+  void traverseSubTypes(HeapType type, Index depth, F func) {
+    // Start by traversing the type itself.
+    func(type, 0);
+
+    // getStrictSubTypes() returns vectors of subtypes, so for efficiency store
+    // pointers to those in our work queue to avoid allocations. See the note
+    // below on typeSubTypes for why this is safe.
+    struct Item {
+      const std::vector<HeapType>* vec;
+      Index depth;
+    };
+
+    // Real-world type hierarchies tend to have a limited depth, so try to avoid
+    // allocations in our work queue with a SmallVector.
+    SmallVector<Item, 10> work;
+
+    // Start with the subtypes of the base type. Those have depth 1.
+    work.push_back({&getStrictSubTypes(type), 1});
+
+    while (!work.empty()) {
+      auto& item = work.back();
+      work.pop_back();
+      auto currDepth = item.depth;
+      auto& vec = *item.vec;
+      if (currDepth > depth || vec.empty()) {
+        // Nothing we need to traverse here.
+        continue;
+      }
+      for (auto type : (*item.vec)) {
+        func(type, currDepth);
+        work.push_back({&getStrictSubTypes(type), currDepth + 1});
+      }
+    }
+  }
+
   // All the types in the program. This is computed here anyhow, and can be
   // useful for callers to iterate on, so it is public.
   std::vector<HeapType> types;
