3b1df7e5c329d9600cfadd513b557af618c007aa
dcode <dcode@dcode.io>
2022-10-19 19:57:07 +0200

    [C API] Align I31ref and Dataref to be nullable (#5153)
    The C API still returned non nullable types for `dataref` (`ref data` instead of `ref null data`) and `i31ref` (`ref i31` instead of `ref null i31`). This PR aligns with the current state of the GC proposal, making them nullable when obtained via the C API.

--- src/binaryen-c.cpp
+++ src/binaryen-c.cpp
@@ -197,10 +197,10 @@ BinaryenType BinaryenTypeEqref(void) {
   return Type(HeapType::eq, Nullable).getID();
 }
 BinaryenType BinaryenTypeI31ref(void) {
-  return Type(HeapType::i31, NonNullable).getID();
+  return Type(HeapType::i31, Nullable).getID();
 }
 BinaryenType BinaryenTypeDataref(void) {
-  return Type(HeapType::data, NonNullable).getID();
+  return Type(HeapType::data, Nullable).getID();
 }
 BinaryenType BinaryenTypeArrayref(void) {
   return Type(HeapType::array, Nullable).getID();
