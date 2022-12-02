d164dd84de755e4ca49e11579a11c24d8c7a65e1
Alon Zakai <azakai@google.com>
2022-12-02 09:43:58 -0800

    Revert "Update src/passes/TypeSSA.cpp"
    This reverts commit cf5f552a4029ab11601a74f62e009a18c140a872.

--- src/passes/TypeSSA.cpp
+++ src/passes/TypeSSA.cpp
@@ -150,8 +150,8 @@ struct TypeSSA : public Pass {
     for (Index i = 0; i < num; i++) {
       auto* curr = structNews[i];
       auto oldType = curr->type.getHeapType();
-      builder[i] = oldType.getStruct();
-      builder[i].subTypeOf(oldType);
+      builder.setHeapType(i, oldType.getStruct());
+      builder.setSubType(i, oldType);
     }
     builder.createRecGroup(0, num);
     auto result = builder.build();
