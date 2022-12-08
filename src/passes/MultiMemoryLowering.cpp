371f159ac8d3a80d0634163c643a2732e45bacd5
Ashley Nelson <nashley@google.com>
2022-12-08 01:09:12 +0000

    clang-format

--- src/passes/MultiMemoryLowering.cpp
+++ src/passes/MultiMemoryLowering.cpp
@@ -23,11 +23,11 @@
 // multi-memories feature also prevents later passes from adding additional
 // memories.
 //
-// The offset computation in function makeReplacement is not precise according to the spec. In the
-// spec offsets do not overflow as twos-complement, but i32.add does.
-// Concretely, a load from address 1000 with offset 0xffffffff should
-// actually trap, as the combined number is greater than 32 bits. But with
-// an add, 1000 + 0xffffffff = 999 due to overflow, which would not trap.
+// The offset computation in function makeReplacement is not precise according
+// to the spec. In the spec offsets do not overflow as twos-complement, but
+// i32.add does. Concretely, a load from address 1000 with offset 0xffffffff
+// should actually trap, as the combined number is greater than 32 bits. But
+// with an add, 1000 + 0xffffffff = 999 due to overflow, which would not trap.
 // In theory we could compute like the spec, by expanding the i32s
 // to i64s and adding there (where we won't overflow), but we don't have
 // i128s to handle i64 overflow.
