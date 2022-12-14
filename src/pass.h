4e88032d99462070bb18e638563face425b2c0f5
Alon Zakai <azakai@google.com>
2022-12-14 13:52:52 -0800

    fix

--- src/pass.h
+++ src/pass.h
@@ -491,7 +491,7 @@ public:
       // TODO: We should almost certainly be propagating pass options here, but
       // that is a widespread change, so make sure it doesn't unacceptably
       // regress compile times.
-      PassRunner runner(module /*, getPassOptions()*/);
+      PassRunner runner(module, getPassOptions());
       runner.setIsNested(true);
       runner.add(create());
       runner.run();
