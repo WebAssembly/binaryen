a22031c896f5367faf3f5ed5ddd01746032627e3
Alon Zakai <azakai@google.com>
2023-01-19 16:17:52 -0800

    Revert "clean"
    This reverts commit 0bc4c81a0725409f53de89d528d608d64efce0a1.

--- src/passes/RemoveUnusedModuleElements.cpp
+++ src/passes/RemoveUnusedModuleElements.cpp
@@ -38,8 +38,10 @@
 #include <memory>
 
 #include "ir/element-utils.h"
+#include "ir/find_all.h"
 #include "ir/intrinsics.h"
 #include "ir/module-utils.h"
+#include "ir/subtypes.h"
 #include "ir/utils.h"
 #include "pass.h"
 #include "wasm-builder.h"
