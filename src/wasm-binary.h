d649bee7bf1e5930b093d534d4b1477d98b51a06
chen ruixiang <ruixiang.chen@partner.bmw.com>
2023-02-24 11:41:41 +0800

    fix: source map test issue and refactor the code

--- src/wasm-binary.h
+++ src/wasm-binary.h
@@ -1430,7 +1430,12 @@ class WasmBinaryBuilder {
   MixedArena& allocator;
   const std::vector<char>& input;
   std::istream* sourceMap;
-  std::tuple<uint32_t, uint32_t, Function::DebugLocation> nextDebugLocation; // available pos, previous pos, next debug location
+  struct NextDebugLocation {
+    uint32_t availablePos;
+    uint32_t previousPos;
+    Function::DebugLocation next;
+  };
+  NextDebugLocation nextDebugLocation;
   bool debugInfo = true;
   bool DWARF = false;
   bool skipFunctionBodies = false;
