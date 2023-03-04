29f2a2cd0b8cacd8e311cfffbda48370aa8d2ffd
Alon Zakai <azakai@google.com>
2023-03-03 16:20:29 -0800

    work

--- src/tools/fuzzing/fuzzing.cpp
+++ src/tools/fuzzing/fuzzing.cpp
@@ -281,7 +281,7 @@ void TranslateToFuzzReader::setupHeapTypes() {
 
   // Filter away uninhabitable heap types, that is, heap types that we cannot
   // construct, like a type with a non-nullable reference to itself.
-  interestingHeapTypes = HeapTypeGenerator::findHabitable(possibleHeapTypes);
+  interestingHeapTypes = HeapTypeGenerator::findInhabitable(possibleHeapTypes);
 }
 
 // TODO(reference-types): allow the fuzzer to create multiple tables
