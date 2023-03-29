e28aaee6cf0931367f35921be4ace483885f156b
Thomas Lively <tlively@google.com>
2023-03-29 14:55:01 -0700

    update wasm2js tests

--- test/wasm2js/atomic_fence.2asm.js
+++ test/wasm2js/atomic_fence.2asm.js
@@ -1,14 +1,5 @@
 
 function asmFunc(imports) {
- var buffer = new ArrayBuffer(1507328);
- var HEAP8 = new Int8Array(buffer);
- var HEAP16 = new Int16Array(buffer);
- var HEAP32 = new Int32Array(buffer);
- var HEAPU8 = new Uint8Array(buffer);
- var HEAPU16 = new Uint16Array(buffer);
- var HEAPU32 = new Uint32Array(buffer);
- var HEAPF32 = new Float32Array(buffer);
- var HEAPF64 = new Float64Array(buffer);
  var Math_imul = Math.imul;
  var Math_fround = Math.fround;
  var Math_abs = Math.abs;
@@ -21,31 +12,6 @@ function asmFunc(imports) {
  var Math_sqrt = Math.sqrt;
  function $0() {
   
- }
- 
- function __wasm_memory_size() {
-  return buffer.byteLength / 65536 | 0;
- }
- 
- function __wasm_memory_grow(pagesToAdd) {
-  pagesToAdd = pagesToAdd | 0;
-  var oldPages = __wasm_memory_size() | 0;
-  var newPages = oldPages + pagesToAdd | 0;
-  if ((oldPages < newPages) && (newPages < 65536)) {
-   var newBuffer = new ArrayBuffer(Math_imul(newPages, 65536));
-   var newHEAP8 = new Int8Array(newBuffer);
-   newHEAP8.set(HEAP8);
-   HEAP8 = new Int8Array(newBuffer);
-   HEAP16 = new Int16Array(newBuffer);
-   HEAP32 = new Int32Array(newBuffer);
-   HEAPU8 = new Uint8Array(newBuffer);
-   HEAPU16 = new Uint16Array(newBuffer);
-   HEAPU32 = new Uint32Array(newBuffer);
-   HEAPF32 = new Float32Array(newBuffer);
-   HEAPF64 = new Float64Array(newBuffer);
-   buffer = newBuffer;
-  }
-  return oldPages;
  }
  
  return {
