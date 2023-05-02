c1ccafdde8785acc99946c1e636337815ac69166
Alon Zakai <azakai@google.com>
2023-05-02 11:20:50 -0700

    format

--- src/tools/wasm-merge.cpp
+++ src/tools/wasm-merge.cpp
@@ -391,7 +391,8 @@ int main(int argc, const char* argv[]) {
 
   const std::string WasmMergeOption = "wasm-merge options";
 
-  ToolOptions options("wasm-merge", R"(Merge wasm files into one.
+  ToolOptions options("wasm-merge",
+                      R"(Merge wasm files into one.
 
 For example,
 
@@ -405,12 +406,12 @@ Note that filenames and modules names are interleaved (which is hopefully less c
     .add("--output",
          "-o",
          "Output file (stdout if not specified)",
-         WasmMergeOption,
-         Options::Arguments::One,
-         [](Options* o, const std::string& argument) {
-           o->extra["output"] = argument;
-           Colors::setEnabled(false);
-         })
+                      WasmMergeOption,
+                      Options::Arguments::One,
+                      [](Options* o, const std::string& argument) {
+                        o->extra["output"] = argument;
+                        Colors::setEnabled(false);
+                      })
     .add_positional("INFILE1 NAME1 INFILE2 NAME2 [..]",
                     Options::Arguments::N,
                     [&](Options* o, const std::string& argument) {
