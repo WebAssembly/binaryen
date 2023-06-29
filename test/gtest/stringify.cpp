db6ea73569b19027aae8915ab74bd31b38120548
Ashley Nelson <nashley@google.com>
2023-06-29 19:10:11 +0000

    more tests

--- test/gtest/stringify.cpp
+++ test/gtest/stringify.cpp
@@ -12,6 +12,8 @@ TEST_F(StringifyTest, Print) {
   auto moduleText = R"wasm(
     (module
     (tag $catch_a (param i32))
+    (tag $catch_b (param i32))
+    (tag $catch_c (param i32))
     (func $d
       (block $block_a
         (drop (i32.const 20))
@@ -24,12 +26,30 @@ TEST_F(StringifyTest, Print) {
         ))
       )
       (block $block_c
+        (drop (if (i32.const 1)
+          (i32.const 30)
+        ))
+      )
+      (block $block_d
         (try $try_a
           (do
             (nop)
           )
           (catch $catch_a
             (drop (i32.const 8))
+          )
+          (catch $catch_b
+            (drop (i32.const 15))
+          )
+        )
+      )
+      (block $block_e
+        (try $try_b
+          (do
+            (nop)
+          )
+          (catch $catch_c
+            (drop (i32.const 33))
           )
         )
       )
@@ -42,6 +62,8 @@ adding unique symbol
 in visitExpression for block $block_a
 in visitExpression for block $block_b
 in visitExpression for block $block_c
+in visitExpression for block $block_d
+in visitExpression for block $block_e
 adding unique symbol
 in visitExpression for i32.const 20
 in visitExpression for drop
@@ -52,15 +74,31 @@ in visitExpression for i32.const 0
 in visitExpression for if
 in visitExpression for drop
 adding unique symbol
+in visitExpression for i32.const 1
+in visitExpression for if
+in visitExpression for drop
+adding unique symbol
 in visitExpression for try $try_a
+adding unique symbol
+in visitExpression for try $try_b
 adding unique symbol
 in visitExpression for i32.const 40
 adding unique symbol
 in visitExpression for i32.const 5
 adding unique symbol
+in visitExpression for i32.const 30
+adding unique symbol
 in visitExpression for nop
 adding unique symbol
 in visitExpression for i32.const 8
+in visitExpression for drop
+adding unique symbol
+in visitExpression for i32.const 15
+in visitExpression for drop
+adding unique symbol
+in visitExpression for nop
+adding unique symbol
+in visitExpression for i32.const 33
 in visitExpression for drop
 adding unique symbol
 )stringify";
