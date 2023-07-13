fecc0ab749c97011373c8967dc4eff240d19980a
Ashley Nelson <nashley@google.com>
2023-07-13 16:57:14 +0000

    moving comment

--- src/passes/HashStringifyWalker.cpp
+++ src/passes/HashStringifyWalker.cpp
@@ -2,12 +2,6 @@
 
 namespace wasm {
 
-// This custom hasher conforms to std::hash<Key>. Its purpose is to provide
-// a custom hash for if expressions, so the if-condition of the if expression is
-// not included in the hash for the if expression. This is needed because in the
-// binary format, the if-condition comes before and is consumed by the if. To
-// match the binary format, we hash the if condition before and separately from
-// the rest of the if expression.
 size_t StringifyHasher::operator()(Expression* curr) const {
   if (Properties::isControlFlowStructure(curr)) {
     if (auto* iff = curr->dynCast<If>()) {
@@ -25,9 +19,6 @@ size_t StringifyHasher::operator()(Expression* curr) const {
   return ExpressionAnalyzer::shallowHash(curr);
 }
 
-// This custom equator conforms to std::equal_to<Key>. Similar to
-// StringifyHasher, it's purpose is to not include the if-condition when
-// evaluating the equality of two if expressions.
 bool StringifyEquator::operator()(Expression* lhs, Expression* rhs) const {
   if (Properties::isControlFlowStructure(lhs) &&
       Properties::isControlFlowStructure(rhs)) {
--- src/passes/stringify-walker.h
+++ src/passes/stringify-walker.h
@@ -78,10 +78,19 @@ private:
 
 namespace wasm {
 
+// This custom hasher conforms to std::hash<Key>. Its purpose is to provide
+// a custom hash for if expressions, so the if-condition of the if expression is
+// not included in the hash for the if expression. This is needed because in the
+// binary format, the if-condition comes before and is consumed by the if. To
+// match the binary format, we hash the if condition before and separately from
+// the rest of the if expression.
 struct StringifyHasher {
   size_t operator()(Expression* curr) const;
 };
 
+// This custom equator conforms to std::equal_to<Key>. Similar to
+// StringifyHasher, it's purpose is to not include the if-condition when
+// evaluating the equality of two if expressions.
 struct StringifyEquator {
   bool operator()(Expression* lhs, Expression* rhs) const;
 };
