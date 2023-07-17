7f964c19acc342bc669aab1c966b6a6fabfcdf22
Ashley Nelson <nashley@google.com>
2023-07-17 20:48:49 +0000

    attribution

--- src/support/suffix_tree.h
+++ src/support/suffix_tree.h
@@ -1,20 +1,5 @@
-// TODO: figure out license and proper attribution
-
-/*
- * Copyright 2023 WebAssembly Community Group participants
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// This file began as an import from LLVM, and so it has the same license as
+// LLVM, copied below together with the code.
 
 //===- llvm/ADT/SuffixTree.h - Tree for substrings --------------*- C++ -*-===//
 //
