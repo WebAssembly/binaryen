1c01add464a7129b79bbf03519accb55a96a4d97
Alon Zakai <azakai@google.com>
2023-07-20 12:14:50 -0700

    two EXPECTs

--- test/gtest/printing.cpp
+++ test/gtest/printing.cpp
@@ -20,28 +20,25 @@ TEST_F(PrintingTest, Print) {
     )
   )wasm";
 
-  auto printText = R"print(
-(func $a (result i32)
+  Module wasm;
+  parseWast(wasm, moduleText);
+
+  {
+    std::stringstream ss;
+    ss << *wasm.getFunction("a");
+    EXPECT_EQ(ss.str(), R"print((func $a (result i32)
  (i32.const 10)
 )
-
-And the other function:
-
-(func $b
+)print");
+  }
+  {
+    std::stringstream ss;
+    ss << *wasm.getFunction("b");
+    EXPECT_EQ(ss.str(), R"print((func $b
  (drop
   (i32.const 20)
  )
 )
-)print";
-
-  Module wasm;
-  parseWast(wasm, moduleText);
-
-  std::stringstream ss;
-  ss << '\n';
-  ss << *wasm.getFunction("a") << '\n';
-  ss << "And the other function:\n\n";
-  ss << *wasm.getFunction("b");
-
-  EXPECT_EQ(ss.str(), printText);
+)print");
+  }
 }
