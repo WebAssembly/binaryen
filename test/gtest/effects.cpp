5471da5f168051132ea20c1d8eb874ad4ef264eb
Alon Zakai <azakai@google.com>
2023-09-28 15:31:53 -0700

    test

--- test/gtest/effects.cpp
+++ test/gtest/effects.cpp
@@ -39,10 +39,35 @@ TEST_F(EffectsTest, TestEquality) {
 
   EXPECT_EQ(a, a);
 
-  {
-    auto b = a;
-    EXPECT_EQ(b, a);
-    b.branchesOut = false;
-    EXPECT_NE(b, a);
+  // Test that a change to a field causes us to compare not equal.
+
+  #define TEST_CHANGE(FIELD, VALUE) { \
+    auto b = a; \
+    EXPECT_EQ(b, a); \
+    b.FIELD = VALUE; \
+    EXPECT_NE(b, a); \
   }
+
+  TEST_CHANGE(branchesOut, false);
+  TEST_CHANGE(calls, false);
+  TEST_CHANGE(readsMemory, false);
+  TEST_CHANGE(writesMemory, false);
+  TEST_CHANGE(readsTable, false);
+  TEST_CHANGE(writesTable, false);
+  TEST_CHANGE(readsMutableStruct, false);
+  TEST_CHANGE(writesStruct, false);
+  TEST_CHANGE(readsArray, false);
+  TEST_CHANGE(writesArray, false);
+  TEST_CHANGE(trap, false);
+  TEST_CHANGE(implicitTrap, false);
+  TEST_CHANGE(trapsNeverHappen, false);
+  TEST_CHANGE(isAtomic, false);
+  TEST_CHANGE(throws_, false);
+  TEST_CHANGE(danglingPop, false);
+  TEST_CHANGE(localsRead, {54321});
+  TEST_CHANGE(localsWritten, {12345});
+  TEST_CHANGE(mutableGlobalsRead, {"glob2"});
+  TEST_CHANGE(globalsWritten, {"other2"});
+  TEST_CHANGE(breakTargets, {"block2"});
+  TEST_CHANGE(delegateTargets, {"delegate2"});
 }
