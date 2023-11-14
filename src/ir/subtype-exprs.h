a5eb3218149c31656aafe29fbc20d003275c74cc
Alon Zakai <azakai@google.com>
2023-11-14 13:04:58 -0800

    fix

--- src/ir/subtype-exprs.h
+++ src/ir/subtype-exprs.h
@@ -340,16 +340,53 @@ struct SubtypingDiscoverer : public OverriddenVisitor<SubType> {
       self()->noteSubtype(value, array.element.type);
     }
   }
-  void visitArrayGet(ArrayGet* curr) {}
+
+  // Similar to visitStructReference above.
+  void visitArrayReference(Expression* ref) {
+    if (!ref->type.isArray()) {
+      // This is a bottom type or unreachable. Do not allow it to change.
+      self()->noteSubtype(ref, ref->type);
+      return;
+    }
+    auto curr = ref->type.getHeapType();
+    // We could in principle allow the reference type to generalize while
+    // allowing field generalization as well, but for now for simplicity keep it
+    // unchanged. TODO handle this with flexibility in the user.
+    auto element = curr.getArray().element;
+    while (true) {
+      auto next = curr.getDeclaredSuperType();
+      if (!next) {
+        // There is no super. Stop, as curr is the one we want.
+        break;
+      }
+      auto last = curr;
+      curr = *next;
+      if (curr.getArray().element != element) {
+        // The element changed. Stop, as |last| is the one we want.
+        curr = last;
+        break;
+      }
+    }
+    self()->noteSubtype(ref, Type(curr, Nullable));
+  }
+
+  void visitArrayGet(ArrayGet* curr) {
+    visitArrayReference(curr->ref);
+  }
   void visitArraySet(ArraySet* curr) {
+    visitArrayReference(curr->ref);
     if (!curr->ref->type.isArray()) {
       return;
     }
     auto array = curr->ref->type.getHeapType().getArray();
     self()->noteSubtype(curr->value->type, array.element.type);
   }
-  void visitArrayLen(ArrayLen* curr) {}
+  void visitArrayLen(ArrayLen* curr) {
+    visitArrayReference(curr->ref);
+  }
   void visitArrayCopy(ArrayCopy* curr) {
+    visitArrayReference(curr->srcRef);
+    visitArrayReference(curr->destRef);
     if (!curr->srcRef->type.isArray() || !curr->destRef->type.isArray()) {
       return;
     }
@@ -358,14 +395,18 @@ struct SubtypingDiscoverer : public OverriddenVisitor<SubType> {
     self()->noteSubtype(src.element.type, dest.element.type);
   }
   void visitArrayFill(ArrayFill* curr) {
+    visitArrayReference(curr->ref);
     if (!curr->ref->type.isArray()) {
       return;
     }
     auto array = curr->ref->type.getHeapType().getArray();
     self()->noteSubtype(curr->value->type, array.element.type);
   }
-  void visitArrayInitData(ArrayInitData* curr) {}
+  void visitArrayInitData(ArrayInitData* curr) {
+    visitArrayReference(curr->ref);
+  }
   void visitArrayInitElem(ArrayInitElem* curr) {
+    visitArrayReference(curr->ref);
     if (!curr->ref->type.isArray()) {
       return;
     }
