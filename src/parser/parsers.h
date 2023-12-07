7b628b6df92009198c4d79260101c21e571217ed
Thomas Lively <tlively@google.com>
2023-12-07 15:09:57 -0800

    [Parser] Parse rethrow
    Like `delegate`, rethrow takes a `Try` label. Refactor the delegate handling so
    that `Try` can share its logic.

--- src/parser/parsers.h
+++ src/parser/parsers.h
@@ -1480,7 +1480,9 @@ template<typename Ctx> Result<> makeThrow(Ctx& ctx, Index pos) {
 }
 
 template<typename Ctx> Result<> makeRethrow(Ctx& ctx, Index pos) {
-  return ctx.in.err("unimplemented instruction");
+  auto label = labelidx(ctx);
+  CHECK_ERR(label);
+  return ctx.makeRethrow(pos, *label);
 }
 
 template<typename Ctx> Result<> makeTupleMake(Ctx& ctx, Index pos) {
