898cb2d817af67bb2a32168e6f58a5a1f8cb1cdb
Eric Vergnaud <eric.vergnaud@wanadoo.fr>
2023-12-13 14:44:16 +0100

    Fix typos

--- src/ts/binaryen_wasm_ts.js
+++ src/ts/binaryen_wasm_ts.js
@@ -1,4 +1,4 @@
-/* the purpose of this file is simply to enable compilation of the binaryen-wrapper.ts file.
-Compilation of the binaryen_ts target creates the real binary_wasm_ts.js file in the build dir */
+/* the purpose of this file is simply to enable local compilation of the binaryen_ts.ts file.
+Compilation of the binaryen_wasm_ts target creates the real binary_wasm_ts.js file in the build dir */
 var Binaryen = {};
 export default Binaryen;
