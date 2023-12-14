a12d5d978637bb7bdc3863488b8f8193d173e59b
Ashley Nelson <nashley@google.com>
2023-12-14 00:21:24 +0000

    try_catch outlining
    Pull Request: https://github.com/WebAssembly/binaryen/pull/6180

--- src/passes/Outlining.cpp
+++ src/passes/Outlining.cpp
@@ -137,6 +137,20 @@ struct ReconstructStringifyWalker
     } else if (auto curr = reason.getLoopStart()) {
       ODBG(desc = "Loop Start at ");
       ASSERT_OK(existingBuilder.visitLoopStart(curr->loop));
+    } else if (auto curr = reason.getTryStart()) {
+      // We preserve the name of the tryy because IRBuilder expects
+      // visitTryStart() to be called on an empty Try, during the normal case of
+      // parsing.
+      auto name = curr->tryy->name;
+      ASSERT_OK(existingBuilder.visitTryStart(curr->tryy, Name()));
+      DBG(desc = "Try Start at ");
+      curr->tryy->name = name;
+    } else if (auto curr = reason.getCatchStart()) {
+      ASSERT_OK(existingBuilder.visitCatch(curr->tag));
+      DBG(desc = "Catch Start at ");
+    } else if (reason.getCatchAllStart()) {
+      ASSERT_OK(existingBuilder.visitCatchAll());
+      DBG(desc = "Catch All Start at");
     } else if (reason.getEnd()) {
       ODBG(desc = "End at ");
       ASSERT_OK(existingBuilder.visitEnd());
