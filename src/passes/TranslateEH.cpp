a707ba72825e1b249685534cf78537eb4fc90bdc
Heejin Ahn <aheejin@gmail.com>
2024-01-16 15:31:43 -0800

    Remove unnecessary `cast`

--- src/passes/TranslateEH.cpp
+++ src/passes/TranslateEH.cpp
@@ -366,10 +366,9 @@ struct TranslateEHOldToNew
         for (auto* throwRef : FindAll<ThrowRef>(catchBody).list) {
           // All throw_refs generated in this pass has a local.get as its child.
           // See visitRethrow().
-          if (auto* localGet = throwRef->exnref->cast<LocalGet>()) {
-            if (localGet->index == *local) {
-              return true;
-            }
+          auto* localGet = throwRef->exnref->cast<LocalGet>();
+          if (localGet->index == *local) {
+            return true;
           }
         }
       }
