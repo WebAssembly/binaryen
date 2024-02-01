b99314d1b7d8abf173878315416517df53f04275
Alon Zakai <azakai@google.com>
2024-02-01 08:49:35 -0800

    add test

---
+++ test/gtest/json.cpp
@@ -0,0 +1,16 @@
+#include "gtest/gtest.h"
+#include "support/json.h"
+
+using JSONTest = ::testing::Test;
+
+TEST_F(JSONTest, Stringify) {
+  // TODO: change the API to not require a copy
+  auto input = "[\"hello\",\"world\"]";
+  auto* copy = strdup(input);
+  json::Value value;
+  value.parse(copy);
+  std::stringstream ss;
+  value.stringify(ss);
+  EXPECT_EQ(ss.str(), input);
+  free(copy);
+}
