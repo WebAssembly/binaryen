62d5cb4e509efa543fdcc381d8f5fed02b162272
Alon Zakai <azakai@google.com>
2024-02-08 13:20:23 -0800

    clean

--- src/passes/StringLowering.cpp
+++ src/passes/StringLowering.cpp
@@ -25,9 +25,6 @@
 // imported globals of type externref, for use with the string imports proposal.
 // String operations will likewise need to be lowered. TODO
 //
-// Specs:
-// https://github.com/WebAssembly/stringref/blob/main/proposals/stringref/Overview.md
-// https://github.com/WebAssembly/js-string-builtins/blob/main/proposals/js-string-builtins/Overview.md
 
 #include <algorithm>
 
