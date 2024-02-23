268ec4dd267d602606ee90f4b7e72df3d6496d54
Alon Zakai <azakai@google.com>
2024-02-23 13:21:03 -0800

    work

--- src/wasm-interpreter.h
+++ src/wasm-interpreter.h
@@ -2696,7 +2696,8 @@ private:
   // stack traces.
   std::vector<Name> functionStack;
 
-  std::unordered_set<Name> droppedSegments;
+  std::unordered_set<Name> droppedDataSegments;
+  std::unordered_set<Name> droppedElementSegments;
 
   struct TableInterfaceInfo {
     // The external interface in which the table is defined.
@@ -2746,6 +2747,8 @@ private:
         Flow ret = self()->visit(segment->data[i]);
         extInterface->tableStore(tableName, offset + i, ret.getSingleValue());
       }
+
+      droppedElementSegments.insert(segment->name);
     });
   }
 
@@ -3630,7 +3633,7 @@ public:
     Address offsetVal(uint32_t(offset.getSingleValue().geti32()));
     Address sizeVal(uint32_t(size.getSingleValue().geti32()));
 
-    if (offsetVal + sizeVal > 0 && droppedSegments.count(curr->segment)) {
+    if (offsetVal + sizeVal > 0 && droppedDataSegments.count(curr->segment)) {
       trap("out of bounds segment access in memory.init");
     }
     if ((uint64_t)offsetVal + sizeVal > segment->data.size()) {
@@ -3652,7 +3655,7 @@ public:
   }
   Flow visitDataDrop(DataDrop* curr) {
     NOTE_ENTER("DataDrop");
-    droppedSegments.insert(curr->segment);
+    droppedDataSegments.insert(curr->segment);
     return {};
   }
   Flow visitMemoryCopy(MemoryCopy* curr) {
@@ -3768,7 +3771,7 @@ public:
     const auto& seg = *wasm.getDataSegment(curr->segment);
     auto elemBytes = element.getByteSize();
     auto end = offset + size * elemBytes;
-    if ((size != 0ull && droppedSegments.count(curr->segment)) ||
+    if ((size != 0ull && droppedDataSegments.count(curr->segment)) ||
         end > seg.data.size()) {
       trap("out of bounds segment access in array.new_data");
     }
@@ -3797,9 +3800,11 @@ public:
 
     const auto& seg = *wasm.getElementSegment(curr->segment);
     auto end = offset + size;
-    // TODO: Handle dropped element segments once we support those.
     if (end > seg.data.size()) {
-      trap("out of bounds segment access in array.new_elem");
+      trap("out of bounds segment access in array.init_elem");
+    }
+    if (end > 0 && droppedElementSegments.count(curr->segment)) {
+      trap("out of bounds segment access in array.init_elem");
     }
     contents.reserve(size);
     for (Index i = offset; i < end; ++i) {
@@ -3848,7 +3853,7 @@ public:
     if (offsetVal + readSize > seg->data.size()) {
       trap("out of bounds segment access in array.init_data");
     }
-    if (offsetVal + sizeVal > 0 && droppedSegments.count(curr->segment)) {
+    if (offsetVal + sizeVal > 0 && droppedDataSegments.count(curr->segment)) {
       trap("out of bounds segment access in array.init_data");
     }
     for (size_t i = 0; i < sizeVal; i++) {
--- test/lit/exec/array.wast
+++ test/lit/exec/array.wast
@@ -5,6 +5,18 @@
 (module
  (type $array (array (mut i8)))
 
+ (type $array-func (array funcref))
+
+ (memory $mem 10 10)
+
+ (data $data (i32.const 0) "a")
+
+ (table $table 10 10 funcref)
+
+ (elem $active (i32.const 0) $func)
+
+ (elem $passive $func)
+
  ;; CHECK:      [fuzz-exec] calling func
  ;; CHECK-NEXT: [fuzz-exec] note result: func => 1
  (func $func (export "func") (result i32)
@@ -14,7 +26,58 @@
    (return (i32.const 2))
   )
  )
+
+ ;; CHECK:      [fuzz-exec] calling new_active
+ ;; CHECK-NEXT: waka 1 : 1 : 1
+ ;; CHECK-NEXT: [trap out of bounds segment access in array.init_elem]
+ (func $new_active (export "new_active")
+  ;; Even though this is reading 0 items, offset 1 is out of bounds in that
+  ;; dropped element segment, and this traps.
+  (drop
+   (array.new_elem $array-func $active
+    (i32.const 1)
+    (i32.const 0)
+   )
+  )
+ )
+
+ ;; CHECK:      [fuzz-exec] calling new_active_in_bounds
+ ;; CHECK-NEXT: waka 0 : 1 : 1
+ (func $new_active_in_bounds (export "new_active_in_bounds")
+  ;; Even though this is dropped, we read 0 from offset 0, which is ok.
+  (drop
+   (array.new_elem $array-func $active
+    (i32.const 0)
+    (i32.const 0)
+   )
+  )
+ )
+
+ ;; CHECK:      [fuzz-exec] calling new_passive
+ ;; CHECK-NEXT: waka 1 : 1 : 0
+ (func $new_passive (export "new_passive")
+  ;; Using the passive segment here works.
+  (drop
+   (array.new_elem $array-func $passive
+    (i32.const 1)
+    (i32.const 0)
+   )
+  )
+ )
 )
 ;; CHECK:      [fuzz-exec] calling func
 ;; CHECK-NEXT: [fuzz-exec] note result: func => 1
+
+;; CHECK:      [fuzz-exec] calling new_active
+;; CHECK-NEXT: waka 1 : 1 : 1
+;; CHECK-NEXT: [trap out of bounds segment access in array.init_elem]
+
+;; CHECK:      [fuzz-exec] calling new_active_in_bounds
+;; CHECK-NEXT: waka 0 : 1 : 1
+
+;; CHECK:      [fuzz-exec] calling new_passive
+;; CHECK-NEXT: waka 1 : 1 : 0
 ;; CHECK-NEXT: [fuzz-exec] comparing func
+;; CHECK-NEXT: [fuzz-exec] comparing new_active
+;; CHECK-NEXT: [fuzz-exec] comparing new_active_in_bounds
+;; CHECK-NEXT: [fuzz-exec] comparing new_passive
