9632fe83d9de5689d506fc395070148cd0da22fb
Alon Zakai <azakai@google.com>
2024-03-20 17:18:31 -0700

    fix compiler warning

--- src/wasm-interpreter.h
+++ src/wasm-interpreter.h
@@ -2174,7 +2174,7 @@ public:
     auto& refValues = refData->values;
     auto startVal = start.getSingleValue().getUnsigned();
     auto endVal = end.getSingleValue().getUnsigned();
-    endVal = std::min(endVal, refValues.size());
+    endVal = std::min<Index>(endVal, refValues.size());
     if (hasNonAsciiUpTo(refValues, endVal)) {
       return Flow(NONCONSTANT_FLOW);
     }
