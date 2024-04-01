72e1808bba9eb963b5a5c940845609346c4ab05f
Alon Zakai <azakai@google.com>
2024-04-01 16:36:10 -0700

    work

--- test/binaryen-embind.js/basics.js
+++ test/binaryen-embind.js/basics.js
@@ -3,21 +3,28 @@ const argv = require('node:process');
 
 const binaryen_js_path = argv.argv[2];
 
+// Compare after normalizing text by removing extra indentation.
+function normEqual(x, y) {
+  function norm(a) {
+    a = a.replace(/^\n */g, '');
+    a = a.replace(/\n +/g, '\n');
+    return a;
+  }
+  assert.equal(norm(x), norm(y));
+}
+
 const binaryenFactory = require(binaryen_js_path);
 
 binaryenFactory().then((binaryen) => {
-  // Basic smoke test that something got exposed at all.
-  assert.ok(binaryen.Builder, 'the Builder class must be exposed');
-
   // Create a Module.
   var module = new binaryen.Module();
 
   // Check it prints out as empty.
-  assert.equal(binaryen.stringify(module),
-`(module
-)
-`
-  );
+  normEqual(binaryen.stringify(module),
+            `
+             (module
+             )
+            `);
 
   console.log('success.');
 });
