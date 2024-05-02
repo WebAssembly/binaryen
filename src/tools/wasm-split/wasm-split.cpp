be6a9dd3ab4e1a64fa0a708d148090897d9d6dc1
Alon Zakai <azakai@google.com>
2024-05-02 13:50:43 -0700

    fix

--- src/tools/wasm-split/wasm-split.cpp
+++ src/tools/wasm-split/wasm-split.cpp
@@ -37,7 +37,7 @@ using namespace wasm;
 
 namespace {
 
-void parseInput(Module& wasm, WasmSplitOptions& options) {
+void parseInput(Module& wasm, const WasmSplitOptions& options) {
   options.applyFeatures(wasm);
   ModuleReader reader;
   reader.setProfile(options.profile);
@@ -100,7 +100,7 @@ void writeModule(Module& wasm,
   writer.write(wasm, filename);
 }
 
-void instrumentModule(WasmSplitOptions& options) {
+void instrumentModule(const WasmSplitOptions& options) {
   Module wasm;
   parseInput(wasm, options);
 
@@ -207,7 +207,7 @@ void writePlaceholderMap(const std::map<size_t, Name> placeholderMap,
   }
 }
 
-void splitModule(WasmSplitOptions& options) {
+void splitModule(const WasmSplitOptions& options) {
   Module wasm;
   parseInput(wasm, options);
 
@@ -430,7 +430,7 @@ void checkExists(const std::string& path) {
   }
 }
 
-void printReadableProfile(WasmSplitOptions& options) {
+void printReadableProfile(const WasmSplitOptions& options) {
   const std::string wasmFile(options.inputFiles[0]);
   checkExists(options.profileFile);
   checkExists(wasmFile);
