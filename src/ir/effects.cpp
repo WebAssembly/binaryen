52a45e7a107cbf4cc2a1dcd479f4fcef6c2abbab
Alon Zakai <azakai@google.com>
2024-05-13 15:12:55 -0700

    format

--- src/ir/effects.cpp
+++ src/ir/effects.cpp
@@ -21,44 +21,120 @@ namespace std {
 
 std::ostream& operator<<(std::ostream& o, wasm::EffectAnalyzer& effects) {
   o << "EffectAnalyzer{\n";
-  if (effects.branchesOut) { o << "branchesOut\n"; }
-  if (effects.calls) { o << "calls\n"; }
-  if (effects.localsRead.size()) { o << "localsRead\n"; }
-  if (effects.localsWritten.size()) { o << "localsWritten\n"; }
-  if (effects.mutableGlobalsRead.size()) { o << "mutableGlobalsRead\n"; }
-  if (effects.globalsWritten.size()) { o << "globalsWritten\n"; }
-  if (effects.readsMemory) { o << "readsMemory\n"; }
-  if (effects.writesMemory) { o << "writesMemory\n"; }
-  if (effects.readsTable) { o << "readsTable\n"; }
-  if (effects.writesTable) { o << "writesTable\n"; }
-  if (effects.readsMutableStruct) { o << "readsMutableStruct\n"; }
-  if (effects.writesStruct) { o << "writesStruct\n"; }
-  if (effects.readsArray) { o << "readsArray\n"; }
-  if (effects.writesArray) { o << "writesArray\n"; }
-  if (effects.trap) { o << "trap\n"; }
-  if (effects.implicitTrap) { o << "implicitTrap\n"; }
-  if (effects.isAtomic) { o << "isAtomic\n"; }
-  if (effects.throws_) { o << "throws_\n"; }
-  if (effects.tryDepth) { o << "tryDepth\n"; }
-  if (effects.catchDepth) { o << "catchDepth\n"; }
-  if (effects.danglingPop) { o << "danglingPop\n"; }
-  if (effects.mayNotReturn) { o << "mayNotReturn\n"; }
-  if (effects.hasReturnCallThrow) { o << "hasReturnCallThrow\n"; }
-  if (effects.accessesLocal()) { o << "accessesLocal\n"; }
-  if (effects.accessesMutableGlobal()) { o << "accessesMutableGlobal\n"; }
-  if (effects.accessesMemory()) { o << "accessesMemory\n"; }
-  if (effects.accessesTable()) { o << "accessesTable\n"; }
-  if (effects.accessesMutableStruct()) { o << "accessesMutableStruct\n"; }
-  if (effects.accessesArray()) { o << "accessesArray\n"; }
-  if (effects.throws()) { o << "throws\n"; }
-  if (effects.transfersControlFlow()) { o << "transfersControlFlow\n"; }
-  if (effects.writesGlobalState()) { o << "writesGlobalState\n"; }
-  if (effects.readsMutableGlobalState()) { o << "readsMutableGlobalState\n"; }
-  if (effects.hasNonTrapSideEffects()) { o << "hasNonTrapSideEffects\n"; }
-  if (effects.hasSideEffects()) { o << "hasSideEffects\n"; }
-  if (effects.hasUnremovableSideEffects()) { o << "hasUnremovableSideEffects\n"; }
-  if (effects.hasAnything()) { o << "hasAnything\n"; }
-  if (effects.hasExternalBreakTargets()) { o << "hasExternalBreakTargets\n"; }
+  if (effects.branchesOut) {
+    o << "branchesOut\n";
+  }
+  if (effects.calls) {
+    o << "calls\n";
+  }
+  if (effects.localsRead.size()) {
+    o << "localsRead\n";
+  }
+  if (effects.localsWritten.size()) {
+    o << "localsWritten\n";
+  }
+  if (effects.mutableGlobalsRead.size()) {
+    o << "mutableGlobalsRead\n";
+  }
+  if (effects.globalsWritten.size()) {
+    o << "globalsWritten\n";
+  }
+  if (effects.readsMemory) {
+    o << "readsMemory\n";
+  }
+  if (effects.writesMemory) {
+    o << "writesMemory\n";
+  }
+  if (effects.readsTable) {
+    o << "readsTable\n";
+  }
+  if (effects.writesTable) {
+    o << "writesTable\n";
+  }
+  if (effects.readsMutableStruct) {
+    o << "readsMutableStruct\n";
+  }
+  if (effects.writesStruct) {
+    o << "writesStruct\n";
+  }
+  if (effects.readsArray) {
+    o << "readsArray\n";
+  }
+  if (effects.writesArray) {
+    o << "writesArray\n";
+  }
+  if (effects.trap) {
+    o << "trap\n";
+  }
+  if (effects.implicitTrap) {
+    o << "implicitTrap\n";
+  }
+  if (effects.isAtomic) {
+    o << "isAtomic\n";
+  }
+  if (effects.throws_) {
+    o << "throws_\n";
+  }
+  if (effects.tryDepth) {
+    o << "tryDepth\n";
+  }
+  if (effects.catchDepth) {
+    o << "catchDepth\n";
+  }
+  if (effects.danglingPop) {
+    o << "danglingPop\n";
+  }
+  if (effects.mayNotReturn) {
+    o << "mayNotReturn\n";
+  }
+  if (effects.hasReturnCallThrow) {
+    o << "hasReturnCallThrow\n";
+  }
+  if (effects.accessesLocal()) {
+    o << "accessesLocal\n";
+  }
+  if (effects.accessesMutableGlobal()) {
+    o << "accessesMutableGlobal\n";
+  }
+  if (effects.accessesMemory()) {
+    o << "accessesMemory\n";
+  }
+  if (effects.accessesTable()) {
+    o << "accessesTable\n";
+  }
+  if (effects.accessesMutableStruct()) {
+    o << "accessesMutableStruct\n";
+  }
+  if (effects.accessesArray()) {
+    o << "accessesArray\n";
+  }
+  if (effects.throws()) {
+    o << "throws\n";
+  }
+  if (effects.transfersControlFlow()) {
+    o << "transfersControlFlow\n";
+  }
+  if (effects.writesGlobalState()) {
+    o << "writesGlobalState\n";
+  }
+  if (effects.readsMutableGlobalState()) {
+    o << "readsMutableGlobalState\n";
+  }
+  if (effects.hasNonTrapSideEffects()) {
+    o << "hasNonTrapSideEffects\n";
+  }
+  if (effects.hasSideEffects()) {
+    o << "hasSideEffects\n";
+  }
+  if (effects.hasUnremovableSideEffects()) {
+    o << "hasUnremovableSideEffects\n";
+  }
+  if (effects.hasAnything()) {
+    o << "hasAnything\n";
+  }
+  if (effects.hasExternalBreakTargets()) {
+    o << "hasExternalBreakTargets\n";
+  }
   o << "}";
   return o;
 }
