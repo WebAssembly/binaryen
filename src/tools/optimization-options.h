994d8d31381dbc9dbaa89809c75b2c8d804e0d7f
Jérôme Vouillon <jerome.vouillon@gmail.com>
2024-07-17 11:30:07 -0400

    Make it possible to skip several passes (#6714)
    --skip-pass can now be specified more than once on the commandline.

--- src/tools/optimization-options.h
+++ src/tools/optimization-options.h
@@ -311,7 +311,7 @@ struct OptimizationOptions : public ToolOptions {
            "-sp",
            "Skip a pass (do not run it)",
            OptimizationOptionsCategory,
-           Options::Arguments::One,
+           Options::Arguments::N,
            [this](Options*, const std::string& pass) {
              passOptions.passesToSkip.insert(pass);
            });
