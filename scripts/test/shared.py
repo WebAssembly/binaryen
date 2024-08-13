59f4b953a63ca9040c142829b30e3567aad9364f
Alon Zakai <azakai@google.com>
2024-08-13 14:18:24 -0700

    simplify

--- scripts/test/shared.py
+++ scripts/test/shared.py
@@ -195,7 +195,7 @@ NATIVECC = (os.environ.get('CC') or which('mingw32-gcc') or
             which('gcc') or which('clang'))
 NATIVEXX = (os.environ.get('CXX') or which('mingw32-g++') or
             which('g++') or which('clang++'))
-NODEJS = os.getenv('NODE', which('node') or which('nodejs'))
+NODEJS = os.environ.get('NODE') or which('node') or which('nodejs')
 MOZJS = which('mozjs') or which('spidermonkey')
 
 V8 = os.environ.get('V8') or which('v8') or which('d8')
