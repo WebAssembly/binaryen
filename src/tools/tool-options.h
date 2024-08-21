db03632caea53f8ed45d82694cf632bf332c8c5e
Brendan Dahl <brendan.dahl@gmail.com>
2024-08-21 22:01:06 +0000

    [FP16] Add a feature flag for FP16.
    Ensure the "fp16" feature is enabled for FP16 instructions.

--- src/tools/tool-options.h
+++ src/tools/tool-options.h
@@ -95,6 +95,7 @@ struct ToolOptions : public Options {
       .addFeature(FeatureSet::MultiMemory, "multimemory")
       .addFeature(FeatureSet::TypedContinuations, "typed continuations")
       .addFeature(FeatureSet::SharedEverything, "shared-everything threads")
+      .addFeature(FeatureSet::FP16, "float 16 operations")
       .add("--enable-typed-function-references",
            "",
            "Deprecated compatibility flag",
