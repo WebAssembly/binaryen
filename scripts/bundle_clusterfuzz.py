6fb3e45decc90ab9adc827b4933b9ea52ce9abab
Alon Zakai <azakai@google.com>
2024-11-12 13:42:23 -0800

    work

--- scripts/bundle_clusterfuzz.py
+++ scripts/bundle_clusterfuzz.py
@@ -16,6 +16,7 @@ This assumes you build wasm-opt into the bin dir, and that it is a static build
 import os
 import sys
 import tarfile
+import time
 
 # Read the output filename first, as importing |shared| changes the directory.
 output_file = os.path.abspath(sys.argv[1])
@@ -32,8 +33,15 @@ with tarfile.open(output_file, "w:gz") as tar:
     tar.add(os.path.join(shared.options.binaryen_root, 'scripts', 'fuzz_shell.js'),
             arcname='scripts/fuzz_shell.js')
     # wasm-opt binary
-    tar.add(os.path.join(shared.options.binaryen_bin, 'wasm-opt'),
-            arcname='bin/wasm-opt')
+    wasm_opt = os.path.join(shared.options.binaryen_bin, 'wasm-opt')
+    tar.add(wasm_opt, arcname='bin/wasm-opt')
+
+    # Static builds, which we require, are much larger than dynamic ones. For
+    # lack of a better way to test, warn the build might not be static if it
+    # is too small. (Numbers on one machine: 1.6M dynamic, 23MB static.)
+    if os.path.getsize(wasm_opt) < 10 * 1024 * 1024:
+        print('WARNING: wasm-opt size seems small. Is it a static build?')
+        time.sleep(10)
 
 print('Done.')
 
