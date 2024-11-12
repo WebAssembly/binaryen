fe8b47a82ac62aab95e9e2e1956610032285ec35
Alon Zakai <azakai@google.com>
2024-11-12 13:00:03 -0800

    work

---
+++ scripts/clusterfuzz/bundle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+'''
+Bundle files for ClusterFuzz.
+
+Usage:
+
+bundle.py OUTPUT_FILE
+'''
+
+import os
+import sys
+
+output_file = sys.argv[1]
+print(f'Bundling to {output_file}.')
+
