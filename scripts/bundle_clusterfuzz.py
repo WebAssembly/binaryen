1b9750185c3c6abe041481605b6eb0616f4de916
Alon Zakai <azakai@google.com>
2024-11-12 13:27:21 -0800

    work

--- scripts/bundle_clusterfuzz.py
+++ scripts/bundle_clusterfuzz.py
@@ -9,21 +9,20 @@ bundle.py OUTPUT_FILE.tgz
 
 The output file will be a .tgz file.
 
-This assumes you build wasm-opt into the bin dir, and that it is a static build.
+This assumes you build wasm-opt into the bin dir, and that it is a static build
+(cmake -DBUILD_STATIC_LIB=1).
 '''
 
 import os
 import sys
 import tarfile
 
-from test import shared
-
-def in_binaryen(*args):
-    return os.path.join(shared.options.binaryen_root, *args)
-
-output_file = sys.argv[1]
+# Read the output filename first, as importing |shared| changes the directory.
+output_file = os.path.abspath(sys.argv[1])
 print(f'Bundling to: {output_file}')
 assert output_file.endswith('.tgz'), 'Can only generate a .tgz'
+
+from test import shared
 
 with tarfile.open(output_file, "w:gz") as tar:
     # run.py
