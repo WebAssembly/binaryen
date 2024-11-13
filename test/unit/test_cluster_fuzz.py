a1e82572c49eb2f41ba3a78a336deac078995dec
Alon Zakai <azakai@google.com>
2024-11-13 09:37:33 -0800

    test

--- test/unit/test_cluster_fuzz.py
+++ test/unit/test_cluster_fuzz.py
@@ -1,17 +1,32 @@
 import os
+import tarfile
+import tempfile
 
 from scripts.test import shared
 from . import utils
 
 
 class ClusterFuzz(utils.BinaryenTestCase):
-    def do_test_run_py(self, run_py_path):
-        # Test that run.py works as expected, when run from a particular place.
-        pass
+    # Bundle up our ClusterFuzz package, and unbundle it to a directory.
+    # Return that directory's name.
+    def bundle_and_unpack(self):
+        # Keep the temp dir alive as long as we are.
+        self.bundle_temp_dir = tempfile.TemporaryDirectory()
 
-    def test_run_py_in_tree(self):
-        assert 1 == 2
-        pass
+        print('Bundling')
+        bundle = os.path.join(self.bundle_temp_dir.name, 'bundle.tgz')
+        shared.run_process([shared.in_binaryen('scripts', 'bundle_clusterfuzz.py'), bundle])
+
+        print('Unpacking')
+        tar = tarfile.open(bundle, "r:gz")
+        tar.extractall(path=self.bundle_temp_dir.name)
+        tar.close()
+
+        return self.bundle_temp_dir.name
+
+    def test_bundle(self):
+        temp_dir = self.bundle_and_unpack()
+
 #        p = shared.run_process(shared.WASM_OPT + ['-o', os.devnull],
 #                               input=module, capture_output=True)
 #        self.assertIn('Some VMs may not accept this binary because it has a large number of parameters in function foo.',
