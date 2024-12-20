f91d80ca84288dd430020787fc3a2876b72f1315
Alon Zakai <azakai@google.com>
2024-12-20 10:29:04 -0800

    better

--- scripts/test/fuzzing.py
+++ scripts/test/fuzzing.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import os
+
 
 # Tests that the fuzzers should not operate on.
 unfuzzable = [
