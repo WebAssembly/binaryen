1c3876658a600ce2628a6e8eb5cf4e6d3da55322
Alon Zakai <azakai@google.com>
2025-01-09 14:10:18 -0800

    wasm2js fuzzing: Fix up call-* import names (#7203)
    call-*, call-export-* are needed for the same reasons as log-*.

--- scripts/fuzz_opt.py
+++ scripts/fuzz_opt.py
@@ -1089,6 +1089,8 @@ class Wasm2JS(TestCaseHandler):
             # wasm2js converts exports to valid JS forms, which affects some of
             # the names in the test suite. Fix those up.
             x = x.replace('log-', 'log_')
+            x = x.replace('call-', 'call_')
+            x = x.replace('export-', 'export_')
 
             # check if a number is 0 or a subnormal, which is basically zero
             def is_basically_zero(x):
