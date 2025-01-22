09300e47969923487faafed06861bf1a6b93684c
Thomas Lively <tlively@google.com>
2025-01-22 13:21:17 -0800

    [interpreter] Begin implementation of a new interpreter (#7227)
    The current interpreter used in wasm-shell, the fuzzer, and
    optimizations like precompute works by recursively walking the
    expression tree and computing expression results as it goes. This kind
    of recursive interpretation is not going to work for stack switching,
    since stack switching requires stashing context away and restoring it
    later. The recursive interpreter stores intermediate values on the
    native stack and returns early to implement control flow, so there is no
    way to suspend a computation and resume it later.
    
    To support stack switching and support other use future interpreter use
    cases such as running the full spec test suite and fuzzing multithreaded
    programs, introduce a new interpreter that is not recursive and does not
    store intermediate state that needs to persist beyond the execution of a
    single instruction on the native stack. The new interpreter works by
    iterating through instructions and visiting them one at a time in a
    loop. The visitor pushes and pops values from a stack and signals
    control flow via its return values. Control flow transfers are handled
    by the main interpreter loop, so expressions are only visited when they
    are actually executed. This design will not only support stack switching
    and other features better than the old interpreter, but it will also
    significantly decrease the amount of code in the interpreter.
    
    In addition to the core interpreter loop, also lay out a skeleton of the
    execution context for the new interpreter, including a call stack and
    store. The code contains several TODOs describing how these runtime
    structures will need to be extended to support interpreting the full
    spec test suite, including the ability to interpret over multiple linked
    instances at once.
    
    Most of the actual interpretation of expressions is left as future work,
    but the interpretation of `Const` expressions and i32.add is implemented
    and tested in a new gtest file to demonstrate it working end-to-end. One
    of the first milestones for the new interpreter will be getting real
    spec tests running with it, at which point the gtest file can be
    removed.

---
+++ test/gtest/interpreter.cpp
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2025 WebAssembly Community Group participants
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// TODO: Replace this test file with spec tests as soon as possible.
+
+#include "interpreter/interpreter.h"
+#include "literal.h"
+#include "wasm-ir-builder.h"
+#include "wasm.h"
+
+#include "gtest/gtest.h"
+
+using namespace wasm;
+
+TEST(InterpreterTest, Add) {
+  Module wasm;
+  IRBuilder builder(wasm);
+
+  ASSERT_FALSE(builder.makeConst(Literal(uint32_t(1))).getErr());
+  ASSERT_FALSE(builder.makeConst(Literal(uint32_t(2))).getErr());
+  ASSERT_FALSE(builder.makeBinary(AddInt32).getErr());
+
+  auto expr = builder.build();
+  ASSERT_FALSE(expr.getErr());
+
+  auto results = Interpreter{}.run(*expr);
+  std::vector<Literal> expected{Literal(uint32_t(3))};
+
+  EXPECT_EQ(results, expected);
+}
