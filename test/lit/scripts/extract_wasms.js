7502e7afdbda715d618f725856a8c1e0a491d557
Alon Zakai <azakai@google.com>
2025-01-29 16:27:35 -0800

    test

--- test/lit/scripts/extract_wasms.js
+++
@@ -1,9 +0,0 @@
-;; Test extracting wasm files from JS.
-
-foo(new Uint8Array([0x00, 0x61, 0x73, 0x6D, 0x01]));
-
-;; RUN: python %S/../../../scripts/clusterfuzz/extract_wasms.py %s %t
-;; RUN: cat %t.js | filecheck %s
-;;
-;; CHECK: foo(undefined /* extracted wasm */)
-
