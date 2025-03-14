425cb49cc45f62fe3b00bbb3d90eccee28c3bcfe
Thomas Lively <tlively@google.com>
2025-03-13 18:54:03 -0700

    oops

--- scripts/clang-format-diff.sh
+++ scripts/clang-format-diff.sh
@@ -15,8 +15,8 @@ LLVM_VERSION=${LLVM_VERSION:=17}
 
 MERGE_BASE=$(git merge-base $BRANCH HEAD)
 FORMAT_ARGS="--binary=clang-format-${LLVM_VERSION} ${MERGE_BASE}"
-if [ -n "$FORMAT_MSG" -a "$FORMAT_MSG" != "no modified files to format" ]
 FORMAT_MSG=$(git clang-format ${FORMAT_ARGS} -q --diff || true)
+if [ -n "$FORMAT_MSG" -a "$FORMAT_MSG" != "no modified files to format" ]
 then
   echo "Please run git clang-format with clang-format-${LLVM_VERSION} before committing, or apply this diff:"
   echo
