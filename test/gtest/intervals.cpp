fd7c6c85b7546265be681743db148276e47982d0
Ashley Nelson <nashley@google.com>
2025-03-15 00:29:12 +0000

    changing test

--- test/gtest/intervals.cpp
+++ test/gtest/intervals.cpp
@@ -8,7 +8,8 @@ TEST(IntervalsTest, TestNoOverlapFound) {
   std::vector<Interval> intervals;
   intervals.emplace_back(Interval{0, 4, 2});
   intervals.emplace_back(Interval{4, 8, 2});
-  ASSERT_EQ(IntervalProcessor::filterOverlaps(intervals).size(), 2u);
+  std::vector<int> expected{0, 1};
+  ASSERT_EQ(IntervalProcessor::filterOverlaps(intervals), expected);
 }
 
 TEST(IntervalsTest, TestOverlapFound) {
@@ -16,16 +17,19 @@ TEST(IntervalsTest, TestOverlapFound) {
   intervals.emplace_back(Interval{0, 2, 5});
   intervals.emplace_back(Interval{1, 4, 10});
   intervals.emplace_back(Interval{4, 5, 15});
-  ASSERT_EQ(IntervalProcessor::filterOverlaps(intervals).size(), 2u);
+  std::vector<int> expected{1, 2};
+  ASSERT_EQ(IntervalProcessor::filterOverlaps(intervals), expected);
 }
 
 TEST(IntervalsTest, TestEmpty) {
   std::vector<Interval> intervals;
-  ASSERT_EQ(IntervalProcessor::filterOverlaps(intervals).size(), 0u);
+  std::vector<int> expected;
+  ASSERT_EQ(IntervalProcessor::filterOverlaps(intervals), expected);
 }
 
 TEST(IntervalsTest, TestOne) {
   std::vector<Interval> intervals;
   intervals.emplace_back(Interval{0, 2, 5});
-  ASSERT_EQ(IntervalProcessor::filterOverlaps(intervals).size(), 1u);
+  std::vector<int> expected{0};
+  ASSERT_EQ(IntervalProcessor::filterOverlaps(intervals), expected);
 }
