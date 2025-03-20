b9ce258b41a453ca5b4eaa9ed9e7b5a83cc495e9
Alon Zakai <azakai@google.com>
2025-03-20 13:13:41 -0700

    work

--- src/passes/StringLifting.cpp
+++ src/passes/StringLifting.cpp
@@ -41,6 +41,17 @@ struct StringLifting : public Pass {
   // Maps the global name of an imported string to the actual string.
   std::unordered_map<Name, Name> importedStrings;
 
+  // Imported string functions. Imports that do not exist remain null.
+  Name fromCharCodeArrayImport;
+  Name intoCharCodeArrayImport;
+  Name fromCodePointImport;
+  Name concatImport;
+  Name equalsImport;
+  Name compareImport;
+  Name lengthImport;
+  Name charCodeAtImport;
+  Name substringImport;
+
   void run(Module* module) override {
     // Imported string constants look like
     //
@@ -48,13 +59,50 @@ struct StringLifting : public Pass {
     //
     // That is, they are imported from module "'" and the basename is the
     // actual string. Find them all so we can apply them.
+    bool found = false;
     for (auto& global : module->globals) {
-      if (global->imported() && global->module == WasmStringConstsModule) {
+      if (!global->imported()) {
+        continue;
+      }
+      if (global->module == WasmStringConstsModule) {
         importedStrings[global->name] = global->base;
+        found = true;
+      }
+      if (global->module != WasmStringsModule) {
+        continue;
+      }
+      if (global->base == "fromCharCodeArray") {
+        fromCharCodeArrayImport = global->name;
+        found = true;
+      } else if (global->base == "fromCodePoint") {
+        fromCodePointImport = global->name;
+        found = true;
+      } else if (global->base == "concat") {
+        concatImport = global->name;
+        found = true;
+      } else if (global->base == "intoCharCodeArray") {
+        intoCharCodeArrayImport = global->name;
+        found = true;
+      } else if (global->base == "equals") {
+        equalsImport = global->name;
+        found = true;
+      } else if (global->base == "compare") {
+        compareImport = global->name;
+        found = true;
+      } else if (global->base == "length") {
+        lengthImport = global->name;
+        found = true;
+      } else if (global->base == "charCodeAt") {
+        charCodeAtImport = global->name;
+        found = true;
+      } else if (global->base == "substring") {
+        substringImport = global->name;
+        found = true;
       }
     }
 
-    if (importedStrings.empty()) { // TODO: remove when we have operations too
+    if (!found) {
+      // Nothing to do.
       return;
     }
 
@@ -88,6 +136,20 @@ struct StringLifting : public Pass {
         }
       }
 
+      void visitCall(Call* curr) {
+        if (curr->target == fromCharCodeArrayImport) {
+          Builder(*getModule()).makeStringNew(StringNewWTF16Array, curr->operands[0], curr->operands[1], curr->operands[2]);
+        } else if (curr->target == fromCodePointImport) {
+        } else if (curr->target == concatImport) {
+        } else if (curr->target == intoCharCodeArrayImport) {
+        } else if (curr->target == equalsImport) {
+        } else if (curr->target == compareImport) {
+        } else if (curr->target == lengthImport) {
+        } else if (curr->target == charCodeAtImport) {
+        } else if (curr->target == substringImport) {
+        }
+      }
+
       void visitFunction(Function* curr) {
         // If we made modifications then we need to refinalize, as we replace
         // externrefs with stringrefs, a subtype.
@@ -102,150 +164,6 @@ struct StringLifting : public Pass {
     applier.walkModuleCode(module);
   }
 };
-
-/*
-  // Imported string functions.
-  Name fromCharCodeArrayImport;
-  Name intoCharCodeArrayImport;
-  Name fromCodePointImport;
-  Name concatImport;
-  Name equalsImport;
-  Name compareImport;
-  Name lengthImport;
-  Name charCodeAtImport;
-  Name substringImport;
-
-  // Creates an imported string function, returning its name (which is equal to
-  // the true name of the import, if there is no conflict).
-  Name addImport(Module* module, Name trueName, Type params, Type results) {
-    auto name = Names::getValidFunctionName(*module, trueName);
-    auto sig = Signature(params, results);
-    Builder builder(*module);
-    auto* func = module->addFunction(builder.makeFunction(name, sig, {}));
-    func->module = WasmStringsModule;
-    func->base = trueName;
-    return name;
-  }
-
-  void replaceInstructions(Module* module) {
-    // Add all the possible imports up front, to avoid adding them during
-    // parallel work. Optimizations can remove unneeded ones later.
-
-    // string.fromCharCodeArray: array, start, end -> ext
-    fromCharCodeArrayImport = addImport(
-      module, "fromCharCodeArray", {nullArray16, Type::i32, Type::i32}, nnExt);
-    // string.fromCodePoint: codepoint -> ext
-    fromCodePointImport = addImport(module, "fromCodePoint", Type::i32, nnExt);
-    // string.concat: string, string -> string
-    concatImport = addImport(module, "concat", {nullExt, nullExt}, nnExt);
-    // string.intoCharCodeArray: string, array, start -> num written
-    intoCharCodeArrayImport = addImport(module,
-                                        "intoCharCodeArray",
-                                        {nullExt, nullArray16, Type::i32},
-                                        Type::i32);
-    // string.equals: string, string -> i32
-    equalsImport = addImport(module, "equals", {nullExt, nullExt}, Type::i32);
-    // string.compare: string, string -> i32
-    compareImport = addImport(module, "compare", {nullExt, nullExt}, Type::i32);
-    // string.length: string -> i32
-    lengthImport = addImport(module, "length", nullExt, Type::i32);
-    // string.codePointAt: string, offset -> i32
-    charCodeAtImport =
-      addImport(module, "charCodeAt", {nullExt, Type::i32}, Type::i32);
-    // string.substring: string, start, end -> string
-    substringImport =
-      addImport(module, "substring", {nullExt, Type::i32, Type::i32}, nnExt);
-
-    // Replace the string instructions in parallel.
-    struct Replacer : public WalkerPass<PostWalker<Replacer>> {
-      bool isFunctionParallel() override { return true; }
-
-      StringLowering& lowering;
-
-      std::unique_ptr<Pass> create() override {
-        return std::make_unique<Replacer>(lowering);
-      }
-
-      Replacer(StringLowering& lowering) : lowering(lowering) {}
-
-      void visitStringNew(StringNew* curr) {
-        Builder builder(*getModule());
-        switch (curr->op) {
-          case StringNewWTF16Array:
-            replaceCurrent(builder.makeCall(lowering.fromCharCodeArrayImport,
-                                            {curr->ref, curr->start, curr->end},
-                                            lowering.nnExt));
-            return;
-          case StringNewFromCodePoint:
-            replaceCurrent(builder.makeCall(
-              lowering.fromCodePointImport, {curr->ref}, lowering.nnExt));
-            return;
-          default:
-            WASM_UNREACHABLE("TODO: all of string.new*");
-        }
-      }
-
-      void visitStringConcat(StringConcat* curr) {
-        Builder builder(*getModule());
-        replaceCurrent(builder.makeCall(
-          lowering.concatImport, {curr->left, curr->right}, lowering.nnExt));
-      }
-
-      void visitStringEncode(StringEncode* curr) {
-        Builder builder(*getModule());
-        switch (curr->op) {
-          case StringEncodeWTF16Array:
-            replaceCurrent(
-              builder.makeCall(lowering.intoCharCodeArrayImport,
-                               {curr->str, curr->array, curr->start},
-                               Type::i32));
-            return;
-          default:
-            WASM_UNREACHABLE("TODO: all of string.encode*");
-        }
-      }
-
-      void visitStringEq(StringEq* curr) {
-        Builder builder(*getModule());
-        switch (curr->op) {
-          case StringEqEqual:
-            replaceCurrent(builder.makeCall(
-              lowering.equalsImport, {curr->left, curr->right}, Type::i32));
-            return;
-          case StringEqCompare:
-            replaceCurrent(builder.makeCall(
-              lowering.compareImport, {curr->left, curr->right}, Type::i32));
-            return;
-          default:
-            WASM_UNREACHABLE("invalid string.eq*");
-        }
-      }
-
-      void visitStringMeasure(StringMeasure* curr) {
-        Builder builder(*getModule());
-        replaceCurrent(
-          builder.makeCall(lowering.lengthImport, {curr->ref}, Type::i32));
-      }
-
-      void visitStringWTF16Get(StringWTF16Get* curr) {
-        Builder builder(*getModule());
-        replaceCurrent(builder.makeCall(
-          lowering.charCodeAtImport, {curr->ref, curr->pos}, Type::i32));
-      }
-
-      void visitStringSliceWTF(StringSliceWTF* curr) {
-        Builder builder(*getModule());
-        replaceCurrent(builder.makeCall(lowering.substringImport,
-                                        {curr->ref, curr->start, curr->end},
-                                        lowering.nnExt));
-      }
-    };
-
-    Replacer replacer(*this);
-    replacer.run(getPassRunner(), module);
-    replacer.walkModuleCode(module);
-  }
-*/
 
 Pass* createStringLiftingPass() { return new StringLifting(); }
 
