fcb5ac7f2e9bcef09c6267652c68a575395f3555
GulgDev <programm.jeremiah@yandex.ru>
2025-04-05 23:14:44 +0500

    Add TypeBuilder API

--- test/binaryen.js/gc.js
+++ test/binaryen.js/gc.js
@@ -1,5 +1,18 @@
 var module = new binaryen.Module();
 
-// TODO
+var builder = new binaryen.TypeBuilder(1);
+builder.setArrayType(0, binaryen.i32);
+var [arrayHeapType] = builder.buildAndDispose();
+
+var arrayType = binaryen.getTypeFromHeapType(arrayHeapType);
+
+module.addGlobal("global-array",
+    arrayType,
+    true,
+    module.array.new_default(
+        binaryen.getHeapType(arrayType),
+        module.i32.const(4)
+    )
+);
 
 console.log(module.emitText());
