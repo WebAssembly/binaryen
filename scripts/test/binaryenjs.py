fac54ec6599b2b240ea46ccf3071cf450f0ed0be
Alon Zakai <azakai@google.com>
2025-04-10 11:52:38 -0700

    filter

--- scripts/test/binaryenjs.py
+++ scripts/test/binaryenjs.py
@@ -29,7 +29,7 @@ def do_test_binaryen_js_with(which):
 
     print('\n[ checking binaryen.js testcases (' + which + ')... ]\n')
 
-    for s in sorted(os.listdir(os.path.join(shared.options.binaryen_test, 'binaryen.js'))):
+    for s in shared.get_tests(shared.get_test_dir('binaryen.js'), ['.js']):
         if not s.endswith('.js'):
             continue
         print(s)
