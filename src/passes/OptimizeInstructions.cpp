1723a46de0833d1683878d1caaaeb61ca8b178c0
Ruiyang Xu <xury5520@outlook.com>
2025-04-11 10:16:51 +0800

    generalize existing rule

--- src/passes/OptimizeInstructions.cpp
+++ src/passes/OptimizeInstructions.cpp
@@ -2922,18 +2922,17 @@ private:
             return binary;
           }
         }
-      } else if (binary->op == LtUInt32) {
-        auto value = Properties::getFallthrough(
-          binary->right, getPassOptions(), *getModule());
-        if (Properties::isConstantExpression(value)) {
-          if (Properties::getLiteral(value).isZero()) {
-            // Hoist potential zero to be good for constant propagation
-            // in case of being blocked by side effect.
-            // (unsigned)x < 0   ==>  i32(0)
-            Builder builder(*getModule());
-            return getDroppedChildrenAndAppend(
-              binary, LiteralUtils::makeZero(Type::i32, *getModule()));
-          }
+      }
+      if (auto* block = binary->right->dynCast<Block>()) {
+        auto fallthrough =
+          Properties::getFallthrough(block, getPassOptions(), *getModule());
+        if (auto* c = fallthrough->dynCast<Const>()) {
+          Builder builder(*getModule());
+          auto ret = getDroppedChildrenAndAppend(
+            binary,
+            LiteralUtils::makeFromInt32(
+              c->value.geti32(), Type::i32, *getModule()));
+          return ret;
         }
       }
       if (auto* ext = Properties::getSignExtValue(binary)) {
