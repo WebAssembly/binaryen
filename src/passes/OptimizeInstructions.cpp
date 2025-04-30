9658b7dc33a4a9e1b14e4672937b02c990e73355
Ruiyang Xu <xury5520@outlook.com>
2025-04-30 13:49:14 +0800

    Format

--- src/passes/OptimizeInstructions.cpp
+++ src/passes/OptimizeInstructions.cpp
@@ -2893,13 +2893,8 @@ private:
         }
         if (unary->op == EqZInt32 || unary->op == EqZInt64) {
           if (auto* c = getFallthrough(unary->value)->dynCast<Const>()) {
-            if (c->value.isZero()) {
-              return getDroppedChildrenAndAppend(unary,
-                                                 Literal::makeOne(Type::i32));
-            } else {
-              return getDroppedChildrenAndAppend(unary,
-                                                 Literal::makeZero(Type::i32));
-            }
+            return getDroppedChildrenAndAppend(
+              unary, Literal::makeFromInt32(c->value.isZero(), Type::i32));
           }
         }
       }
