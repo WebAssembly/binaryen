c47fceed4ef4b3ac4e9c04d373158e64aeb5054a
Alon Zakai <azakai@google.com>
2025-05-02 14:37:49 -0700

    work

--- src/wasm/wasm-binary.cpp
+++ src/wasm/wasm-binary.cpp
@@ -1503,9 +1503,11 @@ void WasmBinaryWriter::writeDebugLocation(Expression* curr, Function* func) {
     }
   }
   // If this is an instruction in a function, and if the original wasm had
-  // binary locations tracked, then track it in the output as well.
-// here XXX!
-  if (func && !func->expressionLocations.empty()) {
+  // binary locations tracked, then track it in the output as well. We also
+  // track locations of instructions that have code annotations, as their binary
+  // location goes in the custom section.
+  if (func &&
+      (!func->expressionLocations.empty() || func->codeAnnotations.count(curr))) {
     binaryLocations.expressions[curr] =
       BinaryLocations::Span{BinaryLocation(o.size()), 0};
     binaryLocationTrackedExpressionsForFunc.push_back(curr);
