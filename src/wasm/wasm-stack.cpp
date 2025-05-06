f400bffa09431ed93332998a65e9ce08cc113e88
Alon Zakai <azakai@google.com>
2025-05-06 09:29:25 -0700

    oops

--- src/wasm/wasm-stack.cpp
+++ src/wasm/wasm-stack.cpp
@@ -3212,12 +3212,10 @@ void StackIRToBinaryWriter::write() {
       case StackInst::IfBegin:
       case StackInst::LoopBegin:
       case StackInst::TryTableBegin: {
-        parent.trackExpressionStart(inst->origin, func);
         if (sourceMap) {
           parent.writeSourceMapLocation(inst->origin, func);
         }
         writer.visit(inst->origin);
-        parent.trackExpressionEnd(inst->origin, func);
         break;
       }
       case StackInst::TryEnd:
