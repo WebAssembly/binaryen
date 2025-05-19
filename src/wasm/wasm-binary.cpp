27f08ea4cd2c5055e003f7f16e44497c012df5a9
Alon Zakai <azakai@google.com>
2025-05-19 13:25:44 -0700

    fix another

--- src/wasm/wasm-binary.cpp
+++ src/wasm/wasm-binary.cpp
@@ -947,7 +947,7 @@ void WasmBinaryWriter::writeNames() {
       auto substart =
         startSubsection(BinaryConsts::CustomSections::Subsection::NameLocal);
       o << U32LEB(functionsWithLocalNames.size());
-      Index emitted = 0;
+      [[maybe_unused]] Index emitted = 0;
       for (auto& [index, func] : functionsWithLocalNames) {
         // Pairs of (local index in IR, name).
         std::vector<std::pair<Index, Name>> localsWithNames;
