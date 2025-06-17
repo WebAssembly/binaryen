7845be50ade640c417d675649bf3b88e05ec9d93
Thomas Lively <tlively@google.com>
2025-06-16 18:06:05 -0700

    more comments in header

--- src/ir/principal-type.h
+++ src/ir/principal-type.h
@@ -24,17 +24,24 @@
 
 namespace wasm {
 
-// The syntax of types needs to be extended with type variables for nullability,
-// exactness, sharedness, heap types, and types, as well as bottom heap type and
-// sharedness.
+// Due to various kinds of polymorphism, describing principal types requires
+// extensions to the normal syntax of types. These extensions include the
+// introduction of type variables, which we represent as indices.
+
+// A sharedness that is a subtype of both `shared` and `unshared`.
 struct BottomShare : std::monostate {};
 
+// A heap type that is a subtype of all other heap types, no matter what
+// hierarchy they belong to.
 struct BottomHeapType : std::monostate {};
 
+// Either a nullability or a nullability type variable.
 using VarNullability = std::variant<Nullability, Index>;
 
+// Bottom sharedness, a normal sharedness, or a sharedness type variable.
 using VarSharedness = std::variant<BottomShare, Shareability, Index>;
 
+// An abstract heap type with some given sharedness.
 struct VarAbsHeapType {
   VarSharedness share;
   HeapType ht;
@@ -47,8 +54,10 @@ struct VarAbsHeapType {
   }
 };
 
+// Either an exactness or an exactness type variable.
 using VarExactness = std::variant<Exactness, Index>;
 
+// A defined heap type with a given exactness.
 struct VarDefHeapType {
   VarExactness exact;
   HeapType ht;
@@ -61,9 +70,12 @@ struct VarDefHeapType {
   }
 };
 
+// A bottom heap type, an abstract heap type, a defined heap type, or a heap
+// type variable.
 using VarHeapType =
   std::variant<BottomHeapType, VarAbsHeapType, VarDefHeapType, Index>;
 
+// A reference comprising a given nullability and heap type.
 struct VarRef {
   VarNullability null;
   VarHeapType ht;
@@ -74,8 +86,19 @@ struct VarRef {
   bool operator!=(const VarRef& other) const { return !(*this == other); }
 };
 
+// A normal type, a reference type using the extended syntax, or a type
+// variable. If a reference type can be expressed in the normal syntax rather
+// than the extended syntax, it should be represented here as a `Type`, not a
+// `VarRef`.
 using VarType = std::variant<Type, VarRef, Index>;
 
+// A principal type, including extended input and output types for an
+// instruction sequence and whether the sequence is stack-polymorphic, i.e.
+// contains an instruction that unconditionally transfers control flow out of
+// the sequence, i.e. is unreachable. The parameters are stored in reverse order
+// for efficiency. Variable indices for each kind of variable must be introduced
+// contiguously in order starting at zero from the end to beginning of the
+// params (beginning to end of the reversed params).
 struct PrincipalType {
   std::vector<VarType> rparams;
   std::vector<VarType> results;
