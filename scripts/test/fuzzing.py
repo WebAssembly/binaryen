b89d65ac994e48ebcfe4d86e7f0d0af0ce8a5b06
Thomas Lively <tlively@google.com>
2025-06-28 03:03:02 +0200

    [Custom Descriptors] Optimize struct.new (#7691)
    Optimize struct.new instructions that are known to trap due to having
    null descriptors. Also optimize out non-null casts of descriptors, since
    struct.new will implicitly perform that cast.

--- scripts/test/fuzzing.py
+++ scripts/test/fuzzing.py
@@ -126,6 +126,7 @@ unfuzzable = [
     'precompute-desc.wast',
     'gc-desc.wast',
     'simplify-locals-desc.wast',
+    'optimize-instructions-desc.wast',
     # TODO: fix split_wast() on tricky escaping situations like a string ending
     #       in \\" (the " is not escaped - there is an escaped \ before it)
     'string-lifting-section.wast',
