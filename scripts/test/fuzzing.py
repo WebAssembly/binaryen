6b27dea061d9fe3c25c81f0b513ad73b388a49c8
Thomas Lively <tlively@google.com>
2025-07-08 12:07:43 -0700

    [Custom Descriptors] Do not optimize in TypeSSA
    Optimizing allocations of descriptor or described types in TypeSSA would
    be complicated. Creating a subtype of a described type would necesitate
    creating a subtype of its descriptor type as well, possibly in an
    arbitrarily long chain. For now, simply do not optimize these types.
    
    Add tests to exercise interesting corner cases that would come up if we
    did optimize, though. These tests also show that we successfully avoid
    crashing when optimizing modules with descriptor and described types.

--- scripts/test/fuzzing.py
+++ scripts/test/fuzzing.py
@@ -128,6 +128,7 @@ unfuzzable = [
     'simplify-locals-desc.wast',
     'optimize-instructions-desc.wast',
     'gto-desc.wast',
+    'type-ssa-desc.wast',
     # TODO: fix split_wast() on tricky escaping situations like a string ending
     #       in \\" (the " is not escaped - there is an escaped \ before it)
     'string-lifting-section.wast',
