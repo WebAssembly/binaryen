025ed9f8caf05464d828665859d7e2e93be183a4
Alon Zakai <azakai@google.com>
2025-07-11 13:58:34 -0700

    [Branch Hinting] Fix Inlining's flipping of ifs, and add hint copying (#7714)
    Split inlining will flip conditions in the copied code.
    
    To do this, add support for copying code annotations when we
    copy instructions (this is the first PR that can test that). This is done
    in a new metadata.h helper, that subsumes part of the previous
    debuginfo.h, that is, the new metadata::copyBetweenFunctions
    will copy all metadata, both debug info and code annotations.

---
+++ src/ir/metadata.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2019 WebAssembly Community Group participants
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef wasm_ir_metadata_h
+#define wasm_ir_metadata_h
+
+#include "wasm.h"
+
+namespace wasm::metadata {
+
+// Given an expression and a copy of it in another function, copy the all
+// metadata - debug info, code annotations - into the second function.
+void copyBetweenFunctions(Expression* origin,
+                          Expression* copy,
+                          Function* originFunc,
+                          Function* copyFunc);
+
+} // namespace wasm::metadata
+
+#endif // wasm_ir_metadata_h
