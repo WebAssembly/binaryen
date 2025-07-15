7dbf36700f7c51136980d567158960a135eae5e4
Alon Zakai <azakai@google.com>
2025-07-15 11:57:12 -0700

    work

--- src/passes/RemoveUnusedModuleElements.cpp
+++ src/passes/RemoveUnusedModuleElements.cpp
@@ -737,13 +737,6 @@ struct RemoveUnusedModuleElements : public Pass {
         }
       });
 
-    // For now, all functions that can be called indirectly are marked as roots.
-    // TODO: Compute this based on which ElementSegments are actually used,
-    //       and which functions have a call_indirect of the proper type.
-    ElementUtils::iterAllElementFunctionNames(module, [&](Name name) {
-      roots.emplace_back(ModuleElementKind::Function, name);
-    });
-
     // Just as out-of-bound segments may cause observable traps at instantiation
     // time, so can struct.new instructions with null descriptors cause traps in
     // global or element segment initializers.
