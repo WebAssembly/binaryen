a5afb91597be4656bc83bb4db6badba0ca6e1c5f
Derek Mauro <dmauro@google.com>
2025-07-21 15:49:26 -0400

    Fix undefined behavior in construction of wasm::NameType
    wasm::Name::Name(nullptr) ends up calling std::string_view(nullptr),
    which is undefined and triggers an assertion in some standard libraries.
    https://en.cppreference.com/w/cpp/string/basic_string_view/basic_string_view.html

--- src/wasm-builder.h
+++ src/wasm-builder.h
@@ -29,7 +29,7 @@ namespace wasm {
 struct NameType {
   Name name;
   Type type;
-  NameType() : name(nullptr), type(Type::none) {}
+  NameType() : name(), type(Type::none) {}
   NameType(Name name, Type type) : name(name), type(type) {}
 };
 
