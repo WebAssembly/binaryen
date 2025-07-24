53c3657ff663da865ae342d10a4779b8ee433b36
Alon Zakai <azakai@google.com>
2025-07-24 09:11:30 -0700

    work

--- a.py
+++ a.py
@@ -6,12 +6,18 @@ if diff:
   print('cannot run since diff exists')
   sys.exit(1)
 
+new = []
+
 for line in open('src/wasm-interpreter.h').readlines():
-  print(line)
   # look for
   #           Flow flow = visit(expression);
   # or
   #           Flow flow = self()->visit(expression);
   if ' = visit(' in line or ' = self()->visit(' in line:
-      Flow flow = self()->visit(expression);
+    print(line.rstrip())
+    parts = line.split(' ')
+    eq_index = parts.index('=')
 
+  new.append(line)
+
+open('src/wasm-interpreter.h', 'w').write(''.join(new))
