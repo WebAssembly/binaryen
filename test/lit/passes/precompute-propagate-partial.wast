WebAssembly/binaryen

commit 5ca16fe8af3b173aba5d125267ac3ff77cc36736

[NFC] Avoid repeated work in Precompute of GC allocations (#7763)
When we precompute struct.new, we have a cache of allocations, as we
need the ID of allocations to remain the same. The logic for managing that
cache was actually first creating a new instance, then throwing it away if
we used the cached value... so we ended up visiting the children and
computing them etc., with exponential (wasted) work in the worst case.

To fix that, just check the cache first.

Fixes #7760

src/passes/Precompute.cpp
@@ -126,11 +126,7 @@ public:
 
   // TODO: Use immutability for values
   Flow visitStructNew(StructNew* curr) {
-    auto flow = Super::visitStructNew(curr);
-    if (flow.breaking()) {
-      return flow;
-    }
-    return getHeapCreationFlow(flow, curr);
+    return getGCAllocation(curr, [&]() { return Super::visitStructNew(curr); });
   }
   Flow visitStructSet(StructSet* curr) { return Flow(NONCONSTANT_FLOW); }
   Flow visitStructGet(StructGet* curr) {
@@ -167,18 +163,11 @@ public:
     return Super::visitStructGet(curr);
   }
   Flow visitArrayNew(ArrayNew* curr) {
-    auto flow = Super::visitArrayNew(curr);
-    if (flow.breaking()) {
-      return flow;
-    }
-    return getHeapCreationFlow(flow, curr);
+    return getGCAllocation(curr, [&]() { return Super::visitArrayNew(curr); });
   }
   Flow visitArrayNewFixed(ArrayNewFixed* curr) {
-    auto flow = Super::visitArrayNewFixed(curr);
-    if (flow.breaking()) {
-      return flow;
-    }
-    return getHeapCreationFlow(flow, curr);
+    return getGCAllocation(curr,
+                           [&]() { return Super::visitArrayNewFixed(curr); });
   }
   Flow visitArraySet(ArraySet* curr) { return Flow(NONCONSTANT_FLOW); }
   Flow visitArrayGet(ArrayGet* curr) {
@@ -197,18 +186,22 @@ public:
   Flow visitArrayCopy(ArrayCopy* curr) { return Flow(NONCONSTANT_FLOW); }
 
   // Generates heap info for a heap-allocating expression.
-  template<typename T> Flow getHeapCreationFlow(Flow flow, T* curr) {
+  Flow getGCAllocation(Expression* curr, std::function<Flow()> visitFunc) {
     // We must return a literal that refers to the canonical location for this
-    // source expression, so that each time we compute a specific struct.new
+    // source expression, so that each time we compute a specific *.new then
     // we get the same identity.
-    std::shared_ptr<GCData>& canonical = heapValues[curr];
-    std::shared_ptr<GCData> newGCData = flow.getSingleValue().getGCData();
-    if (!canonical) {
-      canonical = std::make_shared<GCData>(*newGCData);
-    } else {
-      *canonical = *newGCData;
+    auto iter = heapValues.find(curr);
+    if (iter != heapValues.end()) {
+      // Refer to the same canonical GCData that we already created.
+      return Literal(iter->second, curr->type.getHeapType());
     }
-    return Literal(canonical, curr->type.getHeapType());
+    // Only call the visitor function here, so we do it once per allocation.
+    auto flow = visitFunc();
+    if (flow.breaking()) {
+      return flow;
+    }
+    heapValues[curr] = flow.getSingleValue().getGCData();
+    return flow;
   }
 
   Flow visitStringNew(StringNew* curr) {

test/lit/ctor-eval/slow.wast
@@ -1,0 +1,54 @@
+;; NOTE: Assertions have been generated by update_lit_checks.py --all-items and should not be edited.
+;; RUN: wasm-ctor-eval %s --ctors=test --quiet -all -S -o - | filecheck %s
+
+;; We will execute the arrays here when we try to precompute them. Each such
+;; computation should be done once, so that we preserve identity (which other
+;; tests handle). We should handle that without repeated work: once we compute a
+;; thing, we can reuse that computation. If we do not, this test will still
+;; pass, but it will take many minutes to run (exponential time), hopefully
+;; timing out the bots. With the optimization, it takes only ms.
+
+(module
+  (type $array (sub (shared (array (ref null $array)))))
+
+  ;; CHECK:      (type $0 (func (param f32)))
+
+  ;; CHECK:      (type $1 (func))
+
+  ;; CHECK:      (import "" "" (func $imported (type $0) (param f32)))
+  (import "" "" (func $imported (param f32)))
+
+  (func $test (export "test")
+    (local $local (ref $array))
+    (local.set $local
+      (array.new $array
+        (array.new $array
+          (array.new $array
+            (array.new $array
+              (array.new $array
+                (array.new_default $array
+                  (i32.const 0)
+                )
+                (i32.const 100)
+              )
+              (i32.const 100)
+            )
+            (i32.const 100)
+          )
+          (i32.const 100)
+        )
+        (i32.const 100)
+      )
+    )
+    (call $imported
+      (f32.const 0)
+    )
+  )
+)
+;; CHECK:      (export "test" (func $test_2))
+
+;; CHECK:      (func $test_2 (type $1)
+;; CHECK-NEXT:  (call $imported
+;; CHECK-NEXT:   (f32.const 0)
+;; CHECK-NEXT:  )
+;; CHECK-NEXT: )

commit 97e0ecf77695c1af24db7f0dda7d856681beac53

Precompute: Avoid caching heap values of partial precomputations (#7766)
Fixes a regression from #7763

This is pretty subtle, unfortunately. We cache the results of a struct.new
etc as we go, so that identity is preserved for precomputing RefEq etc.
The old code always computed structs in full before checking the cache.
#7763 removed that code as an optimization. But, it turns out,
that was load-bearing in that it, by pure luck, happened to avoid a bug.

The bug that was avoided, and which this PR fixes, is that when we
partially precompute, we modify a struct.new and see if we can
precompute that version. We were still using the same heap value
cache, so we could cache the value, then later end up using it for
the original (before, we'd still compute the struct.new again and
use those values). To fix that, do such speculative precomputations on a
side (throwaway) cache.

Fixes #7765

src/passes/Precompute.cpp
@@ -672,10 +672,15 @@ struct Precompute
         auto** pointerToSelect =
           getChildPointerInImmediateParent(stack, selectIndex, func);
         *pointerToSelect = select->ifTrue;
-        auto ifTrue = precomputeExpression(parent);
+        // When we perform these speculative precomputations, we must not use
+        // the normal heapValues, as we are testing modified versions of
+        // |parent|. Results here must not be cached for later.
+        HeapValues temp;
+        auto ifTrue = precomputeExpression(parent, true, &temp);
+        temp.clear();
         if (isValidPrecomputation(ifTrue)) {
           *pointerToSelect = select->ifFalse;
-          auto ifFalse = precomputeExpression(parent);
+          auto ifFalse = precomputeExpression(parent, true, &temp);
           if (isValidPrecomputation(ifFalse)) {
             // Wonderful, we can precompute here! The select can now contain the
             // computed values in its arms.
@@ -714,12 +719,19 @@ struct Precompute
 
 private:
   // Precompute an expression, returning a flow, which may be a constant
-  // (that we can replace the expression with if replaceExpression is set).
-  Flow precomputeExpression(Expression* curr, bool replaceExpression = true) {
+  // (that we can replace the expression with if replaceExpression is set). When
+  // |usedHeapValues| is provided, we use those values instead of the normal
+  // |heapValues| (that is, we do not use the normal heap value cache).
+  Flow precomputeExpression(Expression* curr,
+                            bool replaceExpression = true,
+                            HeapValues* usedHeapValues = nullptr) {
+    if (!usedHeapValues) {
+      usedHeapValues = &heapValues;
+    }
     Flow flow;
     try {
       flow = PrecomputingExpressionRunner(
-               getModule(), getValues, heapValues, replaceExpression)
+               getModule(), getValues, *usedHeapValues, replaceExpression)
                .visit(curr);
     } catch (NonconstantException&) {
       return Flow(NONCONSTANT_FLOW);

test/lit/passes/precompute-propagate-partial.wast
@@ -1,0 +1,55 @@
+;; NOTE: Assertions have been generated by update_lit_checks.py and should not be edited.
+
+;; RUN: foreach %s %t wasm-opt --precompute-propagate --optimize-level=2 -all -S -o - | filecheck %s
+
+(module
+  ;; CHECK:      (type $empty (struct))
+  (type $empty (struct))
+
+  ;; CHECK:      (type $i32_any (sub (struct (field i32) (field (ref any)))))
+  (type $i32_any (sub (struct (field i32) (field (ref any)))))
+
+  ;; CHECK:      (func $partial-gc-cache (type $1) (result i32)
+  ;; CHECK-NEXT:  (local $x i32)
+  ;; CHECK-NEXT:  (drop
+  ;; CHECK-NEXT:   (struct.new $i32_any
+  ;; CHECK-NEXT:    (i32.const 0)
+  ;; CHECK-NEXT:    (select (result (ref (exact $empty)))
+  ;; CHECK-NEXT:     (struct.new_default $empty)
+  ;; CHECK-NEXT:     (struct.new_default $empty)
+  ;; CHECK-NEXT:     (i32.trunc_f64_s
+  ;; CHECK-NEXT:      (f64.const 562949953421311)
+  ;; CHECK-NEXT:     )
+  ;; CHECK-NEXT:    )
+  ;; CHECK-NEXT:   )
+  ;; CHECK-NEXT:  )
+  ;; CHECK-NEXT:  (i32.const 0)
+  ;; CHECK-NEXT: )
+  (func $partial-gc-cache (result i32)
+    (local $x i32)
+    ;; The select here looks promising for partial computation. We will therefore
+    ;; try to precompute variations of the outer struct.new. While doing so we
+    ;; should not confuse later computations, and in particular, not store
+    ;; anything in the heap value cache - the risk is that these partial
+    ;; precomputations will not have the i32.trunc which traps, so they will
+    ;; execute without trapping, and if we cached that result for the outer
+    ;; struct.new, we'd later think that results applies here (if we did, we'd
+    ;; think this does not trap, and precompute it into a nop, removing the
+    ;; trap erroneously).
+    (drop
+      (struct.new $i32_any
+        (i32.const 0)
+        (select (result (ref $empty))
+          (struct.new $empty)
+          (struct.new $empty)
+          (i32.trunc_f64_s
+            (f64.const 562949953421311)
+          )
+        )
+      )
+    )
+    ;; This local causes propagation to work, which causes the extra
+    ;; optimizations that lead to the bug above.
+    (local.get $x)
+  )
+)