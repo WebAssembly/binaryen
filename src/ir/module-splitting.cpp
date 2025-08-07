6a738f85cbc42bc9e95090b70aaf282f52fb44a9
Heejin Ahn <aheejin@gmail.com>
2025-08-07 22:26:39 +0000

    Rename variable

--- src/ir/module-splitting.cpp
+++ src/ir/module-splitting.cpp
@@ -625,9 +625,9 @@ void ModuleSplitter::indirectReferencesToSecondaryFunctions() {
     }
     auto* call = builder.makeCall(name, args, oldFunc->getResults());
 
-    auto func = builder.makeFunction(newName, oldFunc->type, {}, call);
-    func->hasExplicitName = oldFunc->hasExplicitName;
-    primary.addFunction(std::move(func));
+    auto trampoline = builder.makeFunction(newName, oldFunc->type, {}, call);
+    trampoline ->hasExplicitName = oldFunc->hasExplicitName;
+    primary.addFunction(std::move(trampoline));
 
     // Update RefFuncs to refer to it.
     for (auto* refFunc : relevantRefFuncs) {
