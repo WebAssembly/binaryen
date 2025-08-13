605620ed1cf5f2818e1e06efe6b3a50acc61d3e8
Alon Zakai <azakai@google.com>
2025-08-13 13:47:23 -0700

    fix

--- src/tools/fuzzing/fuzzing.cpp
+++ src/tools/fuzzing/fuzzing.cpp
@@ -3461,7 +3461,7 @@ Expression* TranslateToFuzzReader::makeBasicRef(Type type) {
         return builder.makeRefNull(HeapTypes::cont.getBasic(share));
       }
       // Emit the simplest possible continuation.
-      auto funcType = Type(Signature(Type::none, Type::none), NonNullable, Exact);
+      auto funcType = Type(Signature(Type::none, Type::none), NonNullable);
       return builder.makeContNew(heapType, makeRefFuncConst(funcType));
     }
     case HeapType::any: {
