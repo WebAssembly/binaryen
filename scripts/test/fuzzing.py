ea621727f2227a908772727a9890337c9fd76b35
Alon Zakai <azakai@google.com>
2025-08-15 11:34:23 -0700

    [Stack Switching] Support Continuations in TypeMerging (#7831)

--- scripts/test/fuzzing.py
+++ scripts/test/fuzzing.py
@@ -118,6 +118,7 @@ unfuzzable = [
     'cont_many_unhandled.wast',
     'cont_export.wast',
     'cont_export_throw.wast',
+    'type-merging-cont.wast',
     # TODO: fix split_wast() on tricky escaping situations like a string ending
     #       in \\" (the " is not escaped - there is an escaped \ before it)
     'string-lifting-section.wast',
