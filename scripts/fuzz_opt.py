05a296aacafc1099a58e9a1a1d7cf4e03fbc5f7f
Alon Zakai <azakai@google.com>
2025-10-06 09:55:24 -0700

    back

--- scripts/fuzz_opt.py
+++ scripts/fuzz_opt.py
@@ -2135,7 +2135,19 @@ class BranchHintPreservation(TestCaseHandler):
 
 # The global list of all test case handlers
 testcase_handlers = [
+    FuzzExec(),
+    CompareVMs(),
+    CheckDeterminism(),
+    Wasm2JS(),
+    TrapsNeverHappen(),
+    CtorEval(),
+    Merge(),
+    Split(),
+    RoundtripText(),
+    ClusterFuzz(),
     Two(),
+    PreserveImportsExports(),
+    BranchHintPreservation(),
 ]
 
 
