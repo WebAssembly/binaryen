f4e39fd9c2f06925207eafc172cc3a18c5b7712a
Alon Zakai <azakai@google.com>
2025-10-08 14:16:35 -0700

    make getImportedTag pure virtual

--- src/wasm-interpreter.h
+++ src/wasm-interpreter.h
@@ -2932,9 +2932,7 @@ public:
     virtual void throwException(const WasmException& exn) = 0;
     // Get the Tag instance for a tag implemented in the host, that is, not
     // among the linked ModuleRunner instances, but imported from the host.
-    virtual Tag* getImportedTag(Tag* tag) {
-      WASM_UNREACHABLE("missing imported tag");
-    }
+    virtual Tag* getImportedTag(Tag* tag) = 0;
 
     // the default impls for load and store switch on the sizes. you can either
     // customize load/store, or the sub-functions which they call
