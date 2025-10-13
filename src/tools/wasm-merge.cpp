adf8b31324264afc51fb7a225172df3bc70fdb98
Alon Zakai <azakai@google.com>
2025-10-13 16:11:43 -0700

    work

--- src/tools/wasm-merge.cpp
+++ src/tools/wasm-merge.cpp
@@ -572,8 +572,6 @@ void updateTypes(Module& wasm) {
       curr->finalize(getModule()->getFunction(curr->func)->type);
     }
 
-    // TODO globals, fuzzer should find first
-
     void visitFunction(Function* curr) {
       ReFinalize().walkFunctionInModule(curr, getModule());
     }
