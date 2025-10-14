b3b76ec77e92db8317fefe8d9634792f99dbf12f
Alon Zakai <azakai@google.com>
2025-10-14 16:01:19 -0700

    reduce

--- src/tools/wasm-reduce/wasm-reduce.cpp
+++ src/tools/wasm-reduce/wasm-reduce.cpp
@@ -1033,8 +1033,8 @@ struct Reducer
           }
           // Try to replace the body with the child, fixing up the function
           // to accept it.
-          func->type = Signature(funcType.getHeapType().getSignature().params,
-                                 child->type);
+          func->type = Type(Signature(funcType.getHeapType().getSignature().params,
+                                 child->type), NonNullable, Exact);
           func->body = child;
           if (writeAndTestReduction()) {
             // great, we succeeded!
