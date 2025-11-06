d5ee2393c00490aaad68507413abb69b2c5a7bb5
Alon Zakai <azakai@google.com>
2025-11-06 13:52:15 -0800

    fix

--- src/passes/LogExecution.cpp
+++ src/passes/LogExecution.cpp
@@ -69,7 +69,7 @@ struct LogExecution : public WalkerPass<PostWalker<LogExecution>> {
   void visitModule(Module* curr) {
     // Add the import
     auto import =
-      Builder::makeFunction(LOGGER, Signature(Type::i32, Type::none), {});
+      Builder::makeFunction(LOGGER, Type(Signature(Type::i32, Type::none), NonNullable, Inexact), {});
 
     if (loggerModule != "") {
       import->module = loggerModule;
