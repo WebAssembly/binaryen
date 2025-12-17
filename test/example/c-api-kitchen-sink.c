ad9efd9001cc44c0441fdb7091c3161ab86e0a42
Spotandjake <spotandjake@hotmail.com>
2025-12-17 11:41:07 -0500

    chore: Update `tests` and `CHANGELOG`

--- test/example/c-api-kitchen-sink.c
+++ test/example/c-api-kitchen-sink.c
@@ -1046,7 +1046,7 @@ void test_core() {
                                4,
                                iIfF,
                                BinaryenTypeInt32()),
-    BinaryenCallRef(module, funcrefExpr, callOperands4b, 4, BinaryenTypeNone()),
+    BinaryenReturnCallRef(module, funcrefExpr, callOperands4b, 4, BinaryenTypeNone()),
     // Reference types
     BinaryenRefIsNull(module, externrefExpr),
     BinaryenRefIsNull(module, funcrefExpr),
