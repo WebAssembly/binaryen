d3dd900aa285be37b7581128a1386c123e937476
Thomas Lively <tlively@google.com>
2026-01-05 18:48:38 -0800

    Remove assertion about repeat types in TypeSSA
    TypeSSA previously asserted that it never saw repeated type shapes when
    collecting the type shapes of the various types in the module. This is
    usually true, but it turns out that DAE can produce repeated type shapes
    that violated this assertion. Since it would be complicated to fix DAE
    to stop producing duplicate type shapes and the duplicate type shapes it
    produces are actually benign, simply remove the assertion in TypeSSA.

--- src/passes/TypeSSA.cpp
+++ src/passes/TypeSSA.cpp
@@ -73,8 +73,7 @@ std::vector<HeapType> ensureTypesAreInNewRecGroup(std::vector<HeapType>&& types,
   UniqueRecGroups unique(wasm.features);
   for (auto group : existing) {
     std::vector<HeapType> types(group.begin(), group.end());
-    [[maybe_unused]] auto uniqueTypes = unique.insert(std::move(types));
-    assert(uniqueTypes.size() == group.size() && "unexpected collision");
+    unique.insertOrGet(std::move(types));
   }
 
   auto num = types.size();
