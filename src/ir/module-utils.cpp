64f781abae4be40df9a130ff86f1e8a3489e5b1f
Alon Zakai <azakai@google.com>
2026-01-14 16:00:06 -0800

    nfc

--- src/ir/module-utils.cpp
+++ src/ir/module-utils.cpp
@@ -393,10 +393,8 @@ struct TypeInfos {
 
 struct CodeScanner : PostWalker<CodeScanner> {
   TypeInfos& info;
-  TypeInclusion inclusion;
 
-  CodeScanner(Module& wasm, TypeInfos& info, TypeInclusion inclusion)
-    : info(info), inclusion(inclusion) {
+  CodeScanner(Module& wasm, TypeInfos& info) : info(info) {
     setModule(&wasm);
   }
 
@@ -476,7 +474,7 @@ InsertOrderedMap<HeapType, HeapTypeInfo> collectHeapTypeInfo(
   Module& wasm, TypeInclusion inclusion, VisibilityHandling visibility) {
   // Collect module-level info.
   TypeInfos info;
-  CodeScanner(wasm, info, inclusion).walkModuleCode(&wasm);
+  CodeScanner(wasm, info).walkModuleCode(&wasm);
   for (auto& curr : wasm.globals) {
     info.note(curr->type);
   }
@@ -501,7 +499,7 @@ InsertOrderedMap<HeapType, HeapTypeInfo> collectHeapTypeInfo(
       // printing an error message on a partially parsed module whose declared
       // function bodies have not all been parsed yet.
       if (func->body) {
-        CodeScanner(wasm, info, inclusion).walk(func->body);
+        CodeScanner(wasm, info).walk(func->body);
       }
     });
 
