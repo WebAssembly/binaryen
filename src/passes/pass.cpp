WebAssembly/binaryen

commit 6c6aae1358d657fe16fcfdc3a0eccfcea66cc841

Function-level pass-debug mode 2 validation (#4897)
In BINARYEN_PASS_DEBUG=2 we save the module before each pass, and if
validation fails afterwards, we print the module before. This PR does the same for
function-parallel passes - in that case, we can actually show the specific function
that broke validation, as opposed to the whole module.

src/passes/pass.cpp
@@ -891,17 +891,47 @@ void PassRunner::runPass(Pass* pass) {
 
 void PassRunner::runPassOnFunction(Pass* pass, Function* func) {
   assert(pass->isFunctionParallel());
+
+  auto passDebug = getPassDebug();
+
+  // Add extra validation logic in pass-debug mode 2. The main logic in
+  // PassRunner::run will work at the module level, and here for a function-
+  // parallel pass we can do the same at the function level: we can print the
+  // function before the pass, run the pass on the function, and then if it
+  // fails to validate we can show an error and print the state right before the
+  // pass broke it.
+  //
+  // Skip nameless passes for this. Anything without a name is an internal
+  // component of some larger pass, and information about it won't be very
+  // useful - leave it to the entire module to fail validation in that case.
+  bool extraFunctionValidation =
+    passDebug == 2 && options.validate && !pass->name.empty();
+  std::stringstream bodyBefore;
+  if (extraFunctionValidation) {
+    bodyBefore << *func->body << '\n';
+  }
+
   // function-parallel passes get a new instance per function
   auto instance = std::unique_ptr<Pass>(pass->create());
   std::unique_ptr<AfterEffectFunctionChecker> checker;
-  if (getPassDebug()) {
+  if (passDebug) {
     checker = std::unique_ptr<AfterEffectFunctionChecker>(
       new AfterEffectFunctionChecker(func));
   }
   instance->runOnFunction(this, wasm, func);
   handleAfterEffects(pass, func);
-  if (getPassDebug()) {
+  if (passDebug) {
     checker->check();
+  }
+
+  if (extraFunctionValidation) {
+    if (!WasmValidator().validate(func, *wasm, WasmValidator::Minimal)) {
+      Fatal() << "Last nested function-parallel pass (" << pass->name
+              << ") broke validation of function " << func->name
+              << ". Here is the function body before:\n"
+              << bodyBefore.str() << "\n\nAnd here it is now:\n"
+              << *func->body << '\n';
+    }
   }
 }
 

src/wasm-validator.h
@@ -56,7 +56,11 @@ struct WasmValidator {
   };
   typedef uint32_t Flags;
 
+  // Validate an entire module.
   bool validate(Module& module, Flags flags = Globally);
+
+  // Validate a specific function.
+  bool validate(Function* func, Module& module, Flags flags = Globally);
 };
 
 } // namespace wasm

src/wasm/wasm-validator.cpp
@@ -223,6 +223,9 @@ struct FunctionValidator : public WalkerPass<PostWalker<FunctionValidator>> {
   // Validate a specific expression.
   void validate(Expression* curr) { walk(curr); }
 
+  // Validate a function.
+  void validate(Function* func) { walkFunction(func); }
+
   std::unordered_map<Name, std::unordered_set<Type>> breakTypes;
   std::unordered_set<Name> delegateTargetNames;
   std::unordered_set<Name> rethrowTargetNames;
@@ -3210,6 +3213,20 @@ bool WasmValidator::validate(Module& module, Flags flags) {
     for (auto& func : module.functions) {
       std::cerr << info.getStream(func.get()).str();
     }
+    std::cerr << info.getStream(nullptr).str();
+  }
+  return info.valid.load();
+}
+
+bool WasmValidator::validate(Function* func, Module& module, Flags flags) {
+  ValidationInfo info(module);
+  info.validateWeb = (flags & Web) != 0;
+  info.validateGlobally = (flags & Globally) != 0;
+  info.quiet = (flags & Quiet) != 0;
+  FunctionValidator(module, &info).validate(func);
+  // print all the data
+  if (!info.valid.load() && !info.quiet) {
+    std::cerr << info.getStream(func).str();
     std::cerr << info.getStream(nullptr).str();
   }
   return info.valid.load();

commit 3bfe5e4568cf38bf1f46014a90b6f0b7034b5f56

Fix a +200% Windows performance regression caused by PR #4897. On Windows with Visual Studio compiler, the constructor of a std::stringstream object is extremely slow, as it incurs a call to a std::locale ctor(), which in turn causes an access to some kind of process global locale mutex lock. (maybe to get the current system locale?). Work around this issue on the very hot function PassRunner::runPassOnFunction() that increased wasm-opt link times from ~20 seconds to ~60 seconds on Windows when wasm-opt -O2 optimizing a large 33MB .wasm file. std::string ctor does not share this performance problem on Windows.

src/passes/pass.cpp
@@ -867,9 +867,11 @@ void PassRunner::run() {
     for (auto& pass : passes) {
       // ignoring the time, save a printout of the module before, in case this
       // pass breaks it, so we can print the before and after
-      std::stringstream moduleBefore;
+      std::string moduleBefore;
       if (passDebug == 2 && !isNested) {
-        moduleBefore << *wasm << '\n';
+        std::stringstream ss;
+        ss << *wasm << '\n';
+        moduleBefore = ss.str();
       }
       // prepare to run
       std::cerr << "[PassRunner]   running pass: " << pass->name << "... ";
@@ -896,7 +898,7 @@ void PassRunner::run() {
           if (passDebug >= 2) {
             Fatal() << "Last pass (" << pass->name
                     << ") broke validation. Here is the module before: \n"
-                    << moduleBefore.str() << "\n";
+                    << moduleBefore << "\n";
           } else {
             Fatal() << "Last pass (" << pass->name
                     << ") broke validation. Run with BINARYEN_PASS_DEBUG=2 "
@@ -1026,9 +1028,12 @@ void PassRunner::runPassOnFunction(Pass* pass, Function* func) {
   // useful - leave it to the entire module to fail validation in that case.
   bool extraFunctionValidation =
     passDebug == 2 && options.validate && !pass->name.empty();
-  std::stringstream bodyBefore;
+
+  std::string bodyBefore;
   if (extraFunctionValidation) {
-    bodyBefore << *func->body << '\n';
+    std::stringstream ss;
+    ss << *func->body << '\n';
+    bodyBefore = ss.str();
   }
 
   // Function-parallel passes get a new instance per function
@@ -1042,7 +1047,7 @@ void PassRunner::runPassOnFunction(Pass* pass, Function* func) {
       Fatal() << "Last nested function-parallel pass (" << pass->name
               << ") broke validation of function " << func->name
               << ". Here is the function body before:\n"
-              << bodyBefore.str() << "\n\nAnd here it is now:\n"
+              << bodyBefore << "\n\nAnd here it is now:\n"
               << *func->body << '\n';
     }
   }