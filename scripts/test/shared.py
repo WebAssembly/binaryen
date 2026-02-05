da559d63c4227e22f9cb49e34220cb3a3c503bb8
stevenfontanella <stevenfont@chromium.org>
2026-02-05 23:54:11 +0000

    Don't test split modules for tests with (module instance ...)

--- scripts/test/shared.py
+++ scripts/test/shared.py
@@ -423,9 +423,7 @@ SPEC_TESTSUITE_TESTS_TO_SKIP = [
     'proposals/threads/memory.wast',  # Missing memory type validation on instantiation
     'annotations.wast',  # String annotations IDs should be allowed
     'id.wast',       # Empty IDs should be disallowed
-    # Requires correct handling of tag imports from different instances of the same module
-    # and splitting for module instances
-    'instance.wast',
+    'instance.wast',  # Requires correct handling of tag imports from different instances of the same module
     'table64.wast',   # Requires validations for table size
     'tag.wast',      # Non-empty tag results allowed by stack switching
     'local_init.wast',  # Requires local validation to respect unnamed blocks
