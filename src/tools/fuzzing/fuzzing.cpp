0a4fd450d4c981420d05aed190470358c57be797
Alon Zakai <azakai@google.com>
2026-02-10 13:44:05 -0800

    okgo

--- src/tools/fuzzing/fuzzing.cpp
+++ src/tools/fuzzing/fuzzing.cpp
@@ -53,8 +53,12 @@ TranslateToFuzzReader::TranslateToFuzzReader(Module& wasm,
   if (wasm.features.hasGC()) {
     loggableTypes.push_back(Type(HeapType::any, Nullable));
     loggableTypes.push_back(Type(HeapType::func, Nullable));
-    loggableTypes.push_back(Type(HeapType::cont, Nullable));
     loggableTypes.push_back(Type(HeapType::ext, Nullable));
+  }
+  if (wasm.features.hasStackSwitching()) {
+    loggableTypes.push_back(Type(HeapType::cont, Nullable));
+  }
+  if (wasm.features.hasExceptionHandling()) {
     loggableTypes.push_back(Type(HeapType::exn, Nullable));
   }
 
