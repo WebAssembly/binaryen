8f96cbd464a7b37d322448f1b7c104d3308698b1
Spotandjake <spotandjake@hotmail.com>
2026-02-10 17:52:04 -0500

    feat(C & JS API): Add `BinaryenHasMemorySegment`
    This pr adds `BinaryenHasMemorySegment(module, name)` to the c api and `module.hasMemorySegment(name)` to the js api.
    
    chore: formatting
    
    fix: Ensure to call `preserveStack` before calling `strToStack`

--- src/binaryen-c.h
+++ src/binaryen-c.h
@@ -3004,6 +3004,8 @@ BINARYEN_API bool BinaryenMemoryIs64(BinaryenModuleRef module,
 // Memory segments. Query utilities.
 
 BINARYEN_API uint32_t BinaryenGetNumMemorySegments(BinaryenModuleRef module);
+BINARYEN_API bool BinaryenHasMemorySegment(BinaryenModuleRef module,
+                                           const char* segmentName);
 BINARYEN_API uint32_t BinaryenGetMemorySegmentByteOffset(
   BinaryenModuleRef module, const char* segmentName);
 BINARYEN_API size_t BinaryenGetMemorySegmentByteLength(BinaryenModuleRef module,
