360e531b76955ecdb26d33dddb1618e790ddcba2
Alon Zakai <azakai@google.com>
2026-02-24 10:14:17 -0800

    nofuzz

--- scripts/test/fuzzing.py
+++ scripts/test/fuzzing.py
@@ -118,6 +118,7 @@ unfuzzable = [
     'strip-toolchain-annotations-func.wast',
     'idempotent.wast',
     'optimize-instructions_idempotent.wast',
+    'once-reduction_idempotent.wast',
     # Not fully implemented.
     'waitqueue.wast',
 ]
