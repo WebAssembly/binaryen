bc68b86842952efb70cdd5cb50690e1227fbef51
Cai Congcong <congcong.cai@bmw.com>
2026-02-25 10:23:42 +0800

    fix

--- test/wasm2js/emscripten-grow-yes.2asm.js
+++ test/wasm2js/emscripten-grow-yes.2asm.js
@@ -65,7 +65,7 @@ function asmFunc(imports) {
   var oldPages = __wasm_memory_size() | 0;
   var newPages = oldPages + pagesToAdd | 0;
   if ((oldPages < newPages) && (newPages < 65536) && (newPages <= 1024)) {
-   var newBuffer = new ArrayBuffer(Math_imul(newPages, 65536));
+   var newBuffer = new ArrayBuffer(newPages << 16);
    var newHEAP8 = new Int8Array(newBuffer);
    newHEAP8.set(HEAP8);
    HEAP8 = new Int8Array(newBuffer);
