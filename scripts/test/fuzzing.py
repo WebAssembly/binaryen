ef597d37247f1fa8f6ca1fc2f95f971616a11521
Alon Zakai <azakai@google.com>
2026-02-25 11:23:35 -0800

    fuzz

--- scripts/test/fuzzing.py
+++ scripts/test/fuzzing.py
@@ -119,6 +119,7 @@ unfuzzable = [
     'idempotent.wast',
     'optimize-instructions_idempotent.wast',
     'duplicate-function-elimination_annotations.wast',
+    'local-cse_idempotent.wast',
     # Not fully implemented.
     'waitqueue.wast',
 ]
