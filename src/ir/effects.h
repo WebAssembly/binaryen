48e47f360b67fcd09f3af69a83887e0a47567220
stevenfontanella <stevenfont@chromium.org>
2026-02-26 20:04:27 +0000

    Remove readsMutableStruct

--- src/ir/effects.h
+++ src/ir/effects.h
@@ -992,25 +992,6 @@ private:
       // user code. Model this as a struct write which prevents reorderings
       // (since isAtomic == true).
       parent.writesStruct = true;
-
-      if (curr->ref->type == Type::unreachable) {
-        return;
-      }
-
-      // If the ref isn't `unreachable`, then the field must exist and be a
-      // packed waitqueue due to validation.
-      assert(curr->ref->type.isStruct());
-      assert(curr->index <
-             curr->ref->type.getHeapType().getStruct().fields.size());
-      assert(curr->ref->type.getHeapType()
-               .getStruct()
-               .fields.at(curr->index)
-               .packedType == Field::PackedType::WaitQueue);
-
-      parent.readsMutableStruct |= curr->ref->type.getHeapType()
-                                     .getStruct()
-                                     .fields.at(curr->index)
-                                     .mutable_ == Mutable;
     }
     void visitArrayNew(ArrayNew* curr) {}
     void visitArrayNewData(ArrayNewData* curr) {
