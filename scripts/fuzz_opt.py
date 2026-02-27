da438a731b1d04aaef2aa97710a119045abb7d17
stevenfontanella <stevenfont@chromium.org>
2026-02-27 23:29:56 +0000

    Pull in DSE branch

--- scripts/fuzz_opt.py
+++ scripts/fuzz_opt.py
@@ -2430,6 +2430,7 @@ opt_choices = [
     ("--remove-unused-names", "--heap2local",),
     ("--heap-store-optimization",),
     ("--generate-stack-ir",),
+    ("--ldse",),
     ("--licm",),
     ("--local-subtyping",),
     ("--memory-packing",),
