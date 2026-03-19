23789e23572a737fce7eee19c7b1cd0c0786e078
Heejin Ahn <aheejin@gmail.com>
2026-03-19 02:06:09 +0000

    typo fix in comment

--- src/ir/module-splitting.cpp
+++ src/ir/module-splitting.cpp
@@ -45,7 +45,7 @@
 //      instantiation.
 //
 //   8. Export globals, tags, tables, and memories from the primary module and
-//      mport them in the secondary modules. If possible, move those module
+//      import them in the secondary modules. If possible, move those module
 //      items instead to the secondary modules.
 //
 // Functions can be used or referenced three ways in a WebAssembly module: they
