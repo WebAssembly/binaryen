078d673858ee7021c5cba4908e84edf3c8ec6bac
stevenfontanella <stevenfont@chromium.org>
2026-03-30 21:23:38 +0000

    Track interfering effects

--- src/passes/DeadStoreElimination.cpp
+++ src/passes/DeadStoreElimination.cpp
@@ -44,8 +44,20 @@ struct ComparingLocalGraph : public LocalGraph {
 struct StoreInfo {
   const StructSet* store = nullptr;
   int duplicateStores = 0;
+
+  // A struct.get observed this store. It may or may not be dead besides that (check `duplicateStores` for that) but it is definitely not dead because of the get.
   int conflictingGets = 0;
+
+
+  bool conflictingEffects = 0;
 };
+
+enum class Barrier {
+  None,
+  Branch,
+};
+
+using Info = std::variant<StoreInfo, Barrier>;
 
 class DeadStoreEliminationPass : public Pass {
   virtual std::unique_ptr<Pass> create() {
@@ -62,23 +74,35 @@ class DeadStoreEliminationPass : public Pass {
 
     // todo might want to use a map here
     // keyed by the ref expression
-    std::vector<StoreInfo> storeInfos;
+    std::vector<Info> storeInfos;
     for (auto& block : cfg) {
       for (const auto* inst : block) {
         if (const StructSet* structSet = inst->dynCast<StructSet>()) {
+          std::vector<Barrier> barriers;
           for (auto it = storeInfos.rbegin(); it != storeInfos.rend(); ++it) {
-            auto& storeInfo = *it;
+            if (auto* storeInfo = std::get_if<StoreInfo>(&*it)) {
+              if (localGraph.equalValues(structSet->ref, storeInfo->store->ref) &&
+                  structSet->index == storeInfo->store->index) {
+                storeInfo->duplicateStores++;
 
-            if (localGraph.equalValues(structSet->ref, storeInfo.store->ref) &&
-                structSet->index == storeInfo.store->index) {
-              storeInfo.duplicateStores++;
-              break;
+                if (!barriers.empty()) {
+                  storeInfo->conflictingEffects = true;
+                }
+                break;
+              }
+            } else if (auto* barrier = std::get_if<Barrier>(&*it)) {
+              barriers.push_back(*barrier);
             }
           }
           storeInfos.push_back(StoreInfo{structSet});
         } else if (const StructGet* structGet = inst->dynCast<StructGet>()) {
           for (auto it = storeInfos.rbegin(); it != storeInfos.rend(); ++it) {
-            auto& storeInfo = *it;
+            // Don't care about barriers here.
+            if (!std::holds_alternative<StoreInfo>(*it)) {
+              continue;
+            }
+
+            auto& storeInfo = std::get<StoreInfo>(*it);
 
             if (localGraph.equalValues(structGet->ref, storeInfo.store->ref) &&
                 structGet->index == storeInfo.store->index) {
@@ -86,14 +110,25 @@ class DeadStoreEliminationPass : public Pass {
               break;
             }
           }
+        } else {
+          ShallowEffectAnalyzer effects(getPassOptions(), *module, const_cast<Expression*>(inst));
+          if (effects.branchesOut) {
+            storeInfos.push_back(Barrier::Branch);
+          }
         }
       }
     }
 
     for (const auto& info : storeInfos) {
-      if (info.duplicateStores && !info.conflictingGets) {
+      if (!std::holds_alternative<StoreInfo>(info)) {
+        continue;
+      }
+
+      auto& storeInfo = std::get<StoreInfo>(info);
+
+      if (storeInfo.duplicateStores && !storeInfo.conflictingGets && !storeInfo.conflictingEffects) {
         std::lock_guard _(m);
-        std::cout<<info.duplicateStores<<"\n";
+        std::cout<<storeInfo.duplicateStores<<"\n";
       }
     }
   }
