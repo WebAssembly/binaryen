8242d1984430f7cb2161675f7b682de75d88f9d9
Alon Zakai <azakai@google.com>
2026-03-31 16:04:49 -0700

    [Stack Switching] Fix ctor-eval handling of unhandled resumes (#8556)
    The logic for this was in the wrong place. It should be where we handle
    a non-constant operation, as it is something that stops us and prevents
    precomputing. (Before, it was down by a branch, but a branch is a very
    special case in which we can ignore the branch, because we know it
    goes to the toplevel block, skipping what is after it.)
    
    Fixes #8553

--- src/tools/wasm-ctor-eval.cpp
+++ src/tools/wasm-ctor-eval.cpp
@@ -1173,6 +1173,14 @@ start_eval:
           std::cout << "  ...stopping due to non-constant flow\n";
         }
         break;
+      } else if (flow.suspendTag) {
+        // A suspend reached the exit of the function, so it is unhandled in
+        // it. TODO: We could support the case of the calling function
+        // handling it.
+        if (!quiet) {
+          std::cout << "  ...stopping due to unhandled suspend\n";
+        }
+        break;
       }
 
       if (flow.breakTo == RETURN_CALL_FLOW) {
@@ -1235,18 +1243,8 @@ start_eval:
       results = flow.values;
 
       if (flow.breaking()) {
-        if (flow.suspendTag) {
-          // A suspend reached the exit of the function, so it is unhandled in
-          // it. TODO: We could support the case of the calling function
-          // handling it.
-          if (!quiet) {
-            std::cout << "  ...stopping due to unhandled suspend\n";
-          }
-          return EvalCtorOutcome();
-        }
-
         // We are returning out of the function (either via a return, or via a
-        // break to |block|, which has the same outcome. That means we don't
+        // break to |block|, which has the same outcome). That means we don't
         // need to execute any more lines, and can consider them to be
         // executed.
         if (!quiet) {
