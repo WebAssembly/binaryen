ae40beae963c3a402212f4b2a819903769fa8c45
Chris Harvey <1362083+chharvey@users.noreply.github.com>
2026-04-29 05:01:05 -0400

    feat: start more class files

---
+++ ts/src/Function.ts
@@ -0,0 +1,32 @@
+import type {FunctionRef} from "./constants.ts";
+import {THIS_PTR} from "./utils.ts";
+
+
+
+class BinaryenFunction {
+	// TODO: static methods are deprecated; convert to instance and log warnings
+	static getName() {}
+	static getType() {}
+	static getParams() {}
+	static getResults() {}
+	static getNumVars() {}
+	static getVar() {}
+	static getNumLocals() {}
+	static hasLocalName() {}
+	static getLocalName() {}
+	static setLocalName() {}
+	static getBody() {}
+	static setBody() {}
+
+
+	readonly [THIS_PTR]: FunctionRef;
+
+	constructor(func: FunctionRef) {
+		this[THIS_PTR] = func;
+	}
+
+	valueOf() {
+		return this[THIS_PTR];
+	}
+}
+export {BinaryenFunction as Function};
---
+++ ts/src/Table.ts
@@ -0,0 +1,28 @@
+import type {TableRef} from "./constants.ts";
+import {THIS_PTR} from "./utils.ts";
+
+
+
+export class Table {
+	// TODO: static methods are deprecated; convert to instance and log warnings
+	static getName() {}
+	static setName() {}
+	static getInitial() {}
+	static setInitial() {}
+	static hasMax() {}
+	static getMax() {}
+	static setMax() {}
+	static getType() {}
+	static setType() {}
+
+
+	readonly [THIS_PTR]: TableRef;
+
+	constructor(table: TableRef) {
+		this[THIS_PTR] = table;
+	}
+
+	valueOf() {
+		return this[THIS_PTR];
+	}
+}
