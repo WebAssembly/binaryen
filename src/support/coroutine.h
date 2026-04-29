fa7dd4782d7e2e25135f49a496fc3c6e6ac7109e
Thomas Lively <tlively@google.com>
2026-04-28 21:59:50 -0700

    [NFC] Refactor delta debugging to use coroutines
    Add a generator utility in a new support/coroutine.h header and use it to refactor away the callback in the delta debugging utility. Now the utility is a struct providing access to the test and working sets as well as `accept()` and `reject()` methods that cause the test and working sets to be updated appropriately. Rather than being refactored into an explicit state machine, the implementation of the delta debugging algorithm remains readable straight-line code the does a co_yield whenever it is ready to return control to the user. It co_yields a pointer to local state object that exposes all the information that the delta debugging utility exposes in its public API. This local object stays alive across suspend points. When the delta debugging algorithm is complete, we suspend the coroutine one final time and make sure never to resume it, which ensures the state remains alive and available after delta debugging has finished. It will ultimately be cleaned up when the outer `DeltaDebugger` struct is cleaned up.

---
+++ src/support/coroutine.h
@@ -0,0 +1,130 @@
+/*
+ * Copyright 2026 WebAssembly Community Group participants
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef wasm_support_coroutine_h
+#define wasm_support_coroutine_h
+
+#include <coroutine>
+#include <exception>
+
+namespace wasm {
+
+template<typename PromiseType> struct GetPromise {
+  PromiseType* promise = nullptr;
+  bool await_ready() const noexcept { return false; }
+  bool await_suspend(std::coroutine_handle<PromiseType> h) noexcept {
+    promise = &h.promise();
+    return false;
+  }
+  PromiseType* await_resume() const noexcept { return promise; }
+};
+
+template<typename T, typename U = void> struct Generator;
+
+// One-way generator
+template<typename T> struct Generator<T, void> {
+  struct promise_type {
+    T current_value;
+
+    Generator get_return_object() {
+      return {std::coroutine_handle<promise_type>::from_promise(*this)};
+    }
+    std::suspend_always initial_suspend() { return {}; }
+    std::suspend_always final_suspend() noexcept { return {}; }
+    void unhandled_exception() { std::terminate(); }
+    void return_void() {}
+
+    std::suspend_always yield_value(T value) {
+      current_value = std::move(value);
+      return {};
+    }
+  };
+
+  std::coroutine_handle<promise_type> handle;
+
+  Generator(std::coroutine_handle<promise_type> h) : handle(h) {}
+  Generator(const Generator&) = delete;
+  Generator(Generator&& other) noexcept : handle(other.handle) {
+    other.handle = nullptr;
+  }
+  ~Generator() {
+    if (handle) {
+      handle.destroy();
+    }
+  }
+
+  bool next() {
+    handle.resume();
+    return !handle.done();
+  }
+
+  T& get() { return handle.promise().current_value; }
+  const T& get() const { return handle.promise().current_value; }
+};
+
+// Two-way generator
+template<typename T, typename U> struct Generator {
+  struct promise_type {
+    T current_value;
+    U received_value;
+
+    Generator get_return_object() {
+      return {std::coroutine_handle<promise_type>::from_promise(*this)};
+    }
+    std::suspend_always initial_suspend() { return {}; }
+    std::suspend_always final_suspend() noexcept { return {}; }
+    void unhandled_exception() { std::terminate(); }
+    void return_void() {}
+
+    auto yield_value(T value) {
+      current_value = std::move(value);
+      return YieldAwaiter{this};
+    }
+
+    struct YieldAwaiter {
+      promise_type* p;
+      bool await_ready() const noexcept { return false; }
+      void await_suspend(std::coroutine_handle<promise_type>) noexcept {}
+      U await_resume() const noexcept { return p->received_value; }
+    };
+  };
+
+  std::coroutine_handle<promise_type> handle;
+
+  Generator(std::coroutine_handle<promise_type> h) : handle(h) {}
+  Generator(const Generator&) = delete;
+  Generator(Generator&& other) noexcept : handle(other.handle) {
+    other.handle = nullptr;
+  }
+  ~Generator() {
+    if (handle) {
+      handle.destroy();
+    }
+  }
+
+  bool resume(U value) {
+    handle.promise().received_value = std::move(value);
+    handle.resume();
+    return !handle.done();
+  }
+
+  T& get() { return handle.promise().current_value; }
+  const T& get() const { return handle.promise().current_value; }
+};
+
+} // namespace wasm
+
+#endif // wasm_support_coroutine_h
