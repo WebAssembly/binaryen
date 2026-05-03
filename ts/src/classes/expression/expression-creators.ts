ed3027536645ba24448c60f159bbb073052238a8
Chris Harvey <1362083+chharvey@users.noreply.github.com>
2026-05-02 21:40:30 -0400

    refactor: replace `extractParam` with `Function#bind`

--- ts/src/classes/expression/expression-creators.ts
+++ ts/src/classes/expression/expression-creators.ts
@@ -19,12 +19,6 @@ import {
 
 
 
-function extractParam<First, Rest extends unknown[], Return>(a: First, fn: (a: First, ...rest: Rest) => Return): (...rest: Rest) => Return {
-	return (...args) => fn(a, ...args);
-}
-
-
-
 function parametric(mod: Module) {
 	return {
 		/** Creates a no-operation `(nop)` instruction. */
@@ -32,9 +26,9 @@ function parametric(mod: Module) {
 		/** Creates an unreachable instruction that will always trap. */
 		unreachable: (): ExpressionRef => BinaryenObj["_BinaryenUnreachable"](mod.ptr),
 		/** Creates a `(drop)` of a value. */
-		drop: extractParam(mod, Drop.drop),
+		drop: Drop.drop.bind(null, mod),
 		/** Creates a `(select)` of one of two values. */
-		select: extractParam(mod, Select.select),
+		select: Select.select.bind(null, mod),
 	} as const;
 }
 
@@ -43,33 +37,33 @@ function parametric(mod: Module) {
 function control(mod: Module) {
 	return {
 		/** Creates a `(block)`. */
-		block: extractParam(mod, Block.block),
+		block: Block.block.bind(null, mod),
 		/** Creates a loop. */
-		loop: extractParam(mod, Loop.loop),
+		loop: Loop.loop.bind(null, mod),
 		/** Creates an unconditional branch `(br)` to a label. */
-		br: extractParam(mod, Break.br),
+		br: Break.br.bind(null, mod),
 		/** Creates a conditional branch `(br_if)` to a label. */
-		br_if: extractParam(mod, Break.br_if),
+		br_if: Break.br_if.bind(null, mod),
 	} as const;
 }
 
 
 
-function variables(mod: Module) {
+function variable(mod: Module) {
 	return {
 		local: {
 			/**
 			 * Creates a `(local.get)` for the local at the specified index.
 			 * Note that we must specify the type here as we may not have created the local being accessed yet.
 			 */
-			get: extractParam(mod, LocalGet.localGet),
+			get: LocalGet.localGet.bind(null, mod),
 			/** Creates a `(local.set)` for the local at the specified index. */
-			set: extractParam(mod, LocalSet.localSet),
+			set: LocalSet.localSet.bind(null, mod),
 			/**
 			 * Creates a `(local.tee)` for the local at the specified index.
 			 * Note that we must specify the type here as we may not have created the local being accessed yet.
 			 */
-			tee: extractParam(mod, LocalSet.localTee),
+			tee: LocalSet.localTee.bind(null, mod),
 		},
 	} as const;
 }
@@ -81,6 +75,6 @@ export function expressionCreator(mod: Module) {
 	return {
 		...parametric(mod),
 		...control(mod),
-		...variables(mod),
+		...variable(mod),
 	} as const;
 }
