9714ee30c6bb4bb40157318901c34063ede87b51
Chris Harvey <1362083+chharvey@users.noreply.github.com>
2026-05-06 00:17:23 -0400

    feat: add table instrs

--- ts/src/services/expression-builder/table.ts
+++ ts/src/services/expression-builder/table.ts
@@ -1,21 +1,43 @@
+import {
+	BinaryenObj,
+} from "../../-pre.ts";
 import type {
 	Module,
 } from "../../classes/module/Module.ts";
-
-
-
-/** Placeholder. */
-const STUB = (..._args: readonly number[]): number => 0;
+import type {
+	ExpressionRef,
+	Type,
+} from "../../constants.ts";
+import {
+	preserveStack,
+	strToStack,
+} from "../../utils.ts";
 
 
 
 /** @see https://webassembly.github.io/spec/core/syntax/instructions.html#table-instructions */
-export function table(_mod: Module) {
+export function table(mod: Module) {
 	return {
-		get: STUB,
-		set: STUB,
-		size: STUB,
-		grow: STUB,
+		/** Load an element in a table. */
+		get: (name: string, index: number, typ: Type): ExpressionRef => (
+			preserveStack(() => BinaryenObj["_BinaryenTableGet"](mod.ptr, strToStack(name), index, typ))
+		),
+
+		/** Store an element in a table. */
+		set: (name: string, index: number, value: ExpressionRef): ExpressionRef => (
+			preserveStack(() => BinaryenObj["_BinaryenTableSet"](mod.ptr, strToStack(name), index, value))
+		),
+
+		/** Returns the current size of a table. */
+		size: (name: string): ExpressionRef => (
+			preserveStack(() => BinaryenObj["_BinaryenTableSize"](mod.ptr, strToStack(name)))
+		),
+
+		/** Grows table by a given delta and returns the previous size, or -1 if not enough space can be allocated. */
+		grow: (name: string, value: ExpressionRef, delta: ExpressionRef): ExpressionRef => (
+			preserveStack(() => BinaryenObj["_BinaryenTableGrow"](mod.ptr, strToStack(name), value, delta))
+		),
+
 		// TODO: fill
 		// TODO: copy
 		// TODO: init
