8d8cc15ff5eb40d4935366e9601d189c88d29058
Chris Harvey <1362083+chharvey@users.noreply.github.com>
2026-05-15 05:05:29 -0400

    test: add binaryen tests

---
+++ ts/test/binaryen.test.ts
@@ -0,0 +1,89 @@
+import * as assert from "node:assert";
+import {
+	suite,
+	test,
+} from "node:test";
+import * as binaryen from "../src/binaryen.ts";
+
+
+
+suite("binaryen", () => {
+	test("namespace exists and has all the right top-level exports.", () => {
+		assert.ok(binaryen);
+
+		// constants
+		assert.ok(binaryen.unreachable);
+		assert.strictEqual(binaryen.none, 0);
+		assert.ok(binaryen.auto);
+		assert.ok(binaryen.i32);
+		assert.ok(binaryen.i64);
+		assert.ok(binaryen.f32);
+		assert.ok(binaryen.f64);
+		assert.ok(binaryen.v128);
+		assert.ok(binaryen.anyref);
+		assert.ok(binaryen.eqref);
+		assert.ok(binaryen.i31ref);
+		assert.ok(binaryen.structref);
+		assert.ok(binaryen.arrayref);
+		assert.ok(binaryen.funcref);
+		assert.ok(!Reflect.has(binaryen, "exnref"));
+		assert.ok(binaryen.externref);
+		assert.ok(binaryen.nullref);
+		assert.ok(binaryen.nullfuncref);
+		assert.ok(!Reflect.has(binaryen, "nullexnref"));
+		assert.ok(binaryen.nullexternref);
+		assert.strictEqual(binaryen.notPacked, 0);
+		assert.ok(binaryen.i8);
+		assert.ok(binaryen.i16);
+		assert.ok(binaryen.stringref);
+
+		// enums
+		assert.strictEqual(typeof binaryen.ExpressionId, "object");
+		assert.strictEqual(typeof binaryen.SideEffect, "object");
+		assert.strictEqual(typeof binaryen.ExternalKind, "object");
+		assert.strictEqual(typeof binaryen.Operation, "object");
+		assert.strictEqual(typeof binaryen.MemoryOrder, "object");
+		assert.strictEqual(typeof binaryen.Feature, "object");
+		assert.strictEqual(typeof binaryen.ExpressionRunnerFlag, "object");
+
+		// functions
+		assert.strictEqual(typeof binaryen.emitText, "function");
+		assert.strictEqual(typeof binaryen.readBinary, "function");
+		assert.strictEqual(typeof binaryen.readBinaryWithFeatures, "function");
+		assert.strictEqual(typeof binaryen.parseText, "function");
+		assert.strictEqual(typeof binaryen.exit, "function");
+		assert.strictEqual(typeof binaryen.createType, "function");
+		assert.strictEqual(typeof binaryen.expandType, "function");
+		assert.strictEqual(typeof binaryen.getTypeFromHeapType, "function");
+		assert.strictEqual(typeof binaryen.getHeapType, "function");
+		assert.strictEqual(typeof binaryen.getExpressionId, "function");
+		assert.strictEqual(typeof binaryen.getExpressionType, "function");
+		assert.strictEqual(typeof binaryen.getExpressionInfo, "function");
+		assert.ok(binaryen.emitText.toString().startsWith("function"));
+		assert.ok(binaryen.readBinary.toString().startsWith("function"));
+		assert.ok(binaryen.readBinaryWithFeatures.toString().startsWith("function"));
+		assert.ok(binaryen.parseText.toString().startsWith("function"));
+		assert.ok(binaryen.exit.toString().startsWith("function"));
+		assert.ok(binaryen.createType.toString().startsWith("function"));
+		assert.ok(binaryen.expandType.toString().startsWith("function"));
+		assert.ok(binaryen.getTypeFromHeapType.toString().startsWith("function"));
+		assert.ok(binaryen.getHeapType.toString().startsWith("function"));
+		assert.ok(binaryen.getExpressionId.toString().startsWith("function"));
+		assert.ok(binaryen.getExpressionType.toString().startsWith("function"));
+		assert.ok(binaryen.getExpressionInfo.toString().startsWith("function"));
+
+		// classes
+		assert.strictEqual(typeof binaryen.Module, "function");
+		assert.strictEqual(typeof binaryen.TypeBuilder, "function");
+		assert.strictEqual(typeof binaryen.ExpressionRunner, "function");
+		assert.strictEqual(typeof binaryen.Relooper, "function");
+		assert.ok(binaryen.Module.toString().startsWith("class"));
+		assert.ok(binaryen.TypeBuilder.toString().startsWith("class"));
+		assert.ok(binaryen.ExpressionRunner.toString().startsWith("class"));
+		assert.ok(binaryen.Relooper.toString().startsWith("class"));
+
+		// other
+		assert.ok(binaryen.settings);
+		assert.ok(binaryen.expressions);
+	});
+});
