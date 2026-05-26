0a17d86043c539a2db28d8a48911f967b43d8853
Alon Zakai <azakai@google.com>
2026-05-26 14:44:19 -0700

    go

--- src/tools/wasm-ctor-eval.cpp
+++ src/tools/wasm-ctor-eval.cpp
@@ -1482,10 +1482,15 @@ void evalCtors(Module& wasm,
       }
     }
   } catch (FailToEvalException& fail) {
-    // that's it, we failed to even create the instance
+    // That's it, we failed to even create the instance.
     if (!quiet) {
       std::cout << "  ...stopping since could not create module instance: "
                 << fail.why << "\n";
+    }
+  } catch (NonconstantException& fail) {
+    // We can also fail during start due to a non-constant operation.
+    if (!quiet) {
+      std::cout << "  ...stopping since could non-constant in start\n";
     }
   } catch (TopologicalSort::CycleException e) {
     // We use a topological sort for GC globals. If there is a non-breakable
