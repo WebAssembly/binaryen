90af4c11d6be24cbf0be3f8849e357c488aafc50
Chris Harvey <1362083+chharvey@users.noreply.github.com>
2026-06-06 00:57:14 -0400

    fix: add missing deprecation `ExpressionInfo` types

--- ts/src/-deprecations.ts
+++ ts/src/-deprecations.ts
@@ -68,14 +68,160 @@ export type SelectInfo = expressions.Select;
 export type BlockInfo = expressions.Block;
 /** @deprecated The `LoopInfo` object type is now called {@link expressions.Loop}. */
 export type LoopInfo = expressions.Loop;
+/** @deprecated The `IfInfo` object type is now called {@link expressions.If}. */
+export type IfInfo = expressions.If;
 /** @deprecated The `BreakInfo` object type is now called {@link expressions.Break}. */
 export type BreakInfo = expressions.Break;
+/** @deprecated The `SwitchInfo` object type is now called {@link expressions.Switch}. */
+export type SwitchInfo = expressions.Switch;
+/** @deprecated The `BrOnInfo` object type is now called {@link expressions.BrOn}. */
+export type BrOnInfo = expressions.BrOn;
+/** @deprecated The `CallInfo` object type is now called {@link expressions.Call}. */
+export type CallInfo = expressions.Call;
+/** @deprecated The `CallRefInfo` object type is now called {@link expressions.CallRef}. */
+export type CallRefInfo = expressions.CallRef;
+/** @deprecated The `CallIndirectInfo` object type is now called {@link expressions.CallIndirect}. */
+export type CallIndirectInfo = expressions.CallIndirect;
+/** @deprecated The `ReturnInfo` object type is now called {@link expressions.Return}. */
+export type ReturnInfo = expressions.Return;
+/** @deprecated The `ThrowInfo` object type is now called {@link expressions.Throw}. */
+export type ThrowInfo = expressions.Throw;
+/** @deprecated The `RethrowInfo` object type is now called {@link expressions.Rethrow}. */
+export type RethrowInfo = expressions.Rethrow;
+/** @deprecated The `TryInfo` object type is now called {@link expressions.Try}. */
+export type TryInfo = expressions.Try;
 /** @deprecated The `LocalGetInfo` object type is now called {@link expressions.LocalGet}. */
 export type LocalGetInfo = expressions.LocalGet;
 /** @deprecated The `LocalSetInfo` object type is now called {@link expressions.LocalSet}. */
 export type LocalSetInfo = expressions.LocalSet;
+/** @deprecated The `GlobalGetInfo` object type is now called {@link expressions.GlobalGet}. */
+export type GlobalGetInfo = expressions.GlobalGet;
+/** @deprecated The `GlobalSetInfo` object type is now called {@link expressions.GlobalSet}. */
+export type GlobalSetInfo = expressions.GlobalSet;
+/** @deprecated The `TableGetInfo` object type is now called {@link expressions.TableGet}. */
+export type TableGetInfo = expressions.TableGet;
+/** @deprecated The `TableSetInfo` object type is now called {@link expressions.TableSet}. */
+export type TableSetInfo = expressions.TableSet;
+/** @deprecated The `TableSizeInfo` object type is now called {@link expressions.TableSize}. */
+export type TableSizeInfo = expressions.TableSize;
+/** @deprecated The `TableGrowInfo` object type is now called {@link expressions.TableGrow}. */
+export type TableGrowInfo = expressions.TableGrow;
+/** @deprecated The `LoadInfo` object type is now called {@link expressions.Load}. */
+export type LoadInfo = expressions.Load;
+/** @deprecated The `StoreInfo` object type is now called {@link expressions.Store}. */
+export type StoreInfo = expressions.Store;
+/** @deprecated The `SIMDLoadInfo` object type is now called {@link expressions.SIMDLoad}. */
+export type SIMDLoadInfo = expressions.SIMDLoad;
+/** @deprecated The `SIMDLoadStoreLaneInfo` object type is now called {@link expressions.SIMDLoadStoreLane}. */
+export type SIMDLoadStoreLaneInfo = expressions.SIMDLoadStoreLane;
+/** @deprecated The `MemorySizeInfo` object type is now called {@link expressions.MemorySize}. */
+export type MemorySizeInfo = expressions.MemorySize;
+/** @deprecated The `MemoryGrowInfo` object type is now called {@link expressions.MemoryGrow}. */
+export type MemoryGrowInfo = expressions.MemoryGrow;
+/** @deprecated The `MemoryFillInfo` object type is now called {@link expressions.MemoryFill}. */
+export type MemoryFillInfo = expressions.MemoryFill;
+/** @deprecated The `MemoryCopyInfo` object type is now called {@link expressions.MemoryCopy}. */
+export type MemoryCopyInfo = expressions.MemoryCopy;
+/** @deprecated The `MemoryInitInfo` object type is now called {@link expressions.MemoryInit}. */
+export type MemoryInitInfo = expressions.MemoryInit;
+/** @deprecated The `DataDropInfo` object type is now called {@link expressions.DataDrop}. */
+export type DataDropInfo = expressions.DataDrop;
+/** @deprecated The `RefFuncInfo` object type is now called {@link expressions.RefFunc}. */
+export type RefFuncInfo = expressions.RefFunc;
+/** @deprecated The `RefIsNullInfo` object type is now called {@link expressions.RefIsNull}. */
+export type RefIsNullInfo = expressions.RefIsNull;
+/** @deprecated The `RefAsInfo` object type is now called {@link expressions.RefAs}. */
+export type RefAsInfo = expressions.RefAs;
+/** @deprecated The `RefEqInfo` object type is now called {@link expressions.RefEq}. */
+export type RefEqInfo = expressions.RefEq;
+/** @deprecated The `RefTestInfo` object type is now called {@link expressions.RefTest}. */
+export type RefTestInfo = expressions.RefTest;
+/** @deprecated The `RefCastInfo` object type is now called {@link expressions.RefCast}. */
+export type RefCastInfo = expressions.RefCast;
+/** @deprecated The `RefI31Info` object type is now called {@link expressions.RefI31}. */
+export type RefI31Info = expressions.RefI31;
+/** @deprecated The `I31GetInfo` object type is now called {@link expressions.I31Get}. */
+export type I31GetInfo = expressions.I31Get;
+/** @deprecated The `TupleMakeInfo` object type is now called {@link expressions.TupleMake}. */
+export type TupleMakeInfo = expressions.TupleMake;
+/** @deprecated The `TupleExtractInfo` object type is now called {@link expressions.TupleExtract}. */
+export type TupleExtractInfo = expressions.TupleExtract;
+/** @deprecated The `StructNewInfo` object type is now called {@link expressions.StructNew}. */
+export type StructNewInfo = expressions.StructNew;
+/** @deprecated The `StructGetInfo` object type is now called {@link expressions.StructGet}. */
+export type StructGetInfo = expressions.StructGet;
+/** @deprecated The `StructSetInfo` object type is now called {@link expressions.StructSet}. */
+export type StructSetInfo = expressions.StructSet;
+/** @deprecated The `ArrayNewInfo` object type is now called {@link expressions.ArrayNew}. */
+export type ArrayNewInfo = expressions.ArrayNew;
+/** @deprecated The `ArrayNewFixedInfo` object type is now called {@link expressions.ArrayNewFixed}. */
+export type ArrayNewFixedInfo = expressions.ArrayNewFixed;
+/** @deprecated The `ArrayNewDataInfo` object type is now called {@link expressions.ArrayNewData}. */
+export type ArrayNewDataInfo = expressions.ArrayNewData;
+/** @deprecated The `ArrayNewElemInfo` object type is now called {@link expressions.ArrayNewElem}. */
+export type ArrayNewElemInfo = expressions.ArrayNewElem;
+/** @deprecated The `ArrayGetInfo` object type is now called {@link expressions.ArrayGet}. */
+export type ArrayGetInfo = expressions.ArrayGet;
+/** @deprecated The `ArraySetInfo` object type is now called {@link expressions.ArraySet}. */
+export type ArraySetInfo = expressions.ArraySet;
+/** @deprecated The `ArrayLenInfo` object type is now called {@link expressions.ArrayLen}. */
+export type ArrayLenInfo = expressions.ArrayLen;
+/** @deprecated The `ArrayFillInfo` object type is now called {@link expressions.ArrayFill}. */
+export type ArrayFillInfo = expressions.ArrayFill;
+/** @deprecated The `ArrayCopyInfo` object type is now called {@link expressions.ArrayCopy}. */
+export type ArrayCopyInfo = expressions.ArrayCopy;
+/** @deprecated The `ArrayInitDataInfo` object type is now called {@link expressions.ArrayInitData}. */
+export type ArrayInitDataInfo = expressions.ArrayInitData;
+/** @deprecated The `ArrayInitElemInfo` object type is now called {@link expressions.ArrayInitElem}. */
+export type ArrayInitElemInfo = expressions.ArrayInitElem;
 /** @deprecated The `ConstInfo` object type is now called {@link expressions.Const}. */
 export type ConstInfo = expressions.Const;
+/** @deprecated The `UnaryInfo` object type is now called {@link expressions.Unary}. */
+export type UnaryInfo = expressions.Unary;
+/** @deprecated The `BinaryInfo` object type is now called {@link expressions.Binary}. */
+export type BinaryInfo = expressions.Binary;
+/** @deprecated The `SIMDTernaryInfo` object type is now called {@link expressions.SIMDTernary}. */
+export type SIMDTernaryInfo = expressions.SIMDTernary;
+/** @deprecated The `SIMDShiftInfo` object type is now called {@link expressions.SIMDShift}. */
+export type SIMDShiftInfo = expressions.SIMDShift;
+/** @deprecated The `SIMDShuffleInfo` object type is now called {@link expressions.SIMDShuffle}. */
+export type SIMDShuffleInfo = expressions.SIMDShuffle;
+/** @deprecated The `SIMDExtractInfo` object type is now called {@link expressions.SIMDExtract}. */
+export type SIMDExtractInfo = expressions.SIMDExtract;
+/** @deprecated The `SIMDReplaceInfo` object type is now called {@link expressions.SIMDReplace}. */
+export type SIMDReplaceInfo = expressions.SIMDReplace;
+/** @deprecated The `AtomicRMWInfo` object type is now called {@link expressions.AtomicRMW}. */
+export type AtomicRMWInfo = expressions.AtomicRMW;
+/** @deprecated The `AtomicCmpxchgInfo` object type is now called {@link expressions.AtomicCmpxchg}. */
+export type AtomicCmpxchgInfo = expressions.AtomicCmpxchg;
+/** @deprecated The `AtomicWaitInfo` object type is now called {@link expressions.AtomicWait}. */
+export type AtomicWaitInfo = expressions.AtomicWait;
+/** @deprecated The `AtomicNotifyInfo` object type is now called {@link expressions.AtomicNotify}. */
+export type AtomicNotifyInfo = expressions.AtomicNotify;
+/** @deprecated The `AtomicFenceInfo` object type is now called {@link expressions.AtomicFence}. */
+export type AtomicFenceInfo = expressions.AtomicFence;
+/** @deprecated The `StringNewInfo` object type is now called {@link expressions.StringNew}. */
+export type StringNewInfo = expressions.StringNew;
+/** @deprecated The `StringConstInfo` object type is now called {@link expressions.StringConst}. */
+export type StringConstInfo = expressions.StringConst;
+/** @deprecated The `StringMeasureInfo` object type is now called {@link expressions.StringMeasure}. */
+export type StringMeasureInfo = expressions.StringMeasure;
+/** @deprecated The `StringEncodeInfo` object type is now called {@link expressions.StringEncode}. */
+export type StringEncodeInfo = expressions.StringEncode;
+/** @deprecated The `StringConcatInfo` object type is now called {@link expressions.StringConcat}. */
+export type StringConcatInfo = expressions.StringConcat;
+/** @deprecated The `StringEqInfo` object type is now called {@link expressions.StringEq}. */
+export type StringEqInfo = expressions.StringEq;
+/** @deprecated The `StringWTF16GetInfo` object type is now called {@link expressions.StringWTF16Get}. */
+export type StringWTF16GetInfo = expressions.StringWTF16Get;
+/** @deprecated The `StringSliceWTFInfo` object type is now called {@link expressions.StringSliceWTF}. */
+export type StringSliceWTFInfo = expressions.StringSliceWTF;
+/** @deprecated The `WideIntAddSubInfo` object type is now called {@link expressions.WideIntAddSub}. */
+export type WideIntAddSubInfo = expressions.WideIntAddSub;
+/** @deprecated The `WideIntMulInfo` object type is now called {@link expressions.WideIntMul}. */
+export type WideIntMulInfo = expressions.WideIntMul;
+
+
 
 /** @deprecated The `Function` class now lives under the `Module` namespace. Use {@link Module.Function}. */
 export const Function = Module.Function;
