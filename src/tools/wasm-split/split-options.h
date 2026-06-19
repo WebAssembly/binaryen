388213f94f35ebdb4f98ec403936d09f99defb5d
Martin Kustermann <kustermann@google.com>
2026-06-19 15:10:26 +0200

    more clang format

--- src/tools/wasm-split/split-options.h
+++ src/tools/wasm-split/split-options.h
@@ -52,7 +52,6 @@ struct WasmSplitOptions : ToolOptions {
   bool placeholderMap = false;
   bool stripDebug = false;
 
-
   std::string profileFile;
   std::string profileExport = DEFAULT_PROFILE_EXPORT;
 
