e11289e0cd4a86940f24d294c57350ebb9af2e9f
Alon Zakai <azakai@google.com>
2026-07-24 10:16:30 -0700

    work

--- test/gtest/constraint.cpp
+++ test/gtest/constraint.cpp
@@ -288,12 +288,14 @@ TEST(ConstraintTest, TestOrLoop) {
 
   // Change 5 on the left to 99:
   // { x == 99 } || { x > 5 && x <= 42 }   ==>   { x > 5 }
+  // TODO: we could emit a range (5, 99]
   AndedConstraintSet left99{Constraint{Eq, {Literal(int32_t(99))}}};
   AndedConstraintSet rightOnly5{Constraint{GtS, {Literal(int32_t(5))}}};
   checkOr(left99, right, rightOnly5);
 
   // Change 5 on the left to 4:
   // { x == 4 } || { x > 5 && x <= 42 }   ==>   { x <= 42 }
+  // TODO: we could emit a range [4, 42]
   AndedConstraintSet left4{Constraint{Eq, {Literal(int32_t(4))}}};
   AndedConstraintSet rightOnly42({{LeS, {Literal(int32_t(42))}}});
   checkOr(left4, right, rightOnly42);
@@ -307,6 +309,8 @@ TEST(ConstraintTest, TestOrLoop) {
   // Changes to operations:
 
   // Change the Eq on the left to Ne. We fail to find anything for the OR.
+  // { x != 5 } || { x > 5 && x <= 42 }   ==>   {}
+  // TODO: we could emit x != 5
   AndedConstraintSet leftNe{Constraint{Ne, {Literal(int32_t(5))}}};
   auto empty = AndedConstraintSet::makeProvesNothing();
   checkOr(leftNe, right, empty);
