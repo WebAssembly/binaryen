a8d6827b0d7ab2e20345913e8dbe7322d38d5a74
Alon Zakai <azakai@google.com>
2026-07-24 10:30:31 -0700

    work

--- test/gtest/constraint.cpp
+++ test/gtest/constraint.cpp
@@ -254,13 +254,13 @@ static void checkOr(const AndedConstraintSet& a,
 
 TEST(ConstraintTest, TestOrInequality) {
   // x == 5 || x >= 0  =>  x >= 0
-  AndedConstraintSet eq5{Constraint{Eq, {Literal(int32_t(5))}}};
-  AndedConstraintSet ge0{Constraint{GeU, {Literal(int32_t(0))}}};
+  AndedConstraintSet eq5{{Eq, {Literal(int32_t(5))}}};
+  AndedConstraintSet ge0{{GeU, {Literal(int32_t(0))}}};
   checkOr(eq5, ge0, ge0);
 
   // x == 5 || x > 5  =>  x >= 5
-  AndedConstraintSet gts5{Constraint{GtS, {Literal(int32_t(5))}}};
-  AndedConstraintSet ges5{Constraint{GeS, {Literal(int32_t(5))}}};
+  AndedConstraintSet gts5{{GtS, {Literal(int32_t(5))}}};
+  AndedConstraintSet ges5{{GeS, {Literal(int32_t(5))}}};
   checkOr(eq5, gts5, ges5);
 
   // x == 5 || x >= 5  =>  x >= 5
@@ -272,7 +272,7 @@ TEST(ConstraintTest, TestOrLoop) {
   // { x == A } || { x > A && x <= B }   ==>   { x >= A && x <= B }
 
   // { x == 5 } || { x > 5 && x <= 42 }   ==>   { x >= 5 && x <= 42 }
-  AndedConstraintSet left{Constraint{Eq, {Literal(int32_t(5))}}};
+  AndedConstraintSet left{{Eq, {Literal(int32_t(5))}}};
   AndedConstraintSet right(
     {{GtS, {Literal(int32_t(5))}}, {LeS, {Literal(int32_t(42))}}});
   AndedConstraintSet result(
@@ -283,20 +283,20 @@ TEST(ConstraintTest, TestOrLoop) {
 
   // Change 5 on the left to 7:
   // { x == 7 } || { x > 5 && x <= 42 }   ==>   { x > 5 && x <= 42}
-  AndedConstraintSet left7{Constraint{Eq, {Literal(int32_t(7))}}};
+  AndedConstraintSet left7{{Eq, {Literal(int32_t(7))}}};
   checkOr(left7, right, right);
 
   // Change 5 on the left to 99:
   // { x == 99 } || { x > 5 && x <= 42 }   ==>   { x > 5 }
   // TODO: we could emit a range (5, 99]
-  AndedConstraintSet left99{Constraint{Eq, {Literal(int32_t(99))}}};
-  AndedConstraintSet rightOnly5{Constraint{GtS, {Literal(int32_t(5))}}};
+  AndedConstraintSet left99{{Eq, {Literal(int32_t(99))}}};
+  AndedConstraintSet rightOnly5{{GtS, {Literal(int32_t(5))}}};
   checkOr(left99, right, rightOnly5);
 
   // Change 5 on the left to 4:
   // { x == 4 } || { x > 5 && x <= 42 }   ==>   { x <= 42 }
   // TODO: we could emit a range [4, 42]
-  AndedConstraintSet left4{Constraint{Eq, {Literal(int32_t(4))}}};
+  AndedConstraintSet left4{{Eq, {Literal(int32_t(4))}}};
   AndedConstraintSet rightOnly42({{LeS, {Literal(int32_t(42))}}});
   checkOr(left4, right, rightOnly42);
 
@@ -311,7 +311,7 @@ TEST(ConstraintTest, TestOrLoop) {
   // Change the Eq on the left to Ne. We fail to find anything for the OR.
   // { x != 5 } || { x > 5 && x <= 42 }   ==>   {}
   // TODO: we could emit x != 5
-  AndedConstraintSet leftNe{Constraint{Ne, {Literal(int32_t(5))}}};
+  AndedConstraintSet leftNe{{Ne, {Literal(int32_t(5))}}};
   auto empty = AndedConstraintSet::makeProvesNothing();
   checkOr(leftNe, right, empty);
 
@@ -340,3 +340,37 @@ TEST(ConstraintTest, TestOrLoop) {
                                   {Ne, {Literal(int32_t(21))}}});
   checkOr(left, rightAdded, resultAdded);
 }
+
+static void checkAnd(const AndedConstraintSet& a,
+                     const AndedConstraintSet& b,
+                     const AndedConstraintSet& result) {
+  auto anded = a;
+  for (auto& bc : b) {
+    anded.approximateAnd(bc);
+  }
+  EXPECT_EQ(anded, result);
+
+  anded = b;
+  for (auto& ac : a) {
+    anded.approximateAnd(ac);
+  }
+  EXPECT_EQ(anded, result);
+}
+
+TEST(ConstraintTest, TestAndInequality) {
+  // x == 5 && x >= 0  =>  x == 5
+  AndedConstraintSet eq5{{Eq, {Literal(int32_t(5))}}};
+  AndedConstraintSet ge0{{GeU, {Literal(int32_t(0))}}};
+  checkAnd(eq5, ge0, eq5);
+
+/*
+  // x == 5 || x > 5  =>  x >= 5
+  AndedConstraintSet gts5{{GtS, {Literal(int32_t(5))}}};
+  AndedConstraintSet ges5{{GeS, {Literal(int32_t(5))}}};
+  checkAnd(eq5, gts5, ges5);
+
+  // x == 5 || x >= 5  =>  x >= 5
+  checkAnd(eq5, ges5, ges5);
+*/
+}
+
