WebAssembly/binaryen

commit dd74363c18f04648166e79a1afa4becd1e4fd099

Fix binary location tracking of Try (#8862)

src/wasm/wasm-ir-builder.cpp
@@ -987,8 +987,10 @@ Result<> IRBuilder::visitCatch(Name tag) {
   if (binaryPos && func) {
     auto& delimiterLocs = func->delimiterLocations[tryy];
     delimiterLocs[delimiterLocs.size()] = lastBinaryPos - codeSectionOffset;
-    // TODO: As in visitElse, we likely need to stash the Try start. Here we
-    //       also need to account for multiple catches.
+    if (wasTry) {
+      func->expressionLocations[tryy].start =
+        scope.startPos - codeSectionOffset;
+    }
   }
 
   CHECK_ERR(pushScope(ScopeCtx::makeCatch(std::move(scope), tryy)));
@@ -1027,6 +1029,10 @@ Result<> IRBuilder::visitCatchAll() {
   if (binaryPos && func) {
     auto& delimiterLocs = func->delimiterLocations[tryy];
     delimiterLocs[delimiterLocs.size()] = lastBinaryPos - codeSectionOffset;
+    if (wasTry) {
+      func->expressionLocations[tryy].start =
+        scope.startPos - codeSectionOffset;
+    }
   }
 
   return pushScope(ScopeCtx::makeCatchAll(std::move(scope), tryy));

test/lit/branch-hinting-try.wast
@@ -1,0 +1,59 @@
+;; NOTE: Assertions have been generated by update_lit_checks.py --all-items and should not be edited.
+
+;; RUN: wasm-opt -all --precompute --roundtrip --roundtrip %s -S -o - | filecheck %s
+
+;; After precompute, the If's hint gets attached to a nop. The roundtrips should
+;; not error on branch hint parsing errors (the tuple here affects parsing in a
+;; way that causes an error if we get anything wrong). We need to use precompute
+;; here as a way to get a branch hint onto a non-branch - the text parser just
+;; skips non-branches currently.
+
+(module
+ ;; CHECK:      (type $0 (func (result i32 i32)))
+
+ ;; CHECK:      (type $func (func))
+ (type $func (func))
+
+ ;; CHECK:      (tag $tag (type $func))
+ (tag $tag (type $func))
+
+ ;; CHECK:      (func $test (type $0) (result i32 i32)
+ ;; CHECK-NEXT:  (try (type $0) (result i32 i32)
+ ;; CHECK-NEXT:   (do
+ ;; CHECK-NEXT:    (tuple.make 2
+ ;; CHECK-NEXT:     (i32.const 0)
+ ;; CHECK-NEXT:     (i32.const 1)
+ ;; CHECK-NEXT:    )
+ ;; CHECK-NEXT:   )
+ ;; CHECK-NEXT:   (catch $tag
+ ;; CHECK-NEXT:    (@metadata.code.branch_hint "\01")
+ ;; CHECK-NEXT:    (nop)
+ ;; CHECK-NEXT:    (unreachable)
+ ;; CHECK-NEXT:   )
+ ;; CHECK-NEXT:  )
+ ;; CHECK-NEXT: )
+ (func $test (result i32 i32)
+  (try (result i32 i32)
+   (do
+    (tuple.make 2
+     (i32.const 0)
+     (i32.const 1)
+    )
+   )
+   (catch $tag
+    (@metadata.code.branch_hint "\01")
+    (if
+     (i32.const 0)
+     (then
+      (nop)
+     )
+     (else
+      (nop)
+     )
+    )
+    (unreachable)
+   )
+  )
+ )
+)
+

commit 2d2c8a107196bf7eb9a0fb6654755cedaf0a3601

Fix binary location tracking of wrapped expressions
Fixes assertion failure in `AddrExprMap::add` introduced in #8862.

When `maybeWrapForLabel` creates a synthetic block around a `Try` or
`If` expression, `push` is called on the synthetic block rather than the
underlying expression. Update `expressionLocations.end` for the inner
expression when closing its scope in `visitEnd`, and push synthetic
wrapper blocks with `pushSynthetic` so they are not tracked as duplicate
binary instructions.

src/wasm/wasm-ir-builder.cpp
@@ -1169,6 +1169,21 @@ Result<> IRBuilder::visitEnd() {
     return block;
   };
 
+  auto pushWrapped = [&](Expression* curr) {
+    auto* wrapped = maybeWrapForLabel(curr);
+    if (wrapped != curr) {
+      if (binaryPos && func) {
+        if (auto iter = func->expressionLocations.find(wrapped);
+            iter != func->expressionLocations.end()) {
+          iter->second.end = *binaryPos - codeSectionOffset;
+        }
+      }
+      pushSynthetic(wrapped);
+    } else {
+      push(curr);
+    }
+  };
+
   // The binary position we record for the block instruction should start at the
   // beginning of the block, not at the beginning of the `end`.
   lastBinaryPos = scope.startPos;
@@ -1215,27 +1230,27 @@ Result<> IRBuilder::visitEnd() {
       iff->ifFalse = nullptr;
     }
     iff->finalize(iff->type);
-    push(maybeWrapForLabel(iff));
+    pushWrapped(iff);
   } else if (auto* iff = scope.getElse()) {
     iff->ifFalse = *expr;
     iff->finalize(iff->type);
-    push(maybeWrapForLabel(iff));
+    pushWrapped(iff);
   } else if (auto* tryy = scope.getTry()) {
     tryy->body = *expr;
     tryy->name = scope.label;
     tryy->finalize(tryy->type);
-    push(maybeWrapForLabel(tryy));
+    pushWrapped(tryy);
   } else if (Try* tryy;
              (tryy = scope.getCatch()) || (tryy = scope.getCatchAll())) {
     auto index = scope.getIndex();
     setCatchBody(tryy, *expr, index);
     tryy->name = scope.label;
     tryy->finalize(tryy->type);
-    push(maybeWrapForLabel(tryy));
+    pushWrapped(tryy);
   } else if (auto* trytable = scope.getTryTable()) {
     trytable->body = *expr;
     trytable->finalize(trytable->type, &wasm);
-    push(maybeWrapForLabel(trytable));
+    pushWrapped(trytable);
   } else {
     WASM_UNREACHABLE("unexpected scope kind");
   }