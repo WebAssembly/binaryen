cdcfc19eee947def60b406a705f296f389ca0054
Alon Zakai <azakai@google.com>
2026-08-11 14:19:53 -0700

    fix

--- src/ir/constraint.cpp
+++ src/ir/constraint.cpp
@@ -211,9 +211,11 @@ std::optional<Constraint> fusedApproximateAndPair(const Constraint& a,
 
 // If a set of constraints shows us as equal to a literal, return it.
 std::optional<Literal> equalToLiteral(const AndedConstraintSet& set) {
-  if (set.size() == 1 && set[0].op == Abstract::Eq) {
-    if (auto* c = std::get_if<Literal>(&set[0].term)) {
-      return *c;
+  for (auto& c : set) {
+    if (set[0].op == Abstract::Eq) {
+      if (auto* cc = std::get_if<Literal>(&c.term)) {
+        return *cc;
+      }
     }
   }
   return {};
