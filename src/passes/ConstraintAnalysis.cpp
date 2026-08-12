477c129f8d6622bc0c6e1b50c90844be78ed9e00
Alon Zakai <azakai@google.com>
2026-08-12 16:49:13 -0700

    go

--- src/passes/ConstraintAnalysis.cpp
+++ src/passes/ConstraintAnalysis.cpp
@@ -539,7 +539,24 @@ struct ConstraintAnalysis
       // only track two things, constants and locals. For a constant, it does
       // not change while falling through. For a local, the only way for the
       // local to change while falling through is to go through a tee of that
-      // local - but that would keep the same value there anyhow.
+      // local - but that would keep the same value there anyhow. That is:
+      //
+      //  (local.set $other
+      //    (block
+      //      ..
+      //      (local.tee $source
+      //        (block
+      //          ..
+      //          (local.get $source)
+      //        )
+      //      )
+      //    )
+      //  )
+      //
+      // The fallthrough here is the local.get of $source. We can set $other to
+      // the value in $source, because while $source did have a write while
+      // falling through, it did not alter the value, and there is no
+      // opportunity to write any other value while falling through.      
       auto* value =
         Properties::getFallthrough(set->value, getPassOptions(), *getModule());
       constraints.set(set->index, value);
