bb0caa3fc547e1281391b1235c93f1ed84541c2e
Sam Clegg <sbc@chromium.org>
2026-08-21 18:09:03 -0700

    [wasm2js] Support nontrapping float-to-int conversions directly in JS (#9019)
    Now that 64-bit `trunc_sat` instructions are supported directly in
    `I64ToI32Lowering` (#9017), and 32-bit `trunc_sat` instructions are
    handled directly in `wasm2js` (`~~expr` and `~~expr >>> 0`), we no
    longer need to run `llvm-nontrapping-fptoint-lowering` when
    `--emscripten` is passed.
    
    In `wasm2js`, trapping float-to-int operations (`i32.trunc_*` and
    `i64.trunc_*`) are already lowered to non-trapping JS conversions
    (`~~expr` and float arithmetic in `I64ToI32Lowering`), which do not trap
    on out-of-range values or NaN/Infinity (matching C/C++ UB semantics
    rather than strict Wasm trapping semantics).
    
    Removing `llvm-nontrapping-fptoint-lowering` extends that same direct
    approach to `trunc_sat` instructions under `--emscripten`, eliminating
    branching overhead and reducing code size.
    
    Finally, add a commit to `LLVMNontrappingFPToIntLowering.cpp` about it
    have no known users after this change.

--- test/wasm2js/conversions-emscripten-modified.2asm.js
+++ test/wasm2js/conversions-emscripten-modified.2asm.js
@@ -17,166 +17,102 @@ function asmFunc(imports) {
 ;
  function $0(x) {
   x = Math_fround(x);
-  var $1_1 = Math_fround(0), $8 = 0;
-  $1_1 = x;
-  if (Math_fround(Math_abs($1_1)) < Math_fround(2147483648.0)) {
-   $8 = ~~$1_1
-  } else {
-   $8 = -2147483648
-  }
-  return $8 | 0;
+  return ~~x | 0;
  }
  
  function $1(x) {
   x = Math_fround(x);
-  var $1_1 = Math_fround(0), $10 = 0;
-  $1_1 = x;
-  if ($1_1 < Math_fround(4294967296.0) & $1_1 >= Math_fround(0.0) | 0) {
-   $10 = ~~$1_1 >>> 0
-  } else {
-   $10 = 0
-  }
-  return $10 | 0;
+  return ~~x >>> 0 | 0;
  }
  
  function $2(x) {
   x = +x;
-  var $1_1 = 0.0, $8 = 0;
-  $1_1 = x;
-  if (Math_abs($1_1) < 2147483647.0) {
-   $8 = ~~$1_1
-  } else {
-   $8 = -2147483648
-  }
-  return $8 | 0;
+  return ~~x | 0;
  }
  
  function $3(x) {
   x = +x;
-  var $1_1 = 0.0, $10 = 0;
-  $1_1 = x;
-  if ($1_1 < 4294967295.0 & $1_1 >= 0.0 | 0) {
-   $10 = ~~$1_1 >>> 0
-  } else {
-   $10 = 0
-  }
-  return $10 | 0;
+  return ~~x >>> 0 | 0;
  }
  
  function $4(x) {
   x = Math_fround(x);
-  var i64toi32_i32$0 = Math_fround(0), i64toi32_i32$1 = 0, $1_1 = Math_fround(0), $6_1 = 0, $7_1 = 0, $8 = 0, $8$hi = 0;
-  $1_1 = x;
-  if (Math_fround(Math_abs($1_1)) < Math_fround(9223372036854775808.0)) {
-   i64toi32_i32$0 = $1_1;
-   if (Math_fround(Math_abs(i64toi32_i32$0)) >= Math_fround(1.0)) {
-    if (i64toi32_i32$0 > Math_fround(0.0)) {
-     $6_1 = ~~Math_fround(Math_min(Math_fround(Math_floor(Math_fround(i64toi32_i32$0 / Math_fround(4294967296.0)))), Math_fround(Math_fround(4294967296.0) - Math_fround(1.0)))) >>> 0
-    } else {
-     $6_1 = ~~Math_fround(Math_ceil(Math_fround(Math_fround(i64toi32_i32$0 - Math_fround(~~i64toi32_i32$0 >>> 0 >>> 0)) / Math_fround(4294967296.0)))) >>> 0
-    }
-    $7_1 = $6_1;
+  var i64toi32_i32$0 = Math_fround(0), $4_1 = 0, $5_1 = 0, i64toi32_i32$1 = 0, i64toi32_i32$2 = 0;
+  i64toi32_i32$0 = x;
+  if (Math_fround(Math_abs(i64toi32_i32$0)) >= Math_fround(1.0)) {
+   if (i64toi32_i32$0 > Math_fround(0.0)) {
+    $4_1 = ~~Math_fround(Math_min(Math_fround(Math_floor(Math_fround(i64toi32_i32$0 / Math_fround(4294967296.0)))), Math_fround(Math_fround(4294967296.0) - Math_fround(1.0)))) >>> 0
    } else {
-    $7_1 = 0
+    $4_1 = ~~Math_fround(Math_ceil(Math_fround(Math_fround(i64toi32_i32$0 - Math_fround(~~i64toi32_i32$0 >>> 0 >>> 0)) / Math_fround(4294967296.0)))) >>> 0
    }
-   i64toi32_i32$1 = $7_1;
-   $8 = ~~i64toi32_i32$0 >>> 0;
-   $8$hi = i64toi32_i32$1;
+   $5_1 = $4_1;
   } else {
-   i64toi32_i32$1 = -2147483648;
-   $8 = 0;
-   $8$hi = i64toi32_i32$1;
+   $5_1 = 0
   }
-  i64toi32_i32$1 = $8$hi;
+  i64toi32_i32$1 = $5_1;
+  i64toi32_i32$2 = ~~i64toi32_i32$0 >>> 0;
   i64toi32_i32$HIGH_BITS = i64toi32_i32$1;
-  return $8 | 0;
+  return i64toi32_i32$2 | 0;
  }
  
  function $5(x) {
   x = Math_fround(x);
-  var i64toi32_i32$0 = Math_fround(0), i64toi32_i32$1 = 0, $1_1 = Math_fround(0), $6_1 = 0, $7_1 = 0, $10 = 0, $10$hi = 0;
-  $1_1 = x;
-  if ($1_1 < Math_fround(18446744073709551615.0) & $1_1 >= Math_fround(0.0) | 0) {
-   i64toi32_i32$0 = $1_1;
-   if (Math_fround(Math_abs(i64toi32_i32$0)) >= Math_fround(1.0)) {
-    if (i64toi32_i32$0 > Math_fround(0.0)) {
-     $6_1 = ~~Math_fround(Math_min(Math_fround(Math_floor(Math_fround(i64toi32_i32$0 / Math_fround(4294967296.0)))), Math_fround(Math_fround(4294967296.0) - Math_fround(1.0)))) >>> 0
-    } else {
-     $6_1 = ~~Math_fround(Math_ceil(Math_fround(Math_fround(i64toi32_i32$0 - Math_fround(~~i64toi32_i32$0 >>> 0 >>> 0)) / Math_fround(4294967296.0)))) >>> 0
-    }
-    $7_1 = $6_1;
+  var i64toi32_i32$0 = Math_fround(0), $4_1 = 0, $5_1 = 0, i64toi32_i32$1 = 0, i64toi32_i32$2 = 0;
+  i64toi32_i32$0 = x;
+  if (Math_fround(Math_abs(i64toi32_i32$0)) >= Math_fround(1.0)) {
+   if (i64toi32_i32$0 > Math_fround(0.0)) {
+    $4_1 = ~~Math_fround(Math_min(Math_fround(Math_floor(Math_fround(i64toi32_i32$0 / Math_fround(4294967296.0)))), Math_fround(Math_fround(4294967296.0) - Math_fround(1.0)))) >>> 0
    } else {
-    $7_1 = 0
+    $4_1 = ~~Math_fround(Math_ceil(Math_fround(Math_fround(i64toi32_i32$0 - Math_fround(~~i64toi32_i32$0 >>> 0 >>> 0)) / Math_fround(4294967296.0)))) >>> 0
    }
-   i64toi32_i32$1 = $7_1;
-   $10 = ~~i64toi32_i32$0 >>> 0;
-   $10$hi = i64toi32_i32$1;
+   $5_1 = $4_1;
   } else {
-   i64toi32_i32$1 = 0;
-   $10 = 0;
-   $10$hi = i64toi32_i32$1;
+   $5_1 = 0
   }
-  i64toi32_i32$1 = $10$hi;
+  i64toi32_i32$1 = $5_1;
+  i64toi32_i32$2 = ~~i64toi32_i32$0 >>> 0;
   i64toi32_i32$HIGH_BITS = i64toi32_i32$1;
-  return $10 | 0;
+  return i64toi32_i32$2 | 0;
  }
  
  function $6(x) {
   x = +x;
-  var i64toi32_i32$0 = 0.0, i64toi32_i32$1 = 0, $1_1 = 0.0, $6_1 = 0, $7_1 = 0, $8 = 0, $8$hi = 0;
-  $1_1 = x;
-  if (Math_abs($1_1) < 9223372036854775808.0) {
-   i64toi32_i32$0 = $1_1;
-   if (Math_abs(i64toi32_i32$0) >= 1.0) {
-    if (i64toi32_i32$0 > 0.0) {
-     $6_1 = ~~Math_min(Math_floor(i64toi32_i32$0 / 4294967296.0), 4294967296.0 - 1.0) >>> 0
-    } else {
-     $6_1 = ~~Math_ceil((i64toi32_i32$0 - +(~~i64toi32_i32$0 >>> 0 >>> 0)) / 4294967296.0) >>> 0
-    }
-    $7_1 = $6_1;
+  var i64toi32_i32$0 = 0.0, $4_1 = 0, $5_1 = 0, i64toi32_i32$1 = 0, i64toi32_i32$2 = 0;
+  i64toi32_i32$0 = x;
+  if (Math_abs(i64toi32_i32$0) >= 1.0) {
+   if (i64toi32_i32$0 > 0.0) {
+    $4_1 = ~~Math_min(Math_floor(i64toi32_i32$0 / 4294967296.0), 4294967296.0 - 1.0) >>> 0
    } else {
-    $7_1 = 0
+    $4_1 = ~~Math_ceil((i64toi32_i32$0 - +(~~i64toi32_i32$0 >>> 0 >>> 0)) / 4294967296.0) >>> 0
    }
-   i64toi32_i32$1 = $7_1;
-   $8 = ~~i64toi32_i32$0 >>> 0;
-   $8$hi = i64toi32_i32$1;
+   $5_1 = $4_1;
   } else {
-   i64toi32_i32$1 = -2147483648;
-   $8 = 0;
-   $8$hi = i64toi32_i32$1;
+   $5_1 = 0
   }
-  i64toi32_i32$1 = $8$hi;
+  i64toi32_i32$1 = $5_1;
+  i64toi32_i32$2 = ~~i64toi32_i32$0 >>> 0;
   i64toi32_i32$HIGH_BITS = i64toi32_i32$1;
-  return $8 | 0;
+  return i64toi32_i32$2 | 0;
  }
  
  function $7(x) {
   x = +x;
-  var i64toi32_i32$0 = 0.0, i64toi32_i32$1 = 0, $1_1 = 0.0, $6_1 = 0, $7_1 = 0, $10 = 0, $10$hi = 0;
-  $1_1 = x;
-  if ($1_1 < 18446744073709551615.0 & $1_1 >= 0.0 | 0) {
-   i64toi32_i32$0 = $1_1;
-   if (Math_abs(i64toi32_i32$0) >= 1.0) {
-    if (i64toi32_i32$0 > 0.0) {
-     $6_1 = ~~Math_min(Math_floor(i64toi32_i32$0 / 4294967296.0), 4294967296.0 - 1.0) >>> 0
-    } else {
-     $6_1 = ~~Math_ceil((i64toi32_i32$0 - +(~~i64toi32_i32$0 >>> 0 >>> 0)) / 4294967296.0) >>> 0
-    }
-    $7_1 = $6_1;
+  var i64toi32_i32$0 = 0.0, $4_1 = 0, $5_1 = 0, i64toi32_i32$1 = 0, i64toi32_i32$2 = 0;
+  i64toi32_i32$0 = x;
+  if (Math_abs(i64toi32_i32$0) >= 1.0) {
+   if (i64toi32_i32$0 > 0.0) {
+    $4_1 = ~~Math_min(Math_floor(i64toi32_i32$0 / 4294967296.0), 4294967296.0 - 1.0) >>> 0
    } else {
-    $7_1 = 0
+    $4_1 = ~~Math_ceil((i64toi32_i32$0 - +(~~i64toi32_i32$0 >>> 0 >>> 0)) / 4294967296.0) >>> 0
    }
-   i64toi32_i32$1 = $7_1;
-   $10 = ~~i64toi32_i32$0 >>> 0;
-   $10$hi = i64toi32_i32$1;
+   $5_1 = $4_1;
   } else {
-   i64toi32_i32$1 = 0;
-   $10 = 0;
-   $10$hi = i64toi32_i32$1;
+   $5_1 = 0
   }
-  i64toi32_i32$1 = $10$hi;
+  i64toi32_i32$1 = $5_1;
+  i64toi32_i32$2 = ~~i64toi32_i32$0 >>> 0;
   i64toi32_i32$HIGH_BITS = i64toi32_i32$1;
-  return $10 | 0;
+  return i64toi32_i32$2 | 0;
  }
  
  function legalstub$4($0_1) {
