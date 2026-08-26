7afc1b4f0a15e5478ece98b1fc12f07eb2f1e28f
Chris Harvey <1362083+chharvey@users.noreply.github.com>
2026-08-26 14:07:44 -0400

    refactor(Expression): aggregate exports

--- ts/src/classes/expression/index.ts
+++ ts/src/classes/expression/index.ts
@@ -9,117 +9,19 @@
 
 
 
-export {Expression} from "./Expression.ts";
+export * from "./Expression.ts";
 
-export {
-	Drop,
-	Select,
-} from "./parametrics.ts";
-export {
-	Block,
-	Loop,
-	If,
-} from "./blocks.ts";
-export {
-	Break,
-	Switch,
-	BrOn,
-} from "./breaks.ts";
-export {
-	Call,
-	CallRef,
-	CallIndirect,
-	Return,
-} from "./calls.ts";
-export {
-	Throw,
-	Rethrow,
-	Try,
-} from "./throws.ts";
-export {
-	LocalGet,
-	LocalSet,
-	GlobalGet,
-	GlobalSet,
-} from "./variables.ts";
-export {
-	TableGet,
-	TableSet,
-	TableSize,
-	TableGrow,
-} from "./tables.ts";
-export {
-	Load,
-	Store,
-	SIMDLoad,
-	SIMDLoadStoreLane,
-	MemorySize,
-	MemoryGrow,
-	MemoryFill,
-	MemoryCopy,
-	MemoryInit,
-	DataDrop,
-} from "./memories.ts";
-export {
-	RefFunc,
-	// TODO: RefNull,
-	RefIsNull,
-	RefAs,
-	RefEq,
-	RefTest,
-	RefCast,
-	RefI31,
-	I31Get,
-} from "./references.ts";
-export {
-	TupleMake,
-	TupleExtract,
-	StructNew,
-	StructGet,
-	StructSet,
-	StructWait,
-	WaitqueueNew,
-	WaitqueueNotify,
-	ArrayNew,
-	ArrayNewFixed,
-	ArrayNewData,
-	ArrayNewElem,
-	ArrayGet,
-	ArraySet,
-	ArrayLen,
-	ArrayFill,
-	ArrayCopy,
-	ArrayInitData,
-	ArrayInitElem,
-} from "./aggregates.ts";
-export {
-	Const,
-	Unary,
-	Binary,
-	WideIntAddSub,
-	WideIntMul,
-} from "./numerics.ts";
-export {
-	SIMDTernary,
-	SIMDShift,
-	SIMDShuffle,
-	SIMDExtract,
-	SIMDReplace,
-} from "./vectors.ts";
-export {
-	AtomicRMW,
-	AtomicCmpxchg,
-	AtomicWait,
-	AtomicNotify,
-	AtomicFence,
-} from "./atomics.ts";
-export {
-	StringNew,
-	StringConst,
-	StringMeasure,
-	StringEncode,
-	StringConcat,
-	StringEq,
-	StringWTF16Get,
-	StringSliceWTF,
-} from "./strings.ts";
+export * from "./parametrics.ts";
+export * from "./blocks.ts";
+export * from "./breaks.ts";
+export * from "./calls.ts";
+export * from "./throws.ts";
+export * from "./variables.ts";
+export * from "./tables.ts";
+export * from "./memories.ts";
+export * from "./references.ts";
+export * from "./aggregates.ts";
+export * from "./numerics.ts";
+export * from "./vectors.ts";
+export * from "./atomics.ts";
+export * from "./strings.ts";
