e45f5ba1204484a59a38a6881b8e6fb6339f2c71
Alon Zakai <azakai@google.com>
2026-09-17 15:22:45 -0700

    Mark atomic read-write etc. operations as generative (#9112)
    Two such instructions, one after the other, can return different things
    despite appearances, like a `call` or a `struct.new`.

--- scripts/test/fuzzing.py
+++ scripts/test/fuzzing.py
@@ -117,6 +117,7 @@ unfuzzable = [
     # Not fully implemented.
     'waitqueue.wast',
     'gufa-waitqueue.wast',
+    'optimize-instructions-waitqueue.wast',
     'publish.wast',
     'optimize-instructions-publish.wast',
     # TODO: fix handling of the non-utf8 names here
