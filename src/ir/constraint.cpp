ebd23e66850b102ee9e8e62bfd2c48e490fec2c5
Alon Zakai <azakai@google.com>
2026-09-18 13:13:10 -0700

    work

--- src/ir/constraint.cpp
+++ src/ir/constraint.cpp
@@ -201,11 +201,6 @@ Result provesConstantPair(Abstract::Op aOp,
                           bool recursing = false) {
   using namespace Abstract;
 
-  // SIMD types cannot be operated on (wasm does not have v128.eq etc.).
-  if (aConstant.type == Type::v128) {
-    return Unknown;
-  }
-
   // a == A =?=> a op B. Simply apply A to the operation against B.
   if (aOp == Eq) {
     switch (bOp) {
